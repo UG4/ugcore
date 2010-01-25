47e91ab7c9851f9e6d63cbc9c786f4dca55be6dc
Sebastian Reiter <sebastian.reiter@gcsc.uni-frankfurt.de>
2010-01-25 14:20:16 +0000

    added ugbase/ug3layer and ug3layer/gm.h. ug3layer has no functionality yet.

---
+++ ugbase/ug3layer/gm.h
@@ -0,0 +1,10 @@
+// created by Sebastian Reiter
+// s.b.reiter@googlemail.com
+// y10 m01 d25
+
+#ifndef __H__UG3LAYER__GM__
+#define __H__UG3LAYER__GM__
+
+//TODO: wrap macros from the ug3-grid manager.
+
+#endif
