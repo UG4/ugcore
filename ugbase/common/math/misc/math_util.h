6147d3209324b9855cf77d4008c3a3dfbfd89485
Sebastian Reiter <sebastian.reiter@gcsc.uni-frankfurt.de>
2010-01-28 16:48:00 +0000

    added math_util methods (DropAPerpendicular, TriangleArea, ...) work done on SimpleGrid (SwapEdge implemented, ShapeQualityDegree and GeometricApproximationDegree work) Some work done on AdjustEdgeLength

--- ugbase/common/math/misc/math_util.h
+++ ugbase/common/math/misc/math_util.h
@@ -6,38 +6,73 @@
 #define __H__UGMATH__MATH_UTIL__
 
 #include <cstdlib>
+#include "../math_vector_matrix/math_vector.h"
+#include "../math_vector_matrix/math_vector_functions.h"
 
 namespace ug
 {
+
+const number SMALL = 1.0e-12;
 
 ////////////////////////////////////////////////////////////////////////
 //	urand
 ///	uniform distributed random numbers in [lowerBound, upperBound[. Use srand to set a seed.
 template <class TNumber>
 TNumber
-urand(TNumber lowerBound, TNumber upperBound)
-{
-	long t = rand();
-	if(t == RAND_MAX)
-		t -= 1;
-
-	return lowerBound + (TNumber)((upperBound - lowerBound) * ((float)t / (float)RAND_MAX));
-}
+urand(TNumber lowerBound, TNumber upperBound);
 
 ////////////////////////////////////////////////////////////////////////
 //	clip
 ///	clips a number to the given interval [lowerBound, upperBound].
 template <class TNumber>
 TNumber
-clip(TNumber val, TNumber lowerBound, TNumber upperBound)
-{
-	if(val > upperBound)
-		return upperBound;
-	else if(val < lowerBound)
-		return lowerBound;
-	return val;
-}
+clip(TNumber val, TNumber lowerBound, TNumber upperBound);
+
+
+
+////////////////////////////////////////////////////////////////////////
+///	finds the projection of v onto the line defined by v0 and v1
+/**
+ * projects v onto the line defined by v0 and v1.
+ * The projected point is returned in vOut.
+ *
+ * returns s so that vOut = (1.0-s)*v0 + s*v1
+ */
+template <class vector_t>
+number DropAPerpendicular(vector_t& vOut, const vector_t& v0,
+							const vector_t& v1, const vector_t& v);
+
+////////////////////////////////////////////////////////////////////////
+///	calculates the are of the triangle defined by p1, p2 and p3
+template <class vector_t>
+number TriangleArea(const vector_t& p1, const vector_t& p2,
+							 const vector_t& p3);
+
+////////////////////////////////////////////////////////////////////////
+///	the returned degree lies between 0 and 1. The closer to 1 the better.
+/**
+ * returns the minimal dot-product of each normal of the triangle-corners
+ * with the triangles normal.
+ */
+template <class vector_t>
+number GeometricApproximationDegree(vector_t& n1, vector_t& n2, vector_t& n3,
+									vector_t& tn);
+
+////////////////////////////////////////////////////////////////////////
+///	returns a value between 0 and 1. The higher the better.
+/**	The quality is measured by comparing the triangles area with the
+ *	sum of the squared length of each side.
+ *
+ *	a triangle whose sides have all zero length is considered to be
+ *	a bad triangle - 0 is returned.*/
+template <class vector_t>
+number TriangleQuality_Area(const vector_t& p1, const vector_t& p2,
+							const vector_t& p3);
 
 }//	end of namespace
+
+////////////////////////////////////////////////
+//	include implementation
+#include "math_util_impl.hpp"
 
 #endif
---
+++ ugbase/common/math/misc/math_util_impl.hpp
@@ -0,0 +1,105 @@
+//	created by Sebastian Reiter
+//	s.b.reiter@googlemail.com
+//	y08 m10 d14
+
+#ifndef __H__UGMATH__MATH_UTIL_IMPL__
+#define __H__UGMATH__MATH_UTIL_IMPL__
+
+#include <algorithm>
+
+namespace ug
+{
+
+////////////////////////////////////////////////////////////////////////
+//	urand
+template <class TNumber>
+TNumber
+urand(TNumber lowerBound, TNumber upperBound)
+{
+	long t = rand();
+	if(t == RAND_MAX)
+		t -= 1;
+
+	return lowerBound + (TNumber)((upperBound - lowerBound) * ((float)t / (float)RAND_MAX));
+}
+
+////////////////////////////////////////////////////////////////////////
+//	clip
+template <class TNumber>
+TNumber
+clip(TNumber val, TNumber lowerBound, TNumber upperBound)
+{
+	if(val > upperBound)
+		return upperBound;
+	else if(val < lowerBound)
+		return lowerBound;
+	return val;
+}
+
+
+////////////////////////////////////////////////////////////////////////
+template <class vector_t>
+number DropAPerpendicular(vector_t& vOut, const vector_t& v0,
+							const vector_t& v1, const vector_t& v)
+{
+//	project v onto v' on the edge (v0, v1) so that (v'-v)*(v0-v1) = 0
+	vector_t e[2];
+	VecSubtract(e[0], v, v0);
+	VecSubtract(e[1], v1, v0);
+
+	number d1 = VecDot(e[0], e[1]);
+	number d2 = VecDot(e[1], e[1]);
+	
+//	avoid division by zero
+	if(fabs(d2) > SMALL)
+	{
+	//	calculate the projection p'
+		number s = d1/d2;
+		VecScale(e[1], e[1], s);
+		VecAdd(vOut, v0, e[1]);
+		return s;
+	}
+	else
+		vOut = v0;
+	return 0;
+}
+
+////////////////////////////////////////////////////////////////////////
+template <class vector_t>
+number TriangleArea(const vector_t& p1, const vector_t& p2, const vector_t& p3)
+{
+//	the projection of p3 onto the line defined by p1 and p2
+	vector_t v;
+	DropAPerpendicular(v, p1, p2, p3);
+//	calculate the area
+	return 0.5 * sqrt(VecDistanceSq(p1, p2) * VecDistanceSq(v, p3));
+}
+
+////////////////////////////////////////////////////////////////////////
+template <class vector_t>
+number GeometricApproximationDegree(vector_t& n1, vector_t& n2, vector_t& n3,
+									vector_t& tn)
+{
+	return std::min(VecDot(n1, tn), std::min(VecDot(n2, tn), VecDot(n3, tn)));
+}
+
+////////////////////////////////////////////////////////////////////////
+template <class vector_t>
+number TriangleQuality_Area(const vector_t& p1, const vector_t& p2,
+							const vector_t& p3)
+{
+	number edgeSum = VecDistanceSq(p1, p2) + VecDistanceSq(p2, p3)
+					+ VecDistanceSq(p1, p3);
+	if(edgeSum > SMALL)
+	{
+	//	4*sqrt(3) = 6.9282032
+		return 6.9282032 * TriangleArea(p1, p2, p3) / edgeSum;
+	}
+
+//	a triangle whose sides have all zero length is considered to be a bad triangle.
+	return 0;
+}
+
+}//	end of namespace
+
+#endif
