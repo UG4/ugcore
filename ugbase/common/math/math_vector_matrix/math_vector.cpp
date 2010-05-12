a2e6c8d7bfeca2ee99a0e7486f7b24c34c38111c
Sebastian Reiter <sebastian.reiter@gcsc.uni-frankfurt.de>
2010-05-12 12:17:04 +0000

    changed MathVector<...> output. Now contains spaces.

--- ugbase/common/math/math_vector_matrix/math_vector.cpp
+++ ugbase/common/math/math_vector_matrix/math_vector.cpp
@@ -10,18 +10,18 @@
 
 std::ostream& operator<< (std::ostream& outStream, const ug::MathVector<2>& v)
 {
-	outStream << "(" << v.coord(0) << "," << v.coord(1) << ")";
+	outStream << "(" << v.coord(0) << ", " << v.coord(1) << ")";
 	return outStream;
 }
 
 std::ostream& operator<< (std::ostream& outStream, const ug::MathVector<3>& v)
 {
-	outStream << "(" << v.coord(0) << "," << v.coord(1) << "," << v.coord(2) << ")";
+	outStream << "(" << v.coord(0) << ", " << v.coord(1) << ", " << v.coord(2) << ")";
 	return outStream;
 }
 
 std::ostream& operator<< (std::ostream& outStream, const ug::MathVector<4>& v)
 {
-	outStream << "(" << v.coord(0) << "," << v.coord(1) << "," << v.coord(2) << "," << v.coord(3) << ")";
+	outStream << "(" << v.coord(0) << ", " << v.coord(1) << ", " << v.coord(2) << ", " << v.coord(3) << ")";
 	return outStream;
 }
