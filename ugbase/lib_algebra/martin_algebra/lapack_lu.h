ee51b36aab7af6192457fb8100fcb243bad430fe
Martin Rupp <martin.rupp@gcsc.uni-frankfurt.de>
2010-06-29 12:15:51 +0000

    added LU solver in algebra

---
+++ ugbase/lib_algebra/martin_algebra/lapack_lu.h
@@ -0,0 +1,52 @@
+/*
+ *  LapackLU.h
+ *  flexamg
+ *
+ *  Created by Martin Rupp on 26.11.09.
+ *  Copyright 2009 G-CSC, University of Frankfurt. All rights reserved.
+ *
+ */
+#ifndef __H__UG__MARTIN_ALGEBRA__LAPACK_LU__
+#define __H__UG__MARTIN_ALGEBRA__LAPACK_LU__
+
+
+#include "sparsematrix.h"
+#include "vector.h"
+//typedef long int __CLPK_integer;
+
+// TODO: for smallmatrix tasks like this, better use sth. like FLENS
+namespace ug{
+
+//template<typename T>
+class LapackLU
+{
+	//typedef T entry_type;
+public:
+	LapackLU()
+	{
+		densemat = NULL;
+		interchange = NULL;
+	}
+
+	~LapackLU()
+	{
+		if(densemat) delete[] densemat;
+		if(interchange) delete[] interchange;
+	}
+
+	// todo: for non-double
+	void init(const SparseMatrix<double> &A);
+
+	void prepare(const Vector<double> &b, Vector<double> &x) {}
+	void apply(const Vector<double> &b, Vector<double> &x);
+
+private:
+	size_t size;
+	double *densemat;
+	__CLPK_integer *interchange;
+};
+
+} // namespace ug
+#include "lapack_lu_impl.h"
+
+#endif /* __H__UG__MARTIN_ALGEBRA__LAPACK_LU__ */
---
+++ ugbase/lib_algebra/martin_algebra/lapack_lu_impl.h
@@ -0,0 +1,96 @@
+/*
+ *  CoarseSolver.cpp
+ *  flexamg
+ *
+ *  Created by Martin Rupp on 26.11.09.
+ *  Copyright 2009 G-CSC, University of Frankfurt. All rights reserved.
+ *
+ */
+
+#ifndef __H__UG__MARTIN_ALGEBRA__LAPACK_LU_IMPL__
+#define __H__UG__MARTIN_ALGEBRA__LAPACK_LU_IMPL__
+
+/*#include <iostream>
+
+#include <veclib/cblas.h>
+#include <veclib/clapack.h>
+
+#include "misc.h"
+#include "SparseMatrix.h"
+
+#include "CoarseSolver.h" */
+
+#include <veclib/cblas.h>
+#include <clapack.h>
+
+//using namespace std;
+namespace ug{
+
+//template<typename vec_type>
+void LapackLU::apply(const Vector<double> &b, Vector<double> &x)
+{
+	cout << "LapackLU::apply" << endl;
+	// TODO: Variing nr of unknowns
+	//int nrOfUnknowns = block_vector_traits<vec_type>::nrOfUnknowns;
+	const int nrOfUnknowns = 1;
+	UG_ASSERT(size == b.size() * nrOfUnknowns && size == x.size() * nrOfUnknowns, " wrong size! has to be " << size << ", but is " << b << " and " << x);
+
+	x = b;
+	//for(int i=0; i < b.size(); i++)
+		//for(int j=0; j<nrOfUnknowns; j++)
+			//vec[i*nrOfUnknowns+j] = getAt(b[i], j);
+
+	// solve system
+	char trans ='N';
+	__CLPK_integer dim = size;
+	__CLPK_integer nrhs = 1;
+	__CLPK_integer info;
+
+	dgetrs_(&trans, &dim, &nrhs, densemat, &dim, interchange, &x[0], &dim, &info);
+	UG_ASSERT(info == 0, "info is " << info);
+	//for(int i=0; i<x.size(); i++)
+		//for(int j=0; j<nrOfUnknowns; j++)
+			//setAt(x[i], j, vec[i*nrOfUnknowns+j]);
+
+}
+
+
+//template<typename entry_type>
+void LapackLU::init(const SparseMatrix<double> &A)
+{
+	cout << "LapackLU::init, A = " << A.num_rows() << " x " << A.num_cols() << endl;
+	const int nrOfUnknowns = 1 ; //block_matrix_traits<entry_type>::nrOfUnknowns;
+	size = A.row_size() * nrOfUnknowns;
+
+	densemat = new double[size*size];
+	interchange = new __CLPK_integer[size];
+
+	memset(densemat, 0, sizeof(double)*size*size);
+
+	/*for(int r=0; r<A.row_size(); r++)
+		for(typename SparseMatrix<entry_type>::cRowIterator it(A, r); !it.isEnd(); ++it)
+		{
+			int rr = r*nrOfUnknowns;
+			int cc = (*it).iIndex*nrOfUnknowns;
+			for(int r2=0; r2<nrOfUnknowns; r2++)
+					for(int c2=0; c2<nrOfUnknowns; c2++)
+						densemat[(rr+r2) + (cc+c2)*size] = getAt((*it).dValue, r2, c2);
+		}*/
+
+	for(size_t r=0; r<A.row_size(); r++)
+		for(SparseMatrix<double>::cRowIterator it = A.beginRow(r); !it.isEnd(); ++it)
+		{
+			int rr = r*nrOfUnknowns;
+			int cc = (*it).iIndex*nrOfUnknowns;
+			densemat[rr + cc*size] = (*it).dValue;
+		}
+
+	__CLPK_integer info = 0;
+	__CLPK_integer dim = size;
+	dgetrf_(&dim, &dim, densemat, &dim, interchange, &info);
+	UG_ASSERT(info == 0, "info is " << info << ( info > 0 ? ": matrix singular in U(i,i)" : ": i-th argument had had illegal value"));
+}
+
+} // namespace ug
+
+#endif /* __H__UG__MARTIN_ALGEBRA__LAPACK_LU_IMPL__ */
