912391c2b39c113101e77881b2feded59ff14bea
Andreas Vogel <andreas.vogel@gcsc.uni-frankfurt.de>
2010-06-30 08:22:57 +0000

    Grouping includes

---
+++ ugbase/lib_discretization/spacial_discretization/spacial_discretization.h
@@ -0,0 +1,33 @@
+/*
+ * domain_discretization.h
+ *
+ *  Created on: 29.06.2010
+ *      Author: andreasvogel
+ */
+
+#ifndef __H__LIB_DISCRETIZATION__SPACIAL_DISCRETIZATION__SPACIAL_DISCRETIZATION__
+#define __H__LIB_DISCRETIZATION__SPACIAL_DISCRETIZATION__SPACIAL_DISCRETIZATION__
+
+// plug in discs
+#include "lib_discretization/spacial_discretization/plug_in_disc/convection_diffusion_equation/convection_diffusion_assemble.h"
+#include "lib_discretization/spacial_discretization/plug_in_disc/density_driven_flow/density_driven_flow_assemble.h"
+
+//#include "lib_discretization/domain_discretization/coupled_plug_in_disc/convection_diffusion_equation/convection_diffusion_assemble.h"
+//#include "lib_discretization/domain_discretization/coupled_plug_in_disc/density_driven_flow/density_driven_flow_assemble.h"
+
+// domain discretization
+#include "lib_discretization/spacial_discretization/dirichlet_bnd_values.h"
+#include "lib_discretization/spacial_discretization/plug_in_domain_discretization.h"
+#include "lib_discretization/spacial_discretization/domain_discretization.h"
+
+// coupled system discretization
+//#include "lib_discretization/domain_discretization/system_discretization/coupled_system_domain_discretization.h"
+
+////////////////////////
+// element data
+////////////////////////
+
+#include "lib_discretization/spacial_discretization/disc_coupling/element_data.h"
+
+
+#endif /* __H__LIB_DISCRETIZATION__SPACIAL_DISCRETIZATION__SPACIAL_DISCRETIZATION__ */
