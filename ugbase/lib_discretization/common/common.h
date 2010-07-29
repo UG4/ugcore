74da8f4ea3213985bc25ad546658105c2dfbef61
Andreas Vogel <andreas.vogel@gcsc.uni-frankfurt.de>
2010-07-29 09:29:28 +0000

    common.h added. Includes just all other headers of this folder.

---
+++ ugbase/lib_discretization/common/common.h
@@ -0,0 +1,21 @@
+/*
+ * common.h
+ *
+ *  Created on: 29.07.2010
+ *      Author: andreasvogel
+ */
+
+#ifndef __H__LIB_DISCRETIZATION__COMMON__COMMON__
+#define __H__LIB_DISCRETIZATION__COMMON__COMMON__
+
+// function group
+#include "./function_group.h"
+
+// multi indices
+#include "./multi_indices.h"
+
+// local algebra
+#include "./local_algebra.h"
+
+
+#endif /* __H__LIB_DISCRETIZATION__COMMON__COMMON__ */
