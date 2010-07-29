251ddb7f6fd8d2d0b9e8e435e1c4c2df70f37e30
Andreas Vogel <andreas.vogel@gcsc.uni-frankfurt.de>
2010-07-29 08:55:48 +0000

    Deleting not used class DoFValueContainer.

--- ugbase/lib_discretization/dof_manager/dof_manager.h
+++ ugbase/lib_discretization/dof_manager/dof_manager.h
@@ -27,40 +27,4 @@ enum DoFManagerGroupStrategy {
 
 }
 
-template <typename TMultiIndex>
-class DoFValueContainer{
-	public:
-		typedef TMultiIndex index_type;
-		typedef number value_type;
-	public:
-		DoFValueContainer(){};
-
-		void resize(std::size_t n)
-		{
-			m_values.resize(n);
-			m_indices.resize(n);
-		}
-
-		std::size_t size()
-		{
-			return m_values.size();
-		}
-
-		const TMultiIndex& index(std::size_t i)
-		{
-			assert(i < m_indices.size());
-			return m_indices[i];
-		}
-
-		value_type value(std::size_t i)
-		{
-			assert(i < m_values.size());
-			return m_values[i];
-		}
-
-	protected:
-		std::vector<value_type> m_values;
-		std::vector<TMultiIndex> m_indices;
-};
-
 #endif /* __H__LIB_DISCRETIZATION__DOF_MANAGER__DOF_MANAGER__ */
