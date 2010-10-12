7398c501385e3c02be4685a3cf422984323ef4be
Martin Rupp <martin.rupp@gcsc.uni-frankfurt.de>
2010-10-12 11:57:54 +0000

    Added MathTensor2/3/4 for Tensors like 2x3x4x1. Old MathTensor still there.

--- ugbase/common/math/math_vector_matrix/math_tensor.h
+++ ugbase/common/math/math_vector_matrix/math_tensor.h
@@ -15,6 +15,7 @@ namespace ug
 //	MathTensor
 ///	a mathematical Tensor of rank TRank and N entries.
 
+
 template <size_t TRank, size_t N, typename T = number> class MathTensor;
 
 template <size_t TRank, size_t N, typename T>
@@ -101,11 +102,86 @@ class MathTensor<1, N, T>
 template <size_t TRank, size_t N, typename T>
 std::ostream& operator<< (std::ostream& outStream, const ug::MathTensor<TRank, N, T>& v)
 {
+	outStream << "[";
 	for(size_t i = 0; i < v.size()-1; ++i)
-			outStream << v[i] << ' ';
-	outStream << v[v.size()-1] << ',';
+			outStream << v[i] << ", ";
+	outStream << v[v.size()-1] << "]";
 	return outStream;
 }
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// unfortunately, not all tensor are like 3x3x3x3, but also 3x2x3x2.
+// merge this if possible with MathVector<MathVector<2> > ?
+
+template <typename TEntry, size_t N>
+class MathTensorX
+{
+public:
+	// type of value
+	typedef TEntry value_type;
+
+	// size type
+	typedef size_t size_type;
+
+	// Dimension of tensor
+	static const size_t Dimension = N;
+
+	// Rank of tensor (i.e. number of indices)
+	static const size_t Rank = TEntry::Rank+1;
+
+public:
+	MathTensorX() { }
+	MathTensorX(const MathTensorX& v)	{assign(v);}
+
+	// operations with other vectors
+	MathTensorX& operator=  (const MathTensorX& v)
+	{
+		if(this != &v){assign(v);}
+		return *this;
+	}
+
+	inline size_t size() const {return N;}
+	inline size_t rank() const {return Rank;}
+
+	inline value_type& operator[](size_t i)				{UG_ASSERT(i < size(), "Index out of range."); return m_data[i];}
+	inline const value_type& operator[](size_t i) const	{UG_ASSERT(i < size(), "Index out of range."); return m_data[i];}
+
+protected:
+	inline void assign(const MathVector<N>& v) {for(size_t i = 0; i < N; ++i) m_data[i] = v[i];}
+
+protected:
+	TEntry m_data[N];
+};
+
+template <size_t N, typename T = number>
+class MathTensor1 : public MathTensor<1, N, T> { };
+
+template <size_t N1, size_t N2, typename T = number>
+class MathTensor2 : public MathTensorX<MathTensor1<N2, T>, N1> { };
+
+template <size_t N1, size_t N2, size_t N3, typename T = number>
+class MathTensor3 : public MathTensorX< MathTensorX<MathTensor1<N3, T>, N2>, N1> { };
+
+template <size_t N1, size_t N2, size_t N3, size_t N4, typename T = number>
+class MathTensor4 : public MathTensorX< MathTensorX< MathTensorX<MathTensor1<N4, T>, N3>, N2>, N1> { };
+
+
+template <typename TEntry, size_t N>
+std::ostream& operator<< (std::ostream& outStream, const ug::MathTensorX<TEntry, N>& v)
+{
+	outStream << "[";
+	for(size_t i = 0; i < N-1; ++i)
+		outStream << v[i] << ", ";
+	outStream << v[N-1] << "]";
+	return outStream;
+}
+
+
+
+
+
+
 
 }//	end of namespace
 
