22c1adb09b4bfb18310d75697a05616d7a47fd3e
Andreas Vogel <andreas.vogel@gcsc.uni-frankfurt.de>
2010-10-13 11:22:06 +0000

    Addibg string util

---
+++ ugbase/common/string_util.cpp
@@ -0,0 +1,28 @@
+// author: Andreas Vogel
+
+#include "string_util.h"
+
+namespace ug{
+
+// help function to tokenize the parameter string
+void TokenizeString(const std::string& str, std::vector<std::string>& tokens, const std::string& delimiters)
+{
+	using namespace std;
+	tokens.clear();
+	// Skip delimiters at beginning.
+	string::size_type lastPos = str.find_first_not_of(delimiters, 0);
+	// Find first "non-delimiter".
+	string::size_type pos     = str.find_first_of(delimiters, lastPos);
+
+	while (string::npos != pos || string::npos != lastPos)
+	{
+		// Found a token, add it to the vector.
+		tokens.push_back(str.substr(lastPos, pos - lastPos));
+		// Skip delimiters.  Note the "not_of"
+		lastPos = str.find_first_not_of(delimiters, pos);
+		// Find next "non-delimiter"
+		pos = str.find_first_of(delimiters, lastPos);
+	}
+}
+
+}
---
+++ ugbase/common/string_util.h
@@ -0,0 +1,17 @@
+//	string_util.h
+//	created by Andreas Vogel
+
+#ifndef __H__COMMON_STRING_UTIL__
+#define __H__COMMON_STRING_UTIL__
+
+#include <string>
+#include <vector>
+
+namespace ug{
+
+// help function to tokenize the parameter string
+void TokenizeString(const std::string& str, std::vector<std::string>& tokens, const std::string& delimiters);
+
+} // end namespace ug
+
+#endif /*__H__COMMON_STRING_UTIL__*/
