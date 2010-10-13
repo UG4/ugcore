cf61fa366ad142570b8e0cf7034e9cbd8242dd86
Andreas Vogel <andreas.vogel@gcsc.uni-frankfurt.de>
2010-10-13 16:36:24 +0000

    Adding user data interfaces.

---
+++ ugbase/lib_discretization/spacial_discretization/user_data.h
@@ -0,0 +1,62 @@
+/*
+ * user_data.h
+ *
+ *  Created on: 13.10.2010
+ *      Author: andreasvogel
+ */
+
+#ifndef __H__LIB_DISCRETIZATION__SPACIAL_DISCRETIZATION__USER_DATA__
+#define __H__LIB_DISCRETIZATION__SPACIAL_DISCRETIZATION__USER_DATA__
+
+namespace ug {
+
+
+template <int dim>
+class IUserNumberProvider
+{
+	public:
+	//	Functor Type
+		typedef boost::function<void (number& n, const MathVector<dim>& x, number& time)> functor_type;
+
+	/// provides the functor
+		virtual functor_type get_functor() const = 0;
+
+	///	virtual destructor
+		virtual ~IUserNumberProvider(){}
+};
+
+
+
+template <int dim>
+class IUserVectorProvider
+{
+	public:
+	//	Functor Type
+		typedef boost::function<void (MathVector<dim>& v, const MathVector<dim>& x, number& time)> functor_type;
+
+	/// provides the functor
+		virtual functor_type get_functor() const = 0;
+
+	///	virtual destructor
+		virtual ~IUserVectorProvider(){}
+};
+
+
+template <int dim>
+class IUserMatrixProvider
+{
+	public:
+	//	Functor Type
+		typedef boost::function<void (MathMatrix<dim,dim>& D, const MathVector<dim>& x, number& time)> functor_type;
+
+	/// provides the functor
+		virtual functor_type get_functor() const = 0;
+
+	///	virtual destructor
+		virtual ~IUserMatrixProvider(){}
+};
+
+
+}
+
+#endif /* __H__LIB_DISCRETIZATION__SPACIAL_DISCRETIZATION__USER_DATA__ */
