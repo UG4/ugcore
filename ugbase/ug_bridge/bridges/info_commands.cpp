ba406e6826f1eb079effbbd560e9f1e80bf4f75a
Martin Rupp <martin.rupp@gcsc.uni-frankfurt.de>
2010-10-19 12:06:47 +0000

    - added Function ClassInstantiations: prints which objects are Instantiations of a class. - added SubClasses: prints subclasses of a class.

--- ugbase/ug_bridge/bridges/info_commands.cpp
+++ ugbase/ug_bridge/bridges/info_commands.cpp
@@ -30,6 +30,19 @@ namespace ug
 namespace bridge
 {
 
+
+const IExportedClass *FindClass(const char* classname)
+{
+	bridge::Registry &reg = GetUGRegistry();
+	for(size_t j=0; j<reg.num_classes(); ++j)
+		if(strcmp(classname, reg.get_class(j).name()) == 0)
+		{
+			return &reg.get_class(j);
+			break;
+		}
+	return NULL;
+}
+
 /**
  *
  * \brief Gets a description of the i-th parameter of a ParameterStack
@@ -143,22 +156,18 @@ void PrintClassInfo(const IExportedClass &c)
 /**
  *
  * Searches the classname in the Registry and prints info of the class
- * and its subclasses
  */
 bool PrintClassInfo(const char *classname)
 {
-	bridge::Registry &reg = GetUGRegistry();
-
 	// search registry for that class
-	for(size_t j=0; j<reg.num_classes(); ++j)
+	const IExportedClass *c = FindClass(classname);
+	if(c)
 	{
-		if(strcmp(classname, reg.get_class(j).name()) == 0)
-		{
-			PrintClassInfo(reg.get_class(j));
-			return true;
-		}
+		PrintClassInfo(*c);
+		return true;
 	}
-	return false;
+	else
+		return false;
 }
 
 namespace lua
@@ -177,8 +186,15 @@ namespace lua
 bool UGTypeInfo(const char *p)
 {
 	UG_LOG("\n");
-	if(PrintClassInfo(p))
+	const IExportedClass *c = FindClass(p);
+	if(c)
+	{
+		const std::vector<const char*> *names = c->class_names();
+		for(size_t i=0; i < names->size(); ++i)
+			PrintClassInfo(names->at(i));
+		UG_LOG(endl);
 		return true;
+	}
 
 	lua_State* L = script::GetDefaultLuaState();
 
@@ -211,6 +227,7 @@ bool UGTypeInfo(const char *p)
 		if(lua_getmetatable(L, -1) == 0)
 		{
 			UG_LOG("global variable " << p << " has light user data, but no metatable." << endl);
+			lua_pop(L, 1); // pop globals
 			return false;
 		}
 
@@ -220,20 +237,20 @@ bool UGTypeInfo(const char *p)
 		if(lua_isnil(L, -1) || !lua_isuserdata(L, -1))
 		{
 			UG_LOG("global variable " << p << " has metatable, but cannot access names." << endl);
-			lua_pop(L, 1);
+			lua_pop(L, 3); // pop metatable, userdata, globals
 			return false;
 		}
 		const std::vector<const char*> *names =
 				(const std::vector<const char*>*) lua_touserdata(L, -1);
-		lua_pop(L, 2);
+		lua_pop(L, 2); // pop metatable, userdata
 		UG_LOG("Typeinfo for " << p << ": " << endl);
 		for(size_t i=0; i < names->size(); ++i)
-			PrintClassInfo((*names)[i]);
+			PrintClassInfo(names->at(i));
 	}
 	else
 		UG_LOG(p << " is a " << lua::GetLuaTypeString(L, -1) << endl);
 
-	lua_pop(L, 1);
+	lua_pop(L, 1); // pop globals
 
 	UG_LOG("\n");
 	return true;
@@ -263,14 +280,15 @@ bool IsClassInParameters(const bridge::ParameterStack &par, const char *classnam
  *
  * \param classname the class (and only this class) to print usage in functions/member functions of.
  */
-bool ClassUsageExact(const char *classname)
+bool ClassUsageExact(const char *classname, bool OutParameters)
 {
 	bridge::Registry &reg = GetUGRegistry();
 	// check functions
 	for(size_t i=0; i<reg.num_functions(); i++)
 	{
 		const bridge::ExportedFunctionBase &thefunc = reg.get_function(i);
-		if(IsClassInParameters(thefunc.params_in(), classname) || IsClassInParameters(thefunc.params_out(), classname))
+		if(!OutParameters && IsClassInParameters(thefunc.params_in(), classname) ||
+				OutParameters && IsClassInParameters(thefunc.params_out(), classname))
 			PrintFunctionInfo(thefunc, false, classname);
 	}
 
@@ -281,7 +299,8 @@ bool ClassUsageExact(const char *classname)
 		for(size_t i=0; i<c.num_methods(); i++)
 		{
 			const bridge::ExportedFunctionBase &thefunc = c.get_method(i);
-			if(IsClassInParameters(thefunc.params_in(), classname) || IsClassInParameters(thefunc.params_out(), classname))
+			if(!OutParameters && IsClassInParameters(thefunc.params_in(), classname) ||
+					OutParameters && IsClassInParameters(thefunc.params_out(), classname))
 			{
 				UG_LOG(c.name() << " ::");
 				PrintFunctionInfo(thefunc, false, classname);
@@ -291,13 +310,49 @@ bool ClassUsageExact(const char *classname)
 		for(size_t i=0; i<c.num_const_methods(); i++)
 		{
 			const bridge::ExportedFunctionBase &thefunc = c.get_const_method(i);
-			if(IsClassInParameters(thefunc.params_in(), classname) || IsClassInParameters(thefunc.params_out(), classname))
+			if(!OutParameters && IsClassInParameters(thefunc.params_in(), classname) ||
+					OutParameters && IsClassInParameters(thefunc.params_out(), classname))
 			{
 				UG_LOG(c.name() << " ::");
 				PrintFunctionInfo(thefunc, false, classname);
 			}
 		}
 	}
+	return true;
+}
+
+bool SubClasses(const char *classname)
+{
+	bridge::Registry &reg = GetUGRegistry();
+	const IExportedClass *c = FindClass(classname);
+	if(c == NULL)
+	{
+		UG_LOG("Class name " << classname << " not found\n");
+		return false;
+	}
+
+	UG_LOG("\n");
+	for(size_t j=0; j<reg.num_classes(); ++j)
+	{
+		const std::vector<const char*> *names = reg.get_class(j).class_names();
+		if(names == NULL) continue;
+
+		for(size_t k=0; k<names->size(); k++)
+		{
+			if(strcmp(names->at(k), classname) == 0)
+			{
+				UG_LOG(reg.get_class(j).name() << " (");
+				for(size_t l=0; l<names->size(); l++)
+				{
+					if(l>0) UG_LOG(" :: ");
+					UG_LOG(names->at(l));
+				}
+				UG_LOG(")\n");
+				break;
+			}
+		}
+	}
+	UG_LOG("\n");
 	return true;
 }
 
@@ -309,34 +364,94 @@ bool ClassUsageExact(const char *classname)
 bool ClassUsage(const char *classname)
 {
 	UG_LOG("\n");
-	bridge::Registry &reg = GetUGRegistry();
-	const IExportedClass *c = NULL;
+
 	// find class
-	for(size_t j=0; j<reg.num_classes(); ++j)
-		if(strcmp(classname, reg.get_class(j).name()) == 0)
-		{
-			c = &reg.get_class(j);
-			break;
-		}
+	const IExportedClass *c = FindClass(classname);
 	if(c == NULL)
 	{
 		UG_LOG("Class name " << classname << " not found\n");
 		return false;
 	}
 
+	// print usages in functions
+
+	UG_LOG("--- Functions returning " << classname << ": ---\n");
+	ClassUsageExact(classname, true);
+
 	const std::vector<const char*> *names = c->class_names();
-	if(names == 0 || names->size() < 2)
-		ClassUsageExact(classname);
-	else
+	if(names != NULL && names->size() > 1)
 	{
-		for(size_t i = 0; i<names->size(); i++)
+		for(size_t i = 1; i<names->size(); i++)
 		{
-			UG_LOG("--- as " << names->at(i) << ": ---\n");
-			ClassUsageExact(names->at(i));
+			UG_LOG("--- Functions using " << names->at(i) << ": ---\n");
+			ClassUsageExact(names->at(i), false);
 		}
 	}
 
 	UG_LOG("\n");
+	return true;
+}
+bool ClassNameVecContains(const std::vector<const char*>& names, const char* name);
+bool ClassInstantiations(const char *classname)
+{
+	UG_LOG(endl);
+	bridge::Registry &reg = GetUGRegistry();
+	const IExportedClass *c = FindClass(classname);
+	if(c == NULL)
+	{
+		UG_LOG("Class " << classname << " not found\n");
+		return false;
+	}
+
+	lua_State* L = script::GetDefaultLuaState();
+	bool bFound = false;
+
+	// iterate through all of lua's global string table
+	for(int i=0; i<G(L)->strt.size; i++)
+	{
+		GCObject *obj;
+		for (obj = G(L)->strt.hash[i]; obj != NULL; obj = obj->gch.next)
+		{
+			// get the string
+			TString *ts = rawgco2ts(obj);
+			if(ts == NULL) continue;
+
+			const char *luastr = getstr(ts);
+			// check is of a global variable
+			lua_getglobal(L, luastr);
+			if(lua_isnil(L, -1) || !lua_isuserdata(L, -1) || lua_getmetatable(L, -1) == 0)
+			{
+				lua_pop(L, 1); // remove global from stack
+				continue; // nope
+			}
+
+			// get names
+			lua_pushstring(L, "names");
+			lua_rawget(L, -2);
+			if(lua_isnil(L, -1) || !lua_isuserdata(L, -1))
+			{
+				lua_pop(L, 3); // pop userdata, metatable, globals
+				continue;
+			}
+			const std::vector<const char*> *names =
+					(const std::vector<const char*>*) lua_touserdata(L, -1);
+			lua_pop(L, 3); // pop userdata, metatable, globals
+			if(ClassNameVecContains(*names, classname))
+			{
+				bFound = true;
+				UG_LOG(setw(10) << left << luastr);
+				UG_LOG(" (");
+				for(size_t i=0; i<names->size(); i++)
+				{
+					if(i>0) UG_LOG(" :: ");
+					UG_LOG(names->at(i));
+				}
+				UG_LOG(")\n");
+			}
+		}
+	}
+	if(!bFound) UG_LOG("No instantiations of " << classname << " or subclasses found.");
+	UG_LOG(endl);
 	return true;
 }
 
@@ -344,6 +459,8 @@ void RegisterInfoCommands(Registry &reg)
 {
 	reg.add_function("TypeInfo", &UGTypeInfo);
 	reg.add_function("ClassUsage", &ClassUsage);
+	reg.add_function("ClassInstantiations" ,&ClassInstantiations);
+	reg.add_function("SubClasses" ,&SubClasses);
 }
 
 
