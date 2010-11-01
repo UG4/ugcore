85264c890780295e199c47183cd959cd874d931c
Andreas Vogel <andreas.vogel@gcsc.uni-frankfurt.de>
2010-11-01 18:20:22 +0000

    Adding missing beta1 in Multiplication. (before: = 1.0)

--- ugbase/lib_algebra/small_algebra/small_matrix/densematrix_operations.h
+++ ugbase/lib_algebra/small_algebra/small_matrix/densematrix_operations.h
@@ -26,16 +26,16 @@ struct matrix_algebra_type_traits<DenseMatrix<T> >
 	static const matrix_algebra_type type = MATRIX_USE_GLOBAL_FUNCTIONS;
 };
 
-//! calculates dest = beta1 * A1;
+//! calculates dest = beta1 * A1 * w1;
 template<typename vector_t, typename matrix_t>
 inline void MatMult(DenseVector<vector_t> &dest,
 		const number &beta1, const DenseMatrix<matrix_t> &A1, const DenseVector<vector_t> &w1)
 {
 	for(size_t r = 0; r < dest.size(); ++r)
 	{
-		MatMult(dest[r], 1.0, A1(r,0), w1[0]);
+		MatMult(dest[r], beta1, A1(r,0), w1[0]);
 		for(size_t c = 1; c < w1.size(); ++c)
-			MatMultAdd(dest[r], 1.0, dest[r], 1.0, A1(r,c), w1[c]);
+			MatMultAdd(dest[r], 1.0, dest[r], beta1, A1(r,c), w1[c]);
 	}
 }
 
@@ -49,7 +49,7 @@ inline void MatMultAdd(DenseVector<vector_t> &dest,
 	{
 		VecScaleAssign(dest[r], alpha1, v1[r]);
 		for(size_t c = 0; c < w1.size(); ++c)
-			MatMultAdd(dest[r], 1.0, dest[r], 1.0, A1(r,c), w1[c]);
+			MatMultAdd(dest[r], 1.0, dest[r], beta1, A1(r,c), w1[c]);
 	}
 }
 
@@ -63,7 +63,7 @@ inline void MatMultTransposedAdd(DenseVector<vector_t> &dest,
 	{
 		VecScaleAssign(dest[r], alpha1, v1[r]);
 		for(size_t c = 0; c < w1.size(); ++c)
-			MatMultTransposedAdd(dest[r], 1.0, dest[r], 1.0, A1(c,r), w1[c]);
+			MatMultTransposedAdd(dest[r], 1.0, dest[r], beta1, A1(c,r), w1[c]);
 	}
 }
 
