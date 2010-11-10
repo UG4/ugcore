6b4f1975dbf0a92cf0e4f90a57f5eeda080683d2
Martin Scherer <martin.scherer@gcsc.uni-frankfurt.de>
2010-11-10 15:05:49 +0000

    exported APP_PATH to ug.h and init it with UGInit().
    a check is performed, whether the path exists. UGInit now returns -1, if paths are somehow invalid.

--- ugbase/ug.h
+++ ugbase/ug.h
@@ -4,6 +4,7 @@
 
 #ifndef __H__UG__UG__
 #define __H__UG__UG__
+#include <string>
 
 #include "common/profiler/profiler.h"
 
@@ -23,6 +24,50 @@
 namespace ug
 {
 
+//	we store the path in which scripts are located in this variable
+//	we assume that scripts are located in ../scripts relative to ugshells path.
+static std::string APP_PATH;
+static std::string SCRIPT_PATH;
+static std::string DATA_PATH;
+
+/**
+ *  init app and data paths
+ *  @return wether paths initialized correctly?
+ */
+inline static bool initPaths(const char* argv0) {
+	//TODO: on some systems argv0 does __not__ contain the absolute path to the process!
+	//some ugly macros are needed.
+
+
+	//	extract the application path.
+	cout << "argv[0]: " << argv0 << endl;
+	string tPath = argv0;
+	size_t pos = tPath.find_last_of("/");
+	if(pos == string::npos) {
+		pos = tPath.find_last_of("\\\\");
+	}
+	if(pos != string::npos) { // todo: this could be simply else?
+		APP_PATH = tPath.substr(0, pos);
+	}
+	else {
+		APP_PATH = ".";
+	}
+
+	SCRIPT_PATH = APP_PATH + "/../scripts";
+
+	DATA_PATH=string(APP_PATH);
+	DATA_PATH.append("/../data");
+
+	LOG("app path set to: " << APP_PATH <<
+			endl << "script path set to: " << SCRIPT_PATH <<
+			endl << "data path set to: " << DATA_PATH << endl);
+
+	if(!script::FileExists(APP_PATH.c_str()) || !script::FileExists(DATA_PATH.c_str()) || !script::FileExists(SCRIPT_PATH.c_str()) )  {
+		return false;
+	}
+
+	return true;
+}
 
 ////////////////////////////////////////////////////////////////////////
 ///	initializes ug
@@ -45,7 +90,26 @@ inline int UGInit(int argc, char* argv[], int parallelOutputProcRank = -1)
 	//	initialize ug-interfaces
 		bridge::RegisterStandardInterfaces(bridge::GetUGRegistry());
 	}
+
+	bool pathsCorrect = initPaths(argv[0]);
+	if(!pathsCorrect)
+		return -1;
+
 	return 0;
+}
+
+/// returns the ug app path
+inline const std::string& UGGetApplicationPath() {
+	return APP_PATH;
+}
+/// returns the ug script path
+inline const std::string& UGGetScriptPath() {
+	return SCRIPT_PATH;
+}
+
+/// returns the ug data path
+inline const std::string& UGGetDataPath(){
+	return DATA_PATH;
 }
 
 ////////////////////////////////////////////////////////////////////////
