14a5b0d73b3ddd26a084dee889c2ee5763871e51
Andreas Vogel <andreas.vogel@gcsc.uni-frankfurt.de>
2010-11-15 17:41:40 +0000

    New Communication Policies for Feti Solver: VecSubtract, VecScaleAdd

--- ugbase/lib_algebra/parallelization/communication_policies.h
+++ ugbase/lib_algebra/parallelization/communication_policies.h
@@ -65,6 +65,62 @@ class ComPol_VecCopy : public pcl::ICommunicationPolicy<IndexLayout>
 };
 
 template <class TVector>
+class ComPol_VecScaleCopy : public pcl::ICommunicationPolicy<IndexLayout>
+{
+	public:
+		ComPol_VecScaleCopy() : m_pVec(NULL)	{}
+		ComPol_VecScaleCopy(TVector* pVec, number scale)
+			: m_pVec(pVec), m_scale(scale)	{}
+
+		void set_vector(TVector* pVec)	{m_pVec = pVec;}
+		void set_scale(number scale)	{m_scale = scale;}
+
+		virtual int
+		get_required_buffer_size(Interface& interface)
+		{
+			return interface.size() * sizeof(typename TVector::value_type);
+		}
+
+		virtual bool
+		collect(std::ostream& buff, Interface& interface)
+		{
+			TVector& v = *m_pVec;
+
+			for(typename Interface::iterator iter = interface.begin();
+				iter != interface.end(); ++iter)
+			{
+				const size_t index = interface.get_element(iter);
+				typename TVector::value_type entry = v[index];
+				buff.write((char*)&entry, sizeof(typename TVector::value_type));
+			}
+			return true;
+		}
+
+		virtual bool
+		extract(std::istream& buff, Interface& interface)
+		{
+			typename TVector::value_type entry;
+			TVector& v = *m_pVec;
+
+			for(typename Interface::iterator iter = interface.begin();
+				iter != interface.end(); ++iter)
+			{
+				const size_t index = interface.get_element(iter);
+				buff.read((char*)&entry, sizeof(typename TVector::value_type));
+				entry *= m_scale;
+				v[index] = entry;
+			}
+			return true;
+		}
+
+	private:
+		TVector* m_pVec;
+
+		number m_scale;
+};
+
+
+template <class TVector>
 class ComPol_VecAdd : public pcl::ICommunicationPolicy<IndexLayout>
 {
 	public:
@@ -157,6 +213,56 @@ class ComPol_VecAddSetZero : public pcl::ICommunicationPolicy<IndexLayout>
 				const size_t index = interface.get_element(iter);
 				buff.read((char*)&entry, sizeof(typename TVector::value_type));
 				v[index] += entry;
+			}
+			return true;
+		}
+
+	private:
+		TVector* m_pVec;
+};
+
+template <class TVector>
+class ComPol_VecSubtract : public pcl::ICommunicationPolicy<IndexLayout>
+{
+	public:
+		ComPol_VecSubtract() : m_pVec(NULL)	{}
+		ComPol_VecSubtract(TVector* pVec) : m_pVec(pVec)	{}
+
+		void set_vector(TVector* pVec)	{m_pVec = pVec;}
+
+		virtual int
+		get_required_buffer_size(Interface& interface)
+		{
+			return interface.size() * sizeof(typename TVector::value_type);
+		}
+
+		virtual bool
+		collect(std::ostream& buff, Interface& interface)
+		{
+			TVector& v = *m_pVec;
+
+			for(typename Interface::iterator iter = interface.begin();
+				iter != interface.end(); ++iter)
+			{
+				const size_t index = interface.get_element(iter);
+				typename TVector::value_type entry = v[index];
+				buff.write((char*)&entry, sizeof(typename TVector::value_type));
+			}
+			return true;
+		}
+
+		virtual bool
+		extract(std::istream& buff, Interface& interface)
+		{
+			typename TVector::value_type entry;
+			TVector& v = *m_pVec;
+
+			for(typename Interface::iterator iter = interface.begin();
+				iter != interface.end(); ++iter)
+			{
+				const size_t index = interface.get_element(iter);
+				buff.read((char*)&entry, sizeof(typename TVector::value_type));
+				v[index] -= entry;
 			}
 			return true;
 		}
--- ugbase/lib_algebra/parallelization/parallelization_util.h
+++ ugbase/lib_algebra/parallelization/parallelization_util.h
@@ -106,6 +106,86 @@ void ConsistentToUnique(	TVector* pVec,
 	}
 }
 
+/** subtracts values from one layout from the other (bidirectional)
+ *
+ */
+template <typename TVector>
+void VecSubtractOnLayout(	TVector* pVec,
+							IndexLayout& masterLayout, IndexLayout& slaveLayout,
+							pcl::ParallelCommunicator<IndexLayout>* pCom = NULL)
+{
+	//	create a new communicator if required.
+		pcl::ParallelCommunicator<IndexLayout> tCom;
+		if(!pCom)
+			pCom = &tCom;
+		pcl::ParallelCommunicator<IndexLayout>& com = *pCom;
+
+	//	step 1: subtract slave values from master
+	//	create the required communication policies
+		ComPol_VecSubtract<TVector> cpVecSubtract(pVec);
+
+	//	Subtract slave values from master values
+		com.send_data(slaveLayout, cpVecSubtract);
+		com.receive_data(masterLayout, cpVecSubtract);
+		com.communicate();
+
+	//	step 2: Copy values to slaves
+		ComPol_VecScaleCopy<TVector> cpVecScaleCopy(pVec, -1.0);
+
+		com.send_data(masterLayout, cpVecScaleCopy);
+		com.receive_data(slaveLayout, cpVecScaleCopy);
+		com.communicate();
+}
+
+
+/** adds one vector to another only on the interface
+ *
+ */
+template <typename TVector>
+void VecAddOnLayoutWithoutCommunication(	TVector* pVecDest, const TVector* pVecSrc,
+											IndexLayout& Layout)
+{
+	typename IndexLayout::iterator iter = Layout.begin();
+	typename IndexLayout::iterator end = Layout.end();
+
+	for(; iter != end; ++iter)
+	{
+		typename IndexLayout::Interface& interface = Layout.interface(iter);
+		{
+			for(typename IndexLayout::Interface::iterator iter = interface.begin();
+				iter != interface.end(); ++iter)
+			{
+				const size_t index = interface.get_element(iter);
+				(*pVecDest)[index] += (*pVecSrc)[index];
+			}
+		}
+	}
+}
+
+
+/** sets dirichlet rows for interface indices
+ *
+ */
+template <typename TMatrix>
+void MatSetDirichletWithoutCommunication(	TMatrix* pMatrix,
+											IndexLayout& Layout)
+{
+	typename IndexLayout::iterator iter = Layout.begin();
+	typename IndexLayout::iterator end = Layout.end();
+
+	for(; iter != end; ++iter)
+	{
+		typename IndexLayout::Interface& interface = Layout.interface(iter);
+		{
+			for(typename IndexLayout::Interface::iterator iter = interface.begin();
+				iter != interface.end(); ++iter)
+			{
+				const size_t index = interface.get_element(iter);
+				SetDirichletRow(*pMatrix, index);
+			}
+		}
+	}
+}
 
 }//	end of namespace
 
