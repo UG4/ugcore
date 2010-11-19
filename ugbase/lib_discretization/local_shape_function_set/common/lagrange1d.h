d199b9b7c06d8d19e917db31b17c8db676d8fc5a
Andreas Vogel <andreas.vogel@gcsc.uni-frankfurt.de>
2010-11-19 18:20:03 +0000

    Adding 1D Polynomials.

---
+++ ugbase/lib_discretization/local_shape_function_set/common/lagrange1d.h
@@ -0,0 +1,209 @@
+/*
+ * lagrange1d.h
+ *
+ *  Created on: 17.11.2010
+ *      Author: andreasvogel
+ */
+
+#ifndef __H__UG_LIB_DISCRETIZATION__LOCAL_SHAPE_FUNCTION_SET__COMMON__LAGRANGE1D__
+#define __H__UG_LIB_DISCRETIZATION__LOCAL_SHAPE_FUNCTION_SET__COMMON__LAGRANGE1D__
+
+#include "./polynomial1d.h"
+#include "common/math/ugmath.h"
+#include <vector>
+
+namespace ug{
+
+/** Lagrange Polynomial for arbitrary points
+ *
+ */
+class Lagrange1D
+	: public Polynomial1D
+{
+	public:
+	/**	constructor for lagrange polynomial i using interpolation points pos
+	 * This constructor creates a lagrange polynomial with interpolation points
+	 * given in pos for the i-th point, i.e. value(pos_i) == 1, value(pos_j) == 0
+	 * for j != i. Therefore, it must hold that 0 <= i < pos.size()
+	 */
+		Lagrange1D(const size_t i, const std::vector<number>& vPos)
+		{
+		//	compute coefficients
+			compute_coeffs(i, vPos);
+
+		//	remember positions
+			m_vPos = vPos;
+		}
+
+	///	returns the position of the i'th interpolation point
+		number position(const size_t i) const
+		{
+			UG_ASSERT(i < m_vPos.size(), "Invalid index");
+			return m_vPos[i];
+		}
+
+	protected:
+	/// computes the coefficients for passed interpolation points
+		void compute_coeffs(const size_t i, const std::vector<number> vPos)
+		{
+		//	start coefficients
+			std::vector<number> vStart(1, 1.0);
+
+		//	start polynomial
+			this->set_coefficients(vStart);
+
+		//	help polynomial used for each factor
+			std::vector<number> vFactor(2,1.0);
+
+		//	scaling of polynom
+			number scale = 1.0;
+
+		//	fill coefficients
+			for(size_t j = 0; j < vPos.size(); ++j)
+			{
+				if(j == i) continue;
+
+			//	set first coefficent to minus position
+				vFactor[0] = -vPos[j];
+			//	create polynom for (-vPos, 1)
+				Polynomial1D tmpPol(vFactor);
+			//	multiply
+				(*this) *= tmpPol;
+			// 	multiply scale
+				scale *= 1./(vPos[i]-vPos[j]);
+			}
+
+		//	multiply by scale
+			(*this) *= scale;
+		}
+
+	private:
+		std::vector<number> m_vPos;
+};
+
+/** EquiDistant Lagrange Function
+ *
+ */
+class EquidistantLagrange1D
+	: public Polynomial1D
+{
+	public:
+	/** creates a lagrange polynomial with equidistant interpolation points
+	 * \param[in] 	i		number of interpolation point, where polynom is 1
+	 * \param[in]	degree	degree of polynom
+	 */
+		EquidistantLagrange1D(const size_t i, const size_t degree)
+		{
+			UG_ASSERT(i <= degree, "Only #degree shape functions.");
+			compute_coeffs(i, degree);
+		}
+
+	///	returns the position of the i'th interpolation point
+		static number position(const size_t i, const size_t degree)
+		{
+			UG_ASSERT(i <= degree, "Invalid index");
+			return (number)i/(number)degree;
+		}
+
+	protected:
+	/// computes the coefficients for passed interpolation points
+		void compute_coeffs(const int i, const int p)
+		{
+		//	start coefficients
+			std::vector<number> vStart(1, 1.0);
+
+		//	start polynomial
+			this->set_coefficients(vStart);
+
+		//	help polynomial used for each factor
+			std::vector<number> vFactor(2, p);
+
+		//	scaling of polynom
+			number scale = 1.0;
+
+		//	fill coefficients
+			for(int j = 0; j < p; ++j)
+			{
+				if(j == i) continue;
+
+			//	set first coefficent to minus position
+				vFactor[0] = -j;
+			//	create polynom for (-j, p)
+				Polynomial1D tmpPol(vFactor);
+			//	multiply
+				(*this) *= tmpPol;
+			// 	multiply scale
+				scale *= 1./(i-j);
+			}
+
+		//	multiply by scale
+			(*this) *= scale;
+		}
+};
+
+/** Truncated EquiDistant Lagrange Function
+ *
+ * Creates for given order <tt>p</tt> and iterpolation point <tt>i</tt> the
+ * polynomial \f[ \prod_{j=0}^{i-1} \frac{x - \frac{j}{p}}{\frac{i}{p} -
+ * \frac{j}{p}} \f]
+ */
+class TruncatedEquidistantLagrange1D
+	: public Polynomial1D
+{
+	public:
+	/** creates a lagrange polynomial with equidistant interpolation points
+	 * \param[in] 	i		number of interpolation point, where polynom is 1
+	 * \param[in]	degree	degree of polynom
+	 */
+		TruncatedEquidistantLagrange1D(const size_t i, const size_t degree)
+		{
+			UG_ASSERT(i <= degree, "Only #degree shape functions.");
+
+			compute_coeffs(i, degree);
+		}
+
+	///	returns the position of the i'th interpolation point
+		static number position(const size_t i, const size_t degree)
+		{
+			UG_ASSERT(i <= degree, "Invalid index");
+			return (number)i/(number)degree;
+		}
+
+	protected:
+	/// computes the coefficients for passed interpolation points
+		void compute_coeffs(const int i, const int p)
+		{
+		//	start coefficients
+			std::vector<number> vStart(1, 1.0);
+
+		//	start polynomial
+			this->set_coefficients(vStart);
+
+		//	help polynomial used for each factor
+			std::vector<number> vFactor(2, p);
+
+		//	scaling of polynom
+			number scale = 1.0;
+
+		//	fill coefficients
+			for(int j = 0; j < i; ++j)
+			{
+			//	set first coefficent to minus position
+				vFactor[0] = -j;
+			//	create polynom for (-j, p)
+				Polynomial1D tmpPol(vFactor);
+			//	multiply
+				(*this) *= tmpPol;
+			// 	multiply scale
+				scale *= 1./(i-j);
+			}
+
+		//	multiply by scale
+			(*this) *= scale;
+		}
+};
+
+
+} // end namespace ug
+
+#endif /* __H__UG_LIB_DISCRETIZATION__LOCAL_SHAPE_FUNCTION_SET__COMMON__LAGRANGE1D__ */
---
+++ ugbase/lib_discretization/local_shape_function_set/common/polynomial1d.h
@@ -0,0 +1,135 @@
+/*
+ * polynomial1d.h
+ *
+ *  Created on: 17.11.2010
+ *      Author: andreasvogel
+ */
+
+#ifndef __H__UG_LIB_DISCRETIZATION__LOCAL_SHAPE_FUNCTION_SET__COMMON__POLYNOMIAL1D__
+#define __H__UG_LIB_DISCRETIZATION__LOCAL_SHAPE_FUNCTION_SET__COMMON__POLYNOMIAL1D__
+
+#include "common/math/ugmath.h"
+#include <vector>
+
+namespace ug{
+
+/// \addtogroup lib_discretization
+///	@{
+
+/** base class for one dimensional polynomials
+ * This class is used to represent polynomials in one variable. For the
+ * evaluation the horner scheme is used. Note that using this representation
+ * the computation of higher order derivatives turns out easier than by
+ * hard coded implementations.
+ */
+class Polynomial1D
+{
+	public:
+	///	Constructor producing zero polynomial of degree 'degree'
+		Polynomial1D(size_t degree = 0)
+			: m_vCoeff(degree+1, 0.0)
+		{}
+
+	///	Constructor passing coefficients for the polynomial
+		Polynomial1D(const std::vector<number>& a)
+			: m_vCoeff(a)
+		{
+		//	check that at least constant of polynomial set
+			if(m_vCoeff.empty())
+				m_vCoeff.resize(1, 0.0);
+		};
+
+	/**	returns the degree of the polynomial.
+	 * This function returns the degree of the polynomial, i.e. the
+	 * highest coefficient stored. Note that no checking is performed if
+	 * the leading coefficient is zero.
+	 */
+		size_t degree() const {return m_vCoeff.size() - 1;}
+
+	///	evaluate the value of the polynom at x
+		number value(const number x) const
+		{
+		//	get degree of polynomial (is >= 0 by construction)
+			const size_t deg = m_vCoeff.size() - 1;
+
+		//	loop horner scheme
+			number val = m_vCoeff[deg];
+			for(size_t i = deg; i > 0; --i)
+				val = m_vCoeff[i-1] + val * x;
+
+		//	we're done
+			return val;
+		}
+
+	///	returns the derivative of this polynomial as a polynomial
+		Polynomial1D derivative() const
+		{
+		//	if only constant present, return empty Polynomial
+			if(degree() == 0)
+				return Polynomial1D();
+
+		//	create empty polynomial of with correct size
+			Polynomial1D tmpPol(degree() - 1);
+
+		//	differentiate
+			for(size_t i = 0; i < tmpPol.degree(); ++i)
+				tmpPol.m_vCoeff[i] = (i+1) * m_vCoeff[i+1];
+
+		//	return derivative by copy
+			return tmpPol;
+		}
+
+	///	multiply by a polynomial
+		Polynomial1D& operator *=(const Polynomial1D& v)
+		{
+		//	new size of polynomial
+			size_t newDeg = degree() + v.degree();
+
+		//	create new coefficients
+			std::vector<number> vNewCoeff(newDeg+1, 0.0);
+
+		//	multiply
+			for(size_t i = 0; i <= degree(); ++i)
+				for(size_t j = 0; j <= v.degree(); ++j)
+					vNewCoeff[i+j] += m_vCoeff[i] * v.m_vCoeff[j];
+
+		//	Copy new coeffs
+			m_vCoeff = vNewCoeff;
+
+		//	we're done
+			return *this;
+		}
+
+	///	multiply by a scalar
+		Polynomial1D& operator *=(number scale)
+		{
+		//	multiply
+			for(size_t i = 0; i <= degree(); ++i)
+				m_vCoeff[i] *= scale;
+
+		//	we're done
+			return *this;
+		}
+
+	protected:
+		void set_coefficients(const std::vector<number>& a)
+		{
+		//	assign coefficients
+			m_vCoeff = a;
+
+		//	check that at least constant of polynomial set
+			if(m_vCoeff.empty())
+				m_vCoeff.resize(1, 0.0);
+		};
+
+	private:
+	//	vector holding the coefficients of the polynom
+	//	An empty vector is the Polynomial p = 0;
+	//	else we have p(x) = sum_i m_vCoeff[i] *x^i
+		std::vector<number> m_vCoeff;
+};
+
+/// @}
+} // end namespace ug
+
+#endif /* __H__UG_LIB_DISCRETIZATION__LOCAL_SHAPE_FUNCTION_SET__COMMON__POLYNOMIAL1D__ */
