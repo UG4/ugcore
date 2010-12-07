8268032d9b48777e36c1ec6d1c9f5044c84895e8
Andreas Vogel <andreas.vogel@gcsc.uni-frankfurt.de>
2010-12-07 14:58:52 +0000

    Adding membrane example. The example shows the ability of defining functions on manifolds on a very simple geometry.

---
+++ scripts/membrane.lua
@@ -0,0 +1,319 @@
+----------------------------------------------------------
+--
+--   Example - Script to work on a membrane
+--
+--   Author: Andreas Vogel
+--
+----------------------------------------------------------
+
+ug_load_script("ug_util.lua")
+
+-- choose algebra
+InitAlgebra(CPUAlgebraChooser());
+
+-- Dimension
+dim = 2
+
+-- Grid
+gridName = "unit_square_with_membrane.ugx"
+
+-- Refinements before distributing grid
+numPreRefs = 0
+
+-- Total refinements
+numRefs = 4
+
+--------------------------------
+-- User Data Functions (begin)
+--------------------------------
+	function ourDiffTensor2d(x, y, t)
+		return	1, 0, 
+				0, 1
+	end
+	
+	function ourVelocityField2d(x, y, t)
+		return	0, 0
+	end
+	
+	function ourReaction2d(x, y, t)
+		return	0
+	end
+	
+	function ourRhs2d(x, y, t)
+		local s = 2*math.pi
+		--return	s*s*(math.sin(s*x) + math.sin(s*y))
+		--return -2*y
+		return 0;
+	end
+	
+	function ourNeumannBnd2d(x, y, t)
+		--local s = 2*math.pi
+		--return -s*math.cos(s*x)
+		--return true, -2*x*y
+	end
+	
+	function ourDirichletBnd2d(x, y, t)
+		local s = 2*math.pi
+		--return true, math.sin(s*x) + math.sin(s*y)
+		return true, x
+		--return true, 2.5
+	end
+
+	function membraneDirichletBnd2d(x, y, t)
+	local s = 2*math.pi
+	--return true, math.sin(s*x) + math.sin(s*y)
+	--return true, x*x*y
+	return true, 2.5
+end
+	
+--------------------------------
+-- User Data Functions (end)
+--------------------------------
+
+-- create Instance of a Domain
+print("Create Domain.")
+dom = utilCreateDomain(dim)
+
+-- load domain
+print("Load Domain from File.")
+if utilLoadDomain(dom, gridName) == false then
+   print("Loading Domain failed.")
+   exit()
+end
+
+-- create Refiner
+print("Create Refiner")
+if numPreRefs > numRefs then
+	print("numPreRefs must be smaller/equal than numRefs");
+	exit();
+end
+
+refiner = GlobalMultiGridRefiner()
+refiner:assign_grid(dom:get_grid())
+for i=1,numPreRefs do
+	refiner:refine()
+end
+
+if utilDistributeDomain(dom) == false then
+	print("Error while Distributing Grid.")
+	exit()
+end
+
+print("Refine Parallel Grid")
+for i=numPreRefs+1,numRefs do
+	utilGlobalRefineParallelDomain(dom)
+end
+
+-- get subset handler
+sh = dom:get_subset_handler()
+if sh:num_subsets() ~= 4 then 
+print("Domain must have 4 Subsets for this problem.")
+exit()
+end
+
+-- write grid to file for test purpose
+utilSaveDomain(dom, "refined_grid.ugx")
+
+-- create function pattern
+print("Create Function Pattern")
+pattern = P1ConformFunctionPattern()
+pattern:set_subset_handler(sh)
+AddP1Function(pattern, "c", dim)
+AddP1FunctionOnSubsets(pattern, "c_membrane", "Membrane, MembraneBnd", dim-1)
+pattern:lock()
+
+-- create Approximation Space
+print("Create ApproximationSpace")
+approxSpace = utilCreateApproximationSpace(dom, pattern)
+
+-------------------------------------------
+--  Setup User Functions
+-------------------------------------------
+print ("Setting up Assembling")
+
+-- Diffusion Tensor setup
+	diffusionMatrix = utilCreateLuaUserMatrix("ourDiffTensor2d", dim)
+	--diffusionMatrix = utilCreateConstDiagUserMatrix(1.0, dim)
+
+-- Velocity Field setup
+	velocityField = utilCreateLuaUserVector("ourVelocityField2d", dim)
+	--velocityField = utilCreateConstUserVector(0.0, dim)
+
+-- Reaction setup
+	reaction = utilCreateLuaUserNumber("ourReaction2d", dim)
+	--reaction = utilCreateConstUserNumber(0.0, dim)
+
+-- rhs setup
+	rhs = utilCreateLuaUserNumber("ourRhs2d", dim)
+	--rhs = utilCreateConstUserNumber(0.0, dim)
+
+-- neumann setup
+	neumann = utilCreateLuaBoundaryNumber("ourNeumannBnd2d", dim)
+	--neumann = utilCreateConstUserNumber(0.0, dim)
+
+-- dirichlet setup
+	dirichlet = utilCreateLuaBoundaryNumber("ourDirichletBnd2d", dim)
+	--dirichlet = utilCreateConstBoundaryNumber(3.2, dim)
+	
+-- dirichlet setup
+	membraneDirichlet = utilCreateLuaBoundaryNumber("membraneDirichletBnd2d", dim)
+
+
+-----------------------------------------------------------------
+--  Setup FV Convection-Diffusion Element Discretization
+-----------------------------------------------------------------
+
+elemDisc = utilCreateFV1ConvDiff(approxSpace, "c", "Inner")
+elemDisc:set_upwind_amount(0.0)
+elemDisc:set_diffusion_tensor(diffusionMatrix)
+elemDisc:set_velocity_field(velocityField)
+elemDisc:set_reaction(reaction)
+elemDisc:set_rhs(rhs)
+
+membraneElemDisc = utilCreateFV1ConvDiff(approxSpace, "c_membrane", "Membrane")
+membraneElemDisc:set_upwind_amount(0.0)
+membraneElemDisc:set_diffusion_tensor(diffusionMatrix)
+membraneElemDisc:set_velocity_field(velocityField)
+membraneElemDisc:set_reaction(reaction)
+membraneElemDisc:set_rhs(rhs)
+
+-----------------------------------------------------------------
+--  Setup Neumann Boundary
+-----------------------------------------------------------------
+
+--neumannDisc = utilCreateNeumannBoundary(approxSpace, "Inner")
+--neumannDisc:add_boundary_value(neumann, "c", "NeumannBoundary")
+
+-----------------------------------------------------------------
+--  Setup Dirichlet Boundary
+-----------------------------------------------------------------
+
+dirichletBND = utilCreateDirichletBoundary(approxSpace)
+dirichletBND:add_boundary_value(dirichlet, "c", "Boundary, MembraneBnd")
+
+membraneDirichletBND = utilCreateDirichletBoundary(approxSpace)
+membraneDirichletBND:add_boundary_value(membraneDirichlet, "c_membrane", "MembraneBnd")
+
+-------------------------------------------
+--  Setup Domain Discretization
+-------------------------------------------
+
+domainDisc = DomainDiscretization()
+domainDisc:add_elem_disc(elemDisc)
+domainDisc:add_elem_disc(membraneElemDisc)
+--domainDisc:add_elem_disc(neumannDisc)
+domainDisc:add_post_process(dirichletBND)
+domainDisc:add_post_process(membraneDirichletBND)
+
+-------------------------------------------
+--  Algebra
+-------------------------------------------
+print ("Setting up Algebra Solver")
+
+-- create operator from discretization
+linOp = AssembledLinearOperator()
+linOp:export_rhs(true)
+linOp:set_discretization(domainDisc)
+linOp:set_dof_distribution(approxSpace:get_surface_dof_distribution())
+
+-- get grid function
+u = approxSpace:create_surface_function("u", true)
+b = approxSpace:create_surface_function("b", true)
+
+-- set initial value
+u:set(0.0)
+
+-- init Operator
+print ("Assemble Operator ... ")
+linOp:init()
+print ("done")
+
+-- set dirichlet values in start iterate
+linOp:set_dirichlet_values(u)
+b:assign(linOp:get_rhs())
+
+-- write matrix for test purpose
+SaveMatrixForConnectionViewer(u, linOp, "Stiffness.mat")
+SaveVectorForConnectionViewer(b, "Rhs.mat")
+
+-- create algebraic Preconditioner
+jac = Jacobi()
+jac:set_damp(0.8)
+gs = GaussSeidel()
+sgs = SymmetricGaussSeidel()
+bgs = BackwardGaussSeidel()
+ilu = ILU()
+ilut = ILUT()
+
+-- create GMG ---
+-----------------
+
+	-- Base Solver
+	baseConvCheck = StandardConvergenceCheck()
+	baseConvCheck:set_maximum_steps(500)
+	baseConvCheck:set_minimum_defect(1e-8)
+	baseConvCheck:set_reduction(1e-30)
+	baseConvCheck:set_verbose_level(false)
+	-- base = LapackLUSolver()
+	base = LinearSolver()
+	base:set_convergence_check(baseConvCheck)
+	base:set_preconditioner(jac)
+	
+	-- Transfer and Projection
+	transfer = utilCreateP1Prolongation(approxSpace)
+	transfer:set_dirichlet_post_process(dirichletBND)
+	projection = utilCreateP1Projection(approxSpace)
+	
+	-- Gemoetric Multi Grid
+	gmg = utilCreateGeometricMultiGrid(approxSpace)
+	gmg:set_discretization(domainDisc)
+	gmg:set_surface_level(numRefs)
+	gmg:set_base_level(0)
+	gmg:set_base_solver(base)
+	gmg:set_smoother(jac)
+	gmg:set_cycle_type(1)
+	gmg:set_num_presmooth(3)
+	gmg:set_num_postsmooth(3)
+	gmg:set_prolongation(transfer)
+	gmg:set_projection(projection)
+
+-- create AMG ---
+-----------------
+
+	if false then
+	amg = AMGPreconditioner()
+	amg:set_nu1(2)
+	amg:set_nu2(2)
+	amg:set_gamma(1)
+	amg:set_presmoother(jac)
+	amg:set_postsmoother(jac)
+	amg:set_base_solver(base)
+	--amg:set_debug(u)
+	end
+
+-- create Convergence Check
+convCheck = StandardConvergenceCheck()
+convCheck:set_maximum_steps(100)
+convCheck:set_minimum_defect(1e-11)
+convCheck:set_reduction(1e-12)
+
+-- create Linear Solver
+linSolver = LinearSolver()
+linSolver:set_preconditioner(gmg)
+linSolver:set_convergence_check(convCheck)
+
+-- create CG Solver
+cgSolver = CG()
+cgSolver:set_preconditioner(jac)
+cgSolver:set_convergence_check(convCheck)
+
+-- create BiCGStab Solver
+bicgstabSolver = BiCGStab()
+bicgstabSolver:set_preconditioner(jac)
+bicgstabSolver:set_convergence_check(convCheck)
+
+-- Apply Solver
+ApplyLinearSolver(linOp, u, b, linSolver)
+
+-- Output
+WriteGridFunctionToVTK(u, "Solution")
