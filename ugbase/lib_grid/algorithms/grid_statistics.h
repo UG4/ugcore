4b640db89ed13fecf0fccfcde8b2f4694cce4eac
Sebastian Reiter <sebastian.reiter@gcsc.uni-frankfurt.de>
2010-12-17 13:43:31 +0000

    began work on AssignSubsetsByQuality. discontiued due to other work.

--- ugbase/lib_grid/algorithms/grid_statistics.h
+++ ugbase/lib_grid/algorithms/grid_statistics.h
@@ -1,4 +1,4 @@
-//	created by Martin Stepniewski
+//	created by Martin Stepniewski, Sebastian Reiter
 //	mastep@gmx.de
 //	y09 m11 d11
 
@@ -12,13 +12,7 @@ namespace ug
 {
 	
 //**********************************************************************
-//**********************************************************************
-//**********************************************************************
-//**********************************************************************
 //								declarations
-//**********************************************************************
-//**********************************************************************
-//**********************************************************************
 //**********************************************************************
 
 ////////////////////////////////////////////////////////////////////////
@@ -29,7 +23,39 @@ bool AssignTetrahedronAttributesByAspectRatio(Grid& grid,
 											  AInt& aTetrahedronAspectRatioClass,
 											  std::vector<double>& offsets);
 
+////////////////////////////////////////////////////////////////////////
+///	assigns a subset based on the quality of the given element.
+/**
+ * Currently only faces are supported.
+ *
+ * \param intervals contains the intervals which define into which subset
+ *					an element goes. Numbers have to be sorted, starting at
+ *					0 and ending at 1 (0 and 1 should be contained in intervals).
+ */
+/*
+template <class TIterator>
+bool AssignSubsetByQuality(Grid& grid, SubsetHandler& sh,
+						   TIterator elemsBegin, TIterator elemsEnd,
+						   std::vector<number> intervals)
+{
+	sh.clear();
+	if(intervals.empty()){
+		sh.assign_subset(elemsBegin, elemsEnd, 0);
+		return true;
+	}
 
+//	access position
+	Grid::VertexAttachmentAccessor<APosition> aaPos(grid, aPosition);
+	
+//	iterate over all elements
+	for(TIterator iter = elemsBegin; iter != elemsEnd; ++iter)
+	{
+		typename TIterator::value_type elem = *iter;
+		number quality = FaceQuality(elem, aaPos);
+		//...
+	}
+}
+*/
 }//	end of namespace
 
 #endif
