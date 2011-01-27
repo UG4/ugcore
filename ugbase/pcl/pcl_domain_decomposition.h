f2033e30293ee72a2f39599aeb5abb4732372038
Ingo Heppner <igno.heppner@gcsc.uni-frankfurt.de>
2011-01-27 16:04:21 +0000

    Class containing domain decomposition info added

---
+++ ugbase/pcl/pcl_domain_decomposition.h
@@ -0,0 +1,20 @@
+/*
+ * pcl_domain_decomposition.h
+ *
+ *  Created on: 27.01.2011
+ *      Author: sreiter, avogel, iheppner
+ */
+
+#ifndef __H__PCL__DOMAIN_DECOMPOSITION__
+#define __H__PCL__DOMAIN_DECOMPOSITION__
+
+namespace pcl
+{
+
+////////////////////////////////////////////////////////////////////////
+// Not used!!!
+////////////////////////////////////////////////////////////////////////
+
+
+}//	end of namespace
+#endif
