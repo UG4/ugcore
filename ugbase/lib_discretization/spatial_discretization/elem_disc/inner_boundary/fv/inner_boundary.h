59da67b9c64b2a30c87b8d9c6464e1db35415089
Markus Breit <markus.breit@gcsc.uni-frankfurt.de>
2011-02-22 13:28:02 +0000

    added (so far) non-functional support for flux over inner boundaries

---
+++ ugbase/lib_discretization/spatial_discretization/elem_disc/inner_boundary/fv/inner_boundary.h
@@ -0,0 +1,317 @@
+/*
+ * neumann_boundary.h
+ *
+ *  Created on: 26.02.2010
+ *      Author: markusbreit
+ */
+
+#ifndef __H__LIB_DISCRETIZATION__SPACIAL_DISCRETIZATION__ELEM_DISC__NEUMANN_BOUNDARY__FV1__INNER_BOUNDARY__
+#define __H__LIB_DISCRETIZATION__SPACIAL_DISCRETIZATION__ELEM_DISC__NEUMANN_BOUNDARY__FV1__INNER_BOUNDARY__
+
+// other ug4 modules
+#include "common/common.h"
+#include "lib_grid/lg_base.h"
+#include "lib_algebra/lib_algebra.h"
+
+// library intern headers
+#include "lib_discretization/spatial_discretization/disc_helper/disc_helper.h"
+#include "lib_discretization/spatial_discretization/elem_disc/elem_disc_interface.h"
+#include "lib_discretization/common/local_algebra.h"
+#include "lib_discretization/spatial_discretization/ip_data/user_data.h"
+
+namespace ug{
+
+/// Finite Volume Element Discretization for an inner BndCond that depends on the unknowns (on the bnd)
+/**
+ * This class implements the IElemDisc interface to provide element local
+ * assemblings for the unknown-dependent Neumann-flux over an inner boundary.
+ * The equation of this flux should be given on the script level.
+ * 
+ * \tparam	TFVGeom		Finite Volume Geometry used
+ * \tparam	TDomain		Domain
+ * \tparam	TAlgebra	Algebra
+ */
+
+
+template<template <	class TElem, int TWorldDim> class TFVGeom,
+					typename TDomain,
+					typename TAlgebra>
+class FVInnerBoundaryElemDisc
+	: public IElemDisc<TAlgebra>
+{
+	public:
+		// domain type
+		typedef TDomain domain_type;
+
+		// world dimension
+		static const int dim = TDomain::dim;
+
+		// position type
+		typedef typename TDomain::position_type position_type;
+
+		// algebra type
+		typedef TAlgebra algebra_type;
+
+		// local matrix type
+		typedef typename IElemDisc<TAlgebra>::local_matrix_type local_matrix_type;
+
+		// local vector type
+		typedef typename IElemDisc<TAlgebra>::local_vector_type local_vector_type;
+
+		// local index type
+		typedef typename IElemDisc<TAlgebra>::local_index_type local_index_type;
+		//typedef LocalIndices local_index_type;
+
+	protected:
+		typedef typename IBoundaryNumberProvider<dim>::functor_type BNDNumberFunctor;
+
+	public:
+		FVInnerBoundaryElemDisc()
+		 : m_numFct(0), m_pDomain(NULL)
+			{
+				m_mBoundarySegment.clear();
+				register_assemble_functions(Int2Type<dim>());
+			}
+
+		void set_domain(domain_type& domain) {m_pDomain = &domain;}
+
+		
+		bool add_flux(const char* function, const char* subsets)
+		{
+			//	check that function pattern exists
+			if(this->m_pPattern == NULL)
+			{
+				UG_LOG("FVInnerBoundaryElemDisc:add_flux: Function Pattern not set.\n");
+				return false;
+			}
+
+			//	create Function Group and Subset Group
+			FunctionGroup functionGroup;
+			SubsetGroup subsetGroup;
+
+			//	convert strings
+			if(!ConvertStringToSubsetGroup(subsetGroup, *this->m_pPattern, subsets))
+			{
+				UG_LOG("ERROR while parsing Subsets.\n");
+				return false;
+			}
+			
+			if(!ConvertStringToFunctionGroup(functionGroup, *this->m_pPattern, function))
+			{
+				UG_LOG("ERROR while parsing Functions.\n");
+				return false;
+			}
+			
+			/*
+			//	only one function allowed
+			if(functionGroup.num_fct() != 1)
+			{
+				UG_LOG("FVInnerBoundaryElemDisc:add_flux: Exactly one function needed, but given '"<<function<<"' as functions.\n");
+				return false;
+			}
+			*/
+			
+		//	forward request
+			return add_flux(functionGroup, subsetGroup);
+		}
+
+		bool add_flux(FunctionGroup functionGroup, SubsetGroup bndSubsetGroup)
+		{
+			//	check that function pattern exists
+			if(this->m_pPattern == NULL)
+			{
+				UG_LOG("FVInnerBoundaryElemDisc:add_flux: Function Pattern not set.\n");
+				return false;
+			}
+
+			//	get subsethandler
+			const ISubsetHandler* pSH = this->m_pPattern->get_subset_handler();
+			
+			
+			for (size_t fct = 0; fct < functionGroup.num_fct(); fct++)
+			{
+				size_t fct_id = functionGroup.unique_id(fct);
+				
+				// 	check if functions exist
+				if(fct_id >= this->m_pPattern->num_fct())
+				{
+					UG_LOG("FVInnerBoundaryElemDisc:add_flux: Function "
+							<< fct_id << " does not exist in pattern.\n");
+					return false;
+				}
+				
+				//	add function to function group if needed
+				//	this will force, that the local vector contains the function
+				//	note: 	The local indices for allready contained functions are not changed.
+				//			Therefore an update in the UserDataFunctions is not necessary
+				if(!this->m_FunctionGroup.contains(fct_id))
+				{
+					this->m_FunctionGroup.add(fct_id);
+					m_numFct = this->m_FunctionGroup.num_fct();
+				}
+
+				//	get position of function in function group
+				size_t index = this->m_FunctionGroup.local_index(fct_id);
+				
+				
+				// 	check that function is defined on inner subset
+				if(!this->m_SubsetGroup.empty())
+				{
+					for (size_t si = 0; si < this->m_SubsetGroup.num_subsets(); ++si)
+					{
+						const int subsetIndex = this->m_SubsetGroup[si];
+						if (!this->m_pPattern->is_def_in_subset(fct_id, subsetIndex))
+						{
+							UG_LOG("FVInnerBoundaryElemDisc:add_flux: Function "
+									<< fct_id << " not defined in subset " << subsetIndex << ".\n");
+							return false;
+						}
+					}
+				}
+				
+				
+				// 	loop subsets
+				for(size_t si = 0; si < bndSubsetGroup.num_subsets(); ++si)
+				{
+					//	get subset index
+					const int subsetIndex = bndSubsetGroup[si];
+
+					//	check that subsetIndex is valid
+					if(subsetIndex < 0 || subsetIndex >= pSH->num_subsets())
+					{
+						UG_LOG("FVInnerBoundaryElemDisc:add_flux: Invalid subset Index "
+								<< subsetIndex << ". (Valid is 0, .. , " << pSH->num_subsets() <<").\n");
+						return false;
+					}
+
+					//	get Boundary segment from map
+					std::vector<size_t>& vSegmentFunction = m_mBoundarySegment[subsetIndex];
+					
+					//	remember functor and function
+					vSegmentFunction.push_back(index);
+				}
+			}
+		//	we're done
+			return true;
+		}
+		
+		
+	private:
+		std::map<int, std::vector<size_t> > m_mBoundarySegment;
+		size_t m_numFct;
+
+	
+	public:	// inherited from IElemDisc
+	///	number of functions used
+		virtual size_t num_fct(){return m_numFct;}
+		
+	///	type of trial space for each function used
+		virtual LocalShapeFunctionSetID local_shape_function_set_id(size_t loc_fct)
+		{
+			return LocalShapeFunctionSetID(LocalShapeFunctionSetID::LAGRANGE, 1);
+		}
+	///	returns if hanging nodes are used
+		virtual bool use_hanging() const {return TFVGeom<Edge, dim>::usesHangingNodes;}
+
+	private:
+	
+	///	prepares the loop over all elements
+	/**
+	 * This method prepares the loop over all elements. It resizes the Position
+	 * array for the corner coordinates and schedules the local ip positions
+	 * at the data imports.
+	 */
+		template <typename TElem>
+		inline bool prepare_element_loop();
+
+	///	prepares the element for assembling
+	/**
+	 * This methods prepares an element for the assembling. The Positions of
+	 * the Element Corners are read and the Finite Volume Geometry is updated.
+	 * The global ip positions are scheduled at the data imports.
+	 */
+		template <typename TElem>
+		inline bool prepare_element(TElem* elem, const local_vector_type& u, const local_index_type& glob_ind);
+
+	///	finishes the loop over all elements
+		template <typename TElem>
+		inline bool finish_element_loop();
+
+	///	assembles the local stiffness matrix using a finite volume scheme
+		template <typename TElem>
+		inline bool assemble_JA(local_matrix_type& J, const local_vector_type& u, number time=0.0);
+
+	///	assembles the local mass matrix using a finite volume scheme
+		template <typename TElem>
+		inline bool assemble_JM(local_matrix_type& J, const local_vector_type& u, number time=0.0);
+
+	///	assembles the stiffness part of the local defect
+		template <typename TElem>
+		inline bool assemble_A(local_vector_type& d, const local_vector_type& u, number time=0.0);
+
+	///	assembles the mass part of the local defect
+		template <typename TElem>
+		inline bool assemble_M(local_vector_type& d, const local_vector_type& u, number time=0.0);
+
+	///	assembles the local right hand side
+		template <typename TElem>
+		inline bool assemble_f(local_vector_type& d, number time=0.0);
+
+	private:
+		// domain
+		TDomain* m_pDomain;
+
+		// position access
+		std::vector<position_type> m_vCornerCoords;
+		typename TDomain::position_accessor_type m_aaPos;
+
+	private:
+		///////////////////////////////////////
+		// registering for reference elements
+		///////////////////////////////////////
+		// register for 1D
+		void register_assemble_functions(Int2Type<1>)
+		{
+			register_all_assemble_functions<Edge>(ROID_EDGE);
+		}
+
+		// register for 2D
+		void register_assemble_functions(Int2Type<2>)
+		{
+			register_assemble_functions(Int2Type<1>());
+			register_all_assemble_functions<Triangle>(ROID_TRIANGLE);
+			register_all_assemble_functions<Quadrilateral>(ROID_QUADRILATERAL);
+		}
+
+		// register for 3D
+		void register_assemble_functions(Int2Type<3>)
+		{
+			register_assemble_functions(Int2Type<2>());
+			register_all_assemble_functions<Tetrahedron>(ROID_TETRAHEDRON);
+			register_all_assemble_functions<Pyramid>(ROID_PYRAMID);
+			register_all_assemble_functions<Prism>(ROID_PRISM);
+			register_all_assemble_functions<Hexahedron>(ROID_HEXAHEDRON);
+		}
+
+		///	register all functions for on element type
+		template <typename TElem>
+		void register_all_assemble_functions(int id)
+		{
+			typedef FVInnerBoundaryElemDisc T;
+			register_prepare_element_loop_function(	id, &T::template prepare_element_loop<TElem>);
+			register_prepare_element_function(		id, &T::template prepare_element<TElem>);
+			register_finish_element_loop_function(	id, &T::template finish_element_loop<TElem>);
+			register_assemble_JA_function(			id, &T::template assemble_JA<TElem>);
+			register_assemble_JM_function(			id, &T::template assemble_JM<TElem>);
+			register_assemble_A_function(			id, &T::template assemble_A<TElem>);
+			register_assemble_M_function(			id, &T::template assemble_M<TElem>);
+			register_assemble_f_function(			id, &T::template assemble_f<TElem>);
+		}
+
+};
+
+}
+
+#include "inner_boundary_impl.h"
+
+#endif /*__H__LIB_DISCRETIZATION__SPACIAL_DISCRETIZATION__ELEM_DISC__NEUMANN_BOUNDARY__FV1__INNER_BOUNDARY__*/
---
+++ ugbase/lib_discretization/spatial_discretization/elem_disc/inner_boundary/fv/inner_boundary_impl.h
@@ -0,0 +1,342 @@
+/*
+ * neumann_boundary_impl.h
+ *
+ *  Created on: 14.10.2010
+ *      Author: markusbreit
+ */
+
+#ifndef __H__LIB_DISCRETIZATION__SPACIAL_DISCRETIZATION__ELEM_DISC__NEUMANN_BOUNDARY__FV1__INNER_BOUNDARY_IMPL__
+#define __H__LIB_DISCRETIZATION__SPACIAL_DISCRETIZATION__ELEM_DISC__NEUMANN_BOUNDARY__FV1__INNER_BOUNDARY_IMPL__
+
+#include "inner_boundary.h"
+
+
+// constants (all according to De Young & Keizer)
+#define C1 0.185		// (ER vol) / (cytosolic vol)
+
+						// receptor dissociation
+#define D1 1.3e-7		// IP3
+#define D2 1.049e-5	//1.049e-4	//1.049e-6		// Ca2+ inhibition
+#define D3 9.434e-7		// IP3
+#define D5 8.234e-7	//6.234e-6	//8.234e-8		// Ca2+ activation
+
+#define NY1 6.0			// max Ca2+ channel flux factor (when all channels are open)
+#define NY2 0.0	//0.0011 //0.011	//0.11		// leakage flux
+#define NY3 9.0e-7		// max Ca2+ uptake (SERCA)
+
+#define K3 1.0e-7		// activation constant for ATP-Ca2+ pump
+
+// RyR
+#define NY4 0.15
+#define KA4 1.92e-26
+#define KB3 2.573e-19
+#define KC  7.1e-3	//5.71e-2 
+
+#define KA4I 1.92e-2
+#define KB3I 2.573e-1
+
+#define V_SERCA 1.2e-10
+#define K_SERCA 1.8e-7
+
+#define DAMPENING 1.0
+
+
+
+namespace ug{
+
+
+template<template <class TElem, int TWorldDim> class TFVGeom, typename TDomain, typename TAlgebra>
+template<typename TElem >
+inline
+bool
+FVInnerBoundaryElemDisc<TFVGeom, TDomain, TAlgebra>::
+prepare_element_loop()
+{
+// 	resize corner coordinates
+	typedef typename reference_element_traits<TElem>::reference_element_type ref_elem_type;
+	m_vCornerCoords.resize(ref_elem_type::num_corners);
+	
+//  check domain
+	if(m_pDomain == NULL)
+	{
+		UG_LOG("ERROR in 'FVInnerBoundaryElemDisc::prepare_element_loop':"
+				" Domain not set.");
+		return false;
+	}
+
+// 	remember position attachement
+	m_aaPos = m_pDomain->get_position_accessor();
+
+//	register subsetIndex at Geometry
+	TFVGeom<TElem, dim>& geo = FVGeometryProvider::get_geom<TFVGeom, TElem,dim>();
+
+	typename std::map<int, std::vector<size_t> >::const_iterator subsetIter;
+	for(subsetIter = m_mBoundarySegment.begin(); subsetIter != m_mBoundarySegment.end(); ++subsetIter)
+	{
+		const int bndSubset = (*subsetIter).first;
+		geo.add_boundary_subset(bndSubset);
+	}
+
+//	we're done
+	return true;
+}
+
+
+template<template <class TElem, int TWorldDim> class TFVGeom, typename TDomain, typename TAlgebra>
+template<typename TElem >
+inline
+bool
+FVInnerBoundaryElemDisc<TFVGeom, TDomain, TAlgebra>::
+finish_element_loop()
+{
+//	remove subsetIndex from Geometry
+	TFVGeom<TElem, dim>& geo = FVGeometryProvider::get_geom<TFVGeom, TElem,dim>();
+
+	typename std::map<int, std::vector<size_t> >::const_iterator subsetIter;
+	for(subsetIter = m_mBoundarySegment.begin(); subsetIter != m_mBoundarySegment.end(); ++subsetIter)
+	{
+		const int bndSubset = (*subsetIter).first;
+		geo.remove_boundary_subset(bndSubset);
+	}
+
+//	we're done
+	return true;
+}
+
+
+template<template <class TElem, int TWorldDim> class TFVGeom, typename TDomain, typename TAlgebra>
+
+template<typename TElem >
+inline
+bool
+FVInnerBoundaryElemDisc<TFVGeom, TDomain, TAlgebra>::
+prepare_element(TElem* elem, const local_vector_type& u, const local_index_type& glob_ind)
+{
+	// load corners of this element
+	for(size_t i = 0; i < m_vCornerCoords.size(); ++i)
+	{
+		VertexBase* vert = elem->vertex(i);
+		m_vCornerCoords[i] = m_aaPos[vert];
+	}
+
+	// update Geometry for this element
+	TFVGeom<TElem, dim>& geo = FVGeometryProvider::get_geom<TFVGeom, TElem,dim>();
+	if(!geo.update(elem, m_pDomain->get_subset_handler(), &m_vCornerCoords[0]))
+		{UG_LOG("FVInnerBoundaryElemDisc::prepare_element: Cannot update Finite Volume Geometry.\n"); return false;}
+
+	return true;
+}
+
+
+
+// assemble stiffness part of Jacobian
+template<template <class TElem, int TWorldDim> class TFVGeom, typename TDomain, typename TAlgebra>
+template<typename TElem >
+inline
+bool
+FVInnerBoundaryElemDisc<TFVGeom, TDomain, TAlgebra>::
+assemble_JA(local_matrix_type& J, const local_vector_type& u, number time)
+{
+	// get finite volume geometry
+	static TFVGeom<TElem, dim>& fvgeom = FVGeometryProvider::get_geom<TFVGeom, TElem,dim>();
+
+	typename std::map<int, std::vector<size_t> >::const_iterator subsetIter;
+	for(subsetIter = m_mBoundarySegment.begin(); subsetIter != m_mBoundarySegment.end(); ++subsetIter)
+	{
+		const int bndSubset = (*subsetIter).first;
+		
+		// loop Boundary Faces
+		for (size_t i = 0; i < fvgeom.num_bf(bndSubset); ++i)
+		{
+			// get current BF
+			const typename TFVGeom<TElem, dim>::BF& bf = fvgeom.bf(bndSubset, i);
+
+			// get associated node
+			const int co = bf.node_id();
+			
+			// get values of the unknowns in associated node
+			number caCyt = u(0, co);
+			number caER = u(1, co);
+			number ip3 = u(2, co);
+			//INT n_surf;
+			//surf_id = BNDP_SurfaceId(V_BNDP(vert_to), &n_surf, 0);
+			
+			
+			number d_dCyt = 0.0;
+			number d_dER = 0.0;
+			number d_dIP3 = 0.0;
+	
+			
+			// IP3 parts
+			number schlonz1 = caCyt*ip3 + ip3*D2 + D1*D2 + caCyt*D3;
+			number schlonz = schlonz1 * (caCyt+D5);
+			number x110 = (caCyt*ip3*D2) / schlonz;
+			
+			number blubb1 = NY1*3.0*x110*x110*ip3*D2 * (1.0 - caCyt/schlonz * ( (ip3+D3)*(caCyt+D5) + schlonz1 )) / schlonz;
+			number blubb1a = NY1*3.0*x110*x110*caCyt*D2 * (1.0 - ip3/schlonz * (caCyt+D2)*(caCyt+D5)) / schlonz; 
+			number blubb2 = NY1*x110*x110*x110 + NY2;
+			
+			number release_factor = 1.0; //ip3_channel_density(surf_id);
+			
+			d_dCyt += release_factor * C1 * (blubb1 * (caER-caCyt) - blubb2);
+			d_dER += release_factor * C1 * blubb2;
+			d_dIP3 += release_factor * C1 * blubb1a * (caER-caCyt);
+			
+		
+			// RyR parts
+			number inner = 1.0 + pow(caCyt,3) / KB3;
+			number dInner_dCyt = 3.0 * caCyt * caCyt / KB3;
+			number dKlShice_dCyt = dInner_dCyt - 4.0 * KA4 / pow(caCyt,5);
+			number dKlShice_dCyt_a = 3.0 * inner * inner / KB3I - 4.0 * KA4I / pow(inner,5);
+			number klShice = 1.0 + KA4 / pow(caCyt,4) + pow(caCyt,3) / KB3;
+			number klShice_a = 1.0 + KA4I / pow(inner,4) + pow(inner,3) / KB3I;
+			number grShice_a = 1.0 + KC * klShice_a;
+			number w = KC * klShice_a / grShice_a;
+			number open_prob =  w / klShice;
+			number dOpenProb_dCyt = (dInner_dCyt * dKlShice_dCyt_a / grShice_a - klShice_a / klShice * dKlShice_dCyt) 
+									* KC / (grShice_a * klShice);
+			
+			release_factor = 1.0; //ryr_channel_density(surf_id);
+			
+			d_dCyt += release_factor * NY4 * (dOpenProb_dCyt * (caER-caCyt) - open_prob);
+			d_dER  += release_factor * NY4 * open_prob;
+		
+		
+			// SERCA parts
+			d_dCyt -= V_SERCA*K_SERCA / (caER*(K_SERCA+caCyt)*(K_SERCA+caCyt));	//	2.0*NY3*K3*K3 * caCyt / ((caCyt*caCyt + K3*K3)*(caCyt*caCyt + K3*K3));
+			d_dER  -= - V_SERCA*caCyt / ((K_SERCA+caCyt)*caER*caER);
+		
+		
+			// scale with volume of BF
+			d_dCyt *= bf.volume() * DAMPENING;
+			d_dER *= bf.volume() * DAMPENING;
+			d_dIP3 *= bf.volume() * DAMPENING;
+			
+			// add to Jacobian					<-- hier entsteht der segfault
+			//J(1,co,1,co)	+= d_dER;
+			//J(1,co,0,co)	+= d_dCyt;
+			//J(0,co,1,co)	-= d_dER;
+			//J(0,co,0,co)	-= d_dCyt;
+			//J(1,co,2,co)	+= d_dIP3;
+			//J(0,co,2,co)	-= d_dIP3;
+		}
+	}
+	
+	return true;
+}
+
+
+
+template<template <class TElem, int TWorldDim> class TFVGeom, typename TDomain, typename TAlgebra>
+template<typename TElem >
+inline
+bool
+FVInnerBoundaryElemDisc<TFVGeom, TDomain, TAlgebra>::
+assemble_JM(local_matrix_type& J, const local_vector_type& u, number time)
+{
+	// nothing to be done
+	return true;
+}
+
+
+
+template<template <class TElem, int TWorldDim> class TFVGeom, typename TDomain, typename TAlgebra>
+template<typename TElem >
+inline
+bool
+FVInnerBoundaryElemDisc<TFVGeom, TDomain, TAlgebra>::
+assemble_A(local_vector_type& d, const local_vector_type& u, number time)
+{
+	// get finite volume geometry
+	static TFVGeom<TElem, dim>& fvgeom = FVGeometryProvider::get_geom<TFVGeom, TElem,dim>();
+
+	typename std::map<int, std::vector<size_t> >::const_iterator subsetIter;
+	for(subsetIter = m_mBoundarySegment.begin(); subsetIter != m_mBoundarySegment.end(); ++subsetIter)
+	{
+		const int bndSubset = (*subsetIter).first;
+		
+		// loop Boundary Faces
+		for (size_t i = 0; i < fvgeom.num_bf(bndSubset); ++i)
+		{
+			// get current BF
+			const typename TFVGeom<TElem, dim>::BF& bf = fvgeom.bf(bndSubset, i);
+			
+			// get associated node
+			const int co = bf.node_id();
+			
+			
+			number flux = 0.0;
+			
+			number caCyt = u(0, co);	// cytosolic Ca2+ concentration
+			number caER = u(1, co);		// ER Ca2+ concentration
+			number ip3 = u(2, co);		// IP3 concentration
+			//INT n_surf;
+			//INT surf_id = BNDP_SurfaceId(V_BNDP(vert_to), &n_surf, 0);
+			
+			
+			
+			// IP3 parts
+			number x110 = (caCyt*ip3*D2) / ((caCyt*ip3 + ip3*D2 + D1*D2 + caCyt*D3) * (caCyt+D5));
+			number IP3flux = C1*(NY1*x110*x110*x110 + NY2) * (caER-caCyt);
+			
+			number release_factor = 1.0; //ip3_channel_density(surf_id);
+				
+			flux += release_factor * IP3flux;
+			
+			// RyR parts
+			number inner = 1.0 + pow(caCyt,3)/KB3;
+			number w = (1.0 + KA4I/pow(inner,4) + pow(inner,3)/KB3I) / (1.0 + 1.0/KC + KA4I/pow(inner,4) + pow(inner,3)/KB3I);
+			number open_prob =  w / (1.0 + KA4/pow(caCyt,4) + pow(caCyt,3)/KB3);	//printf("open_prob: %g\n", open_prob);
+			
+			release_factor = 1.0; //ryr_channel_density(surf_id);
+
+			flux += release_factor * NY4 * open_prob * (caER-caCyt);
+
+			// SERCA parts
+			flux -=	V_SERCA*caCyt / ((K_SERCA + caCyt) * caER);
+			
+			
+			
+			// scale with volume of BF
+			flux *= bf.volume() * DAMPENING;
+
+			// add to defect					<-- hier entsteht der segfault
+			//d(0, co) -= flux;	// cytosol
+			//d(1, co) += flux;	// ER
+		}
+	}
+
+	return true;
+}
+
+
+
+template<template <class TElem, int TWorldDim> class TFVGeom, typename TDomain, typename TAlgebra>
+template<typename TElem >
+inline
+bool
+FVInnerBoundaryElemDisc<TFVGeom, TDomain, TAlgebra>::
+assemble_M(local_vector_type& d, const local_vector_type& u, number time)
+{
+	// nothing to be done
+	return true;
+}
+
+
+
+template<template <class TElem, int TWorldDim> class TFVGeom, typename TDomain, typename TAlgebra>
+template<typename TElem >
+inline
+bool
+FVInnerBoundaryElemDisc<TFVGeom, TDomain, TAlgebra>::
+assemble_f(local_vector_type& d, number time)
+{
+	// nothing to be done
+	return true;
+}
+
+
+} // namespace ug
+
+
+#endif /*__H__LIB_DISCRETIZATION__SPACIAL_DISCRETIZATION__ELEM_DISC__NEUMANN_BOUNDARY__FV1__INNER_BOUNDARY_IMPL__*/
