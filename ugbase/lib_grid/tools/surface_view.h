58013f6687bd6032aba4010db4d24a372b0ae3c1
Andreas Vogel <andreas.vogel@gcsc.uni-frankfurt.de>
2011-02-22 13:00:29 +0000

    Adding some methods.

--- ugbase/lib_grid/tools/surface_view.h
+++ ugbase/lib_grid/tools/surface_view.h
@@ -39,9 +39,30 @@ class SurfaceView : public SubsetHandler
 		void register_observer(GridObserver* observer, uint observerType = OT_FULL_OBSERVER);
 		void unregister_observer(GridObserver* observer);
 		
+	///	returns if the element is a shadow, i.e. has children
 		template <class TGeomObj>
-		inline bool is_shadow(TGeomObj* obj)	{return m_pMG->has_children(obj);}
+		inline bool is_shadow(TGeomObj* obj) const	{return m_pMG->has_children(obj);}
+
+	///	returns if the element shadows another
+		template <class TGeomObj>
+		inline bool shadows(TGeomObj* obj) const
+		{
+			GeometricObject* parent = m_pMG->get_parent(obj);
+			if(parent == NULL) return false;
+			else return (get_subset_index(parent) != -1);
+		}
+
+	///	returns father of a shadowing element
+		template <class TGeomObj>
+		inline GeometricObject* get_parent(TGeomObj* obj) const {return m_pMG->get_parent(obj);}
+
+	///	returns child for shadow
+		inline VertexBase* get_child(VertexBase* vrt) const {return m_pMG->get_child_vertex<VertexBase>(vrt);}
 		
+	///	returns the level in grid hierarchy of an element in the surface
+		template <class TGeomObj>
+		inline int get_level(TGeomObj* obj) const	{return m_pMG->get_level(obj);}
+
 		virtual void grid_to_be_destroyed(Grid* grid);
 		
 	protected:
