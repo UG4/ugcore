5807c546a42c991240bbfb6f91fe0d08d4f9d9cd
Andreas Vogel <andreas.vogel@gcsc.uni-frankfurt.de>
2011-02-24 15:06:31 +0000

    Remove unnecessary (old) comment.

--- scripts/reticulum.lua
+++ scripts/reticulum.lua
@@ -205,7 +205,6 @@ neumannDiscCYT:add_boundary_value(neumann, "ca_cyt", "mem_cyt")
 -- we pass here the function needed to evaluate the flux function. The order in 
 -- which the discrete fct are passed is crutial!
 innerDisc = utilCreateInnerBoundary(approxSpace, "ca_cyt, ca_er, ip3", "mem_er")
---innerDisc:add_flux("ca_cyt, ip3", "mem_er")
 
 -----------------------------------------------------------------
 --  Setup Dirichlet Boundary
