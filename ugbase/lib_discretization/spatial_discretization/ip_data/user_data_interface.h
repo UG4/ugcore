2dfbd11e7ef7f2f02bca99e18a76cfa4120348cc
Andreas Vogel <andreas.vogel@gcsc.uni-frankfurt.de>
2011-03-04 14:33:18 +0000

    Splitting files in const_user_data und user_data_interface; adding UserFunction interface

---
+++ ugbase/lib_discretization/spatial_discretization/ip_data/user_data_interface.h
@@ -0,0 +1,98 @@
+/*
+ * user_data_interface.h
+ *
+ *  Created on: 13.10.2010
+ *      Author: andreasvogel
+ */
+
+#ifndef __H__UG__LIB_DISCRETIZATION__SPATIAL_DISCRETIZATION__IP_DATA__USER_DATA_INTERFACE__
+#define __H__UG__LIB_DISCRETIZATION__SPATIAL_DISCRETIZATION__IP_DATA__USER_DATA_INTERFACE__
+
+#include "common/common.h"
+#include "common/math/ugmath.h"
+#include <boost/function.hpp>
+
+#include "ip_data.h"
+
+namespace ug {
+
+/**
+ * \brief User Data
+ *
+ * User Data that can be used in assembling routines.
+ *
+ * \defgroup lib_disc_user_data User Data
+ * \ingroup lib_discretization
+ */
+
+/// \addtogroup lib_disc_user_data
+/// @{
+
+/// User Data Interface
+/**
+ * Specifies and provides the User Data Functor
+ * \tparam		TData	provided data type
+ * \tparam		dim		world dimension
+ */
+template<typename TData, int dim>
+class IUserData
+	: public IPData<TData, dim>
+{
+	public:
+	///	Functor type for evaluation function
+		typedef boost::function<void (TData& value,
+		                              const MathVector<dim>& x,
+		                              number& time)>
+		functor_type;
+
+	/// provides the functor
+		virtual functor_type get_functor() const = 0;
+};
+
+/// User Function Interface
+/**
+ * Specifies and provides the User Function Functor
+ * \tparam		TData	provided data type
+ * \tparam		TPos	Position type of world position
+ */
+template<typename TData>
+class IUserFunction
+{
+	public:
+	///	virtual operator
+		virtual void operator() (TData& out, int numArgs, ...) = 0;
+
+	/// virtual destructor
+		virtual ~IUserFunction() {}
+};
+
+/// scalar boundary user data
+/**
+ * Provides a functor to evaluate scalar user data at boundary. The difference
+ * to IUserNumber is, that the functor returns true iff position is dirichlet
+ * and false else.
+ *
+ * \tparam 	dim		World dimension
+ */
+template <int dim>
+class IBoundaryNumberProvider
+{
+	public:
+	///	Functor Type
+		typedef boost::function<bool (number& value,
+		                              const MathVector<dim>& x,
+		                              number& time)>
+		functor_type;
+
+	/// provides the functor
+		virtual functor_type get_functor() const = 0;
+
+	///	virtual destructor
+		virtual ~IBoundaryNumberProvider(){}
+};
+
+/// @}
+
+} /// end namespace ug
+
+#endif /* __H__UG__LIB_DISCRETIZATION__SPATIAL_DISCRETIZATION__IP_DATA__USER_DATA_INTERFACE__ */
