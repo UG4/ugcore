f3fc200f8bcf147cd0a034d25a361e766851cf9c
Martin Rupp <martin.rupp@gcsc.uni-frankfurt.de>
2011-03-14 14:30:46 +0000

    added ParallelColoring class

---
+++ ugbase/lib_algebra/parallelization/parallel_coloring.h
@@ -0,0 +1,67 @@
+/**
+ * \file parallel_coloring.h
+ *
+ * \author Martin Rupp
+ *
+ * \date 9.3.2011
+ *
+ * header file for coloring of processes
+ *
+ * Goethe-Center for Scientific Computing 2011.
+ *
+ */
+
+
+#include <map>
+#include <vector>
+#include <set>
+#include "parallelization_util.h"
+
+namespace ug
+{
+
+class ParallelColoring
+{
+public:
+	ParallelColoring() : p_colors(NULL), p_processesWithLowerColor(NULL), p_processesWithHigherColor(NULL)
+	{
+
+	}
+	void set_connections(std::set<int> &pids_)
+	{
+		pids = pids_;
+	}
+	/*void set_connections(pcl::ParallelCommunicator<IndexLayout> &master,
+			pcl::ParallelCommunicator<IndexLayout> &slave, bool bAddSlaveSlave)
+	{
+	}*/
+
+	int color(pcl::ParallelCommunicator<IndexLayout> &com);
+
+	void save_processes_with_lower_color_in(std::vector<int> *p)
+	{
+		p_processesWithLowerColor = p;
+	}
+
+	void save_processes_with_higher_color_in(std::vector<int> *p)
+	{
+		p_processesWithHigherColor = p;
+	}
+
+	void save_colors_in(std::map<int, int> *p)
+	{
+		p_colors = p;
+	}
+
+private:
+	std::map<int, int> *p_colors;
+	std::vector<int> *p_processesWithLowerColor;
+	std::vector<int> *p_processesWithHigherColor;
+	std::set<int> pids;
+};
+
+int ColorProcessorGraph(pcl::ParallelCommunicator<IndexLayout> &com, std::set<int> &pids,
+		std::vector<int> &processesWithLowerColor,
+		std::vector<int> &processesWithHigherColor);
+
+}
