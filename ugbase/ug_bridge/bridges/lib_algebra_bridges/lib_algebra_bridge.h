e61831ad9e38d8688e3b7924a9de5e4151b0fe20
Martin Rupp <martin.rupp@gcsc.uni-frankfurt.de>
2011-03-24 12:08:56 +0000

    some documentation

--- ugbase/ug_bridge/bridges/lib_algebra_bridges/lib_algebra_bridge.h
+++ ugbase/ug_bridge/bridges/lib_algebra_bridges/lib_algebra_bridge.h
@@ -18,6 +18,29 @@ namespace ug
 namespace bridge
 {
 
+
+/**
+ * RegisterAlgebraClass function
+ * This template function is intended to simplify the registration of different algebras.
+ * If you want to add an algebra type, you only need to add it here.
+ *
+ * If you want to register classes or functions, you can to that
+ * - in lib_algebra_bridge.cpp's cRegisterAlgebraType::reg function,
+ * - or you do it in another .cpp file.
+ * There you need a class like CRegisterMyClass
+ * with a function static bool reg(Registry &reg, const char *parentGroup)
+ * Then you write a global function
+ \code
+  bool RegisterMyClass(Registry& reg, int algebra_type, const char* parentGroup)
+  {
+		return RegisterAlgebraClass<CRegisterMyClass>(reg, algebra_type, parentGroup);
+  }
+  \endcode
+ * and add it to lib_algebra_bridge.cpp's RegisterDynamicLibAlgebraInterface.
+ *
+ **/
+// add your RegisterMyClass function in lib_algebra_bridge.cpp.
+
 template<template<typename> class TRegister>
 bool RegisterAlgebraClass(Registry& reg, int algebra_type, const char* parentGroup)
 {
