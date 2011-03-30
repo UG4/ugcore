625eb94a2f084d1c6fe77921ddc933c5ba950159
Martin Rupp <martin.rupp@gcsc.uni-frankfurt.de>
2011-03-30 13:59:21 +0000

    nolapack fix

--- ugbase/lib_algebra/small_algebra/no_lapack/no_lapack.h
+++ ugbase/lib_algebra/small_algebra/no_lapack/no_lapack.h
@@ -1,4 +1,8 @@
 #include "lu_decomp.h"
+#include "../small_algebra.h"
+
+#ifndef __H__UG__CPU_ALGEBRA__NO_LAPACK_H__
+#define __H__UG__CPU_ALGEBRA__NO_LAPACK_H__
 
 
 template<typename A_type, typename B_type>
@@ -7,3 +11,6 @@ int GeneralizedEigenvalueProblem(DenseMatrix<A_type> &A, DenseMatrix<A_type> &X,
 {
 	UG_ASSERT(0, "GeneralizedEigenvalueProblem is only implemented for LAPACK at the moment");
 }
+
+
+#endif
