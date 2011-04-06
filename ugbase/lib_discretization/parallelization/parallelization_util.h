8ab37f6e97a549f076b4f01189207d82369ffa12
Andreas Vogel <andreas.vogel@gcsc.uni-frankfurt.de>
2011-04-06 14:16:15 +0000

    Adding "CreateSurfaceIndexLayouts"

--- ugbase/lib_discretization/parallelization/parallelization_util.h
+++ ugbase/lib_discretization/parallelization/parallelization_util.h
@@ -18,76 +18,259 @@ namespace ug
 {
 
 ///	Adds dof-indices of elements in elemLayout to the specified IndexLayout.
-/**	Make sure that TLayout holds elements of type VertexBase*, EdgeBase*,
- *  Face* or Volume*.
+/**
+ * Make sure that TLayout holds elements of type VertexBase*, EdgeBase*,
+ * Face* or Volume*.
  *
  *  \todo: replace IndexLayout with TDoFManager::IndexLayout.
  */
 template <class TDoFDistr, class TLayout>
-bool AddEntriesToIndexLayout(IndexLayout& indexLayoutOut,
-							TDoFDistr& dofDistr,
-							TLayout& elemLayout)
+bool AddEntriesToLevelIndexLayout(IndexLayout& indexLayoutOut,
+                                  TDoFDistr& dofDistr,
+                                  TLayout& elemLayout)
 {
+//	iterator for grid element interfaces
 	typedef typename TLayout::iterator InterfaceIterator;
+
+//	type of grid element interfaces
 	typedef typename TLayout::Interface ElemInterface;
+
+//	iterator for grid elements
 	typedef typename ElemInterface::iterator ElemIterator;
 
+//	type of index interfaces
 	typedef IndexLayout::Interface IndexInterface;
 
-//	iterate over all interfaces
+//	iterate over all grid element interfaces
 	for(InterfaceIterator iIter = elemLayout.begin();
 		iIter != elemLayout.end(); ++iIter)
 	{
+	//	get a grid element interface
 		ElemInterface& elemInterface = elemLayout.interface(iIter);
+
+	//	get a corresponding index interface
 		IndexInterface& indexInterface = indexLayoutOut.interface(
 											elemLayout.proc_id(iIter));
 
-	//	iterate over entries in the elemInterface and add associated
-	//	dofs to the indexInterface
+	//	iterate over entries in the grid element interface
 		for(ElemIterator eIter = elemInterface.begin();
 			eIter != elemInterface.end(); ++eIter)
 		{
+		//	get the grid element
 			typename ElemInterface::Element elem = elemInterface.get_element(eIter);
+
+		//	get the algebraic indices on the grid element
 			typename TDoFDistr::algebra_index_vector_type indices;
 			dofDistr.get_inner_algebra_indices(elem, indices);
+
+		//	add the indices to the interface
 			for(size_t i = 0; i < indices.size(); ++i)
 			{
 				indexInterface.push_back(indices[i]);
 			}
 		}
 	}
+
+//	we're done
 	return true;
 }
 
-
+/// creates the index layout for a level given a GridLayoutMap
+/**
+ * This function creates the Index layout based on a GridLayout. All elements
+ * of the GridLayoutMap are loop on grid level and the indices attached to the
+ * grid elements are added to the interface. Since the ordering of the grid
+ * elements in the interfaces is assumed to be correct, also the ordering in
+ * the index layouts are correct.
+ *
+ * \param[out]		layoutOut		the created index layout
+ * \param[in]		dofDistr		the DoF Distribution
+ * \param[in]		layoutMap		the grid layout map
+ * \param[in]		keyType			key type (e.g. slave or master)
+ * \param[in]		level			level, where layouts should be build
+ */
 template <class TDoFDistribution>
-bool CreateIndexLayout(	IndexLayout& layoutOut,
-						TDoFDistribution& dofDistr,
-						GridLayoutMap& layoutMap,
-						int keyType, int level)
+bool CreateLevelIndexLayout(	IndexLayout& layoutOut,
+                            	TDoFDistribution& dofDistr,
+                            	GridLayoutMap& layoutMap,
+                            	int keyType, int level)
 {
-//TODO: clear the layout!
+//	clear the layout
+	layoutOut.clear();
+
+//	success flag
 	bool bRetVal = true;
-	if(layoutMap.has_layout<VertexBase>(keyType)){
-		bRetVal &= AddEntriesToIndexLayout(layoutOut, dofDistr,
-								layoutMap.get_layout<VertexBase>(keyType).layout_on_level(level));
-	}
-/*
-	if(layoutMap.has_layout<EdgeBase>(keyType)){
-		bRetVal &= AddEntriesToIndexLayout(layoutOut, dofManager,
-								layoutMap.get_layout<EdgeBase>(keyType).layout_on_level(level));
-	}
-	if(layoutMap.has_layout<Face>(keyType)){
-		bRetVal &= AddEntriesToIndexLayout(layoutOut, dofManager,
-								layoutMap.get_layout<Face>(keyType).layout_on_level(level));
-	}
-	if(layoutMap.has_layout<Volume>(keyType)){
-		bRetVal &= AddEntriesToIndexLayout(layoutOut, dofManager,
-								layoutMap.get_layout<Volume>(keyType).layout_on_level(level));
-	}
-*/
+
+// 	add dofs on elements
+	if(dofDistr.template has_dofs_on<VertexBase>())
+		if(layoutMap.has_layout<VertexBase>(keyType))
+		{
+			bRetVal &= AddEntriesToLevelIndexLayout(layoutOut, dofDistr,
+									layoutMap.get_layout<VertexBase>(keyType).layout_on_level(level));
+		}
+
+	if(dofDistr.template has_dofs_on<EdgeBase>())
+		if(layoutMap.has_layout<EdgeBase>(keyType))
+		{
+			bRetVal &= AddEntriesToLevelIndexLayout(layoutOut, dofDistr,
+									layoutMap.get_layout<EdgeBase>(keyType).layout_on_level(level));
+		}
+
+	if(dofDistr.template has_dofs_on<Face>())
+		if(layoutMap.has_layout<Face>(keyType))
+		{
+			bRetVal &= AddEntriesToLevelIndexLayout(layoutOut, dofDistr,
+									layoutMap.get_layout<Face>(keyType).layout_on_level(level));
+		}
+
+	if(dofDistr.template has_dofs_on<Volume>())
+		if(layoutMap.has_layout<Volume>(keyType))
+		{
+			bRetVal &= AddEntriesToLevelIndexLayout(layoutOut, dofDistr,
+									layoutMap.get_layout<Volume>(keyType).layout_on_level(level));
+		}
+
+//	we're done
 	return bRetVal;
 }
+
+
+///	Adds dof-indices of elements in elemLayout to the specified IndexLayout.
+/**
+ * Make sure that TLayout holds elements of type VertexBase*, EdgeBase*,
+ * Face* or Volume*.
+ *
+ *  \todo: replace IndexLayout with TDoFManager::IndexLayout.
+ *
+ * \param[in]		mg				underlying MultiGrid
+ */
+template <class TDoFDistr, class TLayout>
+bool AddEntriesToSurfaceIndexLayout(IndexLayout& indexLayoutOut,
+                                    TDoFDistr& dofDistr,
+                                    TLayout& elemLayout,
+                                    MultiGrid& mg,
+                                    DistributedGridManager& dGrMgr)
+{
+//	iterator for grid element interfaces
+	typedef typename TLayout::iterator InterfaceIterator;
+
+//	type of grid element interfaces
+	typedef typename TLayout::Interface ElemInterface;
+
+//	iterator for grid elements
+	typedef typename ElemInterface::iterator ElemIterator;
+
+//	type of index interfaces
+	typedef IndexLayout::Interface IndexInterface;
+
+//	iterate over all grid element interfaces
+	for(InterfaceIterator iIter = elemLayout.begin();
+		iIter != elemLayout.end(); ++iIter)
+	{
+	//	get a grid element interface
+		ElemInterface& elemInterface = elemLayout.interface(iIter);
+
+	//	get a corresponding index interface
+		IndexInterface& indexInterface = indexLayoutOut.interface(
+											elemLayout.proc_id(iIter));
+
+	//	iterate over entries in the grid element interface
+		for(ElemIterator eIter = elemInterface.begin();
+			eIter != elemInterface.end(); ++eIter)
+		{
+		//	get the grid element
+			typename ElemInterface::Element elem = elemInterface.get_element(eIter);
+
+		//	check if element is on surface (i.e. has no children). Shadows are
+		//	not taken into account here, since their indices are already added
+		//	to the interface by the shadowing objects
+			if(mg.has_children(elem)) {continue;}
+
+		//	check if element is a ghost element, i.e. it is a surface element
+		//	but only due to a hierarchical cut of the grid in order to
+		//	refine it further on another process. These cuts lead to so called
+		//	vertical interfaces.
+			if(dGrMgr.is_ghost(elem)) {continue;}
+
+		//	get the algebraic indices on the grid element
+			typename TDoFDistr::algebra_index_vector_type indices;
+			dofDistr.get_inner_algebra_indices(elem, indices);
+
+		//	add the indices to the interface
+			for(size_t i = 0; i < indices.size(); ++i)
+			{
+				indexInterface.push_back(indices[i]);
+			}
+		}
+	}
+
+//	we're done
+	return true;
+}
+
+/// creates the index layout for a level given a GridLayoutMap
+/**
+ * This function creates the Index layout based on a GridLayout. All elements
+ * of the GridLayoutMap are loop level by level and the indices attached to the
+ * grid elements are added to the interface, if an element does not have a children.
+ * Since the ordering of the grid elements in the interfaces is assumed to be
+ * correct, also the ordering in the index layouts are correct.
+ *
+ * \param[out]		layoutOut		the created index layout
+ * \param[in]		dofDistr		the DoF Distribution
+ * \param[in]		layoutMap		the grid layout map
+ * \param[in]		keyType			key type (e.g. slave or master)
+ * \param[in]		mg				underlying MultiGrid
+ * \param[in]		dGrMgr			distributed Grid Manager
+ */
+template <class TDoFDistribution>
+bool CreateSurfaceIndexLayout(	IndexLayout& layoutOut,
+                            	TDoFDistribution& dofDistr,
+                            	GridLayoutMap& layoutMap,
+                            	int keyType,
+                            	MultiGrid& mg, DistributedGridManager& dGrMgr)
+{
+//	success flag
+	bool bRetVal = true;
+
+// 	add dofs on elements
+	if(dofDistr.template has_dofs_on<VertexBase>())
+		for(size_t level = 0; level < layoutMap.get_layout<VertexBase>(keyType).num_levels(); ++level)
+			if(layoutMap.has_layout<VertexBase>(keyType))
+			{
+				bRetVal &= AddEntriesToSurfaceIndexLayout(layoutOut, dofDistr,
+										layoutMap.get_layout<VertexBase>(keyType).layout_on_level(level), mg, dGrMgr);
+			}
+
+	if(dofDistr.template has_dofs_on<EdgeBase>())
+		for(size_t level = 0; level < layoutMap.get_layout<EdgeBase>(keyType).num_levels(); ++level)
+			if(layoutMap.has_layout<EdgeBase>(keyType))
+			{
+				bRetVal &= AddEntriesToSurfaceIndexLayout(layoutOut, dofDistr,
+										layoutMap.get_layout<EdgeBase>(keyType).layout_on_level(level), mg, dGrMgr);
+			}
+
+	if(dofDistr.template has_dofs_on<Face>())
+		for(size_t level = 0; level < layoutMap.get_layout<Face>(keyType).num_levels(); ++level)
+			if(layoutMap.has_layout<Face>(keyType))
+			{
+				bRetVal &= AddEntriesToSurfaceIndexLayout(layoutOut, dofDistr,
+										layoutMap.get_layout<Face>(keyType).layout_on_level(level), mg, dGrMgr);
+			}
+
+	if(dofDistr.template has_dofs_on<Volume>())
+		for(size_t level = 0; level < layoutMap.get_layout<Volume>(keyType).num_levels(); ++level)
+			if(layoutMap.has_layout<Volume>(keyType))
+			{
+				bRetVal &= AddEntriesToSurfaceIndexLayout(layoutOut, dofDistr,
+										layoutMap.get_layout<Volume>(keyType).layout_on_level(level), mg, dGrMgr);
+			}
+
+//	we're done
+	return bRetVal;
+}
+
+
 
 template <typename TMatrix, typename TDoFDistr>
 void CopyLayoutsAndCommunicatorIntoMatrix(TMatrix& mat, TDoFDistr& dofDistr)
