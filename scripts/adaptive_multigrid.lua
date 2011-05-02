ffacf48ef8bc9a081d454f2e9d7b39b1f3783a80
Andreas Vogel <andreas.vogel@gcsc.uni-frankfurt.de>
2011-05-02 11:27:57 +0000

    Setting dirichlet values again.

--- scripts/adaptive_multigrid.lua
+++ scripts/adaptive_multigrid.lua
@@ -19,8 +19,10 @@ InitAlgebra(CPUAlgebraSelector());
 -- CONSTANTS
 dim = util.GetParamNumber("-dim",    2)
 if dim == 2 then
---	gridName = "open_circle.ugx"
-	gridName = "unit_square_quads_2x2.ugx"
+	gridName = "open_circle.ugx"
+--	gridName = "unit_square_quads_8x8.ugx"
+--	gridName = "unit_square_tri_8x8.ugx"
+--	gridName = "unit_square_tri.ugx"
 elseif dim == 3 then
 	gridName = "open_cube_hex.ugx"
 else
@@ -29,7 +31,7 @@ else
 end
 
 --refinement (default is 5)
-numRefs    = util.GetParamNumber("-numRefs",    2)
+numRefs    = util.GetParamNumber("-numRefs",    1)
 
 -- all elements connected to vertices in this sphere will be refined
 refCenterX = 0.0
@@ -58,8 +60,8 @@ function ourDirichletBnd2d(x, y, t)
 
 	-- line bnd
 	local small = 0.0001
---	if (-small < x and x < small) and y <= 0 then return true, 0.0 end
---	if (-small < y and y < small) and x >= 0 then return true, 0.0 end
+	if (-small < x and x < small) and y <= 0 then return true, 0.0 end
+	if (-small < y and y < small) and x >= 0 then return true, 0.0 end
 	
 	-- circle bnd
 	local phi;
@@ -67,9 +69,9 @@ function ourDirichletBnd2d(x, y, t)
 	if y >= 0 then phi = math.acos(x/r) end
 	if y < 0 then phi =2*math.pi - math.acos(x/r) end
 	
---	return true, math.sin((2*phi)/3)
+	return true, math.sin((2*phi)/3)
 	
-	return true, 10.0;
+--	return true, 10;
 end
 
 -- 3D
@@ -81,9 +83,9 @@ function ourDiffTensor3d(x, y, z, t)
 end
 
 function ourDirichletBnd3d(x, y, z, t)
-	--local s = 2*math.pi
-	--return true, math.sin(s*x) + math.sin(s*y) + math.sin(s*z)
-	return true, x
+	local s = 2*math.pi
+	return true, math.sin(s*x) + math.sin(s*y) + math.sin(s*z)
+--	return true, x
 end
 --------------------------------
 -- User Data Functions (end)
@@ -110,13 +112,25 @@ refiner = HangingNodeDomainRefiner(dom);
 
 -- refine
 local radius = initialRadius
+local refFrom = 0.3
+local refStep = 0.2
+local refCurr = refFrom
+
+local TriagCenterX = 1/6
+local TriagCenterY = 1/6
+local TriagScale = 0.5;
+local TriagRadius = 0.4;
 for i = 1, numRefs do
---	MarkForRefinement_VerticesInSphere(refiner, refCenterX, refCenterY, refCenterZ, radius)
-	MarkForRefinement_VerticesInSquare(refiner, -0.5 + 0.2*i, 1, -1, 1, 0, 0)
+	MarkForRefinement_VerticesInSphere(refiner, refCenterX, refCenterY, refCenterZ, radius)
+--	MarkForRefinement_VerticesInSquare(refiner, refCurr, 1, -1, 1, 0, 0)
+--	MarkForRefinement_VerticesInSquare(refiner, 0.2*i, 1, -1, 1, 0, 0)
+--	MarkForRefinement_FacesInSphere(refiner, TriagCenterX, TriagCenterY, 0.0, TriagRadius)
 	
 	refiner:refine()
 	
+	refCurr = refCurr + refStep/(i+2)
 	radius = radius * radiusFalloff
+	TriagRadius = TriagRadius * TriagScale
 end
 
 -- write grid to file for test purpose
@@ -129,6 +143,8 @@ print("Create ApproximationSpace")
 approxSpace = util.CreateApproximationSpace(dom)
 approxSpace:add_fct("c", "Lagrange", 1)
 approxSpace:init()
+
+SaveGridHierarchyTransformed(dom:get_grid(), approxSpace:get_surface_view(), "surface_view_extrude.ugx", 0.25)
 
 -------------------------------------------
 --  Setup User Functions
@@ -195,7 +211,7 @@ print ("Setting up Algebra Solver")
 
 -- debug writer
 dbgWriter = util.CreateGridFunctionDebugWriter(dim)
-dbgWriter:set_vtk_output(true)
+dbgWriter:set_vtk_output(false)
 
 -- create algebraic Preconditioner
 jac = Jacobi()
@@ -223,6 +239,7 @@ ilut = ILUT()
 	-- Transfer and Projection
 	transfer = util.CreateP1Prolongation(approxSpace)
 	transfer:set_dirichlet_post_process(dirichletBND)
+	transfer:set_restriction_damping(1.0)
 	projection = util.CreateP1Projection(approxSpace)
 	
 	-- Gemoetric Multi Grid
@@ -254,12 +271,12 @@ amg:set_max_nodes_for_base(300)
 amg:set_max_fill_before_base(0.7)
 amg:set_fsmoothing(true)
 
-amg:tostring()
+--amg:tostring()
 
 
 -- create Convergence Check
 convCheck = StandardConvergenceCheck()
-convCheck:set_maximum_steps(300)
+convCheck:set_maximum_steps(200)
 convCheck:set_minimum_defect(1e-12)
 convCheck:set_reduction(1e-12)
 
@@ -270,6 +287,7 @@ exSolver = LU()
 linSolver = LinearSolver()
 linSolver:set_preconditioner(gmg)
 linSolver:set_convergence_check(convCheck)
+--linSolver:set_debug(dbgWriter)
 
 -- create CG Solver
 cgSolver = CG()
@@ -278,7 +296,7 @@ cgSolver:set_convergence_check(convCheck)
 
 -- create BiCGStab Solver
 bicgstabSolver = BiCGStab()
-bicgstabSolver:set_preconditioner(gmg)
+bicgstabSolver:set_preconditioner(jac)
 bicgstabSolver:set_convergence_check(convCheck)
 
 -------------------------------------------
@@ -286,7 +304,7 @@ bicgstabSolver:set_convergence_check(convCheck)
 -------------------------------------------
 
 -- choose some solver
-solver = bicgstabSolver
+solver = linSolver
 
 -- create grid function
 u = approxSpace:create_surface_function()
@@ -302,7 +320,7 @@ end
 LuaStartValue = util.CreateLuaUserNumber("StartValue", dim)
 
 --InterpolateFunction(LuaStartValue, u, "c", 0.0)
-u:set(0.0)
+u:set(1.0)
 
 for i = 1,1 do
 -- 1. init operator
@@ -318,6 +336,7 @@ b:assign(linOp:get_rhs())
 -- write matrix for test purpose
 SaveMatrixForConnectionViewer(u, linOp, "Stiffness.mat")
 SaveVectorForConnectionViewer(b, "Rhs.mat")
+SaveVectorForConnectionViewer(u, "Sol.mat")
 
 -- 4. init solver for linear Operator
 print("Init solver for operator.")
