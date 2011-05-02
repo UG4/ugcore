24d2b7284b23b768782bfc019041862657ffdb3e
Andreas Vogel <andreas.vogel@gcsc.uni-frankfurt.de>
2011-05-02 09:18:26 +0000

    Adding 'MarkForRefinement_FacesInSphere'

--- ugbase/ug_bridge/bridges/lib_grid_bridge.cpp
+++ ugbase/ug_bridge/bridges/lib_grid_bridge.cpp
@@ -372,6 +372,95 @@ void MarkForRefinement_VerticesInSphere(IRefiner& refiner, number centerX,
 	}
 }
 
+
+template <class TAPos, class vector_t>
+void MarkForRefinement_FacesInSphere(Grid& grid, IRefiner& refiner,
+										const vector_t& center,
+										number radius, TAPos& aPos)
+{
+	if(!grid.has_vertex_attachment(aPos)){
+		UG_LOG("WARNING in MarkForRefinement_FacesInSphere: position attachment missing.\n");
+		return;
+	}
+
+	Grid::VertexAttachmentAccessor<TAPos> aaPos(grid, aPos);
+
+//	we'll compare against the square radius.
+	number radiusSq = radius * radius;
+
+//	we'll store associated edges, faces and volumes in those containers
+	vector<EdgeBase*> vEdges;
+	vector<Face*> vFaces;
+	vector<Volume*> vVols;
+
+//	iterate over all faces of the grid. If all vertex of the face are inside
+//	the given sphere then we'll mark all associated elements.
+	for(FaceIterator iter = grid.begin<Face>();
+		iter != grid.end<Face>(); ++iter)
+	{
+	//	get face
+		Face* face = *iter;
+
+	//	bool flag to check if in sphere
+		bool bInSphere = true;
+
+	//	loop all vertices
+		for(size_t i = 0; i < face->num_vertices(); ++i)
+		{
+		//	get vertex
+			VertexBase* vrt = face->vertex(i);
+
+		//	check if vertex is in sphere
+			if(VecDistanceSq(center, aaPos[vrt]) > radiusSq)
+				bInSphere = false;
+		}
+
+		//	mark associated elements
+		if(bInSphere){
+			CollectAssociated(vEdges, grid, face);
+			CollectAssociated(vFaces, grid, face);
+			CollectAssociated(vVols, grid, face);
+
+			refiner.mark_for_refinement(vEdges.begin(), vEdges.end());
+			refiner.mark_for_refinement(vFaces.begin(), vFaces.end());
+			refiner.mark_for_refinement(vVols.begin(), vVols.end());
+		}
+	}
+}
+
+////////////////////////////////////////////////////////////////////////
+void MarkForRefinement_FacesInSphere(IRefiner& refiner, number centerX,
+							number centerY, number centerZ, number radius)
+{
+//	get the associated grid of the refiner.
+	Grid* pGrid = refiner.get_associated_grid();
+	if(!pGrid){
+		UG_LOG("WARNING: Circular refinement failed: no grid assigned.\n");
+		return;
+	}
+
+//	depending on the position attachment, we'll call different versions of
+//	MarkForRefinement_VerticesInSphere.
+	if(pGrid->has_vertex_attachment(aPosition1)){
+		MarkForRefinement_FacesInSphere(*pGrid, refiner, vector1(centerX),
+											radius, aPosition1);
+	}
+	else if(pGrid->has_vertex_attachment(aPosition2)){
+		MarkForRefinement_FacesInSphere(*pGrid, refiner,
+											vector2(centerX, centerY),
+											radius, aPosition2);
+	}
+	else if(pGrid->has_vertex_attachment(aPosition)){
+		MarkForRefinement_FacesInSphere(*pGrid, refiner,
+											vector3(centerX, centerY, centerZ),
+											radius, aPosition);
+	}
+	else{
+		UG_LOG("WARNING in MarkForRefinement_FacesInSphere: No Position attachment found. Aborting.\n");
+	}
+}
+
+
 template <class TAPos, class vector_t>
 void MarkForRefinement_VerticesInSquare(Grid& grid, IRefiner& refiner,
 										const vector_t& min,
@@ -795,6 +884,8 @@ bool RegisterLibGridInterface(Registry& reg, const char* parentGroup)
 			.add_function("SaveGridHierarchy", &SaveGridHierarchy, grp.c_str())
 			.add_function("MarkForRefinement_VerticesInSphere", (void (*)(IRefiner&, number, number, number, number))
 																&MarkForRefinement_VerticesInSphere, grp.c_str())
+			.add_function("MarkForRefinement_FacesInSphere", (void (*)(IRefiner&, number, number, number, number))
+																&MarkForRefinement_FacesInSphere, grp.c_str())
 			.add_function("MarkForRefinement_VerticesInSquare", (void (*)(IRefiner&, number, number, number, number, number, number))
 																&MarkForRefinement_VerticesInSquare, grp.c_str())
 			.add_function("MarkForRefinement_All", &MarkForRefinement_All, grp.c_str())
