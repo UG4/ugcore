60a28d6b7af41effe0062ae005a0bd60e8e6d538
Andreas Vogel <andreas.vogel@gcsc.uni-frankfurt.de>
2011-06-15 12:58:59 +0000

    Cleaning up, renaming Profiling.

--- ugbase/lib_algebra/operator/linear_solver/linear_solver.h
+++ ugbase/lib_algebra/operator/linear_solver/linear_solver.h
@@ -28,61 +28,74 @@
 
 namespace ug{
 
+/// linear solver using abstract preconditioner interface
+/**
+ * This class is a linear iterating scheme, that uses any implementation
+ * of the ILinearIterator interface to precondition the iteration.
+ *
+ * \tparam 		TAlgebra		algebra type
+ */
 template <typename TAlgebra>
-class LinearSolver : public ILinearOperatorInverse<	typename TAlgebra::vector_type,
-													typename TAlgebra::vector_type>
+class LinearSolver
+	: public ILinearOperatorInverse<  typename TAlgebra::vector_type,
+	  	  	  	  	  	  	  	  	  typename TAlgebra::vector_type>
 {
 	public:
-	//	Algebra type
+	///	Algebra type
 		typedef TAlgebra algebra_type;
 
-	//	Vector type
+	///	Vector type
 		typedef typename TAlgebra::vector_type vector_type;
 
 	public:
+	///	Default constructor
 		LinearSolver() :
 			m_A(NULL), m_pPrecond(NULL), m_pConvCheck(NULL)
 		{}
 
-		LinearSolver( 	ILinearIterator<vector_type,vector_type>* Precond,
-						IConvergenceCheck& ConvCheck) :
-							m_A(NULL), m_pPrecond(Precond)
-			{
-				set_convergence_check(ConvCheck);
-			};
-
+	///	returns the name of the solver
 		virtual const char* name() const {return "Iterative Linear Solver";}
 
+	///	sets the convergence check
 		void set_convergence_check(IConvergenceCheck& convCheck)
 		{
 			m_pConvCheck = &convCheck;
 			m_pConvCheck->set_offset(3);
 		}
+
+	///	returns the convergence check
 		IConvergenceCheck* get_convergence_check() {return m_pConvCheck;}
 		void set_preconditioner(ILinearIterator<vector_type, vector_type>& precond)
 		{
 			m_pPrecond = &precond;
 		}
 
+	///	initializes the solver for an operator
 		virtual bool init(ILinearOperator<vector_type, vector_type>& J, const vector_type& u)
 		{
+		//	remember operator
 			m_A = &J;
 
+		//	init the preconditioner
 			if(m_pPrecond != NULL)
 				if(!m_pPrecond->init(J, u))
 				{
 					UG_LOG("ERROR in 'LinearSolver::prepare': Cannot init "
-							"Iterator Operator for Operator J.\n");return false;
+							"Iterator Operator for Operator J.\n");
+					return false;
 				}
 
+		//	we're done
 			return true;
 		}
 
+	///	initializes the solver for an operator
 		virtual bool init(ILinearOperator<vector_type, vector_type>& L)
 		{
+		//	remember operator
 			m_A = &L;
 
-			// init Preconditioner for operator A
+		// 	init Preconditioner for operator A
 			if(m_pPrecond != NULL)
 				if(!m_pPrecond->init(L))
 				{
@@ -91,96 +104,111 @@ class LinearSolver : public ILinearOperatorInverse<	typename TAlgebra::vector_ty
 					return false;
 				}
 
+		//	we're done
 			return true;
 		}
 
-		virtual bool apply_return_defect(vector_type& cNLOut, vector_type& dNLIn)
+	///	solves the system and returns the last defect
+		virtual bool apply_return_defect(vector_type& x, vector_type& b)
 		{
 			if(m_A == NULL)
 			{
-				UG_LOG("ERROR: In 'LinearSolver::apply': Matrix A not set.\n");
+				UG_LOG("ERROR in 'LinearSolver::apply': "
+						"Operator that should be inverted has not been set.\n");
 				return false;
 			}
 
 			if(m_pConvCheck == NULL)
 			{
-				UG_LOG("ERROR: In 'LinearSolver::apply': Convergence check not set.\n");
+				UG_LOG("ERROR in 'LinearSolver::apply': "
+						"Convergence check not set.\n");
 				return false;
 			}
 
 			#ifdef UG_PARALLEL
-			if(!dNLIn.has_storage_type(PST_ADDITIVE) || !cNLOut.has_storage_type(PST_CONSISTENT))
+			if(!b.has_storage_type(PST_ADDITIVE) || !x.has_storage_type(PST_CONSISTENT))
 				{
-					UG_LOG("ERROR: In 'LinearSolver::apply':Inadequate storage format of Vectors.\n");
+					UG_LOG("ERROR in 'LinearSolver::apply': "
+							"Inadequate storage format of Vectors.\n");
 					return false;
 				}
 			#endif
 
-			// rename d_nl as d (for convenience)
-			vector_type& d = dNLIn;
+		// 	rename b as d (for convenience)
+			vector_type& d = b;
 
-			// build defect:  d := d_nl - J(u)*c_nl
-			LS_PROFILE_BEGIN(LinSolve_BuildDefect);
-			if(!m_A->apply_sub(d, cNLOut))
-				{UG_LOG("ERROR in 'LinearSolver::apply': Unable to build defect. Aborting.\n"); return false;}
-			LS_PROFILE_END(); //LinSolve_BuildDefect
+		// 	build defect:  d := b - J(u)*x
+			LS_PROFILE_BEGIN(LS_BuildDefect);
+			if(!m_A->apply_sub(d, x))
+			{
+				UG_LOG("ERROR in 'LinearSolver::apply':"
+						" Unable to build defect. Aborting.\n");
+				return false;
+			}
+			LS_PROFILE_END(); //LS_BuildDefect
 
-			// create correction
-			// todo: 	it would be sufficient to only copy the pattern (and parallel constructor)
-			//			without initializing the values
-			vector_type c; c.create(cNLOut.size()); c = cNLOut;
+		// 	create correction
+		// 	todo: 	it would be sufficient to only copy the pattern (and parallel constructor)
+		//			without initializing the values
+			vector_type c; c.create(x.size()); c = x;
 
 			prepare_conv_check();
 			m_pConvCheck->start(d);
 
-			// Iteration loop
+		// 	Iteration loop
 			while(!m_pConvCheck->iteration_ended())
 			{
-				// Compute a correction c := B*d using one iterative step
-				// Internally the defect is updated d := d - A*c = d - A*(x+c)
+			// 	Compute a correction c := B*d using one iterative step
+			// 	Internally the defect is updated d := d - A*c = d - A*(x+c)
 				if(m_pPrecond != NULL) {
-					LS_PROFILE_BEGIN(LinSolve_PrecondApplyUpdateDefect);
+					LS_PROFILE_BEGIN(LS_ApplyPrecond);
 					if(!m_pPrecond->apply_update_defect(c, d))
-						{UG_LOG("ERROR in 'LinearSolver::apply': Iterator Operator "
-									"applied incorrectly. Aborting.\n"); return false;}
-					LS_PROFILE_END(); //LinSolve_PrecondApplyUpdateDefect
+					{
+						UG_LOG("ERROR in 'LinearSolver::apply': Iterator "
+								"Operator applied incorrectly. Aborting.\n");
+						return false;
+					}
+					LS_PROFILE_END(); //LS_ApplyPrecond
 				}
 
-				// add correction to solution
-				LS_PROFILE_BEGIN(LinSolve_PrecondAddCorrection);
-				cNLOut += c;
-				LS_PROFILE_END(); //LinSolve_PrecondAddCorrection
+			// 	add correction to solution: x += c
+				LS_PROFILE_BEGIN(LS_AddCorrection);
+				x += c;
+				LS_PROFILE_END(); //LS_AddCorrection
 
-				// compute new defect (in parallel)
-				LS_PROFILE_BEGIN(LinSolve_ComputeNewDefect);
+			// 	compute new defect (in parallel) d := d - A*c
+				LS_PROFILE_BEGIN(LS_ComputeNewDefect);
 				m_pConvCheck->update(d);
-				LS_PROFILE_END(); //LinSolve_ComputeNewDefect
+				LS_PROFILE_END(); //LS_ComputeNewDefect
 			}
 
-			LS_PROFILE_BEGIN(LinSolve_ConvCheckPost);
+		//	write some information when ending the iteration
 			if(!m_pConvCheck->post())
 			{
-				UG_LOG("ERROR in 'LinearSolver::apply': post-convergence-check signaled failure. Aborting.\n");
+				UG_LOG("ERROR in 'LinearSolver::apply': post-convergence-check "
+						"signaled failure. Aborting.\n");
 				return false;
 			}
-			LS_PROFILE_END(); //LinSolve_ConvCheckPost
+
+		//	we're done
 			return true;
 		}
 
-		virtual bool apply(vector_type& cNLOut, const vector_type& dNLIn)
+		virtual bool apply(vector_type& x, const vector_type& b)
 		{
 		//	copy defect
-			vector_type d; d.resize(dNLIn.size());
-			d = dNLIn;
+			vector_type b2; b2.resize(b.size());
+			b2 = b;
 
 		//	solve on copy of defect
-			return apply_return_defect(cNLOut, d);
+			return apply_return_defect(x, b2);
 		}
 
 		// destructor
 		virtual ~LinearSolver() {};
 
 	protected:
+	///	prepares the convergence check output
 		void prepare_conv_check()
 		{
 			m_pConvCheck->set_name(name());
@@ -198,13 +226,13 @@ class LinearSolver : public ILinearOperatorInverse<	typename TAlgebra::vector_ty
 		}
 
 	protected:
-		// Operator that is inverted by this Inverse Operator
+	// 	Operator that is inverted by this Inverse Operator
 		ILinearOperator<vector_type,vector_type>* m_A;
 
-		// Iterator used in the iterative scheme to compute the correction and update the defect
+	// 	Iterator used in the iterative scheme to compute the correction and update the defect
 		ILinearIterator<vector_type,vector_type>* m_pPrecond;
 
-		// Convergence Check
+	// 	Convergence Check
 		IConvergenceCheck* m_pConvCheck;
 };
 
