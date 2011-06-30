afebabf6986b42d9d55bbae0b0de892866408111
Andreas Vogel <andreas.vogel@gcsc.uni-frankfurt.de>
2011-06-30 14:52:46 +0000

    Removing default implementation of ReferenceMapping.

--- ugbase/lib_discretization/reference_element/reference_element_mapping.h
+++ ugbase/lib_discretization/reference_element/reference_element_mapping.h
@@ -16,6 +16,21 @@
 
 namespace ug{
 
+/**
+ * This class describes the mapping from a reference element into the real
+ * (physical) world. The mapping is initialized by the physical positions of
+ * the vertices of the real world element. The order of those points must be
+ * given as indicated by the corresponding reference element.
+ *
+ * Let \f$R\f$ be the reference element and \f$T\f$ be the element. Then, the
+ * reference mapping is a mapping:
+ * \f[
+ * 	\phi:	R \mapsto T
+ * \f]
+ *
+ * \tparam	TRefElem		reference element
+ * \tparam	TWorldDim		world dimension
+ */
 template <typename TRefElem, int TWorldDim>
 class ReferenceMapping
 {
@@ -28,25 +43,25 @@ class ReferenceMapping
 
 	public:
 	///	Constructor
-		ReferenceMapping() {}
+		ReferenceMapping();
 
 	///	refresh mapping for new set of corners
-		void update(const MathVector<worldDim>* corners) {}
+		void update(const MathVector<worldDim>* corners);
 
 	///	map local coordinate to global coordinate
 		void local_to_global(	const MathVector<dim> locPos,
-								MathVector<worldDim>& globPos) const {}
+								MathVector<worldDim>& globPos) const;
 
 	///	returns transposed of jacobian
 		void jacobian_transposed(	const MathVector<dim> locPos,
-									MathMatrix<dim, worldDim>& JT) const {}
+									MathMatrix<dim, worldDim>& JT) const;
 
 	///	returns transposed of the inverse of the jacobian
 		void jacobian_transposed_inverse(	const MathVector<dim> locPos,
-											MathMatrix<worldDim, dim>& JTInv) const {}
+											MathMatrix<worldDim, dim>& JTInv) const;
 
 	///	returns the determinate of the jacobian
-		number jacobian_det(const MathVector<dim> locPos) const {return 0.0;}
+		number jacobian_det(const MathVector<dim> locPos) const;
 };
 
 
