946752ba9f1280c164f47c6031c2c4b90ffad1d1
Andreas Vogel <andreas.vogel@gcsc.uni-frankfurt.de>
2011-07-07 17:46:50 +0000

    Less restrict convergence criteria

--- scripts/d3f/elder.lua
+++ scripts/d3f/elder.lua
@@ -342,7 +342,7 @@ end
 -- create Convergence Check
 print("Creating Solver.")
 convCheck = StandardConvergenceCheck()
-convCheck:set_maximum_steps(1000)
+convCheck:set_maximum_steps(3000)
 convCheck:set_minimum_defect(1e-8)
 convCheck:set_reduction(1e-8)
 
