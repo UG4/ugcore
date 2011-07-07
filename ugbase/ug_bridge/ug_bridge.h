05cf6524c414fab28aa63f980c75a81323bcd574
Andreas Vogel <andreas.vogel@gcsc.uni-frankfurt.de>
2011-07-07 15:05:22 +0000

    Adding RegisterDynamicLibDiscretizationInterface again.

--- ugbase/ug_bridge/ug_bridge.h
+++ ugbase/ug_bridge/ug_bridge.h
@@ -59,6 +59,12 @@ bool RegisterStaticLibDiscInterface(Registry& reg, const char* parentGroup = "/u
 bool RegisterDynamicLibDiscDomain(Registry& reg, int algebra_type, const char* parentGroup = "/ug4");
 bool RegisterDynamicLibDiscAlgebra(Registry& reg, int algebra_type, const char* parentGroup = "/ug4");
 
+bool RegisterDynamicLibDiscretizationInterface(Registry& reg, int algebra_type, const char* parentGroup = "/ug4")
+{
+	RegisterDynamicLibDiscDomain(reg, algebra_type, parentGroup);
+	RegisterDynamicLibDiscAlgebra(reg, algebra_type, parentGroup);
+}
+
 /// registers user data
 bool RegisterUserData(Registry& reg, const char* parentGroup = "/ug4");
 
