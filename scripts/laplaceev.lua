f97c01594de908a3ec742e3e19b86ec649659397
Martin Rupp <martin.rupp@gcsc.uni-frankfurt.de>
2011-07-10 14:34:48 +0000

    updated laplaceev

--- scripts/laplaceev.lua
+++ scripts/laplaceev.lua
@@ -1,25 +1,97 @@
 ----------------------------------------------------------
 --
---   Lua - Script to perform the Laplace-EV-Problem
---	 
---   Author: Andreas Vogel / Martin Rupp
+--   Lua - Script to perform the Laplace-Problem
+--
+--   Author: Andreas Vogel
 --
 ----------------------------------------------------------
 
--- make sure that ug_util is in the right path.
--- currently only the path in which you start your application is valid.
-dofile("../scripts/ug_util.lua")
+ug_load_script("ug_util.lua")
+
+--------------------------------------------------------------------------------
+-- Checking for parameters (begin)
+--------------------------------------------------------------------------------
+-- Several definitions which can be changed by command line parameters
+-- space dimension and grid file:
+dim = util.GetParamNumber("-dim", 2)
+
+if dim == 2 then
+	gridName = util.GetParam("-grid", "unit_square_01/unit_square_01_tri_2x2.ugx")
+	--gridName = "unit_square_tri_four_dirichlet_nodes.ugx"
+	--gridName = "unit_square/unit_square_quads_8x8.ugx"
+end
+if dim == 3 then
+	gridName = util.GetParam("-grid", "unit_square/unit_cube_hex.ugx")
+	--gridName = "unit_square/unit_cube_tets_regular.ugx"
+end
+
+-- refinements:
+numPreRefs = util.GetParamNumber("-numPreRefs", 1)
+numRefs    = util.GetParamNumber("-numRefs",    3)
+
+-- parallelisation related stuff
+-- way the domain / the grid will be distributed to the processes:
+distributionType = util.GetParam("-distType", "bisect") -- [grid2d | bisect | metis]
+
+-- amount of output
+verbosity = util.GetParamNumber("-verb", 0)	    -- set to 0 i.e. for time measurements,
+						    -- >= 1 for writing matrix files etc.
+
+activateDbgWriter = 0	  
+activateDbgWriter = util.GetParamNumber("-dbgw", 0) -- set to 0 i.e. for time measurements,
+						    -- >= 1 for debug output: this sets
+						    -- 'fetiSolver:set_debug(dbgWriter)'
+
+
+-- Display parameters (or defaults):
+print(" General parameters chosen:")
+print("    dim        = " .. dim)
+print("    grid       = " .. gridName)
+print("    numRefs    = " .. numRefs)
+print("    numPreRefs = " .. numPreRefs)
+
+print("    verb (verbosity)         = " .. verbosity)
+print("    dbgw (activateDbgWriter) = " .. activateDbgWriter)
+
+print(" Parallelisation related parameters chosen:")
+print("    distType   = " .. distributionType)
+
+--------------------------------------------------------------------------------
+-- Checking for parameters (end)
+--------------------------------------------------------------------------------
+
 
 -- choose algebra
 InitAlgebra(CPUAlgebraSelector());
 
--- constants
-dim = 2
-gridName = "unit_square_01/unit_square_01_tri_2x2.ugx"
-numPreRefs = 0
-numRefs = 4
 
+--------------------------------
+-- User Data Functions (begin)
+--------------------------------
+	function ourDiffTensor2d(x, y, t)
+		return	1, 0, 
+				0, 1
+	end
+	
+	function ourVelocityField2d(x, y, t)
+		return	0, 0
+	end
+	
+	function ourReaction2d(x, y, t)
+		return	0
+	end
+	
+	function ourRhs2d(x, y, t)
+		return 0
+	end
+	
+	function ourDirichletBnd2d(x, y, t)
+		return true, 0
+	end
 
+--------------------------------
+-- User Data Functions (end)
+--------------------------------
 
 -- create Instance of a Domain
 print("Create Domain.")
@@ -28,82 +100,181 @@ dom = util.CreateDomain(dim)
 -- load domain
 print("Load Domain from File.")
 if util.LoadDomain(dom, gridName) == false then
-print("Loading Domain failed.")
-exit()
+   print("Loading Domain failed.")
+   exit()
 end
-
--- get subset handler
-sh = dom:get_subset_handler()
---if sh:num_subsets() ~= 2 then 
---	print("Domain must have 2 Subsets for this problem.")
---	exit()
---end
-sh:set_subset_name("Inner", 0)
-sh:set_subset_name("DirichletBoundary", 1)
---sh:set_subset_name("NeumannBoundary", 2)
 
 -- create Refiner
 print("Create Refiner")
 if numPreRefs > numRefs then
-print("numPreRefs must be smaller/equal than numRefs");
-exit();
+	print("It must be choosen: numPreRefs <= numRefs");
+	exit();
 end
 
-refiner = GlobalMultiGridRefiner()
-refiner:assign_grid(dom:get_grid())
+-- Create a refiner instance. This is a factory method
+-- which automatically creates a parallel refiner if required.
+refiner = GlobalDomainRefiner(dom)
+
+-- Performing pre-refines
+print("Perform (non parallel) pre-refinements of grid")
 for i=1,numPreRefs do
-refiner:refine()
+	print( "PreRefinement step " .. i .. " ...")
+	refiner:refine()
+	print( "... done!")
 end
 
-if util.DistributeDomain(dom) == false then
-print("Error while Distributing Grid.")
-exit()
+-- get number of processes
+numProcs = GetNumProcesses()
+
+-- Distribute the domain to all involved processes
+-- Since only process 0 loaded the grid, it is the only one which has to
+-- fill a partitionMap (but every process needs one and has to return his map
+-- by calling 'RedistributeDomain()', even if in this case the map is empty
+-- for all processes but process 0).
+print("Distribute domain with 'distributionType' = " .. distributionType .. "...")
+partitionMap = PartitionMap()
+
+if GetProcessRank() == 0 then
+	if distributionType == "bisect" then
+		util.PartitionMapBisection(dom, partitionMap, numProcs)
+		
+	elseif distributionType == "grid2d" then
+		local numNodesX, numNodesY = util.FactorizeInPowersOfTwo(numProcs / numProcsPerNode)
+		util.PartitionMapLexicographic2D(dom, partitionMap, numNodesX,
+										 numNodesY, numProcsPerNode)
+
+	elseif distributionType == "metis" then
+		util.PartitionMapMetis(dom, partitionMap, numProcs)
+										 
+	else
+	    print( "distributionType not known, aborting!")
+	    exit()
+	end
+	
+-- save the partition map for debug purposes
+	if verbosity >= 1 then
+		SavePartitionMap(partitionMap, dom, "partitionMap_p" .. GetProcessRank() .. ".ugx")
+	end
 end
 
+if RedistributeDomain(dom, partitionMap, true) == false then
+	print("Redistribution failed. Please check your partitionMap.")
+	exit()
+end
+print("... domain distributed!")
+
+--------------------------------------------------------------------------------
+-- end of partitioning
+--------------------------------------------------------------------------------
+
+-- Perform post-refine
 print("Refine Parallel Grid")
 for i=numPreRefs+1,numRefs do
-GlobalRefineParallelDomain2d(dom)
+	print( "Refinement step " .. i .. " ...")
+	refiner:refine()
+	print( "... done!")
 end
 
+-- get subset handler
+sh = dom:get_subset_handler()
+if sh:num_subsets() ~= 2 then 
+print("Domain must have 2 Subsets for this problem.")
+exit()
+end
+sh:set_subset_name("Inner", 0)
+sh:set_subset_name("DirichletBoundary", 1)
+--sh:set_subset_name("NeumannBoundary", 2)
+
 -- write grid to file for test purpose
-SaveDomain(dom, "refined_grid.ugx")
+if verbosity >= 1 then
+	refinedGridOutName = "refined_grid_p" .. GetProcessRank() .. ".ugx"
+	print("saving domain to " .. refinedGridOutName)
+	if SaveDomain(dom, refinedGridOutName) == false then
+		print("Saving of domain to " .. refinedGridOutName .. " failed. Aborting.")
+		    exit()
+	end
+	
+	hierarchyOutName = "hierachy_p" .. GetProcessRank() .. ".ugx"
+	print("saving hierachy to " .. hierarchyOutName)
+	if SaveGridHierarchy(dom:get_grid(), hierarchyOutName) == false then
+		print("Saving of domain to " .. hierarchyOutName .. " failed. Aborting.")
+		    exit()
+	end
+end
+
+print("NumProcs is " .. numProcs .. ", numPreRefs = " .. numPreRefs .. ", numRefs = " .. numRefs .. ", grid = " .. gridName)
 
 -- create Approximation Space
 print("Create ApproximationSpace")
 approxSpace = util.CreateApproximationSpace(dom)
 approxSpace:add_fct("c", "Lagrange", 1)
 approxSpace:init()
+approxSpace:print_layout_statistic()
+approxSpace:print_statistic()
 
--------------------------------------------
+-- lets order indices using Cuthill-McKee
+if OrderCuthillMcKee(approxSpace, true) == false then
+	print("ERROR when ordering Cuthill-McKee"); exit();
+end
+
+--------------------------------------------------------------------------------
 --  Setup User Functions
+--------------------------------------------------------------------------------
 -------------------------------------------
 print ("Setting up Assembling")
 
+-- depending on the dimension we're choosing the appropriate callbacks.
+-- we're using the .. operator to assemble the names (dim = 2 -> "ourDiffTensor2d")
 -- Diffusion Tensor setup
-diffusionMatrix = util.CreateConstDiagUserMatrix(1.0, dim)
+diffusionMatrix = util.CreateLuaUserMatrix("ourDiffTensor"..dim.."d", dim)
+--diffusionMatrix = util.CreateConstDiagUserMatrix(1.0, dim)
 
 -- Velocity Field setup
-velocityField = util.CreateConstUserVector(0.0, dim)
+velocityField = util.CreateLuaUserVector("ourVelocityField"..dim.."d", dim)
+--velocityField = util.CreateConstUserVector(0.0, dim)
 
 -- Reaction setup
-reaction = util.CreateConstUserNumber(0.0, dim)
+reaction = util.CreateLuaUserNumber("ourReaction"..dim.."d", dim)
+--reaction = util.CreateConstUserNumber(0.0, dim)
 
 -- rhs setup
-rhs = util.CreateConstUserNumber(0.0, dim)
+rhs = util.CreateLuaUserNumber("ourRhs"..dim.."d", dim)
+--rhs = util.CreateConstUserNumber(0.0, dim)
+
+-- neumann setup
+neumann = util.CreateLuaBoundaryNumber("ourNeumannBnd"..dim.."d", dim)
+--neumann = util.CreateConstUserNumber(0.0, dim)
 
 -- dirichlet setup
-dirichlet = util.CreateConstBoundaryNumber(0, dim)
-
------------------------------------------------------------------
+dirichlet = util.CreateLuaBoundaryNumber("ourDirichletBnd"..dim.."d", dim)
+--dirichlet = util.CreateConstBoundaryNumber(3.2, dim)
+	
+--------------------------------------------------------------------------------
 --  Setup FV Convection-Diffusion Element Discretization
------------------------------------------------------------------
+--------------------------------------------------------------------------------
+
+-- Select upwind
+if dim == 2 then 
+--upwind = NoUpwind2d()
+--upwind = FullUpwind2d()
+upwind = WeightedUpwind2d(); upwind:set_weight(0.0)
+--upwind = PartialUpwind2d()
+else print("Dim not supported for upwind"); exit() end
+
 
 elemDisc = util.CreateFV1ConvDiff(approxSpace, "c", "Inner")
-elemDisc:set_upwind_amount(0.0)
+if elemDisc:set_upwind(upwind) == false then exit() end
 elemDisc:set_diffusion_tensor(diffusionMatrix)
 elemDisc:set_velocity_field(velocityField)
 elemDisc:set_reaction(reaction)
 elemDisc:set_source(rhs)
+
+-----------------------------------------------------------------
+--  Setup Neumann Boundary
+-----------------------------------------------------------------
+
+--neumannDisc = util.CreateNeumannBoundary(approxSpace, "Inner")
+--neumannDisc:add_boundary_value(neumann, "c", "NeumannBoundary")
 
 -----------------------------------------------------------------
 --  Setup Dirichlet Boundary
@@ -118,11 +289,12 @@ dirichletBND:add_boundary_value(dirichlet, "c", "DirichletBoundary")
 
 domainDisc = DomainDiscretization()
 domainDisc:add_elem_disc(elemDisc)
+--domainDisc:add_elem_disc(neumannDisc)
 domainDisc:add_post_process(dirichletBND)
 
--------------------------------------------
+--------------------------------------------------------------------------------
 --  Algebra
--------------------------------------------
+--------------------------------------------------------------------------------
 print ("Setting up Algebra Solver")
 
 -- create operator from discretization
@@ -130,24 +302,14 @@ linOp = AssembledLinearOperator()
 linOp:set_discretization(domainDisc)
 linOp:set_dof_distribution(approxSpace:get_surface_dof_distribution())
 
--- init Operator
-print ("Assemble Operator ... ")
-linOp:init()
-print ("done")
-
-
 -- get grid function
-n_ev = 2
-x0 = approxSpace:create_surface_function("x0", true)
-x0:set_random(0.0, 1.0);
-linOp:set_dirichlet_values(x0)
-x1 = approxSpace:create_surface_function("x1", true)
-x1:set_random(0.0, 1.0);
-linOp:set_dirichlet_values(x1)
+u = approxSpace:create_surface_function()
+b = approxSpace:create_surface_function()
 
--- write matrix for test purpose
-SaveMatrixForConnectionViewer(x0, linOp, "Stiffness.mat")
--- SaveVectorForConnectionViewer(b, "Rhs.mat")
+-- debug writer
+dbgWriter = util.CreateGridFunctionDebugWriter(dim)
+dbgWriter:set_reference_grid_function(u)
+dbgWriter:set_vtk_output(false)
 
 -- create algebraic Preconditioner
 jac = Jacobi()
@@ -156,54 +318,100 @@ gs = GaussSeidel()
 sgs = SymmetricGaussSeidel()
 bgs = BackwardGaussSeidel()
 ilu = ILU()
+ilu:set_debug(dbgWriter)
 ilut = ILUT()
 
 -- create GMG ---
 -----------------
 
--- Base Solver
-baseConvCheck = StandardConvergenceCheck()
-baseConvCheck:set_maximum_steps(500)
-baseConvCheck:set_minimum_defect(1e-8)
-baseConvCheck:set_reduction(1e-30)
-baseConvCheck:set_verbose_level(false)
--- base = LapackLUSolver()
-base = LinearSolver()
-base:set_convergence_check(baseConvCheck)
-base:set_preconditioner(jac)
+	-- Base Solver
+	baseConvCheck = StandardConvergenceCheck()
+	baseConvCheck:set_maximum_steps(1000)
+	baseConvCheck:set_minimum_defect(1e-11)
+	baseConvCheck:set_reduction(1e-30)
+	baseConvCheck:set_verbose_level(false)
+	--base = LU()
+	base = LinearSolver()
+	base:set_convergence_check(baseConvCheck)
+	base:set_preconditioner(jac)
+	
+	-- Transfer and Projection
+	transfer = util.CreateP1Prolongation(approxSpace)
+	transfer:set_dirichlet_post_process(dirichletBND)
+	projection = util.CreateP1Projection(approxSpace)
+	
+	-- Geometric Multi Grid
+	gmg = util.CreateGeometricMultiGrid(approxSpace)
+	gmg:set_discretization(domainDisc)
+	gmg:set_base_level(0)
+	gmg:set_base_solver(base)
+	gmg:set_parallel_base_solver(true)
+	gmg:set_smoother(jac)
+	gmg:set_cycle_type(1)
+	gmg:set_num_presmooth(3)
+	gmg:set_num_postsmooth(3)
+	gmg:set_prolongation(transfer)
+	gmg:set_projection(projection)
+	if activateDbgWriter >= 1 then
+		gmg:set_debug(dbgWriter)
+	end
 
--- Transfer and Projection
-transfer = util.CreateP1Prolongation(approxSpace)
-transfer:set_dirichlet_post_process(dirichletBND)
-projection = util.CreateP1Projection(approxSpace)
+-- create AMG ---
+-----------------
 
--- Gemoetric Multi Grid
-gmg = util.CreateGeometricMultiGrid(approxSpace)
-gmg:set_discretization(domainDisc)
-gmg:set_surface_level(numRefs)
-gmg:set_base_level(0)
-gmg:set_base_solver(base)
-gmg:set_smoother(jac)
-gmg:set_cycle_type(1)
-gmg:set_num_presmooth(3)
-gmg:set_num_postsmooth(3)
-gmg:set_prolongation(transfer)
-gmg:set_projection(projection)
-
+	if false then
+	amg = RSAMGPreconditioner()
+	amg:set_nu1(2)
+	amg:set_nu2(2)
+	amg:set_gamma(1)
+	amg:set_presmoother(jac)
+	amg:set_postsmoother(jac)
+	amg:set_base_solver(base)
+	--amg:set_debug(u)
+	end
 
 -- create Convergence Check
 convCheck = StandardConvergenceCheck()
 convCheck:set_maximum_steps(100)
 convCheck:set_minimum_defect(1e-11)
 convCheck:set_reduction(1e-12)
+--convCheck:set_verbose_level(true)
+
+linOp:init_op_and_rhs(b)
+
+B = MatrixOperator()
+-- domainDisc:assemble_stiffness_matrix(A, v, approxSpace:get_surface_dof_distribution())
+v = approxSpace:create_surface_function()
+domainDisc:assemble_mass_matrix(B, v, approxSpace:get_surface_dof_distribution())
+SaveMatrixForConnectionViewer(v, B, "B.mat") 
+SaveMatrixForConnectionViewer(v, linOp, "A.mat")
+v:set(1.0)
+linOp:set_dirichlet_values(v)
+SaveVectorForConnectionViewer(v, "v.mat") 
+
+srand(0)
+nev = 3
 
 eig = EigenSolver()
-eig:add_vector(x0)
-eig:add_vector(x1)
+eig:set_linear_operator_A(linOp)
+eig:set_linear_operator_B(B)
+eig:set_max_iterations(100)
+eig:set_precision(1e-8)
 eig:set_preconditioner(gmg)
-eig:set_linear_operator_A(linOp);
+ev = {}
+for i=1,nev do
+	print("adding ev "..i)
+	ev[i] = approxSpace:create_surface_function()
+	ev[i]:set_random(-1.0, 1.0)
+	linOp:set_dirichlet_values(ev[i])
+	eig:add_vector(ev[i])
+end
+
 eig:apply()
 
+for i=1,nev do
+	WriteGridFunctionToVTK(ev[i], "ev_"..i)
+	SaveVectorForConnectionViewer(ev[i], "ev_"..i..".mat") 
+end
 
--- Output
-WriteGridFunctionToVTK(u, "Solution")
+
