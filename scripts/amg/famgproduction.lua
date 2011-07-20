ed264cc16f7314b29cb2ef62ef7c8c6b43f14f20
Martin Rupp <martin.rupp@gcsc.uni-frankfurt.de>
2011-07-20 12:08:37 +0000

    moved amg scripts to amg directory

---
+++ scripts/amg/famgproduction.lua
@@ -0,0 +1,428 @@
+----------------------------------------------------------
+--
+--   Lua - Script to perform the Laplace-Problem
+--
+--   Author: Martin Rupp / Andreas Vogel
+--
+----------------------------------------------------------
+-- SetOutputProcessRank(0)
+SetOutputProfileStats(false)
+
+ug_load_script("ug_util.lua")
+
+-- choose algebra
+InitAlgebra(CPUAlgebraSelector());
+
+-- constants
+if util.HasParamOption("-3d") then
+	dim = 3
+else
+	dim = 2
+end
+
+if dim == 2 then
+	gridName = util.GetParam("-grid", "unit_square_01/unit_square_01_tri_2x2.ugx")
+	-- gridName = "unit_square/unit_square_quads_8x8.ugx"
+end
+if dim == 3 then
+	gridName = util.GetParam("-grid", "unit_square/unit_cube_hex.ugx")
+	-- gridName = "unit_square/unit_cube_tets_regular.ugx"
+end
+
+-- choose number of total Refinements (incl. pre-Refinements)
+numRefs = util.GetParamNumber("-numRefs", 5)
+
+-- choose number of pre-Refinements (before sending grid onto different processes)	
+numPreRefs = util.GetParamNumber("-numPreRefs", math.min(5, numRefs-2))
+
+maxBase = util.GetParamNumber("-maxBase", 3000)
+
+RAepsilon = util.GetParamNumber("-RAepsilon", 1)
+RAalpha = util.GetParamNumber("-RAalpha", 0)
+
+bFileOutput = true
+bOutput = true
+
+print("Parameters: ")
+print("    numPreRefs = "..numPreRefs)
+print("    numRefs = "..numRefs)
+print("    maxBase = "..maxBase)
+print("    dim = "..dim)
+print("    gridName = "..gridName)
+print("    RAepsilon = "..RAepsilon)
+print("    RAalpha = "..RAalpha.." degree")
+RAalpha = RAalpha * (2*math.pi/360)
+print("    RAalpha = "..RAalpha.." grad")
+
+
+function my_assert(condition, text)
+	if condition == false then
+		error(text)
+	end
+end
+
+--------------------------------
+-- User Data Functions (begin)
+--------------------------------
+	function ourDiffTensor2d(x, y, t)
+		return	1, 0, 
+				0, 1
+	end
+	
+	function ourVelocityField2d(x, y, t)
+		return	0, 0 -- 150 ist grenzwertig
+	end
+	
+	function ourReaction2d(x, y, t)
+		return	0
+	end
+	
+	function ourRhs2d(x, y, t)
+		local s = 2*math.pi
+		-- return	s*s*(math.sin(s*x) + math.sin(s*y))
+		--return -2*y
+		return 0
+	end
+
+	
+	function ourDirichletBnd2d(x, y, t)
+		local s = 2*math.pi
+		--return true, math.sin(s*x) + math.sin(s*y)
+		return true, 0
+		--return true, x*x*y
+		--return true, 2.5
+	end
+
+--------------------------------
+-- User Data Functions (end)
+--------------------------------
+tBefore = os.clock()
+
+-- create Instance of a Domain
+dom = util.CreateDomain(dim)
+if util.LoadDomain(dom, gridName) == false then
+print("Loading Domain failed.")
+exit()
+end
+
+-- create Refiner
+my_assert(numPreRefs < numRefs, "numPreRefs must be smaller than numRefs");
+
+refiner = GlobalDomainRefiner(dom)
+for i=1,numPreRefs do
+refiner:refine()
+end
+
+-- Distribute the domain to all involved processes
+my_assert(DistributeDomain(dom) == true, "Error while Distributing Grid.")
+
+-- Perform post-refine
+for i=numPreRefs+1,numRefs do
+refiner:refine()
+end
+
+tGrid = os.clock()-tBefore
+
+-- get subset handler
+sh = dom:get_subset_handler()
+my_assert(sh:num_subsets() == 2, "Domain must have 2 Subsets for this problem.")
+sh:set_subset_name("Inner", 0)
+sh:set_subset_name("DirichletBoundary", 1)
+--sh:set_subset_name("NeumannBoundary", 2)
+
+-- write grid to file for test purpose
+-- SaveDomain(dom, "refined_grid.ugx")
+
+-- create Approximation Space
+approxSpace = util.CreateApproximationSpace(dom)
+approxSpace:add_fct("c", "Lagrange", 1)
+approxSpace:init()
+
+-------------------------------------------
+--  Setup User Functions
+-------------------------------------------
+diffusionMatrix = util.CreateLuaUserMatrix("ourDiffTensor"..dim.."d", dim)		
+
+-- diffusionMatrix = util.CreateConstDiagUserMatrix(1.0, dim)
+
+-- Velocity Field setup
+velocityField = util.CreateLuaUserVector("ourVelocityField"..dim.."d", dim)
+reaction = util.CreateLuaUserNumber("ourReaction"..dim.."d", dim)
+rhs = util.CreateLuaUserNumber("ourRhs"..dim.."d", dim)
+neumann = util.CreateLuaBoundaryNumber("ourNeumannBnd"..dim.."d", dim)
+dirichlet = util.CreateLuaBoundaryNumber("ourDirichletBnd"..dim.."d", dim)
+
+-----------------------------------------------------------------
+--  Setup FV Convection-Diffusion Element Discretization
+-----------------------------------------------------------------
+if dim == 2 then
+upwind = WeightedUpwind2d(); 
+else
+upwind = WeightedUpwind3d();
+end
+upwind:set_weight(0.0)
+elemDisc = util.CreateFV1ConvDiff(approxSpace, "c", "Inner")
+my_assert(elemDisc:set_upwind(upwind), "could not set upwind")
+elemDisc:set_diffusion_tensor(diffusionMatrix)
+elemDisc:set_velocity_field(velocityField)
+elemDisc:set_reaction(reaction)
+elemDisc:set_source(rhs)
+
+-----------------------------------------------------------------
+--  Setup Neumann Boundary
+-----------------------------------------------------------------
+
+--neumannDisc = util.CreateNeumannBoundary(approxSpace, "Inner")
+--neumannDisc:add_boundary_value(neumann, "c", "NeumannBoundary")
+
+-----------------------------------------------------------------
+--  Setup Dirichlet Boundary
+-----------------------------------------------------------------
+
+dirichletBND = util.CreateDirichletBoundary(approxSpace)
+dirichletBND:add_boundary_value(dirichlet, "c", "DirichletBoundary")
+
+-------------------------------------------
+--  Setup Domain Discretization
+-------------------------------------------
+
+domainDisc = DomainDiscretization()
+domainDisc:add_elem_disc(elemDisc)
+--domainDisc:add_elem_disc(neumannDisc)
+domainDisc:add_post_process(dirichletBND)
+
+
+
+-------------------------------------------
+--  Algebra
+-------------------------------------------
+print ("Setting up Algebra Solver")
+
+-- create operator from discretization
+linOp = AssembledLinearOperator()
+linOp:set_discretization(domainDisc)
+linOp:set_dof_distribution(approxSpace:get_surface_dof_distribution())
+
+-- get grid function
+u = approxSpace:create_surface_function()
+b = approxSpace:create_surface_function()
+
+-- set initial value
+u:set_random(-1.0, 1.0)
+
+-- init Operator and set dirichlet values in start iterate
+print ("Assemble Operator and dirichlet ... ")
+linOp:init_op_and_rhs(b)
+linOp:set_dirichlet_values(u)
+print ("done")
+
+-- write matrix for test purpose
+if bOutput then
+SaveMatrixForConnectionViewer(u, linOp, "Stiffness.mat")
+SaveVectorForConnectionViewer(b, "Rhs.mat")
+end
+
+-- create algebraic Preconditioners
+
+print ("create preconditioners... ")
+jac = Jacobi()
+jac:set_damp(0.8)
+gs = GaussSeidel()
+sgs = SymmetricGaussSeidel()
+bgs = BackwardGaussSeidel()
+ilu = ILU()
+ilut = ILUT()
+
+
+-- create Base Solver
+baseConvCheck = StandardConvergenceCheck()
+baseConvCheck:set_maximum_steps(500)
+baseConvCheck:set_minimum_defect(1e-16)
+baseConvCheck:set_reduction(1e-16)
+baseConvCheck:set_verbose_level(false)
+
+if false then
+	base = LinearSolver()
+	base:set_convergence_check(baseConvCheck)
+	base:set_preconditioner(jac)
+else
+	base = LU()
+end
+
+-- create AMG ---
+-----------------
+
+bUseFAMG = 1
+if bUseFAMG == 1 then
+	print ("create FAMG... ")
+	-- Testvectors for FAMG ---
+	--------------------------	
+	function CreateAMGTestvector(gridfunction, luaCallbackName, dim)
+		local amgTestvector;
+		if dim == 1 then
+			amgTestvector = GridFunctionVectorWriter1d()
+		elseif dim == 2 then
+			amgTestvector = GridFunctionVectorWriter2d()
+		elseif dim == 3 then
+			amgTestvector = GridFunctionVectorWriter3d()
+		end
+		amgTestvector:set_reference_grid_function(gridfunction)
+		amgTestvector:set_user_data(util.CreateLuaUserNumber(luaCallbackName, dim))
+		return amgTestvector	
+	end
+		
+	function CreateAMGTestvectorDirichlet0(dirichletBND, approxSpace)
+		local amgDirichlet0 = GridFunctionVectorWriterDirichlet02d()
+		amgDirichlet0:init(dirichletBND, approxSpace)
+		return amgDirichlet0
+	end
+	
+	amg = FAMGPreconditioner()	
+	amg:set_delta(0.5)
+	amg:set_theta(0.95)
+	amg:set_aggressive_coarsening(false)
+	amg:set_damping_for_smoother_in_interpolation_calculation(0.8)	
+		
+	-- add testvector which is 1 everywhere and only 0 on the dirichlet Boundary.
+	testvectorwriter = CreateAMGTestvectorDirichlet0(dirichletBND, approxSpace)
+	testvector = approxSpace:create_surface_function()
+	testvectorwriter:update(testvector)	
+	amg:add_vector_writer(testvectorwriter, 1.0)
+	amg:set_testvector_damps(1)
+	amg:set_damping_for_smoother_in_interpolation_calculation(0.8)
+		
+	-- amg:set_debug_level_get_ratings(4)
+	-- amg:set_debug_level_coloring(4)
+	-- amg:set_debug_level_communicate_prolongation(4)
+	-- amg:set_debug_level_overlap(4,4)
+	-- amg:set_debug_level_precalculate_coarsening(4)
+	-- amg:set_debug_level_calculate_parent_pairs(4)
+else
+	print ("create AMG... ")
+	amg = RSAMGPreconditioner()
+	amg:enable_aggressive_coarsening_A(2)
+end
+
+
+vectorWriter = GridFunctionPositionProvider2d()
+vectorWriter:set_reference_grid_function(u)
+amg:set_position_provider2d(vectorWriter)
+if bOutput then
+amg:set_matrix_write_path("/Users/mrupp/matrices/")
+end
+
+amg:set_num_presmooth(2)
+amg:set_num_postsmooth(2)
+amg:set_cycle_type(1)
+amg:set_presmoother(jac)
+amg:set_postsmoother(jac)
+amg:set_base_solver(base)
+amg:set_max_levels(20)
+
+-- amg:set_min_nodes_on_one_processor(200) not functional
+amg:set_max_nodes_for_base(maxBase)
+amg:set_max_fill_before_base(0.7)
+amg:set_fsmoothing(true)
+amg:set_epsilon_truncation(0)
+amg:tostring()
+
+
+-- create Convergence Check
+convCheck = StandardConvergenceCheck()
+convCheck:set_maximum_steps(9)
+convCheck:set_minimum_defect(1e-11)
+convCheck:set_reduction(1e-12)
+
+print("done.")
+-- create Linear Solver
+linSolver = LinearSolver()
+linSolver:set_preconditioner(amg)
+linSolver:set_convergence_check(convCheck)
+
+-- Apply Solver
+
+b2 = approxSpace:create_surface_function()
+b2:assign(b)
+
+-------------------------------------------
+--  Apply Solver
+-------------------------------------------
+-- 1. init operator
+print("Init operator (i.e. assemble matrix).")
+tBefore = os.clock()
+linOp:init()
+tAssemble = os.clock()-tBefore
+
+-- 2. init solver for linear Operator
+print("Init solver for operator.")
+linSolver:init(linOp)
+
+-- 3. apply solver
+print("Apply solver.")
+tBefore = os.clock()
+linSolver:apply_return_defect(u,b)
+tSolve = os.clock()-tBefore
+WriteGridFunctionToVTK(u, "Solution")
+
+print("done")
+
+printf = function(s,...)
+	print(s:format(...))
+end -- function
+
+
+function fsize (file)
+	local current = file:seek()      -- get current position
+    local size = file:seek("end")    -- get file size
+    file:seek("set", current)        -- restore position
+    return size
+end
+
+if bFileOutput and GetProcessRank() == 0 then
+	output = io.open("output_"..os.date("y%Ym%md%d")..".txt", "a")
+	if fsize(output) == 0 then 
+		output:write("procs")
+		output:write("\tnumRefs")
+		output:write("\tndofs")
+		output:write("\tsteps")
+		output:write("\tlastReduction")
+		output:write("\ttSetupAmg [ms]")
+		output:write("\tc_A")
+		output:write("\tc_G")
+		output:write("\tused Levels")
+		output:write("\ttSolve [s]")
+		output:write("\ttGrid [s]")
+		output:write("\ttAssemble [s]")
+		output:write("\n")
+	end
+	output:write(GetNumProcesses())
+	output:write("\t"..numRefs)
+	output:write("\t"..amg:get_level_information(0):get_nr_of_nodes())
+	output:write("\t"..convCheck:step())
+	output:write("\t"..convCheck:defect()/convCheck:previous_defect())
+	output:write("\t"..amg:get_timing_whole_setup_ms())
+	output:write("\t"..amg:get_operator_complexity())
+	output:write("\t"..amg:get_grid_complexity())
+	output:write("\t"..amg:get_used_levels())
+	output:write("\t"..tSolve)
+	output:write("\t"..tGrid)
+	output:write("\t"..tAssemble)
+	output:write("\n")
+	print(s)
+	-- else
+end
+
+
+print("NumProcesses: "..GetNumProcesses())
+print("numRefs: "..numRefs)
+print("nr of nodes: "..amg:get_level_information(0):get_nr_of_nodes())
+print("steps: "..convCheck:step())
+print("last reduction: "..convCheck:defect()/convCheck:previous_defect())
+print("tSetupAmg [ms]: "..amg:get_timing_whole_setup_ms())
+print("c_A: "..amg:get_operator_complexity())
+print("c_G: "..amg:get_grid_complexity())
+print("used Levels: "..amg:get_used_levels())
+print("tSolve [s]: "..tSolve)
+print("tGrid [s]: "..tGrid)
+print("tAssemble [s]: "..tAssemble)
+
---
+++ scripts/amg/laplaceamg.lua
@@ -0,0 +1,258 @@
+----------------------------------------------------------
+--
+--   Lua - Script to perform the Laplace-Problem
+--
+--   Author: Andreas Vogel / Martin Rupp
+--
+----------------------------------------------------------
+
+-- make sure that ug_util is in the right path.
+-- currently only the path in which you start your application is valid.
+dofile("../scripts/ug_util.lua")
+
+-- constants
+dim = 2
+gridName = "unit_square_tri_neumann.ugx"
+numRefs = 4
+
+-- name function (will be removed in the future, do not use them)
+_INNER_ = 0
+_BND_ = 1
+_NEUMANN_BND_ = 2
+
+--------------------------------
+-- User Data Functions (begin)
+--------------------------------
+
+function ourDiffTensor(x, y, t)
+return	1, 0, 
+0, 1
+end
+
+function ourVelocityField(x, y, t)
+return	0, 0
+end
+
+function ourReaction(x, y, t)
+return	0
+end
+
+function ourRhs(x, y, t)
+--local s = 2*math.pi
+--return	s*s*(math.sin(s*x) + math.sin(s*y))
+return -2*y
+end
+
+function ourNeumannBnd(x, y, t)
+--local s = 2*math.pi
+--return -s*math.cos(s*x)
+return true, -2*x*y
+end
+
+function ourDirichletBnd(x, y, t)
+--local s = 2*math.pi
+--return true, math.sin(s*x) + math.sin(s*y)
+return true, x*x*y
+end
+
+--------------------------------
+-- User Data Functions (end)
+--------------------------------
+
+-- create Instance of a Domain
+print("Create Domain.")
+dom = util.CreateDomain(dim)
+
+-- load domain
+print("Load Domain from File.")
+if util.LoadDomain(dom, gridName) == false then
+print("Loading Domain failed.")
+exit()
+end
+
+-- get subset handler
+sh = dom:get_subset_handler()
+if sh:num_subsets() ~= 3 then 
+print("Domain must have 3 Subsets for this problem.")
+exit()
+end
+sh:set_subset_name("Inner", 0)
+sh:set_subset_name("DirichletBoundary", 1)
+sh:set_subset_name("NeumannBoundary", 2)
+
+-- create Refiner
+print("Create Refiner")
+refiner = GlobalMultiGridRefiner()
+refiner:assign_grid(dom:get_grid())
+for i=1,numRefs do
+refiner:refine()
+end
+
+-- write grid to file for test purpose
+SaveDomain(dom, "refined_grid.ugx")
+
+-- create Approximation Space
+print("Create ApproximationSpace")
+approxSpace = util.CreateApproximationSpace(dom)
+approxSpace:add_fct("c", "Lagrange", 1)
+approxSpace:init()
+
+-------------------------------------------
+--  Setup User Functions
+-------------------------------------------
+
+-- Diffusion Tensor setup
+diffusionMatrix = util.CreateLuaUserMatrix("ourDiffTensor", dim)
+--diffusionMatrix = util.CreateConstDiagUserMatrix(1.0, dim)
+
+-- Velocity Field setup
+velocityField = util.CreateLuaUserVector("ourVelocityField", dim)
+--velocityField = util.CreateConstUserVector(0.0, dim)
+
+-- Reaction setup
+reaction = util.CreateLuaUserNumber("ourReaction", dim)
+--reaction = util.CreateConstUserNumber(0.0, dim)
+
+-- rhs setup
+rhs = util.CreateLuaUserNumber("ourRhs", dim)
+--rhs = util.CreateConstUserNumber(0.0, dim)
+
+-- neumann setup
+neumann = util.CreateLuaBoundaryNumber("ourNeumannBnd", dim)
+--neumann = util.CreateConstUserNumber(0.0, dim)
+
+-- dirichlet setup
+dirichlet = util.CreateLuaBoundaryNumber("ourDirichletBnd", dim)
+--dirichlet = util.CreateConstBoundaryNumber(0.0, dim)
+
+-----------------------------------------------------------------
+--  Setup FV Convection-Diffusion Element Discretization
+-----------------------------------------------------------------
+
+elemDisc = util.CreateFV1ConvDiffElemDisc(approxSpace, "c", "Inner")
+elemDisc:set_upwind_amount(0.0)
+elemDisc:set_diffusion_tensor(diffusionMatrix)
+elemDisc:set_velocity_field(velocityField)
+elemDisc:set_reaction(reaction)
+elemDisc:set_source(rhs)
+
+-----------------------------------------------------------------
+--  Setup Neumann Boundary
+-----------------------------------------------------------------
+
+neumannDisc = util.CreateNeumannBoundary(approxSpace, "Inner")
+neumannDisc:add_boundary_value(neumann, "c", "NeumannBoundary")
+
+-----------------------------------------------------------------
+--  Setup Dirichlet Boundary
+-----------------------------------------------------------------
+
+dirichletBND = util.CreateDirichletBoundary(approxSpace)
+dirichletBND:add_boundary_value(dirichlet, "c", "DirichletBoundary")
+
+-------------------------------------------
+--  Setup Domain Discretization
+-------------------------------------------
+
+domainDisc = DomainDiscretization()
+domainDisc:add_elem_disc(elemDisc)
+domainDisc:add_elem_disc(neumannDisc)
+domainDisc:add_post_process(dirichletBND)
+
+-------------------------------------------
+--  Algebra
+-------------------------------------------
+
+-- create operator from discretization
+linOp = AssembledLinearOperator()
+linOp:set_discretization(domainDisc)
+linOp:set_dof_distribution(approxSpace:get_surface_dof_distribution())
+
+-- get grid function
+u = approxSpace:create_surface_function()
+b = approxSpace:create_surface_function()
+
+-- set initial value
+u:set(1.0)
+
+-- init Operator
+linOp:init_op_and_rhs(b)
+
+-- set dirichlet values in start iterate
+linOp:set_dirichlet_values(u)
+
+-- write matrix for test purpose
+SaveMatrixForConnectionViewer(u, linOp, "Stiffness.mat")
+SaveVectorForConnectionViewer(b, "Rhs.mat")
+
+-- create algebraic Preconditioner
+jac = JacobiPreconditioner()
+jac:set_damp(0.8)
+gs = GSPreconditioner()
+sgs = SGSPreconditioner()
+bgs = BGSPreconditioner()
+ilu = ILUPreconditioner()
+ilut = ILUTPreconditioner()
+
+-- create GMG
+baseConvCheck = StandardConvergenceCheck()
+baseConvCheck:set_maximum_steps(500)
+baseConvCheck:set_minimum_defect(1e-8)
+baseConvCheck:set_reduction(1e-30)
+baseConvCheck:set_verbose_level(false)
+
+-- base = LapackLUSolver()
+base = LinearSolver()
+base:set_convergence_check(baseConvCheck)
+base:set_preconditioner(jac)
+
+transfer = P1ProlongationOperator2d()
+transfer:set_approximation_space(approxSpace)
+transfer:set_dirichlet_post_process(dirichletBND)
+
+projection = P1ProjectionOperator2d()
+projection:set_approximation_space(approxSpace)
+
+gmg = util.CreateGeometricMultiGridPreconditioner(approxSpace)
+gmg:set_discretization(domainDisc)
+gmg:set_surface_level(numRefs)
+gmg:set_base_level(0)
+gmg:set_base_solver(base)
+gmg:set_smoother(jac)
+gmg:set_cycle_type(1)
+gmg:set_num_presmooth(3)
+gmg:set_num_postsmooth(3)
+gmg:set_prolongation(transfer)
+gmg:set_projection(projection)
+
+amg = AMGPreconditioner()
+amg:set_nu1(2)
+amg:set_nu2(2)
+amg:set_gamma(1)
+amg:set_presmoother(jac)
+amg:set_postsmoother(jac)
+amg:set_base_solver(base)
+amg:set_max_levels(5)
+-- amg:set_debug(u)
+
+-- create Convergence Check
+convCheck = StandardConvergenceCheck()
+convCheck:set_maximum_steps(3)
+convCheck:set_minimum_defect(1e-9)
+convCheck:set_reduction(1e-9)
+
+-- create Linear Solver
+linSolver = LinearSolver()
+linSolver:set_preconditioner(amg)
+linSolver:set_convergence_check(convCheck)
+
+-- create CG Solver
+cgSolver = CGSolver()
+cgSolver:set_preconditioner(ilu)
+cgSolver:set_convergence_check(convCheck)
+
+-- Apply Solver
+ApplyLinearSolver(linOp, u, b, linSolver)
+
+-- Output
+WriteGridFunctionToVTK(u, "Solution")
