42fc98c52d1f938defc645291d961d8a142c0e3c
Martin Rupp <martin.rupp@gcsc.uni-frankfurt.de>
2011-07-20 07:20:49 +0000

    fixed UG_PARALLEL np = 1 case

--- ugbase/lib_algebra/operator/preconditioner/amg/amg_base_impl.h
+++ ugbase/lib_algebra/operator/preconditioner/amg/amg_base_impl.h
@@ -446,7 +446,10 @@ void amg_base<TAlgebra>::create_direct_solver(size_t level)
 			m_emptyPC = pcl::ProcessCommunicator(m_A[level]->get_process_communicator()).create_sub_communicator(false);
 	}
 	else
+	{
 		m_emptyPC = pcl::ProcessCommunicator(pcl::PCD_WORLD);
+		m_basesolver->init(*m_A[level]);
+	}
 #else
 	m_basesolver->init(*m_A[level]);
 #endif
