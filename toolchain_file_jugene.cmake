2f4af14cec301ebbe4b160ffdf24cc558a25aaee
Sebastian Reiter <sebastian.reiter@gcsc.uni-frankfurt.de>
2011-09-26 09:51:43 +0000

    added toolchain_file_jugene.cmake

---
+++ toolchain_file_jugene.cmake
@@ -0,0 +1,16 @@
+# Based on the scripts from Ingo Heppner.
+
+SET(CMAKE_SYSTEM_NAME BlueGeneP)
+SET(CMAKE_SYSTEM_VERSION 1)
+
+SET(CMAKE_C_COMPILER       /bgsys/drivers/ppcfloor/gnu-linux/bin/powerpc-bgp-linux-gcc)
+SET(CMAKE_CXX_COMPILER     /bgsys/drivers/ppcfloor/gnu-linux/bin/powerpc-bgp-linux-g++)
+SET(CMAKE_Fortran_COMPILER /bgsys/drivers/ppcfloor/gnu-linux/bin/powerpc-bgp-linux-gfortran)
+
+include(Platform/BlueGeneP-base)
+
+__BlueGeneP_set_dynamic_flags(GNU CXX)
+
+set_property(GLOBAL PROPERTY TARGET_SUPPORTS_SHARED_LIBS TRUE)
+set(CMAKE_FIND_LIBRARY_PREFIXES "lib")
+set(CMAKE_FIND_LIBRARY_SUFFIXES ".so" ".a")
