1425c086aec544376146608e006cc3e39caf4830
Sebastian Reiter <sebastian.reiter@gcsc.uni-frankfurt.de>
2011-09-29 07:56:51 +0000

    Lapack and Blas are now disabled by default, since errors with the fortran compiler, which is required by cmake to find the lapack and blas libraries, occur on a variety of platforms.

--- toolchain_file_jugene.cmake
+++ toolchain_file_jugene.cmake
@@ -3,13 +3,13 @@
 # This option tells ug_cmake_includes.txt to add the -dynamic option to the compiler flags.
 SET(enableDynamicOption ON)
 
-#SET(CMAKE_C_COMPILER       /bgsys/drivers/ppcfloor/gnu-linux/bin/powerpc-bgp-linux-gcc)
-#SET(CMAKE_CXX_COMPILER     /bgsys/drivers/ppcfloor/gnu-linux/bin/powerpc-bgp-linux-g++)
-#SET(CMAKE_Fortran_COMPILER /bgsys/drivers/ppcfloor/gnu-linux/bin/powerpc-bgp-linux-gfortran)
+SET(CMAKE_C_COMPILER       /bgsys/drivers/ppcfloor/gnu-linux/bin/powerpc-bgp-linux-gcc)
+SET(CMAKE_CXX_COMPILER     /bgsys/drivers/ppcfloor/gnu-linux/bin/powerpc-bgp-linux-g++)
+SET(CMAKE_Fortran_COMPILER /bgsys/drivers/ppcfloor/gnu-linux/bin/powerpc-bgp-linux-gfortran)
 
-#include(Platform/BlueGeneP-base)
+include(Platform/BlueGeneP-base)
 
-#__BlueGeneP_set_dynamic_flags(GNU CXX)
+__BlueGeneP_set_dynamic_flags(GNU CXX)
 
 set_property(GLOBAL PROPERTY TARGET_SUPPORTS_SHARED_LIBS TRUE)
 set(CMAKE_FIND_LIBRARY_PREFIXES "lib")
