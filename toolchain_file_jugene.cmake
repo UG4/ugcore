56ea4ba7e7e5b4ec72e352174969c65b448b14aa
Sebastian Reiter <sebastian.reiter@gcsc.uni-frankfurt.de>
2011-09-29 07:42:58 +0000

    removed -dynamic option from CXX_FLAGS. It now is only enabled, if the toolchain_file_jugene is used for cmake.

--- toolchain_file_jugene.cmake
+++ toolchain_file_jugene.cmake
@@ -1,16 +1,17 @@
 # Based on the scripts from Ingo Heppner.
 
-#SET(CMAKE_SYSTEM_NAME BlueGeneP)
-#SET(CMAKE_SYSTEM_VERSION 1)
+# This option tells ug_cmake_includes.txt to add the -dynamic option to the compiler flags.
+SET(enableDynamicOption ON)
 
-SET(CMAKE_C_COMPILER       /bgsys/drivers/ppcfloor/gnu-linux/bin/powerpc-bgp-linux-gcc)
-SET(CMAKE_CXX_COMPILER     /bgsys/drivers/ppcfloor/gnu-linux/bin/powerpc-bgp-linux-g++)
-SET(CMAKE_Fortran_COMPILER /bgsys/drivers/ppcfloor/gnu-linux/bin/powerpc-bgp-linux-gfortran)
+#SET(CMAKE_C_COMPILER       /bgsys/drivers/ppcfloor/gnu-linux/bin/powerpc-bgp-linux-gcc)
+#SET(CMAKE_CXX_COMPILER     /bgsys/drivers/ppcfloor/gnu-linux/bin/powerpc-bgp-linux-g++)
+#SET(CMAKE_Fortran_COMPILER /bgsys/drivers/ppcfloor/gnu-linux/bin/powerpc-bgp-linux-gfortran)
 
-include(Platform/BlueGeneP-base)
+#include(Platform/BlueGeneP-base)
 
-__BlueGeneP_set_dynamic_flags(GNU CXX)
+#__BlueGeneP_set_dynamic_flags(GNU CXX)
 
 set_property(GLOBAL PROPERTY TARGET_SUPPORTS_SHARED_LIBS TRUE)
 set(CMAKE_FIND_LIBRARY_PREFIXES "lib")
 set(CMAKE_FIND_LIBRARY_SUFFIXES ".so" ".a")
+
