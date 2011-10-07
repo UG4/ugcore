6749358f2a2b8b8553e34f41fab613752323e8a8
Sebastian Reiter <sebastian.reiter@gcsc.uni-frankfurt.de>
2011-10-07 10:20:01 +0000

    fixing script smooth_hierarchies.lua. Not yet complete.

--- scripts/smooth_hierarchies.lua
+++ scripts/smooth_hierarchies.lua
@@ -20,175 +20,189 @@ dim = 2
 InitUG(dim, AlgebraType("CPU", 1));
 
 gridName = "volgrid_b.ugx"
+requiredSubsets = {"Inner", "Boundary"}
 numRefs = 4
 useLoopScheme = true
 
--- name function
-_C_ = 0
-
--- name subsets
-_INNER_ = 0
-_BND_ = 1
 
 -- user functions
-function ourDiffTensor(x, y, t)
-return	1, 0, 
-0, 1
+function DiffTensor(x, y, t)
+	return	1, 0, 
+			0, 1
 end
 
-function ourVelocityField(x, y, t)
-return	0, 0
+function VelocityField(x, y, t)
+	return	0, 0
 end
 
-function ourReaction(x, y, t)
-return	0
+function Reaction(x, y, t)
+	return	0
 end
 
-function ourRhs(x, y, t)
-s = 2*math.pi
-return	s*s*(math.sin(s*x) + math.sin(s*y))
+function Rhs(x, y, t)
+	s = 2*math.pi
+	return	s*s*(math.sin(s*x) + math.sin(s*y))
 end
 
-	
+function DirichletBnd(x, y, t)
+	s = 2*math.pi
+	return	math.sin(s*x) + math.sin(s*y)
+end
+
+
+-- Diffusion Tensor setup
+diffusionMatrix = LuaUserMatrix("DiffTensor")
+-- Velocity Field setup
+velocityField = LuaUserVector("VelocityField")
+-- Reaction setup
+reaction = LuaUserNumber("Reaction")
+-- rhs setup
+rhs = LuaUserNumber("Rhs")
+-- dirichlet
+dirichlet = LuaBoundaryNumber("DirichletBnd")
+
+--------------------------------------------------------------------------------
+-- Domain preparation
+--------------------------------------------------------------------------------
 -- create Instance of a Domain
 print("Create Domain.")
 dom = Domain()
 
 -- load domain
 print("Load Domain from File.")
-if LoadDomain(dom, gridName) == true then
-	-- create Refiner
-	print("Create Hierarchy")
-	if useLoopScheme == true then
-		CreateSmoothHierarchy(dom:get_grid(), numRefs)
-	else
-		CreateSemiSmoothHierarchy(dom:get_grid(), numRefs)
-	end
-	
-	print("Saving domain for debug reasons")
-	-- write grid to file for test purpose
-	SaveDomain(dom, "refined_grid.ugx")
-	SaveGridHierarchy(dom:get_grid(), "refined_grid_hierarchy.ugx")
+if LoadDomain(dom, gridName) == false then
+	print("LoadDomain failed. Make sure the specified file exists!")
+	exit()
+end
 
-	print("creating approximation space...")
-	-- create Approximation Space
-	approxSpace = ApproximationSpace(dom)
-	approxSpace:add_fct("c", "Lagrange", 1)
-	approxSpace:init()
+-- create Refiner
+print("Create Hierarchy")
+if useLoopScheme == true then
+	CreateSmoothHierarchy(dom:get_grid(), numRefs)
+else
+	CreateSemiSmoothHierarchy(dom:get_grid(), numRefs)
+end
 
-	-- name subsets
-	sh = dom:get_subset_handler()
-	if sh:num_subsets() ~= 2 then 
-	   print("Domain must have 2 Subsets for this problem.")
-	   exit()
-	end
-	sh:set_subset_name("Inner", 0)
-	sh:set_subset_name("Boundary", 1)
+print("Saving domain for debug reasons")
+-- write grid to file for test purpose
+SaveDomain(dom, "refined_grid.ugx")
+SaveGridHierarchy(dom:get_grid(), "refined_grid_hierarchy.ugx")
 
-	-- create Discretization
-	domainDisc = DomainDiscretization()
+
+--------------------------------------------------------------------------------
+-- Discretization
+--------------------------------------------------------------------------------
+print("creating approximation space...")
+-- create Approximation Space
+approxSpace = ApproximationSpace(dom)
+approxSpace:add_fct("c", "Lagrange", 1)
+approxSpace:init()
+
+-- make sure that the required subsets exist	
+if util.CheckSubsets(dom, requiredSubsets) == false then 
+   print("Domain must have the following Subsets for this problem.")
+   exit()
+end
+
+-- create Discretization
+print("creating element discretization...")
+upwind = WeightedUpwind(); upwind:set_weight(0.0)
+elemDisc = util.CreateFV1ConvDiff(approxSpace, "c", "Inner")
+if elemDisc:set_upwind(upwind) == false then exit() end
+elemDisc:set_disc_scheme("fv1")
+elemDisc:set_diffusion_tensor(diffusionMatrix)
+elemDisc:set_velocity_field(velocityField)
+elemDisc:set_reaction(reaction)
+elemDisc:set_source(rhs)
+
+print("creating dirichlet boundary...")
+-- create dirichlet boundary and add it to discretization
+dirichletBND = util.CreateDirichletBoundary(approxSpace)
+dirichletBND:add(dirichlet, "c", "Boundary")
+
+print("creating domain discretization...")
+-- The domain discretization
+domainDisc = DomainDiscretization()
 domainDisc:set_approximation_space(approxSpace)
+domainDisc:add(elemDisc)
+domainDisc:add(dirichletBND)
 
-	print("creating dirichlet boundary...")
-	-- create dirichlet boundary and add it to discretization
-	dirichletBND = util.CreateDirichletBnd2d(dom, SinusDirichletBoundaryFunction2d(), _C_)
-	domainDisc:add_dirichlet_bnd(dirichletBND, _BND_)
 
-	------------------------------------------
-	--  Setting up user functions
-	-------------------------------------------
-	
-	-- Diffusion Tensor setup
-		diffusionMatrix = LuaUserMatrix("ourDiffTensor")
-	-- Velocity Field setup
-		velocityField = LuaUserVector("ourVelocityField")
-	-- Reaction setup
-		reaction = LuaUserNumber("ourReaction")
-	-- rhs setup
-		rhs = LuaUserNumber("ourRhs")
+print("creating linear operator...")	
+-- create operator from discretization
+linOp = AssembledLinearOperator()
+linOp:set_discretization(domainDisc)
+linOp:set_dof_distribution(approxSpace:get_surface_dof_distribution())
 
-	print("creating element discretization...")
-	-- create Finite-Volume Element Discretization for Convection Diffusion Equation
-	elemDisc = util.CreateFV1ConvDiffElemDisc(dom, diffusionMatrix, velocityField, reaction, rhs, 0)
-	
-	-- add Element Discretization to discretization
-	domainDisc:add(elemDisc, pattern, "c", "Inner")
+-- get grid function
+u = approxSpace:create_surface_function()
+b = approxSpace:create_surface_function()
 
-	print("creating linear operator...")	
-	-- create operator from discretization
-	linOp = AssembledLinearOperator()
-	linOp:set_discretization(domainDisc)
-	linOp:set_dof_distribution(approxSpace:get_surface_dof_distribution())
+-- set initial value
+u:set(1.0)
 
-	-- get grid function
-	u = approxSpace:create_surface_function()
-	b = approxSpace:create_surface_function()
+-- init Operator
+linOp:init_op_and_rhs(b)
 
-	-- set initial value
-	u:set(1.0)
+-- write matrix for test purpose
+SaveMatrixForConnectionViewer(u, linOp, "Stiffness.mat")
 
-	-- init Operator
-	linOp:init_op_and_rhs(b)
+-- set dirichlet values in start iterate
+linOp:set_dirichlet_values(u)
 
-	-- write matrix for test purpose
-	SaveMatrixForConnectionViewer(u, linOp, "Stiffness.mat")
 
-	-- set dirichlet values in start iterate
-	linOp:set_dirichlet_values(u)
+--------------------------------------------------------------------------------
+-- Solvers
+--------------------------------------------------------------------------------
+-- create algebraic Preconditioner
+jac = JacobiPreconditioner()
+jac:set_damp(0.8)
+gs = GSPreconditioner()
+sgs = SGSPreconditioner()
+bgs = BGSPreconditioner()
+ilu = ILUPreconditioner()
+ilut = ILUTPreconditioner()
 
-	-- create algebraic Preconditioner
-	jac = JacobiPreconditioner()
-	jac:set_damp(0.8)
-	gs = GSPreconditioner()
-	sgs = SGSPreconditioner()
-	bgs = BGSPreconditioner()
-	ilu = ILUPreconditioner()
-	ilut = ILUTPreconditioner()
+-- create GMG
+baseConvCheck = StandardConvergenceCheck()
+baseConvCheck:set_maximum_steps(500)
+baseConvCheck:set_minimum_defect(1e-8)
+baseConvCheck:set_reduction(1e-30)
+baseConvCheck:set_verbose_level(false)
 
-	-- create GMG
-	baseConvCheck = StandardConvergenceCheck()
-	baseConvCheck:set_maximum_steps(500)
-	baseConvCheck:set_minimum_defect(1e-8)
-	baseConvCheck:set_reduction(1e-30)
-	baseConvCheck:set_verbose_level(false)
+-- base = LU()
+base = LinearSolver()
+base:set_convergence_check(baseConvCheck)
+base:set_preconditioner(jac)
 
-	-- base = LU()
-	base = LinearSolver()
-	base:set_convergence_check(baseConvCheck)
-	base:set_preconditioner(jac)
+gmg = GeometricMultiGrid(approxSpace)
+gmg:set_discretization(domainDisc)
+gmg:set_surface_level(numRefs)
+gmg:set_base_level(0)
+gmg:set_base_solver(base)
+gmg:set_smoother(jac)
+gmg:set_cycle_type(1)
+gmg:set_num_presmooth(3)
+gmg:set_num_postsmooth(3)
 
-	gmg = GeometricMultiGrid(approxSpace)
-	gmg:set_discretization(domainDisc)
-	gmg:set_surface_level(numRefs)
-	gmg:set_base_level(0)
-	gmg:set_base_solver(base)
-	gmg:set_smoother(jac)
-	gmg:set_cycle_type(1)
-	gmg:set_num_presmooth(3)
-	gmg:set_num_postsmooth(3)
+-- create Convergence Check
+convCheck = StandardConvergenceCheck()
+convCheck:set_maximum_steps(1000)
+convCheck:set_minimum_defect(1e-12)
+convCheck:set_reduction(1e-12)
 
-	-- create Convergence Check
-	convCheck = StandardConvergenceCheck()
-	convCheck:set_maximum_steps(1000)
-	convCheck:set_minimum_defect(1e-12)
-	convCheck:set_reduction(1e-12)
+-- create Linear Solver
+linSolver = LinearSolver()
+linSolver:set_preconditioner(gmg)
+linSolver:set_convergence_check(convCheck)
 
-	-- create Linear Solver
-	linSolver = LinearSolver()
-	linSolver:set_preconditioner(gmg)
-	linSolver:set_convergence_check(convCheck)
+-- create CG Solver
+cgSolver = CGSolver()
+cgSolver:set_preconditioner(ilu)
+cgSolver:set_convergence_check(convCheck)
 
-	-- create CG Solver
-	cgSolver = CGSolver()
-	cgSolver:set_preconditioner(ilu)
-	cgSolver:set_convergence_check(convCheck)
+-- Apply Solver
+ApplyLinearSolver(linOp, u, b, linSolver)
 
-	-- Apply Solver
-	ApplyLinearSolver(linOp, u, b, linSolver)
-
-	-- Output
-	WriteGridFunctionToVTK(u, "Solution")
-
-else	-- LoadDomain
-	print("Load failed.")
-end		--LoadDomain
+-- Output
+WriteGridFunctionToVTK(u, "Solution")
