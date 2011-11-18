d05a7946c8611a48ed13de92920395f44b6101fc
Martin Rupp <martin.rupp@gcsc.uni-frankfurt.de>
2011-11-18 10:45:11 +0000

    removed operator_impl from operator.h -> faster compile times

--- ugbase/lib_algebra/operator/operator.h
+++ ugbase/lib_algebra/operator/operator.h
@@ -37,8 +37,4 @@
 // matrix operator methods
 #include "matrix_operator_functions.h"
 
-
-// for compatibilty. remove this later.
-#include "operator_impl.h"
-
 #endif /* __H__LIB_ALGEBRA__OPERATOR__OPERATOR__ */
