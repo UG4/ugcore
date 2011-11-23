6d8acd5a39b7a87640462988c1db9cb82195e678
Martin Scherer <martin.scherer@gcsc.uni-frankfurt.de>
2011-11-23 17:19:52 +0000

    appended side prism to top of tkd

--- ugbase/lib_grid/algorithms/grid_generation/tetrakaidecahedron_generator.cpp
+++ ugbase/lib_grid/algorithms/grid_generation/tetrakaidecahedron_generator.cpp
@@ -62,13 +62,14 @@ void GenerateTetrakaidecahedron(Grid& grid, number& height, number& baseEdgeLeng
 void GenerateTetrakaidecahedron(CoordsArray& posOut, IndexArray& indsOut,
 							  number& height, number& baseEdgeLength, number& diameter)
 {
-	// quantity s, overlap of two aligned tkd's
-	// number overlap = 1 / pow(3, 0.5) * (diameter - 2 * baseEdgeLength);
+	// baseEdgeLength
 	number a = baseEdgeLength;
-	number h = height; // / 3;
-
+	// height of one level of decomposition which is 1/3 of overall height of the tkd
+	number h = height/ 3;
+	// quantity s, overlap of two aligned tkd's
+	number s = 1 / pow(3, 0.5) * (diameter - 2 * baseEdgeLength);
 	// height of base triangle of top inner prism
-	number h_Ap = sqrt(3) * a / 2;
+	number hAp = sqrt(3) * a / 2;
 
 
 	// create G(Ki -> ObenInnen) = 6 prism with equilateral sites
@@ -82,23 +83,56 @@ void GenerateTetrakaidecahedron(CoordsArray& posOut, IndexArray& indsOut,
         +sin(t)  0   cos(t) +
 	 */
 	for(number t = 0; t < 2*PI; t += PI/3) {
-		number x = (-2*h_Ap * sin(t) + a*cos(t)) /2;
-		number z = (a*sin(t) + 2*h_Ap * cos(t)) /2;
+		number x = (-2*hAp * sin(t) + a*cos(t)) /2;
+		number z = (a*sin(t) + 2*hAp * cos(t)) /2;
 
 		createPrism(vector3(0, 0, 0),				// invariant
 					vector3(a*cos(t), 0, a*sin(t)), // (a, 0, 0)
-					vector3(x, 0, z), 				// (a/2, 0, h_Ap)
+					vector3(x, 0, z), 				// (a/2, 0, hAp)
 					vector3(0, h, 0), 				// invariant
 					vector3(a*cos(t), h, a*sin(t)), // (a, h, 0)
-					vector3(x, h, z), 				// (a/2, h, h_Ap)
+					vector3(x, h, z), 				// (a/2, h, hAp)
 					posOut, indsOut);
 	}
 
 	// TODO create G(Ki -> ObenAussenPr2T)
+	for(number t = 0; t < 2*PI; t += (2./3 * PI)) {
 
-	// TODO create G(Ki -> ObenAussenPr)
+	}
 
-//	createPrism(vector3());
+
+	// create G(Ki -> ObenAussenPr):
+	// create 3 prisms every 120° or 2/3 PI (rad)
+	for(number t = 0; t < 2*PI; t += (2./3 * PI)) {
+		// for vertex 1 and v2
+		number x = (-2*hAp*sin(t) + a*cos(t))/2;
+		number z = (a*sin(t) + 2*hAp*cos(t))/2;
+
+		// for v3
+		number xs = ((-2*s - 2*hAp)*sin(t) + a* cos(t))/2;
+		number zs = (a*sin(t) + (2*s + 2*hAp)*cos(t))/2;
+
+		// for v4: equal to x, z but with different sign
+		number xn = (-2*hAp*sin(t) - a*cos(t))/2;
+		number zn = (-a*sin(t) + 2*hAp*cos(t))/2;
+
+		// for v5:
+		number xng = (-2*hAp*sin(t) - a*cos(t))/2;
+		number zng = (-a*sin(t) + 2*hAp*cos(t))/2;
+
+		// for v6:
+		number xns = ((-2*s - 2*hAp)*sin(t) - a*cos(t))/2;
+		number zns = (-a*sin(t) + (2*s + 2*hAp)*cos(t))/2;
+
+		createPrism(vector3(x, 0, z), // a/2, 0, hAp
+					vector3(x, h, z), // a/2, h, hAp
+					vector3(xs, 0, zs), // a/2, 0, hAp + s
+
+					vector3(xn, 0, zn), // -a/2, 0, hAp
+					vector3(xng, h, zng), // -a/2, h, hAp
+					vector3(xns, 0, zns), // -a/2, 0, hAp + s
+					posOut, indsOut);
+	}
 }
 
 void createPrism(vec3Ref v1, vec3Ref v2, vec3Ref v3,
