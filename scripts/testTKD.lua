f1125a8c4ec7f94e1a30117333678487764858a8
Martin Scherer <martin.scherer@gcsc.uni-frankfurt.de>
2012-02-23 14:20:03 +0000

    added some parameter checking fixed call to CreateTKDDomain (wrong order of input params)

--- scripts/testTKD.lua
+++ scripts/testTKD.lua
@@ -9,13 +9,20 @@ w = util.GetParamNumber("-w", 30)
 h = util.GetParamNumber("-h", math.sqrt(6)*a)
 d_lipid = util.GetParamNumber("-dl", 3)
 
+if not (w > 2*a) then
+	print("Geometric constraint w > 2a not met. Check parameters!")
+	exit()
+end
+
 -- parameter for stacking the tkd's
-rows = util.GetParamNumber("-rows", 3)
-cols = util.GetParamNumber("-cols", 2)
+rows = util.GetParamNumber("-rows", 5)
+cols = util.GetParamNumber("-cols", 4)
 layers = util.GetParamNumber("-layers", 3)
 
 -- file name to write grid to
 filename = util.GetParam("-filename", "/tmp/tkd/tkdDomain.ugx")
+
+-- subset information
 sh_name_corneocyte = util.GetParam("-sh_name_corneo", "corneocytes")
 sh_name_lipid = util.GetParam("-sh_name_lipid", "corneocytes")
 
@@ -24,13 +31,13 @@ go = GridObject()
 grid = go:grid()
 sh = go:subset_handler()
 
--- set subset information
+-- create the grid
+CreateTKDDomain(grid, sh, a, w, h, d_lipid, rows, cols, layers)
+
+-- optionally override subset information
 green = MakeVec(0, 1, 0, 0)
 blue = MakeVec(0, 0, 1, 0)
 SetTKDSubsetInfos(sh, sh_name_corneocyte, sh_name_lipid, blue, green)
-
--- create the grid
-CreateTKDDomain(grid, sh, h, a, w, d_lipid, rows, cols, layers)
 
 if SaveGrid(grid, sh, filename) then
 	print("TKD Domain successfully written to " .. filename)
