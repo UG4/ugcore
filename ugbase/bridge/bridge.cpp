c8b426dc58914381afff4f370be6cc2d5d157c3f
Andreas Vogel <andreas.vogel@gcsc.uni-frankfurt.de>
2012-03-01 14:03:42 +0000

    Adding check if algebra is compiled into binary when requested in InitUG

--- ugbase/bridge/bridge.cpp
+++ ugbase/bridge/bridge.cpp
@@ -39,6 +39,29 @@ void InitUG(int dim, const AlgebraType& algType)
 {
 //	get tag of algebra type
 	std::string algTag = GetAlgebraTag(algType);
+	int blocksize = algType.blocksize();
+	if( (blocksize < 0 || blocksize > 4) && blocksize != AlgebraType::VariableBlockSize)
+		UG_THROW_FATAL("ERROR in InitUG: Only Algebra Blocksizes '1x1', '2x2', '3x3', '4x4' and 'variable' are supported.");
+#ifndef UG_CPU_1
+	if(blocksize == 1)
+		UG_THROW_FATAL("ERROR in InitUG: Requested Algebra Blocksize '1x1' is not compiled into binary.");
+#endif
+#ifndef UG_CPU_2
+	if(blocksize == 2)
+		UG_THROW_FATAL("ERROR in InitUG: Requested Algebra Blocksize '2x2' is not compiled into binary.");
+#endif
+#ifndef UG_CPU_3
+	if(blocksize == 3)
+		UG_THROW_FATAL("ERROR in InitUG: Requested Algebra Blocksize '3x3' is not compiled into binary.");
+#endif
+#ifndef UG_CPU_4
+	if(blocksize == 4)
+		UG_THROW_FATAL("ERROR in InitUG: Requested Algebra Blocksize '4x4' is not compiled into binary.");
+#endif
+#ifndef UG_CPU_VAR
+	if(blocksize == AlgebraType::VariableBlockSize)
+		UG_THROW_FATAL("ERROR in InitUG: Requested Algebra Blocksize 'variable' is not compiled into binary.");
+#endif
 
 //	get dim tag
 	std::string dimTag = GetDomainTag(dim);
