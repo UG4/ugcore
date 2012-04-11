394eb3508c59b0ff3fc2479abae0b33ddad19912
Martin Rupp <martin.rupp@gcsc.uni-frankfurt.de>
2012-04-11 15:03:52 +0000

    moved amg and laplaceev

--- scripts/amg/util.lua
+++
@@ -1,7 +0,0 @@
-function PrintParallelProfileNode(name)
-	pn = GetProfileNode(name)
-	t = pn:get_avg_total_time_ms()/to100 * 100
-	tmin = ParallelMin(t)
-	tmax = ParallelMax(t)
-	printf("%s:\n%.2f %%, min: %.2f %%, max: %.2f %%", name, t, tmin, tmax)
-end
