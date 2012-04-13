db01870493759e997b2c141f346a7c7eb1440e4a
Martin Scherer <martin.scherer@gcsc.uni-frankfurt.de>
2012-04-13 13:24:51 +0000

    fixed const correctness

--- ugbase/common/node_tree/collision_element_info.h
+++ ugbase/common/node_tree/collision_element_info.h
@@ -29,7 +29,7 @@ struct CollisionElementID{
 
 	inline bool is_valid()	{return m_intID != -1;}
 	
-	bool operator == (const CollisionElementID& id)	{return m_ptrID == id.m_ptrID;}
+	bool operator == (const CollisionElementID& id) const {return m_ptrID == id.m_ptrID;}
 
 	union{
 		void* 	m_ptrID;
