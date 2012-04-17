42509aed670c35b0443e8e12a57985389973f955
Martin Rupp <martin.rupp@gcsc.uni-frankfurt.de>
2012-04-17 13:32:21 +0000

    added some doku

--- ugbase/bridge/lib_disc_bridges/algebra_bridge.cpp
+++ ugbase/bridge/lib_disc_bridges/algebra_bridge.cpp
@@ -72,8 +72,10 @@ static void Register__Algebra(Registry& reg, string parentGroup)
 		typedef IAssemble<TAlgebra> T;
 		string name = string("IAssemble").append(algSuffix);
 		reg.add_class_<T>(name, grp)
-			.add_method("assemble_jacobian", static_cast<void (T::*)(matrix_type&, const vector_type&)>(&T::assemble_jacobian))
-			.add_method("assemble_defect", static_cast<void (T::*)(vector_type&, const vector_type&)>(&T::assemble_defect))
+			.add_method("assemble_jacobian", static_cast<void (T::*)(matrix_type&, const vector_type&)>(&T::assemble_jacobian),
+					"", "J#u", "assembles jacobian on surface grid")
+			.add_method("assemble_defect", static_cast<void (T::*)(vector_type&, const vector_type&)>(&T::assemble_defect),
+					"d(u)#iterate_u#DoFdistr", "Assembles Defect at a given Solution u.")
 			.add_method("assemble_linear", static_cast<void (T::*)(matrix_type&, vector_type&)>(&T::assemble_linear))
 			.add_method("adjust_solution", static_cast<void (T::*)(vector_type&)>(&T::adjust_solution));
 		reg.add_class_to_group(name, "IAssemble", algTag);
@@ -99,12 +101,12 @@ static void Register__Algebra(Registry& reg, string parentGroup)
 		typedef ITimeDiscretization<TAlgebra> T;
 		string name = string("ITimeDiscretization").append(algSuffix);
 		reg.add_class_<T,TBase>(name, grp)
-			.add_method("prepare_step", &T::prepare_step)
+			.add_method("prepare_step", &T::prepare_step, "", "prepares the assembling of Defect/Jacobian for a time step")
 			.add_method("prepare_step_elem", static_cast<void (T::*)(SmartPtr<VectorTimeSeries<vector_type> >, number)>(&T::prepare_step_elem))
 			.add_method("finish_step_elem", static_cast<void (T::*)(SmartPtr<VectorTimeSeries<vector_type> >, number)>(&T::finish_step_elem))
-			.add_method("num_stages", &T::num_stages)
-			.add_method("set_stage", &T::set_stage)
-			.add_method("future_time", &T::future_time)
+			.add_method("num_stages", &T::num_stages, "the number of stages")
+			.add_method("set_stage", &T::set_stage, "", "stage")
+			.add_method("future_time", &T::future_time, "the future time point (i.e. the one that will be computed)")
 			.add_method("num_prev_steps", &T::num_prev_steps);
 		reg.add_class_to_group(name, "ITimeDiscretization", algTag);
 	}
@@ -165,21 +167,21 @@ static void Register__Algebra(Registry& reg, string parentGroup)
 		string name = string("NewtonSolver").append(algSuffix);
 		reg.add_class_<T, TBase, TBase2>(name, grp)
 			.add_constructor()
-			.add_method("set_linear_solver", &T::set_linear_solver)
-			.add_method("set_convergence_check", &T::set_convergence_check)
-			.add_method("set_line_search", &T::set_line_search)
-			.add_method("init", &T::init)
-			.add_method("prepare", &T::prepare)
-			.add_method("apply", &T::apply)
+			.add_method("set_linear_solver", &T::set_linear_solver, "", "linSolver")
+			.add_method("set_convergence_check", &T::set_convergence_check, "", "convCheck")
+			.add_method("set_line_search", &T::set_line_search, "", "lineSeach")
+			.add_method("init", &T::init, "success", "op")
+			.add_method("prepare", &T::prepare, "success", "u")
+			.add_method("apply", &T::apply, "success", "u")
 			.add_method("print_average_convergence", &T::print_average_convergence)
 			.add_method("clear_average_convergence", &T::clear_average_convergence)
-			.add_method("num_newton_steps", &T::num_newton_steps)
-			.add_method("num_linsolver_calls", &T::num_linsolver_calls)
-			.add_method("num_linsolver_steps", &T::num_linsolver_steps)
-			.add_method("average_linear_steps", &T::average_linear_steps)
-			.add_method("total_linsolver_calls", &T::total_linsolver_calls)
-			.add_method("total_linsolver_steps", &T::total_linsolver_steps)
-			.add_method("total_average_linear_steps", &T::total_average_linear_steps)
+			.add_method("num_newton_steps", &T::num_newton_steps, "number of newton steps in history")
+			.add_method("num_linsolver_calls", &T::num_linsolver_calls, "number of linsolver calls in iNewtonStep", "iNewtonStep")
+			.add_method("num_linsolver_steps", &T::num_linsolver_steps, "number of linsolver steps in newton step iNewtonStep", "iNewtonStep")
+			.add_method("average_linear_steps", &T::average_linear_steps, "average number of linsolver steps per linsolver call in newton step iNewtonStep", "iNewtonStep")
+			.add_method("total_linsolver_calls", &T::total_linsolver_calls, "total number of linsolver calls", "")
+			.add_method("total_linsolver_steps", &T::total_linsolver_steps, "total number of linsolver steps", "")
+			.add_method("total_average_linear_steps", &T::total_average_linear_steps, "total average number of linsolver steps per linsolver call", "")
 			.set_construct_as_smart_pointer(true);
 		reg.add_class_to_group(name, "NewtonSolver", algTag);
 	}
@@ -193,8 +195,8 @@ static void Register__Algebra(Registry& reg, string parentGroup)
 		reg.add_class_<T, TBase>(name, grp)
 			.add_constructor()
 			.template add_constructor<void (*)(IAssemble<TAlgebra>&)>("Assembling Routine")
-			.add_method("set_discretization", &T::set_discretization)
-			.add_method("set_level", &T::set_level)
+			.add_method("set_discretization", &T::set_discretization, "", "ass")
+			.add_method("set_level", &T::set_level, "", "gridLevel")
 			.add_method("init", &T::init)
 			.set_construct_as_smart_pointer(true);
 		reg.add_class_to_group(name, "AssembledOperator", algTag);
@@ -209,9 +211,12 @@ static void Register__Algebra(Registry& reg, string parentGroup)
 //	some functions
 	{
 		std::string grp = parentGroup; grp.append("/Algebra/Operation");
-		reg.add_function("MatIdentity", &MatIdentity<vector_type, vector_type, matrix_type>, grp);
-		reg.add_function("MatAdd", &MatAdd<vector_type, vector_type, matrix_type>, grp);
-		reg.add_function("MatScale", &MatScale<vector_type, vector_type, matrix_type>, grp);
+		reg.add_function("MatIdentity", &MatIdentity<vector_type, vector_type, matrix_type>, grp,
+				"", "opOut", "sets matrix to identity");
+		reg.add_function("MatAdd", &MatAdd<vector_type, vector_type, matrix_type>, grp,
+				"", "res#alpha1#A1#alpha2#A2", "calculates res = alpha1*A1 + alpha2*A2");
+		reg.add_function("MatScale", &MatScale<vector_type, vector_type, matrix_type>, grp,
+				"", "mat#alpha", "calculates mat = mat*alpha");
 	}
 
 //	ILineSearch
@@ -231,13 +236,13 @@ static void Register__Algebra(Registry& reg, string parentGroup)
 		string name = string("StandardLineSearch").append(algSuffix);
 		reg.add_class_<T, TBase>(name, grp)
 			.add_constructor()
-			.add_method("set_maximum_steps", &T::set_maximum_steps)
-			.add_method("set_lambda_start", &T::set_lambda_start)
-			.add_method("set_reduce_factor", &T::set_reduce_factor)
-			.add_method("set_accept_best", &T::set_accept_best)
-			.add_method("set_maximum_defect", &T::set_maximum_defect)
-			.add_method("set_verbose", &T::set_verbose)
-			.add_method("set_offset", &T::set_offset)
+			.add_method("set_maximum_steps", &T::set_maximum_steps, "", "steps")
+			.add_method("set_lambda_start", &T::set_lambda_start, "", "start")
+			.add_method("set_reduce_factor", &T::set_reduce_factor, "", "factor")
+			.add_method("set_accept_best", &T::set_accept_best, "", "bAcceptBest")
+			.add_method("set_maximum_defect", &T::set_maximum_defect, "", "maxDef")
+			.add_method("set_verbose", &T::set_verbose, "", "verboseLevel")
+			.add_method("set_offset", &T::set_offset, "", "strOffset")
 			.set_construct_as_smart_pointer(true);
 		reg.add_class_to_group(name, "StandardLineSearch", algTag);
 	}
@@ -249,13 +254,16 @@ static void Register__Algebra(Registry& reg, string parentGroup)
 		typedef VectorTimeSeries<vector_type> T;
 		reg.add_class_<T>(name, grp)
 			.add_constructor()
-			.add_method("size", &T::size)
-			.add_method("push_discard_oldest", &T::push_discard_oldest)
-			.add_method("push", &T::push)
-			.add_method("solution", static_cast<ConstSmartPtr<vector_type> (T::*)(size_t) const>(&T::solution))
-			.add_method("oldest", static_cast<SmartPtr<vector_type> (T::*)()>(&T::oldest))
-			.add_method("latest", static_cast<SmartPtr<vector_type> (T::*)()>(&T::latest))
-			.add_method("time", &T::time)
+			.add_method("size", &T::size, "number of time steps handled")
+			.add_method("push_discard_oldest", &T::push_discard_oldest, "oldest solution", "vec#time", "adds new time point, oldest solution is discarded and returned")
+			.add_method("push", &T::push, "", "vec#time", "adds new time point, not discarding the oldest")
+			.add_method("solution", static_cast<ConstSmartPtr<vector_type> (T::*)(size_t) const>(&T::solution),
+					"the local vector for the i'th time point", "i")
+			.add_method("oldest", static_cast<SmartPtr<vector_type> (T::*)()>(&T::oldest),
+					"oldest solution")
+			.add_method("latest", static_cast<SmartPtr<vector_type> (T::*)()>(&T::latest),
+					"latest solution")
+			.add_method("time", &T::time, "point in time for solution", "i")
 			.set_construct_as_smart_pointer(true);
 		reg.add_class_to_group(name, "SolutionTimeSeries", algTag);
 	}
