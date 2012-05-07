7654843ce2cf58813032e472cc0e13b2a0c83ca3
Torbjörn Klatt <torbjoern.klatt@gcsc.uni-frankfurt.de>
2012-05-07 11:24:38 +0000

    add C++11 high resolution to lib_algebra/precon/amg/stopwath.h
    If compiled with CXX11=ON, C++11's chrono library is used providing high
    resolution time measurements.

--- ugbase/lib_algebra/operator/preconditioner/amg/stopwatch.h
+++ ugbase/lib_algebra/operator/preconditioner/amg/stopwatch.h
@@ -2,6 +2,7 @@
  *  stopwatch.h
  *
  *  Created by Martin Rupp on 03.03.10.
+ *  C++11 high resolution added by Torbjörn Klatt on 2012-05-07
  *  Copyright 2010 G-CSC, University of Frankfurt. All rights reserved.
  *
  */
@@ -9,47 +10,73 @@
 #ifndef __H__UG__LIB_ALGEBRA__STOPWATCH_H__
 #define __H__UG__LIB_ALGEBRA__STOPWATCH_H__
 
+#ifdef UG_CXX11
+#include <chrono>
+#else
 #include <ctime>
-namespace ug{
+#endif
+
+using namespace std;
+
+namespace ug
+{
 ////////////////////////////////////////////////////////////////////////////////
 //!
 //! stopwatch class for quickly taking times
 //! seems to be ok for measuring times > 100 ms
 class stopwatch
 {
-public:
-	stopwatch() 
-	{
-		// you cant be really sure when constructor is called
-		beg = end = clock();
-		bRunning = false;
-	}
-	void start()
-	{
-		std::cout.flush();
-		beg = clock();
-		bRunning = true;
-	}
-	void stop()
-	{
-		end = clock();
-		bRunning = false;
-	}
-	friend std::ostream &operator << (std::ostream &out, stopwatch &s)
-	{
-		out << s.ms() << " ms";
-		return out;
-	}
-	
-	double ms()
-	{
-		if(bRunning) end = clock();
-		return (end-beg)/((double)0.001*CLOCKS_PER_SEC);
-	}
+  public:
+    stopwatch() {
+      // you cant be really sure when constructor is called
+#ifdef UG_CXX11
+      begin = chrono::high_resolution_clock::now();
+      end = chrono::high_resolution_clock::now() - begin;
+#else
+      beg = end = clock();
+#endif
+      bRunning = false;
+    }
+    void start() {
+      cout.flush();
+#ifdef UG_CXX11
+      begin = chrono::high_resolution_clock::now();
+#else
+      beg = clock();
+#endif
+      bRunning = true;
+    }
+    void stop() {
+#ifdef UG_CXX11
+      end = chrono::high_resolution_clock::now() - begin;
+#else
+      end = clock();
+#endif
+      bRunning = false;
+    }
+    friend std::ostream &operator << ( std::ostream &out, stopwatch &s ) {
+      out << s.ms() << " ms";
+      return out;
+    }
 
-private:
-	clock_t beg, end;
-	bool bRunning;
+    double ms() {
+#ifdef UG_CXX11
+      if ( bRunning ) end = chrono::high_resolution_clock::now() - begin;
+      return end.count() / 100.0;
+#else
+      if( bRunning ) end = clock();
+      return ( end - beg ) / ( ( double )0.001 * CLOCKS_PER_SEC );
+#endif
+    }
+
+  private:
+#ifdef UG_CXX11
+    chrono::high_resolution_clock::time_point begin;
+    chrono::microseconds end;
+#else
+    clock_t beg, end;
+#endif
+    bool bRunning;
 };
 
 
