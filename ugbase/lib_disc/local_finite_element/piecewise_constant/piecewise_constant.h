09b2e6ee8060020d7cb626bee57646b3832329bf
Christian Wehner <christian.wehner@gcsc.uni-frankfurt.de>
2012-06-20 15:53:41 +0000

    new folder for piecewise constant functions

---
+++ ugbase/lib_disc/local_finite_element/piecewise_constant/piecewise_constant.h
@@ -0,0 +1,102 @@
+/*
+ * piecewise_constant.h
+ *
+ * Created on: 19.06.2012
+ * Author: Christian Wehner
+ */
+
+#ifndef __H__UG__LIB_DISC__LOCAL_SHAPE_FUNCTION_SET__CROUZEIX_RAVIART__PIECEWISE_CONSTANT__
+#define __H__UG__LIB_DISC__LOCAL_SHAPE_FUNCTION_SET__CROUZEIX_RAVIART__PIECEWISE_CONSTANT__
+
+#include "../common/lagrange1d.h"
+#include "../local_shape_function_set.h"
+#include "../local_dof_set.h"
+#include "lib_disc/common/multi_index.h"
+#include "common/util/provider.h"
+#include "common/util/metaprogramming_util.h"
+#include "lib_grid/grid/geometric_base_objects.h"
+
+namespace ug{
+
+/// Elementwise constant shape functions
+template <typename TRefElem>
+class PiecewiseConstantLSFS
+	: public BaseLocalShapeFunctionSet<PiecewiseConstantLSFS<TRefElem>, TRefElem::dim>
+{
+	private:
+	///	base class
+		typedef BaseLocalShapeFunctionSet<PiecewiseConstantLSFS<TRefElem>, TRefElem::dim> base_type;
+
+	public:
+	///	Domain position type
+		typedef typename base_type::position_type position_type;
+
+	///	Shape type
+		typedef typename base_type::shape_type shape_type;
+
+	///	Gradient type
+		typedef typename base_type::grad_type grad_type;
+
+	public:
+	///	Reference Element type
+		typedef TRefElem reference_element_type;
+
+	///	Order of Shape functions
+		static const size_t order = 0;
+
+	///	Dimension, where shape functions are defined
+		static const int dim = reference_element_type::dim;
+
+	/// Number of shape functions
+		static const size_t nsh = 1;
+
+	protected:
+		MathVector<dim> bary;
+
+	public:
+	///	Constructor
+		PiecewiseConstantLSFS()
+		{
+			const TRefElem& rRef = Provider<TRefElem>::get();
+			//	get corner position integer
+			int num = rRef.num(0);
+
+			bary = rRef.corner(0);
+            for (int j=1;j<num;j++){
+            	bary+=rRef.corner(j);
+            }
+            bary*=1./(number)num;
+		}
+
+	///	\copydoc ug::LocalShapeFunctionSet::type()
+		inline static LFEID type() {return LFEID(LFEID::PIECEWISE_CONSTANT, 0);}
+
+	///	\copydoc ug::LocalShapeFunctionSet::num_sh()
+		inline size_t num_sh() const {return 1;}
+
+	///	\copydoc ug::LocalShapeFunctionSet::position()
+		inline bool position(size_t i, position_type& pos) const
+		{
+			for(int d = 0; d < dim; d++)
+				pos[d]=bary[d];
+			return true;
+		}
+
+	///	\copydoc ug::LocalShapeFunctionSet::shape()
+		inline number shape(const size_t i, const MathVector<dim>& x) const
+		{
+			return 1;
+		}
+
+	///	\copydoc ug::LocalShapeFunctionSet::shape()
+		inline void grad(grad_type& g, const size_t i,	const position_type& x) const
+		{
+			TRefElem::check_position(x);
+			VecSet(g, 0.0);
+		}
+};
+
+} //namespace ug
+
+#endif /* __H__UG__LIB_DISC__LOCAL_SHAPE_FUNCTION_SET__CROUZEIX_RAVIART__PIECEWISE_CONSTANT__ */
+
---
+++ ugbase/lib_disc/local_finite_element/piecewise_constant/piecewise_constant_local_dof.h
@@ -0,0 +1,96 @@
+/*
+ * piecewise_constant_local_dof.h
+ *
+ * Created on: 19.06.2012
+ * Author: Christian Wehner
+ */
+
+#ifndef __H__UG__LIB_DISC__LOCAL_SHAPE_FUNCTION_SET__LAGRANGE__CROUZEIX_RAVIART__PIECEWISE_CONSTANT_LOCAL_DOF__
+#define __H__UG__LIB_DISC__LOCAL_SHAPE_FUNCTION_SET__LAGRANGE__CROUZEIX_RAVIART__PIECEWISE_CONSTANT_LOCAL_DOF__
+
+#include "common/util/provider.h"
+#include "../local_dof_set.h"
+#include "lib_disc/reference_element/reference_element_util.h"
+#include "lib_disc/common/multi_index.h"
+
+namespace ug{
+
+///////////////////////////////////////////////////////////////////////////////
+// Piecewise constant Set
+///////////////////////////////////////////////////////////////////////////////
+
+/// Piecewise constant Set
+template <typename TRefElem>
+class PiecewiseConstantLDS : public ILocalDoFSet
+{
+	protected:
+	///	dimension of reference element
+		static const int refDim = TRefElem::dim;
+
+	public:
+	///	constructor
+		PiecewiseConstantLDS()
+		{
+			if(refDim > 0)
+			{
+				nsh = 1;
+
+			//	set local DoFs (all located at dim objects)
+				m_vLocalDoF.resize(nsh);
+				m_vLocalDoF[0] = LocalDoF(refDim, 0, 0);
+			}
+			else
+			{
+				nsh = 0;
+				m_vLocalDoF.clear();
+			}
+		}
+
+	///	returns the reference dimension
+		int dim() const {return refDim;}
+
+	///	returns the type of reference element
+		ReferenceObjectID roid() const {return TRefElem::REFERENCE_OBJECT_ID;}
+
+	///	returns the total number of DoFs on the finite element
+		size_t num_dof() const {return nsh;};
+
+	///	returns the number of DoFs on a sub-geometric object type
+		int num_dof(ReferenceObjectID type) const
+		{
+			return max_num_dof(ReferenceElementDimension(type));
+		}
+
+	///	returns the number of DoFs on sub-geometric object in dimension and id
+		size_t num_dof(int d, size_t id) const
+		{
+			if (d == refDim) return 1;
+			else return 0;
+		}
+
+	///	returns if the storage needs objects of a given dimension
+		size_t max_num_dof(int d) const
+		{
+			if (d == refDim)   return 1;
+			else if (d > refDim) return -1;
+			else return 0;
+		}
+
+	///	returns the dof storage
+		const LocalDoF& local_dof(size_t dof) const {return m_vLocalDoF[dof];}
+
+	protected:
+	///	number of shapes
+		size_t nsh;
+
+	///	order
+		static const size_t p=0;
+
+	///	association to elements
+		std::vector<LocalDoF> m_vLocalDoF;
+};
+
+
+} //namespace ug
+
+#endif /* __H__UG__LIB_DISC__LOCAL_SHAPE_FUNCTION_SET__LAGRANGE__CROUZEIX_RAVIART__PIECEWISE_CONSTANT_LOCAL_DOF__ */
