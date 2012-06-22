77643caac9fd3e7e61fbb92bac61a076f0e00bda
Andreas Vogel <andreas.vogel@gcsc.uni-frankfurt.de>
2012-06-22 08:01:02 +0000

    Adding String for Binary License.

--- ugbase/common/authors.h
+++ ugbase/common/authors.h
@@ -13,18 +13,57 @@ namespace ug {
      * Names of the UG Core developer team, seperated by ','.
      */
     static std::string UG_AUTHORS
-    	="Sebastian Reiter, Martin Rupp, Andreas Vogel, etc ...";
+    	="Sebastian Reiter, Martin Rupp, Andreas Vogel, et al.";
 
     /**
      * License for binary usage.
      */
     static std::string UG_BINARY_LICENSE
-    	="UG4 -- Binary License\n"
-         "---------------------\n"
-    	 "No licence available.\n"
-    	 "If you recieved this code, you are not allowed to use, run or redistribute it.\n"
-    	 "Contact the authors to obtain an adequate license:\n"
-    	 "sebastian.reiter@gcsc.uni-frankfurt.de";
+    =	"ug4 Binary Code License\n"
+    	"\n"
+    	"ug4 (as defined below) is licensed to the licensee only upon the "
+    	"condition that the licensee accepts all of the terms contained in this"
+    	" Binary Code License Agreement (\"Agreement\"). By downloading, "
+    	"installing and/or using ug4, the licensee accepts the full terms "
+    	"of this Agreement.\n"
+    	"\n"
+    	"\n"
+    	"Definitions\n"
+    	"1. The binary distribution of the ug4 simulation framework (\"ug4\")"
+    	" consists of the following binary components: The ugshell application, "
+    	"the VRL-UG4 plugin and the ug4-plugins ConvectionDiffusion and ConstantEquation.\n"
+    	"2. Licensee means the individual that uses ug4 under the terms of "
+    	"this License Agreement.\n"
+   		"\n"
+    	"License To Use\n"
+    	"1. Subject to the terms and conditions of this Agreement, the licensee "
+    	"is hereby granted a non-exclusive, non-transferable, limited license "
+    	"to use the binary distribution of ug4 complete and unmodified for the "
+    	"sole purpose of running programs and for non-commercial use only.\n"
+    	"2. Usage and/or publication of results obtained with the aid of ug4 is "
+    	"only permitted if the following citation is included: "
+    	"\"A. Vogel, S. Reiter, M. Rupp, A. NŠgel, G. Wittum: UG4 - A Novel "
+    	"Flexible Software System for Simulating PDE Based Models on High "
+    	"Performance Computers, Comp. Vis. Sci.\n"
+    	"3. Redistribution of ug4 is not permitted by this Agreement. Additional "
+    	"licenses for developers and/or publishers are not covered by this Agreement.\n"
+    	"\n"
+    	"Warranty\n"
+    	"The program is licensed free of charge and there is no warranty for the "
+    	"program, to the extent permitted by applicable law. The program is provided "
+    	"\"as is\" without warranty of any kind, either expressed or implied, "
+    	"including, but not limited to, the implied warranties of merchantability "
+    	"and fitness for a particular purpose. The entire risk as to the quality "
+    	"and performance of the program is with the licensee. Should the program "
+    	"prove defective, the licensee assumes the cost of all necessary "
+    	"servicing, repair or correction.\n"
+    	"\n"
+    	"In no event unless required by applicable law will the licensor be "
+    	"liable to the licensee for damages, including any general, special, "
+    	"incidental or consequential damages arising out of the use or inability "
+    	"to use the program (including but not limited to loss of data or data "
+    	"being rendered inaccurate or losses sustained by the licensee or a "
+    	"failure of the program to operate with any other programs).\n";
 }
 
 #endif	/* AUTHORS_H */
