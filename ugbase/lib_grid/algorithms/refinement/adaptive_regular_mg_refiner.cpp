760d8395b802863b2768924239f6c3f5f4e92c10
Sebastian Reiter <sebastian.reiter@gcsc.uni-frankfurt.de>
2012-09-13 15:03:40 +0000

    Added missing files... Sorry for all the jenkins mails.

---
+++ ugbase/lib_grid/algorithms/refinement/adaptive_regular_mg_refiner.cpp
@@ -0,0 +1,398 @@
+// created by Sebastian Reiter
+// s.b.reiter@gmail.com
+// 12.09.2012 (d,m,y)
+
+#include "lib_grid/algorithms/selection_util.h"
+#include "adaptive_regular_mg_refiner.h"
+
+using namespace std;
+
+namespace ug{
+
+AdaptiveRegularRefiner_MultiGrid::
+AdaptiveRegularRefiner_MultiGrid(IRefinementCallback* refCallback) :
+	HangingNodeRefiner_MultiGrid(refCallback)
+{
+}
+
+AdaptiveRegularRefiner_MultiGrid::
+AdaptiveRegularRefiner_MultiGrid(MultiGrid& mg,
+								 IRefinementCallback* refCallback) :
+	HangingNodeRefiner_MultiGrid(refCallback)
+{
+	set_grid(&mg);
+}
+
+AdaptiveRegularRefiner_MultiGrid::
+~AdaptiveRegularRefiner_MultiGrid()
+{
+}
+
+void AdaptiveRegularRefiner_MultiGrid::
+assign_grid(MultiGrid& mg)
+{
+	set_grid(&mg);
+}
+
+void AdaptiveRegularRefiner_MultiGrid::
+set_grid(MultiGrid* mg)
+{
+	HangingNodeRefiner_MultiGrid::set_grid(mg);
+
+	m_closureElems.enable_autoselection(false);
+	m_closureElems.enable_selection_inheritance(false);
+	m_closureElems.enable_strict_inheritance(false);
+
+	m_closureElems.assign_grid(mg);
+}
+
+
+void AdaptiveRegularRefiner_MultiGrid::
+remove_closure_elements()
+{
+//	remove all closure elements from the current grid
+
+	if(!multi_grid()){
+		UG_THROW("AdaptiveRegularRefiner_MultiGrid has to be associated with a grid!");
+	}
+
+	EraseSelectedObjects(m_closureElems);
+}
+
+
+void AdaptiveRegularRefiner_MultiGrid::
+create_closure_elements()
+{
+	if(!multi_grid()){
+		UG_THROW("AdaptiveRegularRefiner_MultiGrid has to be associated with a grid!");
+	}
+
+	MultiGrid& mg = *multi_grid();
+
+//	temporarily create refinement callback, if none is available.
+//todo:	A better solution should be used for this!
+	bool localRefCallbackSet = false;
+	if(!m_refCallback){
+		if(mg.has_vertex_attachment(aPosition)){
+			localRefCallbackSet = true;
+			m_refCallback = new RefinementCallbackLinear<APosition>(mg, aPosition);
+		}
+		else if(mg.has_vertex_attachment(aPosition2)){
+			localRefCallbackSet = true;
+			m_refCallback = new RefinementCallbackLinear<APosition2>(mg, aPosition2);
+		}
+		else if(mg.has_vertex_attachment(aPosition1)){
+			localRefCallbackSet = true;
+			m_refCallback = new RefinementCallbackLinear<APosition1>(mg, aPosition1);
+		}
+	}
+
+	if(mg.num<Volume>() > 0)
+		create_closure_elements_3d();
+	else if(mg.num<Face>() > 0)
+		create_closure_elements_2d();
+
+//	clear the refinement-callback if necessary
+	if(localRefCallbackSet){
+		delete m_refCallback;
+		m_refCallback = NULL;
+	}
+}
+
+
+void AdaptiveRegularRefiner_MultiGrid::
+create_closure_elements_2d()
+{
+//todo:	This method currently only works for one type of elements. No manifolds
+//		are currently supported.
+
+//	for each surface element (faces in 2d, volumes in 3d) adjacent to a constraining
+//	element, we generate closure elements in the level above.
+
+	if(!multi_grid()){
+		UG_THROW("AdaptiveRegularRefiner_MultiGrid has to be associated with a grid!");
+	}
+
+//	remove all closure elements. This is currently required, since the selector
+//	m_closureElems is currently also used to store non-closer elements, which are
+//	to be refined.
+	remove_closure_elements();
+
+	MultiGrid& mg = *multi_grid();
+
+//	iterate over all constraining edges and collect associated surface faces
+//	Those then have to be refined to generate a closure.
+
+	Grid::face_traits::secure_container	assElems;
+	Grid::edge_traits::secure_container assEdges;
+	Face::ConstVertexArray vrts;
+	std::vector<VertexBase*> newVrtVrts;
+	std::vector<VertexBase*> newEdgeVrts;
+	std::vector<Face*>	newFaces;
+	EdgeDescriptor ed;
+
+//	we'll select all new elements on the fly
+	m_closureElems.enable_autoselection(true);
+
+	for(Grid::traits<ConstrainingEdge>::iterator i_edge = mg.begin<ConstrainingEdge>();
+		i_edge != mg.end<ConstrainingEdge>(); ++i_edge)
+	{
+	//	check all associated elements of i_edge, whether one is a surface element.
+	//	If so, create the closure.
+		mg.associated_elements(assElems, *i_edge);
+
+		for(size_t i_elem = 0; i_elem < assElems.size(); ++i_elem){
+			Face* elem = assElems[i_elem];
+			if(mg.has_children(elem))
+				continue;
+
+			newVrtVrts.clear();
+			newEdgeVrts.clear();
+
+		//	copy associated vertices and edges to the next level
+			vrts = elem->vertices();
+			size_t numVrts = elem->num_vertices();
+			for(size_t i_vrt = 0; i_vrt < numVrts; ++i_vrt){
+				VertexBase* vrt = vrts[i_vrt];
+				if(!mg.has_children(vrt)){
+					newVrtVrts.push_back(*mg.create<Vertex>(vrt));
+					if(m_refCallback)
+						m_refCallback->new_vertex(newVrtVrts.back(), vrt);
+				}
+				else
+					newVrtVrts.push_back(mg.get_child_vertex(vrt));
+			}
+
+			mg.associated_elements_sorted(assEdges, elem);
+			for(size_t i = 0; i < assEdges.size(); ++i){
+				EdgeBase* e = assEdges[i];
+				if(!mg.has_children(e)){
+					ed.set_vertices(mg.get_child_vertex(e->vertex(0)),
+									mg.get_child_vertex(e->vertex(1)));
+					mg.create<Edge>(ed, e);
+					newEdgeVrts.push_back(NULL);
+				}
+				else
+					newEdgeVrts.push_back(mg.get_child_vertex(e));
+			}
+
+		//	refine the element
+			VertexBase* newFaceVrt;
+			if(elem->refine(newFaces, &newFaceVrt, &newEdgeVrts.front(),
+							NULL, &newVrtVrts.front()))
+			{
+				if(newFaceVrt){
+					mg.register_element(newFaceVrt, elem);
+					if(m_refCallback)
+						m_refCallback->new_vertex(newFaceVrt, elem);
+				}
+				for(size_t i = 0; i < newFaces.size(); ++i)
+					mg.register_element(newFaces[i], elem);
+			}
+		}
+	}
+
+//	stop selecting new elements
+	m_closureElems.enable_autoselection(false);
+}
+
+void AdaptiveRegularRefiner_MultiGrid::
+create_closure_elements_3d()
+{
+//todo:	This method currently only works for one type of elements. No manifolds
+//		are currently supported.
+
+//	for each surface element (faces in 2d, volumes in 3d) adjacent to a constraining
+//	element, we generate closure elements in the level above.
+
+	if(!multi_grid()){
+		UG_THROW("AdaptiveRegularRefiner_MultiGrid has to be associated with a grid!");
+	}
+
+//	remove all closure elements. This is currently required, since the selector
+//	m_closureElems is currently also used to store non-closer elements, which are
+//	to be refined.
+	remove_closure_elements();
+
+	MultiGrid& mg = *multi_grid();
+
+//	iterate over all constraining edges and collect associated surface faces / volumes.
+//	Those then have to be refined to generate a closure.
+
+	Grid::volume_traits::secure_container	assElems;
+	Grid::edge_traits::secure_container assEdges;
+	Grid::face_traits::secure_container assFaces;
+	Volume::ConstVertexArray vrts;
+	std::vector<VertexBase*> newVolVrtVrts;
+	std::vector<VertexBase*> newVolEdgeVrts;
+	std::vector<VertexBase*> newVolFaceVrts;
+	std::vector<Volume*> newVols;
+	EdgeDescriptor ed;
+	FaceDescriptor fd;
+	
+//	when refining the associated faces, we need some structs, too
+	Grid::edge_traits::secure_container assFaceEdges;
+	std::vector<VertexBase*> newFaceVrtVrts;
+	std::vector<VertexBase*> newFaceEdgeVrts;
+	std::vector<Face*> newFaces;
+
+//	we'll select all new elements on the fly
+	m_closureElems.enable_autoselection(true);
+
+	for(Grid::traits<ConstrainingEdge>::iterator i_edge = mg.begin<ConstrainingEdge>();
+		i_edge != mg.end<ConstrainingEdge>(); ++i_edge)
+	{
+	//	check all associated elements of i_edge, whether one is a surface element.
+	//	If so, select it into m_closureElems. This is only temporary, since it
+	//	isn't a real closure element.
+		mg.associated_elements(assElems, *i_edge);
+
+		for(size_t i_elem = 0; i_elem < assElems.size(); ++i_elem){
+			Volume* elem = assElems[i_elem];
+			if(mg.has_children(elem))
+				continue;
+
+			newVolVrtVrts.clear();
+			newVolEdgeVrts.clear();
+			newVolFaceVrts.clear();
+
+		//	copy associated vertices and edges to the next level
+			vrts = elem->vertices();
+			size_t numVrts = elem->num_vertices();
+			for(size_t i_vrt = 0; i_vrt < numVrts; ++i_vrt){
+				VertexBase* vrt = vrts[i_vrt];
+				if(!mg.has_children(vrt)){
+					newVolVrtVrts.push_back(*mg.create<Vertex>(vrt));
+					if(m_refCallback)
+						m_refCallback->new_vertex(newVolVrtVrts.back(), vrt);
+				}
+				else
+					newVolVrtVrts.push_back(mg.get_child_vertex(vrt));
+			}
+
+			mg.associated_elements_sorted(assEdges, elem);
+			for(size_t i = 0; i < assEdges.size(); ++i){
+				EdgeBase* e = assEdges[i];
+				if(!mg.has_children(e)){
+					ed.set_vertices(mg.get_child_vertex(e->vertex(0)),
+									mg.get_child_vertex(e->vertex(1)));
+					mg.create<Edge>(ed, e);
+					newVolEdgeVrts.push_back(NULL);
+				}
+				else
+					newVolEdgeVrts.push_back(mg.get_child_vertex(e));
+			}
+
+		//	we have to either refine or copy associated faces
+			mg.associated_elements_sorted(assFaces, elem);
+			for(size_t i_face = 0; i_face < assFaces.size(); ++i_face){
+				
+				Face* f = assFaces[i_face];
+				if(mg.has_children(f)){
+					newVolFaceVrts.push_back(mg.get_child_vertex(f));
+					continue;
+				}
+				
+			//	collect child vertices of associated edges
+				mg.associated_elements_sorted(assFaceEdges, f);
+				newFaceEdgeVrts.clear();
+				
+				bool faceRefinement = false;
+				for(size_t i = 0; i < assFaceEdges.size(); ++i){
+					VertexBase* child = mg.get_child_vertex(assFaceEdges[i]);
+					newFaceEdgeVrts.push_back(child);
+					faceRefinement |= (child != NULL);
+				}				
+				
+				if(faceRefinement){
+					newFaceVrtVrts.clear();
+					for(size_t i = 0; i < f->num_vertices(); ++i)
+						newFaceVrtVrts.push_back(mg.get_child_vertex(f->vertex(i)));
+					
+					VertexBase* newFaceVrt = NULL;
+					if(f->refine(newFaces, &newFaceVrt, &newFaceEdgeVrts.front(),
+								 NULL, &newFaceVrtVrts.front()))
+					{
+						if(newFaceVrt){
+							mg.register_element(newFaceVrt, f);
+							if(m_refCallback)
+								m_refCallback->new_vertex(newFaceVrt, f);
+						}
+						for(size_t i = 0; i < newFaces.size(); ++i)
+							mg.register_element(newFaces[i], f);
+					}
+					newVolFaceVrts.push_back(newFaceVrt);
+				}	
+				else{
+					Face::ConstVertexArray fvrts = f->vertices();
+					if(f->num_vertices() == 3)
+						mg.create<Triangle>(TriangleDescriptor(
+												mg.get_child_vertex(fvrts[0]),
+												mg.get_child_vertex(fvrts[1]),
+												mg.get_child_vertex(fvrts[2])),
+											f);
+					else if(f->num_vertices() == 4)
+						mg.create<Quadrilateral>(QuadrilateralDescriptor(
+													mg.get_child_vertex(fvrts[0]),
+													mg.get_child_vertex(fvrts[1]),
+													mg.get_child_vertex(fvrts[2]),
+													mg.get_child_vertex(fvrts[3])),
+												f);
+					newVolFaceVrts.push_back(NULL);
+				}
+			}
+
+		//	refine the element
+		//	if we're performing tetrahedral refinement, we have to collect
+		//	the corner coordinates, so that the refinement algorithm may choose
+		//	the best interior diagonal.
+			vector3 corners[4];
+			vector3* pCorners = NULL;
+			if((elem->num_vertices() == 4) && m_refCallback){
+				for(size_t i = 0; i < 4; ++i){
+					m_refCallback->current_pos(&corners[i].x, vrts[i], 3);
+				}
+				pCorners = corners;
+			}
+
+			VertexBase* newVolVrt;
+			if(elem->refine(newVols, &newVolVrt, &newVolEdgeVrts.front(),
+							&newVolFaceVrts.front(), NULL, Vertex(),
+							&newVolVrtVrts.front(), pCorners))
+			{
+				if(newVolVrt){
+					mg.register_element(newVolVrt, elem);
+					if(m_refCallback)
+						m_refCallback->new_vertex(newVolVrt, elem);
+				}
+
+				for(size_t i = 0; i < newVols.size(); ++i)
+					mg.register_element(newVols[i], elem);
+			}
+		}
+	}
+
+//	stop selecting new elements
+	m_closureElems.enable_autoselection(false);
+}
+
+void AdaptiveRegularRefiner_MultiGrid::
+perform_refinement()
+{
+//	todo: copy refinement marks from closure elements to their parents
+	remove_closure_elements();
+	HangingNodeRefiner_MultiGrid::perform_refinement();
+	create_closure_elements();
+}
+
+bool AdaptiveRegularRefiner_MultiGrid::
+perform_coarsening()
+{
+//	todo: copy coarsen marks from closure elements to their parents
+	remove_closure_elements();
+	bool bSuccess = HangingNodeRefiner_MultiGrid::perform_coarsening();
+	create_closure_elements();
+	return bSuccess;
+}
+
+}// end of namespace
---
+++ ugbase/lib_grid/algorithms/refinement/adaptive_regular_mg_refiner.h
@@ -0,0 +1,80 @@
+// created by Sebastian Reiter
+// s.b.reiter@googlemail.com
+// 11.01.2011 (m,d,y)
+
+#ifndef __H__UG__ADAPTIVE_REGULAR_REFINER_MULTI_GRID__
+#define __H__UG__ADAPTIVE_REGULAR_REFINER_MULTI_GRID__
+
+#include "hanging_node_refiner_multi_grid.h"
+#include "lib_grid/tools/selector_grid.h"
+
+namespace ug
+{
+
+///	\addtogroup lib_grid_algorithms_refinement
+///	@{
+
+///	Specialization of IRefiner for adaptive multigrid refinement with closure.
+/**	The implementation is based on HangingNodeRefiner_MultiGrid.
+ * Hanging nodes are most likely present in the resulting grids, even though
+ * they should not be used by discretizations etc. Their sole purpose is to
+ * allow efficient grid adaption
+ *
+ * \todo	The current implementation has a performance overhead, since currently
+ * 			all closure elements are removed prior to grid adaption. This should be
+ * 			reduced to only remove closure elements which are affected by adaption.
+ * 			One could therefore replace Selector by BoolMarker and adjust the
+ * 			collect_objects_for_refinement / coarsening routines to use callbacks
+ * 			when identifying valid refine / coarsen objects.
+ */
+class AdaptiveRegularRefiner_MultiGrid : public HangingNodeRefiner_MultiGrid
+{
+	public:
+		using HangingNodeRefiner_MultiGrid::mark;
+
+	public:
+		AdaptiveRegularRefiner_MultiGrid(IRefinementCallback* refCallback = NULL);
+		AdaptiveRegularRefiner_MultiGrid(MultiGrid& mg,
+										 IRefinementCallback* refCallback = NULL);
+
+		virtual ~AdaptiveRegularRefiner_MultiGrid();
+
+		virtual void assign_grid(MultiGrid& mg);
+
+	protected:
+	///	performs registration and deregistration at a grid.
+	/**	Initializes all grid related variables.
+	 *  call set_grid(NULL) to unregister the observer from a grid.
+	 *
+	 * 	Please note that though the base grid features a set_grid method,
+	 *  it is not declared virtual. This is because we want to call it
+	 *  during construction and destruction.*/
+		void set_grid(MultiGrid* mg);
+
+	///	remove closure elements
+		void remove_closure_elements();
+
+	///	calls create_closure_elements_2d / 3d, depending on the presence of volume elements.
+		void create_closure_elements();
+
+	///	creates closure elements for 2d geometries
+		void create_closure_elements_2d();
+
+	///	creates closure elements for 3d geometries
+		void create_closure_elements_3d();
+
+	///	removes all closure elements, calls the base implementation and creates a new closure
+		virtual void perform_refinement();
+
+	///	removes all closure elements, calls the base implementation and creates a new closure
+		virtual bool perform_coarsening();
+
+	protected:
+		Selector	m_closureElems;
+};
+
+/// @}	// end of add_to_group command
+
+}//	end of namespace
+
+#endif
