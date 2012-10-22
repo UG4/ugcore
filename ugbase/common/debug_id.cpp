d436bca857adcfd82b9bb459e37e8aab61cc8081
Martin Rupp <martin.rupp@gcsc.uni-frankfurt.de>
2012-10-22 14:38:14 +0000

    adding debug_id

---
+++ ugbase/common/debug_id.cpp
@@ -0,0 +1,67 @@
+/* 
+ * File:   DebugID.cpp
+ * Author: mrupp
+ * 
+ * Created on 22. Oktober 2012, 13:55
+ */
+
+#include "debug_id.h"
+#include "common/log.h"
+#include "common/assert.h"
+
+namespace ug{
+
+DebugID::DebugID(const char *str)
+{
+	m_hash = crc32(str);
+	GetDebugIDManager().register_debug_id(str);
+}
+
+DebugIDManager& DebugIDManager::instance()
+{
+	static DebugIDManager m;
+	return m;
+}
+
+
+bool DebugIDManager::
+set_debug_levels(int lev)
+{
+	for(std::map<uint32, int>::iterator it=m_dbgLevels.begin(); it != m_dbgLevels.end(); ++it)
+		(*it).second = lev;
+	return true;
+}
+
+bool DebugIDManager::
+set_debug_level(const char *debugID, int level)
+{
+	if(set_debug_level(crc32(debugID), level) == false)
+	{
+		UG_LOG("DebugID " << debugID << " not registered.\n");
+		return false;
+	}
+	else return true;
+}
+
+bool DebugIDManager::
+register_debug_id(const char *debugID)
+{
+	if(debug_id_registered(debugID) == false)
+	{
+		m_dbgLevelIdentifiers.push_back(std::string(debugID));
+		m_dbgLevels[crc32(debugID)] = -1;
+		return true;
+	}
+	else
+	{
+		// not quite clear if cout is defined yet.
+		// note that this could be caused by double-registering libraries.
+		printf("\nFATAL ERROR: DebugID %s already registered.\n\n", debugID);
+		assert(0);
+		return false;
+	}
+}
+
+
+
+}
---
+++ ugbase/common/debug_id.h
@@ -0,0 +1,152 @@
+/* 
+ * File:   DebugID.h
+ * Author: Martin Rupp
+ *
+ * Created on 22. Oktober 2012, 13:55
+ */
+
+#ifndef __H__UG__COMMON__DEBUG_ID_H
+#define	__H__UG__COMMON__DEBUG_ID_H
+#include <vector>
+#include <map>
+#include <string>
+#include "common/util/crc32.h"
+#include <sstream>
+
+namespace ug{
+class DebugIDManager;
+class DebugID
+{
+public:
+/// registers the DebugID at DebugIDManager
+	DebugID(const char *str);
+	
+	
+	DebugID(uint32 hash) { m_hash = hash; }
+/// returns the debug level via GetDebugIDManager.
+	inline int get_debug_level() const;
+/// sets the debug level via GetDebugIDManager.
+	inline bool set_debug_level(int level) const;
+
+	friend class DebugIDManager;
+private:
+	uint32 m_hash;
+};
+
+extern DebugID MAIN,
+		APP,
+		LIB_GRID,
+		LIB_GRID_REFINER,
+		LIB_DISC,
+		LIB_DISC_ASSEMBLE,
+		LIB_DISC_D3F,
+		LIB_DISC_MULTIGRID,
+		LIB_DISC_NEWTON,
+		LIB_DISC_LINKER,
+		LIB_DISC_TRANSFER,
+		LIB_DISC_DISCRETE_FUNCTION,
+		LIB_DISC_OUTPUT,
+		LIB_DISC_OPERATOR_INVERSE,
+		LIB_ALG_LINEAR_OPERATOR,
+		LIB_ALG_LINEAR_SOLVER,
+		LIB_ALG_VECTOR,
+		LIB_ALG_MATRIX,
+		LIB_ALG_AMG,
+		LIB_PCL;
+
+
+class DebugIDManager
+{
+private:
+	/// returns the debug level of debugIDhash, -1 if not found
+		inline int get_debug_level(uint32 debugIDhash) const
+		{
+			std::map<uint32, int>::const_iterator it = m_dbgLevels.find(debugIDhash);
+			if(it == m_dbgLevels.end()) return -1;
+			else return (*it).second;
+		}
+		
+		/// sets the debug level of debugIDhash if registered to 'level'
+		inline bool set_debug_level(uint32 debugIDhash, int level)
+		{
+			std::map<uint32, int>::iterator it = m_dbgLevels.find(debugIDhash);
+			if(it == m_dbgLevels.end()) return false;
+			else
+			{
+				(*it).second = level;
+				return true;
+			}
+		}
+public:
+	///	returns a reference to the single instance of LogAssistant
+		static DebugIDManager& instance();
+
+	/// returns true if there is a DebugID with name debugIDname
+		inline bool debug_id_registered(const char *debugIDname) const
+		{
+			return m_dbgLevels.find(crc32(debugIDname)) != m_dbgLevels.end();
+		}
+	/// returns the debug level of debugID, -1 if not found
+		inline int get_debug_level(DebugID &debugID) const
+		{
+			return get_debug_level(debugID.m_hash);
+		}
+	/// returns the debug level of debugID, -1 if not found
+		inline int get_debug_level(const char *debugID) const
+		{
+			return get_debug_level(crc32(debugID));
+		}
+		
+	/// 
+		inline DebugID get_debug_id(const char *debugID) const
+		{
+			return DebugID(crc32(debugID));
+		}
+		
+		inline DebugID get_or_create_debug_id(const char *debugID)
+		{
+			if(debug_id_registered(debugID)==false)
+				register_debug_id(debugID);
+			return DebugID(crc32(debugID));
+		}
+				
+
+	/// sets the debug level of debugID if registered to 'level'
+		inline bool set_debug_level(DebugID &debugID, int level)
+		{
+			return set_debug_level(debugID.m_hash, level);
+		}
+	/// sets the debug level of debugID if registered to 'level'
+		bool set_debug_level(const char *debugID, int level);
+
+	
+	/// sets the debug level of all registered debugIDs to 'lev'
+		bool set_debug_levels(int lev);
+
+	/// returns a string describing all registered debugIDs.
+		std::string get_registered_debug_IDs()
+		{
+			std::stringstream str;
+			str << "DebugIDs:\n";
+			for(size_t i=0; i<m_dbgLevelIdentifiers.size(); i++)
+				str << m_dbgLevelIdentifiers[i] << " : Level " << get_debug_level(m_dbgLevelIdentifiers[i].c_str()) << "\n";
+			return str.str();
+		}
+
+
+
+	bool register_debug_id(const char *debugID);
+
+private:
+	std::vector<std::string> m_dbgLevelIdentifiers;
+	std::map<uint32, int> m_dbgLevels;
+};
+
+inline DebugIDManager& GetDebugIDManager()
+{
+	return DebugIDManager::instance();
+}
+
+}
+#endif	/* __H__UG__COMMON__DEBUG_ID_H */
+
