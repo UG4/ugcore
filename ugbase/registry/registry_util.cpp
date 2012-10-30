21772a68c62a9cf96b48d1517163fedb56bff112
Martin Rupp <martin.rupp@gcsc.uni-frankfurt.de>
2012-10-30 12:11:40 +0000

    allowed functions starting with __, added docu

--- ugbase/registry/registry_util.cpp
+++ ugbase/registry/registry_util.cpp
@@ -4,12 +4,29 @@
 
 namespace ug{
 
+/*
+ * 
+ * For compatibility to other languages like VRL we need some mappings of
+ * classes/interfaces and functions
+ * 
+ * F_ function
+ * C_ class
+ * I_ interface
+ * const__ for const methods
+ * 
+ * Otherwise we will get name collisions
+ * At the moment, __ is reserved for further extensions of this scheme,
+ * but not if an identifier is starting with __, like LUAs
+ * __index and __tostring methods.
+ * 
+ */
+	
 bool IsValidRegistryIdentifier(const std::string& name) {
-	return !Contains(name,"__") &&
+	return StartsWith(name,"__") || (!Contains(name,"__") &&
 			!StartsWith(name, "F_") &&
 			!StartsWith(name, "C_") &&
 			!StartsWith(name, "I_") &&
-			name!="constructor";
+			name!="constructor");
 }
 
 std::string GetRegistryIdentifierMessage() {
