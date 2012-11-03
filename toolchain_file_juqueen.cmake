8855a06f7955504c013c291fbbaa1b2cf6673d96
Ingo Heppner <igno.heppner@gcsc.uni-frankfurt.de>
2012-11-03 18:31:13 +0000

    Toolchain for JuQueen, the BG/Q system at FZJ added. Please note that this up to now needs an "upgraded" version of CMake! See the comments within the file for details!

---
+++ toolchain_file_juqueen.cmake
@@ -0,0 +1,62 @@
+################################################################################
+# created by Ingo Heppner
+# Ingo.Heppner@gcsc.uni-frankfurt.de
+#
+# Toolchain file for JuQueen (type IBM Blue Gene/Q) FZ Juelich
+#
+# Attention:
+# For this to work a CMake version with platform files for Blue Gene/Q is required!
+# The currently (October 2012) installed version on JuQueen does not (the FZJ
+# support is asked to support one).
+#
+# In the meantime one can resort to a local installation of (the most recent
+# version of) CMake, where appropriate platform files are copied into the
+# 'Modules/Platform/' sub directory:
+#
+#  ~/local/share/cmake-2.8/Modules/Platform/BlueGeneQ-*.cmake
+#
+# The following is based on information from
+# 'https://code.google.com/p/elemental/source/browse/cmake/toolchains/BGQ-gnu-netlib.cmake',
+# utilising platform files from
+# 'http://www.cmake.org/Bug/bug_relationship_graph.php?bug_id=13512&graph=dependency'
+# (found 03092012), which are copied into the 'Modules/Platform/' directory of
+# a locally installed CMake, version 2.8.9 (installed 03092012).
+################################################################################
+
+# Important: Setting the "cmake system name" will lead to automatic inclusion of
+# the corresponding platform files:
+set(CMAKE_SYSTEM_NAME BlueGeneQ-static)
+
+# This option tells ug_cmake_includes.txt to add the -dynamic option to the compiler flags.
+SET(enableDynamicOption OFF)
+
+# This option tells ug_cmake_includes.txt to add the -fno-strict-aliasing option to the compiler flags.
+SET(enableNoStrictAliasingOption ON)
+
+# Some variables for pathes
+set(GCC_ROOT  "/bgsys/drivers/ppcfloor/gnu-linux")
+set(GCC_NAME  "powerpc64-bgq-linux")
+set(MPI_ROOT  "/bgsys/drivers/ppcfloor/comm/gcc")
+#set(PAMI_ROOT "/bgsys/drivers/ppcfloor/comm/sys")
+#set(SPI_ROOT  "/bgsys/drivers/ppcfloor/spi")
+
+# The serial GNU compilers
+set(CMAKE_C_COMPILER       ${GCC_ROOT}/bin/${GCC_NAME}-gcc)
+set(CMAKE_CXX_COMPILER     ${GCC_ROOT}/bin/${GCC_NAME}-g++)
+set(CMAKE_Fortran_COMPILER ${GCC_ROOT}/bin/${GCC_NAME}-gfortran)
+
+# The MPI wrappers for the GNU compilers
+set(MPI_C_COMPILER       ${MPI_ROOT}/bin/mpicc)
+set(MPI_CXX_COMPILER     ${MPI_ROOT}/bin/mpicxx)
+set(MPI_Fortran_COMPILER ${MPI_ROOT}/bin/mpif90)
+
+message(STATUS "TMP INFO: Value of 'CMAKE_C_COMPILER'       is: "${CMAKE_C_COMPILER})       # TMP
+message(STATUS "TMP INFO: Value of 'CMAKE_CXX_COMPILER'     is: "${CMAKE_CXX_COMPILER})     # TMP
+message(STATUS "TMP INFO: Value of 'CMAKE_Fortran_COMPILER' is: "${CMAKE_Fortran_COMPILER}) # TMP
+
+message(STATUS "TMP INFO: Value of 'MPI_C_COMPILER'         is: "${MPI_C_COMPILER})       # TMP
+message(STATUS "TMP INFO: Value of 'MPI_CXX_COMPILER'       is: "${MPI_CXX_COMPILER})     # TMP
+message(STATUS "TMP INFO: Value of 'MPI_Fortran_COMPILER'   is: "${MPI_Fortran_COMPILER}) # TMP
+
+# For debugging purposes
+#include(CMakePrintSystemInformation)
