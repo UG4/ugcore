e0f7c0a9b75f7bf0af802988a1e0bd3cbc2f1d68
Stephan Grein <stephan.grein@gcsc.uni-frankfurt.de>
2012-11-08 15:09:07 +0000

    Next try.

--- ugbase/bridge/disc_bridges/output_bridge.cpp
+++ ugbase/bridge/disc_bridges/output_bridge.cpp
@@ -81,7 +81,9 @@ static void DomainAlgebra(Registry& reg, string grp)
 			.add_method("print", static_cast<void (T::*)(const char*, function_type&, int, number, bool)>(&T::print))
 			.add_method("print", static_cast<void (T::*)(const char*, function_type&, int, number)>(&T::print))
 			.add_method("print", static_cast<void (T::*)(const char*, function_type&, bool)>(&T::print))
-			.add_method("print", static_cast<void (T::*)(const char*, function_type&)>(&T::print));
+			.add_method("print", static_cast<void (T::*)(const char*, function_type&)>(&T::print))
+			.add_method("set_write_interval", (&T::set_write_interval))
+		    .add_method("get_write_interval", (&T::get_write_interval));
 	}
 
 
