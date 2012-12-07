fdc97000c7fd81d721adbc5a616e1431162fbc50
Sebastian Reiter <sebastian.reiter@gcsc.uni-frankfurt.de>
2012-12-07 12:52:38 +0000

    Fixed linker errors in serial build. Sorry for the inconveniance!

--- ugbase/pcl/pcl_util.h
+++ ugbase/pcl/pcl_util.h
@@ -9,6 +9,7 @@
 #include <vector>
 #include <algorithm>
 #include <string>
+#include "pcl_layout_util.h"
 #include "common/util/hash.h"
 #include "pcl_base.h"
 #include "pcl_communication_structs.h"
@@ -42,136 +43,6 @@ bool AllProcsTrue(bool bFlag, ProcessCommunicator comm = ProcessCommunicator());
 /// performs an allreduce and returns true at least one process called the
 /// function with bFlag = true
 bool OneProcTrue(bool bFlag, ProcessCommunicator comm = ProcessCommunicator());
-
-
-///	removes all empty interfaces from the given layout.
-template <class TLayout>
-void RemoveEmptyInterfaces(TLayout& layout)
-{
-	typedef typename TLayout::iterator TInterfaceIter;
-	typedef typename TLayout::Interface TInterface;
-
-	for(size_t lvl = 0; lvl < layout.num_levels(); ++lvl){
-		for(TInterfaceIter iter = layout.begin(lvl); iter != layout.end(lvl);)
-		{
-			TInterface& intfc = layout.interface(iter);
-			if(intfc.empty())
-				iter = layout.erase(iter, lvl);
-			else
-				++iter;
-		}
-	}
-}
-////////////////////////////////////////////////////////////////////////
-///	collects the ids of all processes to which interfaces exist.
-/**
- * Fills a vector with the process-ids, to which interfaces exist in
- * the given layout.
- *
- * TLayout has to be compatible with pcl::Layout or pcl::MultiLevelLayout.
- *
- * \returns the number of associated processes.
- */
-template <class TLayout>
-size_t CollectAssociatedProcesses(std::vector<int>& procIDsOut,
-								  TLayout& layout)
-{
-	procIDsOut.clear();
-	
-//	iterate through the levels of the layout
-	for(size_t i = 0; i < layout.num_levels(); ++i){
-	//	iterate through the interfaces on that level
-		for(typename TLayout::iterator iIter = layout.begin(i);
-			iIter != layout.end(i); ++iIter)
-		{
-			int procID = layout.proc_id(iIter);
-		//	check whether the process is already contained in procIDsOut
-			if(i > 0){
-				if(find(procIDsOut.begin(), procIDsOut.end(), procID)
-				   == procIDsOut.end())
-				{
-				//	the entry has not yet been added
-					procIDsOut.push_back(procID);
-				}
-			}
-			else{
-			//	on level 0 each process exists only once
-				procIDsOut.push_back(procID);
-			}
-		}
-	}
-	
-	return procIDsOut.size();
-}
-
-///	writes all elements in the interfaces into the vector.
-/**
- * This function extracts all elements from a layout and stores them into
- * a std::vector. Doubles may occur and are not removed. The container is
- * clear as default, before extracting.
- */
-template <class TLayout>
-void CollectElements(std::vector<typename TLayout::Element>& elemsOut,
-					TLayout& layout,
-					bool clearContainer = true)
-{
-	typedef typename TLayout::Interface Interface;
-	typedef typename TLayout::Element TElem;
-
-//	clear the return value
-	if(clearContainer) elemsOut.clear();
-
-//	iterate over all interfaces
-	for(size_t lvl = 0; lvl < layout.num_levels(); ++lvl){
-		for(typename TLayout::iterator interfaceIter = layout.begin(lvl);
-			interfaceIter != layout.end(lvl); ++interfaceIter)
-		{
-		//	iterate over the entries of the interface
-			Interface& interface = layout.interface(interfaceIter);
-			for(typename Interface::iterator iter = interface.begin();
-				iter != interface.end(); ++iter)
-			{
-			//	add elem to vector
-				elemsOut.push_back(interface.get_element(iter));
-			}
-		}
-	}
-}
-
-///	writes all elements in the interfaces into the resulting vector. avoids doubles.
-template <class TLayout>
-void CollectUniqueElements(std::vector<typename TLayout::Element>& elemsOut,
-							TLayout& layout)
-{
-	typedef typename TLayout::Interface Interface;
-	typedef typename TLayout::Element TElem;
-
-//	clear the return value
-	elemsOut.clear();
-
-//	we'll use a hash to make sure that each element only exists once
-	ug::Hash<int, TElem> hash(layout.num_interface_elements());
-
-//	iterate over all interfaces
-	for(size_t lvl = 0; lvl < layout.num_levels(); ++lvl){
-		for(typename TLayout::iterator interfaceIter = layout.begin(lvl);
-			interfaceIter != layout.end(lvl); ++interfaceIter)
-		{
-		//	iterate over the entries of the interface
-			Interface& interface = layout.interface(interfaceIter);
-			for(typename Interface::iterator iter = interface.begin();
-				iter != interface.end(); ++iter)
-			{
-			//	check whether the entry already exists in the hash
-				if(!hash.has_entries(interface.get_element(iter))){
-				//	we don't care about the value
-					hash.add(0, interface.get_element(iter));
-					elemsOut.push_back(interface.get_element(iter));
-				}
-			}
-		}
-	}
-}
 
 ////////////////////////////////////////////////////////////////////////
 ///	exchanges information about which process wants to communicate with which other process.
