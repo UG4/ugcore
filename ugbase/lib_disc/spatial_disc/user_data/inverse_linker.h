1c3ef70f67f320a88974e4000451b63e7e77526e
Ivo Muha <ivo.muha@gcsc.uni-frankfurt.de>
2013-02-14 15:26:55 +0000

    Inverse_linker added. Linker can be used to multiply or divide.

---
+++ ugbase/lib_disc/spatial_disc/user_data/inverse_linker.h
@@ -0,0 +1,142 @@
+/*
+ * inverse_linker.h
+ *
+ *  Created on: 12.02.2013
+ *      Author: ivomuha, andreasvogel used scale_add_linker ass template
+ */
+
+#ifndef __H__UG__LIB_DISC__SPATIAL_DISC__INVERSE_LINKER__
+#define __H__UG__LIB_DISC__SPATIAL_DISC__INVERSE_LINKER__
+
+#include "lib_disc/spatial_disc/user_data/data_linker.h"
+
+namespace ug{
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Scaled adding of Data
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * This linker recombines the data like
+ *
+ * l = a / b
+ *
+ * \tparam		dim			world dimension
+ * \tparam		TDataScale 	type of scaling data
+ */
+template <int dim>
+class InverseLinker
+	: public StdDataLinker<InverseLinker<dim>, number, dim>
+{
+	public:
+	//	type of base class
+		typedef DataLinker<number, dim> base_type;
+
+	public:
+	///	constructor
+		InverseLinker() {}
+
+	///	constructor
+		InverseLinker(const InverseLinker& linker);
+
+		void divide(SmartPtr<UserData<number, dim> > dividend,
+		         SmartPtr<UserData<number, dim> > divisor);
+		void divide(number dividend,
+		         SmartPtr<UserData<number, dim> > divisor);
+		void divide(SmartPtr<UserData<number, dim> > dividend,
+		         number divisor);
+		void divide(number dividend,
+		         number divisor);
+
+	///	computes the value
+		virtual void compute(LocalVector* u, GeometricObject* elem, bool bDeriv = false);
+
+		inline void evaluate (number& value,
+		                      const MathVector<dim>& globIP,
+		                      number time, int si) const;
+
+		template <int refDim>
+		inline void evaluate (number& value,
+		                      const MathVector<dim>& globIP,
+		                      number time, int si,
+		                      LocalVector& u,
+		                      GeometricObject* elem,
+		                      const MathVector<dim> vCornerCoords[],
+		                      const MathVector<refDim>& locIP) const;
+
+		template <int refDim>
+		inline void evaluate(number vValue[],
+		                     const MathVector<dim> vGlobIP[],
+		                     number time, int si,
+		                     LocalVector& u,
+		                     GeometricObject* elem,
+		                     const MathVector<dim> vCornerCoords[],
+		                     const MathVector<refDim> vLocIP[],
+		                     const size_t nip,
+		                     const MathMatrix<refDim, dim>* vJT = NULL) const;
+
+	protected:
+	///	divisor at ip of input
+		const number& divisor_value(size_t i, size_t s, size_t ip) const
+		{
+			UG_ASSERT(i < m_vpDivisorData.size(), "Input not needed");
+			UG_ASSERT(m_vpDivisorData[i].valid(), "Input invalid");
+			return m_vpDivisorData[i]->value(this->series_id(2*i,s), ip);
+		}
+
+	///	divisor of data at input at ip
+		const number& divisor_deriv(size_t i, size_t s, size_t ip, size_t fct, size_t dof) const
+		{
+			UG_ASSERT(i < m_vpDependData.size(), "Input not needed");
+			UG_ASSERT(m_vpDependData[i].valid(), "Input invalid");
+			return m_vpDependData[i]->deriv(this->series_id(2*i,s), ip, fct, dof);
+		}
+
+	///	dividend at ip of input
+		const number& dividend_value(size_t i, size_t s, size_t ip) const
+		{
+			UG_ASSERT(i < m_vpDividendData.size(), "Input not needed");
+			UG_ASSERT(m_vpDividendData[i].valid(), "Input invalid");
+			return m_vpDividendData[i]->value(this->series_id(2*i+1,s), ip);
+		}
+
+	///	derivative of dividend at input at ip
+		const number& dividend_deriv(size_t i, size_t s, size_t ip, size_t fct, size_t dof) const
+		{
+			UG_ASSERT(i < m_vpDividendDependData.size(), "Input not needed");
+			UG_ASSERT(m_vpDividendDependData[i].valid(), "Input invalid");
+			return m_vpDividendDependData[i]->deriv(this->series_id(2*i+1,s), ip, fct, dof);
+		}
+
+	///	returns number of functions the divisor depends on
+		size_t divisor_num_fct(size_t i) const {return base_type::input_num_fct(2*i);}
+
+	///	returns the number in the common FctGrp for a fct of an divisor
+		size_t divisor_common_fct(size_t i, size_t fct) const	{return base_type::input_common_fct(2*i, fct);}
+
+	///	returns number of functions the dividend depends on
+		size_t dividend_num_fct(size_t i) const {return base_type::input_num_fct(2*i+1);}
+
+	///	returns the number in the common FctGrp for a fct of a dividend
+		size_t dividend_common_fct(size_t i, size_t fct) const	{return base_type::input_common_fct(2*i+1, fct);}
+
+	protected:
+	///	data Dividend
+		std::vector<SmartPtr<UserData<number, dim> > > m_vpDividendData;
+
+	///	data Dividend casted to dependend data
+		std::vector<SmartPtr<DependentUserData<number, dim> > > m_vpDividendDependData;
+
+	///	data Divisor
+		std::vector<SmartPtr<UserData<number, dim> > > m_vpDivisorData;
+
+	///	data Divisor casted to dependend data
+		std::vector<SmartPtr<DependentUserData<number, dim> > > m_vpDependData;
+};
+
+} // end namespace ug
+
+#include "inverse_linker_impl.h"
+
+#endif /* __H__UG__LIB_DISC__SPATIAL_DISC__INVERSE_LINKER__ */
---
+++ ugbase/lib_disc/spatial_disc/user_data/inverse_linker_impl.h
@@ -0,0 +1,258 @@
+/*
+ * inverse_linker_impl.h
+ *
+ *  Created on: 12.02.2013
+ *      Author: ivomuha, andreasvogel scale_add_linker_impl.h used as template
+ */
+
+#ifndef __H__UG__LIB_DISC__SPATIAL_DISC__INVERSE_LINKER_IMPL__
+#define __H__UG__LIB_DISC__SPATIAL_DISC__INVERSE_LINKER_IMPL__
+
+#include "inverse_linker.h"
+#include "lib_disc/spatial_disc/user_data/data_linker_traits.h"
+#include "lib_disc/spatial_disc/user_data/const_user_data.h"
+
+namespace ug{
+
+////////////////////////////////////////////////////////////////////////////////
+//	InverseLinker
+////////////////////////////////////////////////////////////////////////////////
+
+template <int dim>
+InverseLinker<dim>::
+InverseLinker(const InverseLinker& linker)
+{
+	if(linker.m_vpDividendData.size() != linker.m_vpDivisorData.size())
+		UG_THROW("InverseLinker: number of inputs mismatch.");
+
+	for(size_t i = 0; i < linker.m_vpDivisorData.size(); ++i)
+	{
+		this->divide(linker.m_vpDividendData[i], linker.m_vpDivisorData[i]);
+	}
+}
+
+
+template <int dim>
+void InverseLinker<dim>::
+divide(SmartPtr<UserData<number, dim> > dividend, SmartPtr<UserData<number, dim> > divisor)
+{
+
+//	current number of inputs
+	const size_t numInput = base_type::num_input() / 2;
+
+//	resize scaling
+	m_vpDivisorData.resize(numInput+1, NULL);
+	m_vpDependData.resize(numInput+1, NULL);
+	m_vpDividendData.resize(numInput+1, NULL);
+	m_vpDividendDependData.resize(numInput+1, NULL);
+
+//	remember userdata
+	UG_ASSERT(divisor.valid(), "Null Pointer as Input set.");
+	m_vpDivisorData[numInput] = divisor;
+	m_vpDependData[numInput] = divisor.template cast_dynamic<DependentUserData<number, dim> >();
+
+//	remember userdata
+	UG_ASSERT(dividend.valid(), "Null Pointer as Scale set.");
+	m_vpDividendData[numInput] = dividend;
+	m_vpDividendDependData[numInput] = dividend.template cast_dynamic<DependentUserData<number, dim> >();
+
+//	increase number of inputs by one and set inputs at base class
+	base_type::set_num_input(2*numInput+2);
+	base_type::set_input(2*numInput, divisor);
+	base_type::set_input(2*numInput+1, dividend);
+}
+
+template <int dim>
+void InverseLinker<dim>::
+divide(number dividend, SmartPtr<UserData<number, dim> > divisor)
+{
+	divide(CreateConstUserData<dim>(dividend, number()), divisor);
+}
+
+template <int dim>
+void InverseLinker<dim>::
+divide(SmartPtr<UserData<number, dim> > dividend, number divisor)
+{
+	divide(dividend, CreateConstUserData<dim>(divisor, number()));
+}
+
+template <int dim>
+void InverseLinker<dim>::
+divide(number dividend, number divisor)
+{
+	divide(CreateConstUserData<dim>(dividend, number()),
+	    CreateConstUserData<dim>(divisor, number()));
+}
+
+//Scale ist Dividend! UserData ist Divisor
+template <int dim>
+void InverseLinker<dim>::
+evaluate (number& value,
+          const MathVector<dim>& globIP,
+          number time, int si) const
+{
+	//	reset value
+	value = 1.0;
+
+	number valDivisor=0;
+	number valDividend=0;
+
+	for(size_t c = 0; c < m_vpDivisorData.size(); ++c)
+	{
+		(*m_vpDivisorData[c])(valDivisor, globIP, time, si);
+		(*m_vpDividendData[c])(valDividend, globIP, time, si);
+		value *= valDividend/valDivisor;
+	}
+}
+
+template <int dim>
+template <int refDim>
+void InverseLinker<dim>::
+evaluate (number& value,
+          const MathVector<dim>& globIP,
+          number time, int si,
+          LocalVector& u,
+          GeometricObject* elem,
+          const MathVector<dim> vCornerCoords[],
+          const MathVector<refDim>& locIP) const
+{
+	//	reset value
+	value = 1.0;
+
+	number valDivisor = 0;
+	number valDividend = 0;
+
+
+
+//	add contribution of each summand
+	for(size_t c = 0; c < m_vpDivisorData.size(); ++c)
+	{
+		(*m_vpDivisorData[c])(valDivisor, globIP, time, si, u, elem, vCornerCoords, locIP);
+		(*m_vpDividendData[c])(valDividend, globIP, time, si, u, elem, vCornerCoords, locIP);
+
+		value *= valDividend/valDivisor;
+	}
+}
+
+template <int dim>
+template <int refDim>
+void InverseLinker<dim>::
+evaluate(number vValue[],
+         const MathVector<dim> vGlobIP[],
+         number time, int si,
+         LocalVector& u,
+         GeometricObject* elem,
+         const MathVector<dim> vCornerCoords[],
+         const MathVector<refDim> vLocIP[],
+         const size_t nip,
+         const MathMatrix<refDim, dim>* vJT) const
+{
+	//	reset value
+	for(size_t ip = 0; ip < nip; ++ip)
+		vValue[ip] = 1.0;
+
+	std::vector<number> vValData(nip);
+	std::vector<number> vValScale(nip);
+
+//	add contribution of each summand
+	for(size_t c = 0; c < m_vpDivisorData.size(); ++c)
+	{
+		(*m_vpDivisorData[c])(&vValData[0], vGlobIP, time, si, u,
+						elem, vCornerCoords, vLocIP, nip, vJT);
+		(*m_vpDividendData[c])(&vValScale[0], vGlobIP, time, si, u,
+							elem, vCornerCoords, vLocIP, nip, vJT);
+
+		for(size_t ip = 0; ip < nip; ++ip)
+			vValue[ip] *=  vValScale[ip]/vValData[ip];
+	}
+}
+
+template <int dim>
+void InverseLinker<dim>::
+compute(LocalVector* u, GeometricObject* elem, bool bDeriv)
+{
+//	check that size of Scalings and inputs is equal
+	UG_ASSERT(m_vpDivisorData.size() == m_vpDividendData.size(), "Wrong num Scales.");
+
+//	compute value
+	for(size_t s = 0; s < this->num_series(); ++s)
+		for(size_t ip = 0; ip < this->num_ip(s); ++ip)
+		{
+		//	reset value
+			this->value(s,ip) = 1.0;
+
+		//	add contribution of each summand
+			for(size_t c = 0; c < m_vpDivisorData.size(); ++c)
+			{
+				this->value(s, ip) *= dividend_value(c,s,ip)/divisor_value(c,s,ip);
+			}
+		}
+
+//	check if derivative is required
+	if(!bDeriv || this->zero_derivative()) return;
+
+//	check sizes
+	UG_ASSERT(m_vpDependData.size() == m_vpDividendDependData.size(),
+	          	  	  	  	  	  	  	  	  	  "Wrong num Scales.");
+
+//	clear all derivative values
+	this->clear_derivative_values();
+
+//	loop all inputs
+	for(size_t c = 0; c < m_vpDivisorData.size(); ++c)
+	{
+	//	check if Divisor has derivative
+		if(!m_vpDivisorData[c]->zero_derivative())
+		{
+			for(size_t s = 0; s < this->num_series(); ++s)
+				for(size_t ip = 0; ip < this->num_ip(s); ++ip)
+				{
+				//	loop functions
+					for(size_t fct = 0; fct < divisor_num_fct(c); ++fct)
+					{
+					//	get common fct id for this function
+						const size_t commonFct = divisor_common_fct(c, fct);
+
+					//	loop dofs
+						for(size_t sh = 0; sh < this->num_sh(fct); ++sh)
+						{
+							this->deriv(s, ip, commonFct, sh) += this->value(s,ip) / (dividend_value(c,s,ip)/divisor_value(c,s,ip))*(-1.0)*(dividend_value(c,s,ip))/divisor_value(c,s,ip)/divisor_value(c,s,ip)*divisor_deriv(c, s, ip, fct, sh);
+							 //        input_deriv(c, s, ip, fct, sh),
+							 //        scale_value(c, s, ip));
+						}
+					}
+				}
+		}
+
+	//	check if Dividend has derivative
+		if(!m_vpDividendData[c]->zero_derivative())
+		{
+			for(size_t s = 0; s < this->num_series(); ++s)
+				for(size_t ip = 0; ip < this->num_ip(s); ++ip)
+				{
+				//	loop functions
+					for(size_t fct = 0; fct < dividend_num_fct(c); ++fct)
+					{
+					//	get common fct id for this function
+						const size_t commonFct = dividend_common_fct(c, fct);
+
+					//	loop dofs
+						for(size_t sh = 0; sh < this->num_sh(fct); ++sh)
+						{
+
+							this->deriv(s, ip, commonFct, sh) += this->value(s,ip) / (dividend_value(c,s,ip)/divisor_value(c,s,ip))*dividend_deriv(c, s, ip, fct, sh)/divisor_value(c,s,ip);
+							//linker_traits<TData, TDataScale>::
+							//mult_add(this->deriv(s, ip, commonFct, sh),
+							//		 input_value(c, s, ip),
+							//		 scale_deriv(c, s, ip, fct, sh));
+						}
+					}
+				}
+		}
+	}
+}
+
+
+} // end namespace ug
+
+#endif /* __H__UG__LIB_DISC__SPATIAL_DISC__INVERSE_LINKER_IMPL__ */
