1093103a2200148084f8dc26bd889c72aa66a465
Martin Scherer <martin.scherer@gcsc.uni-frankfurt.de>
2013-02-21 15:21:51 +0000

    handling of cxx flags changed to more non invasive method. - CMAKE_BUILD_TYPE is not set anymore - Possibility to use build types in combination with our own flags (possible   because we do not overwrite cmakes flags for specific build type) - added warnings if DEBUG=ON and build type set to Release (leads to "incompatible" flags

--- cmake/compiler_flags.cmake
+++ cmake/compiler_flags.cmake
@@ -1,26 +1,19 @@
+# This function sets the CMAKE_C_FLAGS and CMAKE_CXX_FLAGS variabels.
+# 
+# TODO: maybe add sanity checks for flags
+#
 function(add_cxx_flag flag)
-    if(NOT ${CMAKE_BUILD_TYPE})
-       message(WARNING, "No build type set. Your cflags would have been ignored.")
-       message(WARNING, "Do not call add_cxx_flag before set(CMAKE_BUILD_TYPE ...)")
-       return()
-    endif()
-    
-    # reuse initial cflag of build type, if set, once.
-    # eg. debug cflag -g etc.
-    if(NOT FIRST_CALL_ADD_CXX_FLAG)
-        set(FIRST_CALL_ADD_CXX_FLAG 42 PARENT_SCOPE)
-            foreach(lang C CXX)
-                 set(CMAKE_${lang}_FLAGS_${CMAKE_BUILD_TYPE} 
-                    "${CMAKE_${lang}_FLAGS_${CMAKE_BUILD_TYPE}_INIT}"
-                    CACHE STRING "overriden flags!" FORCE)
-                 #message("set init flag to: ${CMAKE_${lang}_FLAGS_${CMAKE_BUILD_TYPE}_INIT}")
-            endforeach()
-    endif()
-
-    # now append given flag to build type flags
-    foreach(lang C CXX)
-      set(CMAKE_${lang}_FLAGS_${CMAKE_BUILD_TYPE} 
-        "${CMAKE_${lang}_FLAGS_${CMAKE_BUILD_TYPE}} ${flag}" 
-        CACHE STRING "overriden flags!" FORCE)
-    endforeach()
+	foreach(lang C CXX)
+		set(CMAKE_${lang}_FLAGS
+			"${CMAKE_${lang}_FLAGS} ${flag}"
+			CACHE STRING "overriden flags!" FORCE)
+	endforeach()
 endfunction(add_cxx_flag)
+
+
+# used to clear c/cxx flags
+function(reset_cxx_flags)
+	foreach(lang C CXX)
+			set(CMAKE_${lang}_FLAGS "" CACHE STRING "clear flags" FORCE)
+	endforeach()
+endfunction(reset_cxx_flags)
