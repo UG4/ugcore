828f9ad4e804cf3413cc553718062a50321cfbb8
Andreas Vogel <andreas.vogel@gcsc.uni-frankfurt.de>
2013-02-26 13:47:36 +0000

    Changing structure of ConvectionDiffusion: Now one common base class and all concrete implementations separated into derived classes. This structure is reflected by a subfolder system.

---
+++ scripts/util/domain_disc_util.lua
@@ -0,0 +1,21 @@
+--[[!
+\file scripts/util/domain_disc_util.lua
+\brief	functions to create DomainDiscs using a string disc-type identifier
+]]--
+
+
+--!	Returns a ConvectionDiffusion Element-Disc of the requested type
+--! @return Returns the domain discreatization
+--! @param fcts (String) names of symbolic functions 
+--! @param subsets (String) names of symbolic subsets 
+--! @param discType (String) discretizatin scheme 
+function ConvectionDiffusion(fcts, subsets, discType)
+	if 		discType == "fv1"  then return ConvectionDiffusionFV1(fcts, subsets)
+	elseif  discType == "fe"   then return ConvectionDiffusionFE(fcts, subsets)
+	elseif  discType == "fvcr" then return ConvectionDiffusionFVCR(fcts, subsets)
+	elseif  discType == "fv"   then return ConvectionDiffusionFV(fcts, subsets)
+	else 
+		print("ConvectionDiffusion: no disc type '"..discType.."' available. Aborting")
+		exit();
+	end
+end
