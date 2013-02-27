bacc5ce874d6adfef485b622e8a6c3591a763c72
Andreas Vogel <andreas.vogel@gcsc.uni-frankfurt.de>
2013-02-27 14:43:36 +0000

    Using Provider based on order identifier.

--- ugbase/common/util/provider.h
+++ ugbase/common/util/provider.h
@@ -8,6 +8,8 @@
 #ifndef __H__COMMON__UTIL__PROVIDER__
 #define __H__COMMON__UTIL__PROVIDER__
 
+#include <vector>
+
 namespace ug{
 
 /// Provider, holding a single instance of an object
@@ -19,18 +21,59 @@ namespace ug{
  * same object is intended to be used, but no passing is possible or wanted.
  */
 template <typename TClass>
-struct Provider
+class Provider
 {
-	///	type of provided object
+	protected:
+		/// private constructor
+		Provider() {}
+
+		/// singleton provider
+		static Provider<TClass>& inst() {
+			static Provider<TClass> inst;
+			return inst;
+		}
+
+		/// vector holding instances
+		static std::vector<TClass*> m_vClass;
+
+		/// returns class based on identifier
+		static TClass& get_class(size_t p) {
+			if(p >= m_vClass.size()){
+				m_vClass.resize(p+1, NULL);
+			}
+			if(m_vClass[p] == NULL){
+				m_vClass[p] = new TClass();
+			}
+			return *m_vClass[p];
+		}
+
+		/// clears all instances
+		static void clear_classes(){
+			for(size_t i = 0; m_vClass.size(); ++i)
+				if(m_vClass[i])
+					delete m_vClass[i];
+
+			m_vClass.clear();
+		}
+
+	public:
+		///	type of provided object
 		typedef TClass Type;
 
-	///	returns access to the singleton
-		inline static TClass& get()
-		{
-			static TClass myInst;
-			return myInst;
+		///	returns a singleton based on the identifier
+		static inline TClass& get(size_t p = 1){
+			return inst().get_class(p);
+		}
+
+		///	clears all singletons
+		static inline void clear(){
+			return inst().clear_classes();
 		}
 };
+
+template <typename TClass>
+std::vector<TClass*> Provider<TClass>::m_vClass = std::vector<TClass*>();
+
 
 } // end namespace ug
 
