7611783c1324c1abc0e9e687b72df70213cd342c
Andreas Vogel <andreas.vogel@gcsc.uni-frankfurt.de>
2013-03-07 16:31:26 +0000

    Restructuring NavierStokes module: grouping discretization type dependent files into folders, splitting objects and creating separated registering files.

--- scripts/util/domain_disc_util.lua
+++ scripts/util/domain_disc_util.lua
@@ -65,3 +65,61 @@ function DensityDrivenFlow(fcts, subsets, discType)
 		exit();
 	end
 end
+
+--!	Returns a NavierStokes Element-Disc of the requested type
+--! @return Returns the domain discreatization
+--! @param fcts (String) names of symbolic functions 
+--! @param subsets (String) names of symbolic subsets 
+--! @param discType (String) discretizatin scheme 
+function NavierStokes(fcts, subsets, discType)
+	if discType == nil then discType = "fv1" end
+	if 		discType == "fv1"  then return NavierStokesFV1(fcts, subsets)
+	elseif  discType == "fe"   then return NavierStokesFE(fcts, subsets)
+	elseif  discType == "fvcr" then return NavierStokesFVCR(fcts, subsets)
+	elseif  discType == "fv"   then return NavierStokesFV(fcts, subsets)
+	else 
+		print("NavierStokes: no disc type '"..discType.."' available. Aborting")
+		exit();
+	end
+end
+
+--!	Returns a NavierStokesInflow Element-Disc of the requested type
+--! @return Returns the domain discreatization
+--! @param fcts (String) names of symbolic functions 
+--! @param subsets (String) names of symbolic subsets 
+--! @param discType (String) discretizatin scheme 
+function NavierStokesInflow(spMaster)
+	if spMaster == nil then
+		print("NavierStokesInflow: master disc is not available. Aborting")
+		exit();
+	end
+
+	discType = spMaster:disc_type();	
+	if 		discType == "fv1"  then return NavierStokesInflowFV1(spMaster)
+	elseif  discType == "fvcr" then return NavierStokesInflowFVCR(spMaster)
+	elseif  discType == "fv"   then return NavierStokesInflowFV(spMaster)
+	else 
+		print("NavierStokesInflow: no disc type '"..discType.."' available. Aborting")
+		exit();
+	end
+end
+
+--!	Returns a NavierStokesNoNormalStressOutflow Element-Disc of the requested type
+--! @return Returns the domain discreatization
+--! @param fcts (String) names of symbolic functions 
+--! @param subsets (String) names of symbolic subsets 
+--! @param discType (String) discretizatin scheme 
+function NavierStokesNoNormalStressOutflow(spMaster)
+	if spMaster == nil then
+		print("NavierStokesNoNormalStressOutflow: master disc is not available. Aborting")
+		exit();
+	end
+
+	discType = spMaster:disc_type();	
+	if 		discType == "fv1"  then return NavierStokesNoNormalStressOutflowFV1(spMaster)
+	elseif  discType == "fvcr"   then return NavierStokesNoNormalStressOutflowFVCR(spMaster)
+	else 
+		print("NavierStokesNoNormalStressOutflow: no disc type '"..discType.."' available. Aborting")
+		exit();
+	end
+end
