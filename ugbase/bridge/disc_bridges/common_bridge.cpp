4f936b38260a7bf2dd9e745f7fafcbebd73d3d0e
Raphael Prohl <raphael.prohl@gcsc.uni-frankfurt.de>
2013-03-14 12:07:06 +0000

    register MultiIndex<2>

--- ugbase/bridge/disc_bridges/common_bridge.cpp
+++ ugbase/bridge/disc_bridges/common_bridge.cpp
@@ -13,6 +13,7 @@
 
 // includes of lib_discretization
 #include "lib_disc/common/function_group.h"
+#include "lib_disc/common/multi_index.h"
 #include "lib_disc/dof_manager/function_pattern.h"
 #include "lib_disc/spatial_disc/elem_disc/elem_disc_interface.h"
 
@@ -30,22 +31,28 @@ void RegisterBridge_DiscCommon(Registry& reg, string parentGroup)
 
 	try
 	{
+		//  MultiIndex2
+		{
+			typedef MultiIndex<2, size_t> T;
+			string name = string("MultiIndex2");
+			reg.add_class_<T>(name, grp);
+		}
 
 #ifdef UG_PARALLEL
-//	IDomainDecompositionInfo, StandardDomainDecompositionInfo
-	{
-	typedef pcl::IDomainDecompositionInfo Tbase;
-	reg.add_class_<Tbase>("IDomainDecompositionInfo", grp);
-	typedef pcl::StandardDomainDecompositionInfo T;
-	reg.add_class_<T, Tbase>("StandardDomainDecompositionInfo", grp)
-		.add_constructor()
-		.add_method("map_proc_id_to_subdomain_id", &T::map_proc_id_to_subdomain_id)
-		.add_method("set_num_subdomains",          &T::set_num_subdomains)
-		.add_method("get_num_subdomains",          &T::get_num_subdomains)
-		.add_method("set_num_spatial_dimensions",  &T::set_num_spatial_dimensions)
-		.add_method("get_num_spatial_dimensions",  &T::get_num_spatial_dimensions)
-		.set_construct_as_smart_pointer(true);
-	}
+	//	IDomainDecompositionInfo, StandardDomainDecompositionInfo
+		{
+		typedef pcl::IDomainDecompositionInfo Tbase;
+		reg.add_class_<Tbase>("IDomainDecompositionInfo", grp);
+		typedef pcl::StandardDomainDecompositionInfo T;
+		reg.add_class_<T, Tbase>("StandardDomainDecompositionInfo", grp)
+			.add_constructor()
+			.add_method("map_proc_id_to_subdomain_id", &T::map_proc_id_to_subdomain_id)
+			.add_method("set_num_subdomains",          &T::set_num_subdomains)
+			.add_method("get_num_subdomains",          &T::get_num_subdomains)
+			.add_method("set_num_spatial_dimensions",  &T::set_num_spatial_dimensions)
+			.add_method("get_num_spatial_dimensions",  &T::get_num_spatial_dimensions)
+			.set_construct_as_smart_pointer(true);
+		}
 #endif
 
 	}
