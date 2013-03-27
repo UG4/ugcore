80cab754937670e076d5b4782b3cd4f0bd52e914
Martin Scherer <martin.scherer@gcsc.uni-frankfurt.de>
2013-03-27 13:31:36 +0000

    added handling for custom CXX_FLAGS and C_FLAGS: CXX_FLAGS="-march=native" cmake .. will add your cflags to the cache which means you only need to invoke this once. To reset your custom cflags: CXX_FLAGS="" cmake ..
    The same applies for C_FLAGS

--- cmake/compiler_flags.cmake
+++ cmake/compiler_flags.cmake
@@ -3,11 +3,11 @@
 # TODO: maybe add sanity checks for flags
 #
 function (add_c_flag flag)
-    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${flag}" CACHE STRING "overriden flags!" FORCE)
+	set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${flag}" CACHE STRING "overriden flags!" FORCE)
 endfunction(add_c_flag)
 
 function (add_cpp_flag flag)
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${flag}" CACHE STRING "overriden flags!" FORCE)
+	set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${flag}" CACHE STRING "overriden flags!" FORCE)
 endfunction(add_cpp_flag)
 
 function(add_cxx_flag flag)
@@ -15,10 +15,24 @@ function(add_cxx_flag flag)
 	add_cpp_flag(${flag})
 endfunction(add_cxx_flag)
 
-
-# used to clear c/cxx flags
+################################################################################
+# Used to clear c/cxx flags. Handles environment variables CXX_FLAGS and C_FLAGS
 function(reset_cxx_flags)
+	# check env for set flags, and overwrite present cxx flag string
+	set(CXX_FLAGS "$ENV{CXX_FLAGS}" CACHE STRING "custom cxx flags" FORCE)
+	set(C_FLAGS "$ENV{C_FLAGS}" CACHE STRING "custom c flags" FORCE)
+	
+	# first reset flags, then check env for given flags
 	foreach(lang C CXX)
-			set(CMAKE_${lang}_FLAGS "" CACHE STRING "clear flags" FORCE)
+		set(CMAKE_${lang}_FLAGS "" CACHE STRING "clear flags" FORCE)
 	endforeach()
+	
+	# re-add custom flags for c and c++
+	if(CXX_FLAGS)
+		add_cpp_flag(${CXX_FLAGS})
+	endif()
+	
+	if(C_FLAGS)
+		add_c_flag(${CXX_FLAGS})
+	endif()
 endfunction(reset_cxx_flags)
