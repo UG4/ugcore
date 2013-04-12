eba7932b7d5cd0cc6d8812b303d2b4db66acb4cd
Torbjörn Klatt <torbjoern.klatt@gcsc.uni-frankfurt.de>
2013-04-12 18:06:16 +0000

    [docu] fixing some reference bugs of grouped docu

--- docs/generate_amalgamated_docu.sh
+++ docs/generate_amalgamated_docu.sh
@@ -1,3 +1,46 @@
 #!/bin/bash
 
-doxygen - < doxy_config_ugbase_tags.txt && doxygen - < doxy_config_plugins.txt && doxygen - < doxy_config_apps.txt && doxygen - < doxy_config_ug4.txt && mkdir ug4/html/apps ug4/html/plugins && mv apps/html/* ug4/html/apps/. && mv plugins/html/* ug4/html/plugins/.
+# remove old html and tags
+function cleanup_old_docu {
+	rm -rf ug4/html ug4/plugins/html ug4/apps/html *.tags
+}
+
+# generate tags for ugbase
+function generate_ugbase {
+	doxygen - < doxy_config_ugbase_tags.txt
+}
+
+# generate tags and html for plugins
+function generate_plugins {
+	doxygen - < doxy_config_plugins.txt
+}
+
+# generate html for apps
+function generate_apps {
+	doxygen - < doxy_config_apps.txt
+}
+
+# generate html for ugbase
+function generate_ug4 {
+	doxygen - < doxy_config_ug4_mathjax.txt
+}
+
+# prepare amalgamation of html
+function prepare_amalgamate {
+	mkdir -p ug4/html/apps ug4/html/plugins
+}
+
+# move all html into a single directory
+function amalgamate {
+	mv -fu apps/html/* ug4/html/apps/. && mv -fu plugins/html/* ug4/html/plugins/.
+}
+
+# stitch all steps into a single command list
+# (if any fails, all fail)
+cleanup_old_docu \
+&& generate_ugbase \
+&& generate_plugins \
+&& generate_apps \
+&& generate_ug4 \
+&& prepare_amalgamate \
+&& amalgamate
