1daa3bdfa75a9f51f08f3b3e4384574ff038440e
Torbjörn Klatt <torbjoern.klatt@gcsc.uni-frankfurt.de>
2013-04-29 14:02:02 +0000

    [docu] including Registry Docu into complete Docu

--- docs/generate_amalgamated_docu.sh
+++ docs/generate_amalgamated_docu.sh
@@ -1,47 +1,73 @@
 #!/bin/bash
 
 doxylog="$1"
+with_regdocu="${2:-0}"
 
 # remove old html and tags
 function cleanup_old_docu {
-	echo "Step 1/7: Removing old docu if existend"
-	rm -rf ug4/html ug4/plugins/html ug4/apps/html *.tags &> $doxylog
+	echo "Step 1/8: Removing old docu if existend"
+	if [ "$with_regdocu" -eq "1" ]; then
+		rm -rf ug4/html ug4/plugins/html ug4/apps/html ug4/regdocu/html *.tags &> "${doxylog}1"
+	else
+		rm -rf ug4/html ug4/plugins/html ug4/apps/html *.tags &> "${doxylog}1"
+	fi
 }
 
 # generate tags for ugbase
 function generate_ugbase {
-	echo "Step 2/7: Generating tags for ugbase"
-	doxygen - < doxy_config_ugbase_tags.txt 1>> $doxylog 2>> $doxylog
+	echo "Step 2/8: Generating tags for ugbase"
+	doxygen - < doxy_config_ugbase_tags.txt &> "${doxylog}2"
 }
 
 # generate tags and html for plugins
 function generate_plugins {
-	echo "Step 3/7: Generating tags and html for plugins"
-	doxygen - < doxy_config_plugins.txt 1>> $doxylog 2>> $doxylog
+	echo "Step 3/8: Generating tags and html for plugins"
+	doxygen - < doxy_config_plugins.txt &> "${doxylog}3"
+}
+
+function generate_regdocu {
+	if [ "$with_regdocu" -eq "1" ]; then
+		echo "Step 4/8: Generating tags and html for Registry"
+		doxygen - < doxy_config_regdocu.txt &> "${doxylog}4"
+	else
+		echo "Step 4/8: Skipping tags and html for Registry"
+		echo "  WARNING This is not recommended as some links in the final docu will be broken!"
+	fi
 }
 
 # generate html for apps
 function generate_apps {
-	echo "Step 4/7: Generating tags and html for apps"
-	doxygen - < doxy_config_apps.txt 1>> $doxylog 2>> $doxylog
+	echo "Step 5/8: Generating tags and html for apps"
+	doxygen - < doxy_config_apps.txt &> "${doxylog}5"
 }
 
 # generate html for ugbase
 function generate_ug4 {
-	echo "Step 5/7: Generating html for ugbase"
-	doxygen - < doxy_config_ug4_mathjax.txt 1>> $doxylog 2>> $doxylog
+	echo "Step 6/8: Generating html for ugbase"
+	doxygen - < doxy_config_amalgamated.txt &> "${doxylog}6"
 }
 
 # prepare amalgamation of html
 function prepare_amalgamate {
-	echo "Step 6/7: Prepare amalgamation"
-	mkdir -p ug4/html/apps ug4/html/plugins 1>> $doxylog 2>> $doxylog
+	echo "Step 7/8: Prepare amalgamation"
+	if [ "$with_regdocu" -eq 1 ]; then
+		mkdir -p ug4/html/apps ug4/html/plugins ug4/html/regdocu &> "${doxylog}7"
+	else
+		mkdir -p ug4/html/apps ug4/html/plugins &> "${doxylog}7"
+	fi
 }
 
 # move all html into a single directory
 function amalgamate {
-	echo "Step 7/7: Amalgamate html of ubgase, plugins and apps"
-	mv -fu apps/html/* ug4/html/apps/. && mv -fu plugins/html/* ug4/html/plugins/. 1>> $doxylog 2>> $doxylog
+	echo "Step 8/8: Amalgamate html of ubgase, plugins and apps"
+	if [ "$with_regdocu" -eq "1" ]; then
+		mv -fu apps/html/* ug4/html/apps/. \
+			&& mv -fu plugins/html/* ug4/html/plugins/. \
+			&& mv -fu regdocu/html/* ug4/html/regdocu/. &> "${doxylog}8"
+	else
+		mv -fu apps/html/* ug4/html/apps/. \
+			&& mv -fu plugins/html/* ug4/html/plugins/. &> "${doxylog}8"
+	fi
 }
 
 # stitch all steps into a single command list
@@ -49,7 +75,9 @@ function amalgamate {
 cleanup_old_docu \
 && generate_ugbase \
 && generate_plugins \
+&& generate_regdocu \
 && generate_apps \
 && generate_ug4 \
 && prepare_amalgamate \
 && amalgamate
+
