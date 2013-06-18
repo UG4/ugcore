49d019f3eadbd159286e9b309d283adfab1a4344
Martin Rupp <martin.rupp@gcsc.uni-frankfurt.de>
2013-06-18 10:19:12 +0000

    IsNormalAndNotTooBig to check results for NaN etc

---
+++ ugbase/lib_algebra/algebra_common/vector_util.h
@@ -0,0 +1,34 @@
+/*
+ * vector_util.h
+ *
+ *  Created on: 18.06.2013
+ *      Author: mrupp
+ */
+
+#ifndef VECTOR_UTIL_H_
+#define VECTOR_UTIL_H_
+
+#include "lib_algebra/small_algebra/blocks.h"
+#include <boost/math/special_functions/fpclassify.hpp>
+
+namespace ug{
+template<typename TVector>
+bool IsNormalAndNotTooBig(TVector &v, double tooBigValue=1e24)
+{
+	for(size_t i=0; i<v.size(); i++)
+	{
+		for(size_t j=0; j< GetSize(v[i]); j++)
+		{
+			double d = BlockRef(v[i], j);
+			if(d > tooBigValue || d < -tooBigValue
+					|| isnormal(d) == false)
+			{
+				return false;
+			}
+		}
+	}
+	return true;
+}
+
+}
+#endif /* VECTOR_UTIL_H_ */
