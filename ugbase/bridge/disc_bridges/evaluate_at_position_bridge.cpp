2c7520b2c616b2a36c60d22f66d27c6d9e691ceb
Ivo Muha <ivo.muha@gcsc.uni-frankfurt.de>
2013-08-02 09:18:03 +0000

    EvaluateAtClosestVertex finished, thank you Andreas!

--- ugbase/bridge/disc_bridges/evaluate_at_position_bridge.cpp
+++ ugbase/bridge/disc_bridges/evaluate_at_position_bridge.cpp
@@ -18,7 +18,6 @@
 // lib_disc includes
 #include "lib_disc/dof_manager/surface_dof_distribution.h"
 #include "lib_disc/function_spaces/grid_function.h"
-#include "lib_disc/function_spaces/evaluate_at_position.h"
 
 using namespace std;
 
@@ -31,6 +30,109 @@ namespace Evaluate{
  * \ingroup disc_bridge
  * \{
  */
+
+template <typename TGridFunction>
+number EvaluateAtVertex(const MathVector<TGridFunction::dim>& globPos,
+                           SmartPtr<TGridFunction> spGridFct,
+                           size_t fct,
+                           const SubsetGroup& ssGrp)
+{
+
+//	domain type and position_type
+	typedef typename TGridFunction::domain_type domain_type;
+	typedef typename domain_type::position_type position_type;
+
+// get position accessor
+	const typename domain_type::position_accessor_type& aaPos
+										= spGridFct->domain()->position_accessor();
+
+	std::vector<MultiIndex<2> > ind;
+	typename TGridFunction::template dim_traits<0>::const_iterator iterEnd, iter,chosen;
+	double min_distance;
+
+	bool bInit = false;
+	for(size_t i = 0; i < ssGrp.size(); ++i)
+	{
+	//	get subset index
+		const int si = ssGrp[i];
+
+	//	skip if function is not defined in subset
+		if(!spGridFct->is_def_in_subset(fct, si)) continue;
+
+	// 	iterate over all elements
+		iterEnd = spGridFct->template end<VertexBase>(si);
+		iter = spGridFct->template begin<VertexBase>(si);
+		for(; iter != iterEnd; ++iter)
+		{
+		//	get element
+			VertexBase* vrt = *iter;
+
+		//	global position
+			if(!bInit)
+			{
+				bInit = true;
+				min_distance = VecDistance(globPos, aaPos[vrt]);
+				chosen = iter;
+			}
+			else
+			{
+				double buffer = VecDistance(globPos, aaPos[vrt]);
+				if(buffer<min_distance)
+				{
+					min_distance = buffer;
+					chosen = iter;
+				}
+			}
+		}
+
+
+	}
+
+	VertexBase* vrt = *chosen;
+	spGridFct->inner_multi_indices(vrt, fct, ind);
+	return 	DoFRef(*spGridFct, ind[0]);
+
+}
+
+
+template <typename TGridFunction>
+number EvaluateAtClosestVertex(const std::vector<number>& globPos,
+                                  SmartPtr<TGridFunction> spGridFct, const char* cmp,
+                                  const char* subsets)
+{
+	static const int dim = TGridFunction::dim;
+
+	if(globPos.size() != dim)
+		UG_THROW("Dimension is "<<dim<<", but passed.");
+
+	MathVector<dim> pos;
+	for(size_t i = 0; i < globPos.size(); ++i)
+		pos[i] = globPos[i];
+
+//	get function id of name
+	const size_t fct = spGridFct->fct_id_by_name(cmp);
+
+//	check that function found
+	if(fct > spGridFct->num_fct())
+		UG_THROW("Evaluate: Name of component '"<<cmp<<"' not found.");
+
+
+//	create subset group
+	SubsetGroup ssGrp(spGridFct->domain()->subset_handler());
+	if(subsets != NULL)
+	{
+		ssGrp.add(TokenizeString(subsets));
+	}
+	else
+	{
+	//	add all subsets and remove lower dim subsets afterwards
+		ssGrp.add_all();
+	}
+
+	return EvaluateAtVertex<TGridFunction>(pos, spGridFct, fct, ssGrp);
+
+}
+
 
 /**
  * Class exporting the functionality. All functionality that is to
@@ -61,7 +163,7 @@ static void DomainAlgebra(Registry& reg, string grp)
 	{
 	//	reg.add_function("Integral", static_cast<number (*)(SmartPtr<UserData<number,dim> >, SmartPtr<TFct>, const char*, number, int)>(&Integral<TFct>), grp, "Integral", "Data#GridFunction#Subsets#Time#QuadOrder");
 
-		reg.add_function("Evaluate_at_closest_vertex2D", static_cast<number (*)(number,number, SmartPtr<TFct>, const char*, const char*)>(&Evaluate_at_closest_vertex<TFct>),grp, "Evaluate_at_closest_vertex2D", "PositionX#PositionY#GridFunction#Component#Subsets");
+		reg.add_function("EvaluateAtClosestVertex", static_cast<number (*)(const std::vector<number>&, SmartPtr<TFct>, const char*, const char*)>(&EvaluateAtClosestVertex<TFct>),grp, "Evaluate_at_closest_vertex", "Position#GridFunction#Component#Subsets");
 
 	}
 }
