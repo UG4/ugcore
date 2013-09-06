eefb125d360e09ede5649b5b8a6e17d7ae9db8a0
Andreas Vogel <andreas.vogel@gcsc.uni-frankfurt.de>
2013-09-06 14:52:32 +0000

    for components now

--- scripts/util/conv_rates_static.lua
+++ scripts/util/conv_rates_static.lua
@@ -1,11 +1,12 @@
 
-function util.resetErrorRatesStorage(errRates, minLev, maxLev, defValue)
+function util.resetErrorRatesStorage(errRates, minLev, maxLev, FctCmp, defValue)
 
 	if defValue == nil then defValue = "--" end
 	
 	errRates.minLev = minLev
 	errRates.maxLev = maxLev
 	errRates.numDoFs = {}
+	errRates.FctCmp = FctCmp
 	
 	function util.createNormStorage(norm)
 
@@ -18,11 +19,18 @@ function util.resetErrorRatesStorage(errRates, minLev, maxLev, defValue)
 			end
 
 			normType.value = {}
-			util.createNormTypeRatesStorage(normType.value)
 			normType.fac = {}
-			util.createNormTypeRatesStorage(normType.fac)
 			normType.rate = {}
-			util.createNormTypeRatesStorage(normType.rate)
+			
+			for i = 1, #FctCmp do
+				local f = FctCmp[i]
+				normType.value[f] = {}
+				util.createNormTypeRatesStorage(normType.value[f])
+				normType.fac[f] = {}
+				util.createNormTypeRatesStorage(normType.fac[f])
+				normType.rate[f] = {}
+				util.createNormTypeRatesStorage(normType.rate[f])
+			end
 		end
 		
 		-- error w.r.t to exact solution
@@ -65,13 +73,17 @@ end
 
 function util.computeErrorRates(errRates)
 	
+	local FctCmp = errRates.FctCmp
 	function util.computeNormErrorRates(norm)
 	
 		function util.computeNormTypeErrorRates(normType, minLev, maxLev)
-			for lev = minLev + 1, maxLev do
-				normType.fac[lev] = normType.value[lev-1]/normType.value[lev]
-				normType.rate[lev] = math.log(normType.fac[lev]) / math.log(2)
-			end		
+			for i = 1, #FctCmp do
+				local f = FctCmp[i]
+				for lev = minLev + 1, maxLev do
+					normType.fac[f][lev] = normType.value[f][lev-1]/normType.value[f][lev]
+					normType.rate[f][lev] = math.log(normType.fac[f][lev]) / math.log(2)
+				end		
+			end
 		end
 
 		local minLev = errRates.minLev
@@ -86,13 +98,13 @@ function util.computeErrorRates(errRates)
 	util.computeNormErrorRates(errRates.h1)
 end
 
-function util.updateErrorRatesScreenOutput(err)
+function util.updateErrorRatesScreenOutput(err, f)
 
 	function util.addConvRateOutputNorm(err, norm)
 		
 		function util.addConvRateOutputNormType(err, norm, type)
 			
-			err.screen.values = gnuplot.array_concat(err.screen.values, {type.value, type.rate}) 
+			err.screen.values = gnuplot.array_concat(err.screen.values, {type.value[f], type.rate[f]}) 
 			err.screen.title  = gnuplot.array_concat(err.screen.title,  {norm.title.." "..type.title, "rate"})
 			err.screen.format = gnuplot.array_concat(err.screen.format, {"%.2e", "%.3f"})
 		end
@@ -113,11 +125,13 @@ end
 
 function util.writeAndScheduleGnuplotData(err, discType, p)
 
-	function util.writeAndScheduleGnuplotDataType(err, discType, p, errSuffix, l2value, h1value,
+	local FctCmp = err.FctCmp
+
+	function util.writeAndScheduleGnuplotDataType(err, discType, p, f, errSuffix, l2value, h1value,
 												  typeString)
 	
 		-- write l2 and h1 to data file
-		local singleFileName = "error_"..errSuffix.."_"..discType.."_"..p
+		local singleFileName = "error_"..errSuffix.."_"..discType.."_"..p.."_"..f
 		local file = err.dataPath..singleFileName..".dat"
 		local dataCols = {err.numDoFs, err.h, l2value, h1value}
 		gnuplot.write_data(file, dataCols)
@@ -153,135 +167,52 @@ function util.writeAndScheduleGnuplotData(err, discType, p)
 			if typeString == "maxlevel" 	then cmpSolSuffix = "L_{max}" end
 			if typeString == "prevlevel" 	then cmpSolSuffix = "L-1" end
 			
-			err.gnuplot[plotFileName].title = normString.."-Error"..titelWRT
+			err.gnuplot[plotFileName].title = normString.."-Error"..titelWRT.." for Fct "..f
 			err.gnuplot[plotFileName].xlabel = yLabelString
-			err.gnuplot[plotFileName].ylabel = "|| u_L - u_{"..cmpSolSuffix.."} ||_{ "..normString.."}"
+			err.gnuplot[plotFileName].ylabel = "|| "..f.."_L - "..f.."_{"..cmpSolSuffix.."} ||_{ "..normString.."}"
 		end
 		
 		-- schedule for plots of same disc type
-		util.scheduleGnuplotFilename(err, err.plotPath..discType.."_"..errSuffix.."_l2_DoF.pdf", L2_DoF_Data, "L_2", "# DoFs")
-		util.scheduleGnuplotFilename(err, err.plotPath..discType.."_"..errSuffix.."_h1_DoF.pdf", H1_DoF_Data, "H^1", "# DoFs")
-		util.scheduleGnuplotFilename(err, err.plotPath..discType.."_"..errSuffix.."_l2_h.pdf", L2_h_Data, "L_2", "h (mesh size)")
-		util.scheduleGnuplotFilename(err, err.plotPath..discType.."_"..errSuffix.."_h1_h.pdf", H1_h_Data, "H^1", "h (mesh size)")
+		util.scheduleGnuplotFilename(err, err.plotPath..discType.."_"..errSuffix.."_"..f.."_l2_DoF.pdf", L2_DoF_Data, "L_2", "# DoFs")
+		util.scheduleGnuplotFilename(err, err.plotPath..discType.."_"..errSuffix.."_"..f.."_h1_DoF.pdf", H1_DoF_Data, "H^1", "# DoFs")
+		util.scheduleGnuplotFilename(err, err.plotPath..discType.."_"..errSuffix.."_"..f.."_l2_h.pdf", L2_h_Data, "L_2", "h (mesh size)")
+		util.scheduleGnuplotFilename(err, err.plotPath..discType.."_"..errSuffix.."_"..f.."_h1_h.pdf", H1_h_Data, "H^1", "h (mesh size)")
 	
 		-- schedule for plots of all types
-		util.scheduleGnuplotFilename(err, err.plotPath.."all_"..errSuffix.."_l2_DoF.pdf", L2_DoF_Data, "L_2", "# DoFs")
-		util.scheduleGnuplotFilename(err, err.plotPath.."all_"..errSuffix.."_h1_DoF.pdf", H1_DoF_Data, "H^1", "# DoFs")
-		util.scheduleGnuplotFilename(err, err.plotPath.."all_"..errSuffix.."_l2_h.pdf", L2_h_Data, "L_2", "h (mesh size)")
-		util.scheduleGnuplotFilename(err, err.plotPath.."all_"..errSuffix.."_h1_h.pdf", H1_h_Data, "H^1", "h (mesh size)")
+		util.scheduleGnuplotFilename(err, err.plotPath.."all_"..errSuffix.."_"..f.."_l2_DoF.pdf", L2_DoF_Data, "L_2", "# DoFs")
+		util.scheduleGnuplotFilename(err, err.plotPath.."all_"..errSuffix.."_"..f.."_h1_DoF.pdf", H1_DoF_Data, "H^1", "# DoFs")
+		util.scheduleGnuplotFilename(err, err.plotPath.."all_"..errSuffix.."_"..f.."_l2_h.pdf", L2_h_Data, "L_2", "h (mesh size)")
+		util.scheduleGnuplotFilename(err, err.plotPath.."all_"..errSuffix.."_"..f.."_h1_h.pdf", H1_h_Data, "H^1", "h (mesh size)")
 	end
 	
 	if err.bUseExact then
+		for i = 1, #FctCmp do
+		local f = FctCmp[i]
 		util.writeAndScheduleGnuplotDataType(
-			err, discType, p, err.l2.exact.title, err.l2.exact.value, err.h1.exact.value, "exact")
+			err, discType, p, f, err.l2.exact.title, err.l2.exact.value[f], err.h1.exact.value[f], "exact")
+		end
 	end	
 	if err.bPrevLevel then
+		for i = 1, #FctCmp do
+		local f = FctCmp[i]
 		util.writeAndScheduleGnuplotDataType(
-			err, discType, p, err.l2.prevlevel.title, err.l2.prevlevel.value, err.h1.prevlevel.value, "prevlevel")
+			err, discType, p, f, err.l2.prevlevel.title, err.l2.prevlevel.value[f], err.h1.prevlevel.value[f], "prevlevel")
+		end
 	end	
 	if err.bMaxLevel then
+		for i = 1, #FctCmp do
+		local f = FctCmp[i]
 		-- finest level compared to finest level is not senseful --> remove it
 		err.numDoFs[err.maxLev] = nil
 		err.h[err.maxLev] = nil
-		err.l2.maxlevel.value[err.maxLev] = nil
-		err.h1.maxlevel.value[err.maxLev] = nil
-		
+		err.l2.maxlevel.value[f][err.maxLev] = nil
+		err.h1.maxlevel.value[f][err.maxLev] = nil
+				
 		util.writeAndScheduleGnuplotDataType(
-			err, discType, p, err.l2.maxlevel.title, err.l2.maxlevel.value, err.h1.maxlevel.value, "maxlevel")
+			err, discType, p, f, err.l2.maxlevel.title, err.l2.maxlevel.value[f], err.h1.maxlevel.value[f], "maxlevel")
+		end
 	end	
 end
-
-
-function util.computeConvRatesStaticForSpace(	err, dom, minLev, maxLev, discType, p,
-													ConvRateSetup)	
-	print("\n")
-	print(">> -------------------------------------------------------------------")
-	print(">>    Computing solutions and error norms of the following problem")
-	print(">> -------------------------------------------------------------------")
-	print(">>     dim        = " .. dim)
-	print(">>     grid       = " .. gridName)
-	print(">>     minLev     = " .. minLev)
-	print(">>     maxLev     = " .. maxLev)
-	print(">>     type       = " .. discType)
-	print(">>     order      = " .. p)
-	print("\n")
-	
-	-- create Approximation Space
-	print(">> Create ApproximationSpace: "..discType..", "..p)
-	local approxSpace = ConvRateSetup.createApproxSpace(dom, discType, p)
-	
-	print(">> Create Domain Disc: "..discType..", "..p)
-	local domainDisc = ConvRateSetup.createDomainDisc(discType, p, approxSpace)
-	
-	print(">> Create Solver")
-	local solver = ConvRateSetup.createSolver(approxSpace)
-	
-	--------------------------------------------------------------------------------
-	--  Apply Solver
-	--------------------------------------------------------------------------------
-	
-	local u = {}
-
-	write("\n>> Allocating storage for solution vectors.\n")
-	for lev = minLev, maxLev do
-		u[lev] = GridFunction(approxSpace, lev)
-	end
-	
-	for lev = minLev, maxLev do
-		write("\n>> Computing Level "..lev..".\n")
-
-		write(">> Preparing inital guess on level "..lev..".\n")
-		ConvRateSetup.prepareInitialGuess(u, lev, minLev, maxLev, domainDisc, solver)
-		
-		write(">> Computing solution on level "..lev..".\n")
-		ConvRateSetup.computeSolution(u, lev, approxSpace, domainDisc, solver)
-		
-		write(">> Solver done.\n")
-	end
-
-	-- 5. compute error
-	for lev = maxLev, minLev, -1 do
-		write("\n>> Error Norm values on Level "..lev..".\n")
-		
-		quadOrder = p+3
-		err.numDoFs[lev] 			= u[lev]:size()
-		write(">> #DoF       on Level "..lev.." is "..err.numDoFs[lev] .."\n");
-
-		-- w.r.t exact solution		
-		if err.bUseExact then 
-		err.l2.exact.value[lev] 	= L2Error(err.exactSol, u[lev], "c", 0.0, quadOrder)
-		err.h1.exact.value[lev] 	= H1Error(err.exactSol, err.exactGrad, u[lev], "c", 0.0, quadOrder)
-		write(">> L2 l-exact on Level "..lev.." is "..string.format("%.3e", err.l2.exact.value[lev]) .."\n");
-		write(">> H1 l-exact on Level "..lev.." is "..string.format("%.3e", err.h1.exact.value[lev]) .."\n");
-		end
-		
-		-- w.r.t max level solution
-		if err.bMaxLevel and lev < maxLev then 
-		err.l2.maxlevel.value[lev] 	= L2Error(u[maxLev], "c", u[lev], "c", quadOrder)
-		err.h1.maxlevel.value[lev] 	= H1Error(u[maxLev], "c", u[lev], "c", quadOrder)
-		write(">> L2 l-lmax  on Level "..lev.." is "..string.format("%.3e", err.l2.maxlevel.value[lev]) .."\n");
-		write(">> H1 l-lmax  on Level "..lev.." is "..string.format("%.3e", err.h1.maxlevel.value[lev]) .."\n");
-		end
-
-		-- w.r.t previous level solution
-		if err.bPrevLevel then 
-		if lev > minLev then 
-		err.l2.prevlevel.value[lev] = L2Error(u[lev], "c", u[lev-1], "c", quadOrder)
-		err.h1.prevlevel.value[lev] = H1Error(u[lev], "c", u[lev-1], "c", quadOrder)
-		write(">> L2 l-(l-1) on Level "..lev.." is "..string.format("%.3e", err.l2.prevlevel.value[lev]) .."\n");
-		write(">> H1 l-(l-1) on Level "..lev.." is "..string.format("%.3e", err.h1.prevlevel.value[lev]) .."\n");
-		else
-		write(">> L2 l-(l-1) on Level "..lev.." is "..err.l2.prevlevel.value[lev] .."\n");	
-		write(">> H1 l-(l-1) on Level "..lev.." is "..err.h1.prevlevel.value[lev].."\n");
-		end
-		end
-		
-		-- 6. write solution
-		WriteGridFunctionToVTK(u[lev], ConvRateSetup.solPath.."sol_"..discType..p.."_l"..lev)
-		write(">> Solution written to: "..ConvRateSetup.solPath.."sol_"..discType..p.."_l"..lev.."\n");	
-	end
-end
-
 
 function util.computeStaticConvRates_StdPrepareInitialGuess(u, lev, minLev, maxLev,
 															domainDisc, solver)
@@ -379,9 +310,13 @@ function util.computeConvRatesStatic(ConvRateSetup)
 	err.bMaxLevel = true
 	err.bPrevLevel = true
 
-	-- compute problem
+	--------------------------------------------------------------------
+	--  Loop Discs
+	--------------------------------------------------------------------
+	
 	local DiscTypes = ConvRateSetup.DiscTypes
 	for type = 1,#DiscTypes do
+	
 		local discType 	= DiscTypes[type].type
 		local pmin 		= DiscTypes[type].pmin
 		local pmax 		= DiscTypes[type].pmax
@@ -407,22 +342,135 @@ function util.computeConvRatesStatic(ConvRateSetup)
 		
 			local maxLev = lmax - math.floor(p/2)
 			local minLev = lmin
+	
+			--------------------------------------------------------------------
+			--  Print Setup
+			--------------------------------------------------------------------
+	
+			print("\n")
+			print(">> -------------------------------------------------------------------")
+			print(">>    Computing solutions and error norms of the following problem")
+			print(">> -------------------------------------------------------------------")
+			print(">>     dim        = " .. dim)
+			print(">>     grid       = " .. gridName)
+			print(">>     minLev     = " .. minLev)
+			print(">>     maxLev     = " .. maxLev)
+			print(">>     type       = " .. discType)
+			print(">>     order      = " .. p)
+			print("\n")
+			
+			--------------------------------------------------------------------
+			--  Create ApproxSpace, Disc and Solver
+			--------------------------------------------------------------------
 
+			print(">> Create ApproximationSpace: "..discType..", "..p)
+			local approxSpace = ConvRateSetup.createApproxSpace(dom, discType, p)
+			
+			print(">> Create Domain Disc: "..discType..", "..p)
+			local domainDisc = ConvRateSetup.createDomainDisc(discType, p, approxSpace)
+			
+			print(">> Create Solver")
+			local solver = ConvRateSetup.createSolver(approxSpace)
+			
+			--------------------------------------------------------------------
+			--  Create Solutions on each level
+			--------------------------------------------------------------------
+			
+			local u = {}
+			
+			write("\n>> Allocating storage for solution vectors.\n")
+			for lev = minLev, maxLev do
+				u[lev] = GridFunction(approxSpace, lev)
+			end
+
+			--------------------------------------------------------------------
+			--  Compute Solution on each level
+			--------------------------------------------------------------------
+			
+			for lev = minLev, maxLev do
+				write("\n>> Computing Level "..lev..".\n")
+			
+				write(">> Preparing inital guess on level "..lev..".\n")
+				ConvRateSetup.prepareInitialGuess(u, lev, minLev, maxLev, domainDisc, solver)
+				
+				write(">> Computing solution on level "..lev..".\n")
+				ConvRateSetup.computeSolution(u, lev, approxSpace, domainDisc, solver)
+				
+				write(">> Solver done.\n")
+			end
+
+			--------------------------------------------------------------------
+			--  Compute Error Norms on each level
+			--------------------------------------------------------------------
+			
+			-- get names in approx space
+			local FctCmp = {}
+			for i = 0, approxSpace:num_fct()-1 do FctCmp[i+1] = approxSpace:name(i) end
+
+			-- prepare error measurement			
 			err.h = {}
-			for lev = minLev, maxLev do	err.h[lev] = MaxElementDiameter(dom, lev) end
-		
-			util.resetErrorRatesStorage(err, minLev, maxLev)
-		
-			util.computeConvRatesStaticForSpace(err,
-				 		dom, minLev, maxLev, discType, p, 
-				 		ConvRateSetup)
+			for lev = minLev, maxLev do	err.h[lev] = MaxElementDiameter(dom, lev) end	
+			util.resetErrorRatesStorage(err, minLev, maxLev, FctCmp)
+
+			-- loop levels and compute error
+			for lev = maxLev, minLev, -1 do
+				write("\n>> Error Norm values on Level "..lev..".\n")
+				
+				quadOrder = p+3
+				err.numDoFs[lev] 			= u[lev]:size()
+				write(">> #DoF       on Level "..lev.." is "..err.numDoFs[lev] .."\n");
+			
+				-- compute for each component
+				for i = 1, #FctCmp do
+					local f = FctCmp[i] 
+					
+					-- w.r.t exact solution		
+					if err.bUseExact then 
+					err.l2.exact.value[f][lev] 	= L2Error(err.exactSol, u[lev], f, 0.0, quadOrder)
+					err.h1.exact.value[f][lev] 	= H1Error(err.exactSol, err.exactGrad, u[lev], f, 0.0, quadOrder)
+					write(">> L2 l-exact for "..f.." on Level "..lev.." is "..string.format("%.3e", err.l2.exact.value[f][lev]) .."\n");
+					write(">> H1 l-exact for "..f.." on Level "..lev.." is "..string.format("%.3e", err.h1.exact.value[f][lev]) .."\n");
+					end
+					
+					-- w.r.t max level solution
+					if err.bMaxLevel and lev < maxLev then 
+					err.l2.maxlevel.value[f][lev] 	= L2Error(u[maxLev], f, u[lev], f, quadOrder)
+					err.h1.maxlevel.value[f][lev] 	= H1Error(u[maxLev], f, u[lev], f, quadOrder)
+					write(">> L2 l-lmax  for "..f.." on Level "..lev.." is "..string.format("%.3e", err.l2.maxlevel.value[f][lev]) .."\n");
+					write(">> H1 l-lmax  for "..f.." on Level "..lev.." is "..string.format("%.3e", err.h1.maxlevel.value[f][lev]) .."\n");
+					end
+				
+					-- w.r.t previous level solution
+					if err.bPrevLevel then 
+					if lev > minLev then 
+					err.l2.prevlevel.value[f][lev] = L2Error(u[lev], f, u[lev-1], f, quadOrder)
+					err.h1.prevlevel.value[f][lev] = H1Error(u[lev], f, u[lev-1], f, quadOrder)
+					write(">> L2 l-(l-1) for "..f.." on Level "..lev.." is "..string.format("%.3e", err.l2.prevlevel.value[f][lev]) .."\n");
+					write(">> H1 l-(l-1) for "..f.." on Level "..lev.." is "..string.format("%.3e", err.h1.prevlevel.value[f][lev]) .."\n");
+					else
+					write(">> L2 l-(l-1) for "..f.." on Level "..lev.." is "..err.l2.prevlevel.value[f][lev] .."\n");	
+					write(">> H1 l-(l-1) for "..f.." on Level "..lev.." is "..err.h1.prevlevel.value[f][lev].."\n");
+					end
+					end
+				end
+								
+				-- write solution
+				WriteGridFunctionToVTK(u[lev], ConvRateSetup.solPath.."sol_"..discType..p.."_l"..lev)
+				write(">> Solution written to: "..ConvRateSetup.solPath.."sol_"..discType..p.."_l"..lev.."\n");	
+			end
 	
 			util.computeErrorRates(err)				 		
 
+			--------------------------------------------------------------------
+			--  Write Erorr Data
+			--------------------------------------------------------------------
+
 			-- write data to screen
-			print("\n>> Statistic for type: "..discType..", order: "..p.."\n")
-			util.updateErrorRatesScreenOutput(err)
-			table.print(err.screen.values, {title = err.screen.title, format = err.screen.format, hline = true, vline = true})
+			for i = 1, #FctCmp do
+				print("\n>> Statistic for type: "..discType..", order: "..p..", comp: "..FctCmp[i].."\n")			
+				util.updateErrorRatesScreenOutput(err, FctCmp[i])
+				table.print(err.screen.values, {title = err.screen.title, format = err.screen.format, hline = true, vline = true})
+			end
 
 			-- write data to gnuplot						
 			util.writeAndScheduleGnuplotData(err, discType, p)
