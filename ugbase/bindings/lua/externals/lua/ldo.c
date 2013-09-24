4e81b15f9fdfa563fa5c5ef1aa90c6b52a7fc723
Martin Rupp <martin.rupp@gcsc.uni-frankfurt.de>
2013-09-24 14:24:22 +0000

    added a bit more clear error message when using wrong function names: attempt to call (uninitialized or missspelled) method 'init_level' (a nil value)

--- ugbase/bindings/lua/externals/lua/ldo.c
+++ ugbase/bindings/lua/externals/lua/ldo.c
@@ -244,8 +244,10 @@ static StkId tryfuncTM (lua_State *L, StkId func) {
   const TValue *tm = luaT_gettmbyobj(L, func, TM_CALL);
   StkId p;
   ptrdiff_t funcr = savestack(L, func);
-  if (!ttisfunction(tm))
-    luaG_typeerror(L, func, "call");
+  if(ttisnil(tm))
+	  luaG_typeerror(L, func, "call (uninitialized or missspelled)");
+  else if (!ttisfunction(tm))
+	  luaG_typeerror(L, func, "call");
   /* Open a hole inside the stack at `func' */
   for (p = L->top; p > func; p--) setobjs2s(L, p, p-1);
   incr_top(L);
