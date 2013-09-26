d45e6fbd0f740d02b3ccbbe21ec7851751bb0d93
Sebastian Reiter <sebastian.reiter@gcsc.uni-frankfurt.de>
2013-09-26 10:44:57 +0000

    Instead of performing a hard-exit in domain_distribution_util.lua, the script now only warns if there are less elements than target-processes during distribution.

--- scripts/util/domain_distribution_util.lua
+++ scripts/util/domain_distribution_util.lua
@@ -61,15 +61,13 @@ function util.DistributeDomain(dom, partitioningMethod, verticalInterfaces,
 	end
 	
 	if dom:domain_info():num_elements_on_level(distributionLevel) < numTargetProcs then
-		print("\nERROR in DistributeDomain:")
-		print("    Currently there have to be at least as many elements in the")
-		print("    distribution level as there are target processes.")
-		print("    Consider increasing numPreRefs!")
+		print("\nWARNING in DistributeDomain:")
+		print("    There are less elements on distributionLevel than there are target processes.")
+		print("    If ug hangs during parallel execution, consider increasing numPreRefs to avoid this!")
 		print("    num elements on level " .. distributionLevel .. ": "
 			.. dom:domain_info():num_elements_on_level(distributionLevel))
 		print("    num target processes: " .. numTargetProcs)
 		print("")
-		exit()
 	end
 	
 	if partitioningMethod == "bisection" then
