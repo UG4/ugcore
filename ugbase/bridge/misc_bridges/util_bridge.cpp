a3f772d22f1d782cc116a6cef0c935f7d1205d5d
Martin Rupp <martin.rupp@gcsc.uni-frankfurt.de>
2013-10-29 13:57:47 +0000

    added CreateDirectory, FileCompare and GetTmpPath

--- ugbase/bridge/misc_bridges/util_bridge.cpp
+++ ugbase/bridge/misc_bridges/util_bridge.cpp
@@ -98,6 +98,10 @@ void RegisterBridge_Util(Registry& reg, string parentGroup)
 	                  "", "", "Immediatly terminates the application.")
 		.add_function("quit", &UGForceExit, grp,
 	                  "", "", "Immediatly terminates the application.");
+
+	reg.add_function("CreateDirectory", static_cast<bool (*)(const char *) > (&CreateDirectory) );
+	reg.add_function("GetTmpPath", GetTmpPath);
+	reg.add_function("FileCompare", FileCompare);
 }
 
 // end group util_bridge
