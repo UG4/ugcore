4676f503d7941c021fc9081973f441447fc478c9
Martin Rupp <martin.rupp@gcsc.uni-frankfurt.de>
2013-10-31 15:16:05 +0000

    fix

--- ugbase/lib_algebra/cpu_algebra/sparsematrix_impl.h
+++ ugbase/lib_algebra/cpu_algebra/sparsematrix_impl.h
@@ -148,7 +148,6 @@ void SparseMatrix<T>::apply_ignore_zero_rows(vector_t &dest,
 			// res[i] += conn.value() * x[conn.index()];
 			MatMultAdd(dest[i], 1.0, dest[i], beta1, conn.value(), w1[conn.index()]);
 	}
-	return true;
 }
 
 
@@ -251,7 +250,6 @@ void SparseMatrix<T>::apply_transposed_ignore_zero_rows(vector_t &dest,
 				MatMultTransposedAdd(dest[conn.index()], 1.0, dest[conn.index()], beta1, conn.value(), w1[i]);
 		}
 	}
-	return true;
 }
 
 
