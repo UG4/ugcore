71c7e2e31c269073f854831f7a9beface90a8bf7
Stephan Grein <stephan.grein@gcsc.uni-frankfurt.de>
2013-12-05 09:31:35 +0000

    Improved comments (sorry).

--- cmake/ug/export_includes.cmake
+++ cmake/ug/export_includes.cmake
@@ -3,14 +3,14 @@
 # to a common library.
 #######################
 # Export include paths to global variable.
-# PURPOSE: use this function to add local package lib dependencies to global
-#          ugLibDependencies property which is used to build libug4
-# @param sources sources list to export
+# PURPOSE: use this function to add local package include directories to global
+#          ug4LibIncludes property which is used to build libug4
+# @param includes list of include directories to be exported
 function(ExportIncludes includes)
     # iterate over all arguments and insert given prefix
     foreach(includeDir ${ARGV})
-            # retrieve the global property ugDependencies and store it
-            # in temp variable
+            # retrieve the global property ugIncludes and store its values
+            # in a temp variable
             # NOTE: properties must be assigned to variables before being used
             get_property(temp GLOBAL PROPERTY ugIncludes)
             # append tmp to the global ugDependencies property using the correct prefix
