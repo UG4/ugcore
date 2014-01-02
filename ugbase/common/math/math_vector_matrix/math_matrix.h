0915e72091db17b8126dec15cf30626bcc380b9a
Andreas Vogel <andreas.vogel@gcsc.uni-frankfurt.de>
2014-01-02 12:46:03 +0000

    better fix for xl compiler that works also with clang (hopefully :-))

--- ugbase/common/math/math_vector_matrix/math_matrix.h
+++ ugbase/common/math/math_vector_matrix/math_matrix.h
@@ -67,10 +67,8 @@ class MathMatrix
 		 */
 		MathMatrix& operator+= (const MathMatrix& B)
 		{
-			for(std::size_t i = 0; i < N; ++i)
-			{
-				for(std::size_t j = 0; j < M; ++j)
-				{
+			for(std::size_t i = 0; i < N; ++i){
+				for(std::size_t j = 0; j < M; ++j){
 					m_data[i][j] += B(i,j);
 				}
 			}
@@ -85,10 +83,8 @@ class MathMatrix
 		 */
 		MathMatrix& operator-= (const MathMatrix& B)
 		{
-			for(std::size_t i = 0; i < N; ++i)
-			{
-				for(std::size_t j = 0; j < M; ++j)
-				{
+			for(std::size_t i = 0; i < N; ++i){
+				for(std::size_t j = 0; j < M; ++j){
 					m_data[i][j] -= B(i,j);
 				}
 			}
@@ -103,10 +99,8 @@ class MathMatrix
 		 */
 		MathMatrix& operator= (const value_type& val)
 		{
-			for(std::size_t i = 0; i < N; ++i)
-			{
-				for(std::size_t j = 0; j < M; ++j)
-				{
+			for(std::size_t i = 0; i < N; ++i){
+				for(std::size_t j = 0; j < M; ++j){
 					m_data[i][j] = val;
 				}
 			}
@@ -121,10 +115,8 @@ class MathMatrix
 		 */
 		MathMatrix& operator+= (const value_type& val)
 		{
-			for(std::size_t i = 0; i < N; ++i)
-			{
-				for(std::size_t j = 0; j < M; ++j)
-				{
+			for(std::size_t i = 0; i < N; ++i){
+				for(std::size_t j = 0; j < M; ++j){
 					m_data[i][j] += val;
 				}
 			}
@@ -139,10 +131,8 @@ class MathMatrix
 		 */
 		MathMatrix& operator-= (const value_type& val)
 		{
-			for(std::size_t i = 0; i < N; ++i)
-			{
-				for(std::size_t j = 0; j < M; ++j)
-				{
+			for(std::size_t i = 0; i < N; ++i){
+				for(std::size_t j = 0; j < M; ++j){
 					m_data[i][j] -= val;
 				}
 			}
@@ -157,10 +147,8 @@ class MathMatrix
 		 */
 		MathMatrix& operator/= (const value_type& val)
 		{
-			for(std::size_t i = 0; i < N; ++i)
-			{
-				for(std::size_t j = 0; j < M; ++j)
-				{
+			for(std::size_t i = 0; i < N; ++i){
+				for(std::size_t j = 0; j < M; ++j){
 					m_data[i][j] /= val;
 				}
 			}
@@ -175,10 +163,8 @@ class MathMatrix
 		 */
 		MathMatrix& operator*= (const value_type& val)
 		{
-			for(std::size_t i = 0; i < N; ++i)
-			{
-				for(std::size_t j = 0; j < M; ++j)
-				{
+			for(std::size_t i = 0; i < N; ++i){
+				for(std::size_t j = 0; j < M; ++j){
 					m_data[i][j] *= val;
 				}
 			}
@@ -194,33 +180,40 @@ class MathMatrix
 		value_type operator* (const MathMatrix& v) const
 		{
 			value_type res = 0.0;
-			for(std::size_t i = 0; i < N; ++i)
-			{
-				for(std::size_t j = 0; j < M; ++j)
-				{
+			for(std::size_t i = 0; i < N; ++i){
+				for(std::size_t j = 0; j < M; ++j){
 					res += m_data[i][j] * v.m_data[i][j];
 				}
 			}
 			return res;
 		}
 
-		//inline std::size_t row_size() const {return N;}
-		//inline std::size_t col_size() const {return M;}
-
 		inline std::size_t num_rows() const {return N;}
 		inline std::size_t num_cols() const {return M;}
 
-		inline value_type& entry(std::size_t row, std::size_t col)				{UG_ASSERT(row < N && col < M, "Accessing "<<N<<"x"<<M<<"Matrix at entry ("<<row<<","<<col<<")"); return m_data[row][col];}
-		inline const value_type& entry(std::size_t row, std::size_t col) const	{UG_ASSERT(row < N && col < M, "Accessing "<<N<<"x"<<M<<"Matrix at entry ("<<row<<","<<col<<")"); return m_data[row][col];}
+		inline value_type* operator[](std::size_t index){
+			UG_ASSERT(index < N, "Invalid index");
+			return m_data[index];
+		}
+		inline const value_type* operator[](std::size_t index) const{
+			UG_ASSERT(index < N, "Invalid index");
+			return m_data[index];
+		}
 
-		inline value_type* operator[](std::size_t index)				{UG_ASSERT(index < N, "Invalid index"); return m_data[index];}
-		inline const value_type* operator[](std::size_t index) const	{UG_ASSERT(index < N, "Invalid index"); return m_data[index];}
+		inline value_type& operator() (std::size_t row, std::size_t col){
+			UG_ASSERT(row < N && col < M, "Accessing "<<N<<"x"<<M<<"Matrix at entry ("<<row<<","<<col<<")");
+			return m_data[row][col];
+		}
+		inline const value_type& operator() (std::size_t row, std::size_t col) const{
+			UG_ASSERT(row < N && col < M, "Accessing "<<N<<"x"<<M<<"Matrix at entry ("<<row<<","<<col<<")");
+			return m_data[row][col];
+		}
 
-		inline value_type& operator() (std::size_t row, std::size_t col)				{UG_ASSERT(row < N && col < M, "Accessing "<<N<<"x"<<M<<"Matrix at entry ("<<row<<","<<col<<")"); return m_data[row][col];}
-		inline const value_type& operator() (std::size_t row, std::size_t col) const	{UG_ASSERT(row < N && col < M, "Accessing "<<N<<"x"<<M<<"Matrix at entry ("<<row<<","<<col<<")"); return m_data[row][col];}
+		inline value_type& entry(std::size_t row, std::size_t col)				{return (*this)(row,col);}
+		inline const value_type& entry(std::size_t row, std::size_t col) const	{return (*this)(row,col);}
 
 		inline void assign(const MathVector<N, value_type>& vec, const std::size_t row) {
-			assert(vec.Size == N);
+			UG_ASSERT(vec.Size == N, "Wrong vector size");
 			for(std::size_t j = 0; j < N; j++)
 				m_data[row][j] = vec[j];
 		}
@@ -230,22 +223,33 @@ class MathMatrix
 
 		inline void assign(const MathMatrix& v)
 		{
-			for(std::size_t i = 0; i < N; ++i)
-			{
-				for(std::size_t j = 0; j < M; ++j)
-				{
-					m_data[i][j] = v.entry(i,j);
+			for(std::size_t i = 0; i < N; ++i){
+				for(std::size_t j = 0; j < M; ++j){
+					m_data[i][j] = v.m_data[i][j] ;
 				}
 			}
 		}
 
 };
 
-#ifdef __bg__ // check for BlueGene/System-Q
-template <typename T> class MathMatrix<0,0,T>{};
-template <std::size_t N, typename T> class MathMatrix<N,0,T>{};
-template <std::size_t N, typename T> class MathMatrix<0,N,T>{};
-#endif
+// this are explicit instantiations to avoid compiler errors on XL
+// {
+template <typename T> class MathMatrix<0,0,T>{
+	public:
+		typedef T value_type;
+		typedef std::size_t size_type;
+};
+template <std::size_t N, typename T> class MathMatrix<N,0,T>{
+	public:
+		typedef T value_type;
+		typedef std::size_t size_type;
+};
+template <std::size_t N, typename T> class MathMatrix<0,N,T>{
+	public:
+		typedef T value_type;
+		typedef std::size_t size_type;
+};
+// }
 
 /// Print MathMatrix<N,M> to standard output
 template <std::size_t N, std::size_t M>
