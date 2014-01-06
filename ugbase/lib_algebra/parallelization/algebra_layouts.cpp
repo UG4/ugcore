43a321f879a035bb08e02284ac0f223f19bebe49
Martin Rupp <martin.rupp@gcsc.uni-frankfurt.de>
2014-01-06 09:43:13 +0000

    fix

--- ugbase/lib_algebra/parallelization/algebra_layouts.cpp
+++ ugbase/lib_algebra/parallelization/algebra_layouts.cpp
@@ -34,8 +34,8 @@ std::ostream &operator << (std::ostream &out, const AlgebraLayouts &layouts)
 	out << "AlgebraLayouts:\n";
 	out << " master: " << OstreamShift(layouts.master()) << "\n";
 	out << " slave: " << OstreamShift(layouts.slave()) << "\n";
-	out << " vertical master: " << OstreamShift(layouts.master()) << "\n";
-	out << " vertical slave: " << OstreamShift(layouts.slave()) << "\n";
+	out << " vertical master: " << OstreamShift(layouts.vertical_master()) << "\n";
+	out << " vertical slave: " << OstreamShift(layouts.vertical_slave()) << "\n";
 	out << " process communicator: " << OstreamShift(layouts.proc_comm()) << "\n";
 	return out;
 }
