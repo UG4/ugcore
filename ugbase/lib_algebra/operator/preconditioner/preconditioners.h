8f4cf938154952f5f285482bd23cc7e29a637b42
Martin Rupp <martin.rupp@gcsc.uni-frankfurt.de>
2014-01-08 11:35:41 +0000

    removed schur (handled by schur_bridge.cpp)

--- ugbase/lib_algebra/operator/preconditioner/preconditioners.h
+++ ugbase/lib_algebra/operator/preconditioner/preconditioners.h
@@ -14,6 +14,5 @@
 #include "lib_algebra/operator/preconditioner/ilut.h"
 #include "lib_algebra/operator/preconditioner/iterator_product.h"
 #include "lib_algebra/operator/preconditioner/vanka.h"
-#include "lib_algebra/operator/preconditioner/schur/schur.h"
 
 #endif /* __UG__PRECONDITIONERS_H__ */
