2d9b35715a223025bbf19655157a9f42fb73fee9
Susanne Höllbacher <susanne.hoellbacher@gcsc.uni-frankfurt.de>
2014-01-16 13:16:09 +0000

    added new method for class 'GridFunctionDebugWriter': set_grid_level'

--- ugbase/bridge/disc_bridges/output_bridge.cpp
+++ ugbase/bridge/disc_bridges/output_bridge.cpp
@@ -103,6 +103,7 @@ static void DomainAlgebra(Registry& reg, string grp)
 			.add_method("set_conn_viewer_output", &T::set_conn_viewer_output, "", "bCVOutput")
 			.add_method("set_print_consistent",  &T::set_print_consistent, "", "printConsistent")
 		    .add_method("set_base_dir", &T::set_base_dir, "", "")
+		    .add_method("set_grid_level", &T::set_grid_level, "Sets the grid level", "GridLevel")
 			.set_construct_as_smart_pointer(true);
 		reg.add_class_to_group(name, "GridFunctionDebugWriter", tag);
 	}
