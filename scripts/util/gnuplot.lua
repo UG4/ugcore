83db3e2309e98ab49256cf5a2f49d8143241746d
Andreas Vogel <andreas.vogel@gcsc.uni-frankfurt.de>
2014-03-12 10:38:55 +0000

    removing unused lua fct

--- scripts/util/gnuplot.lua
+++ scripts/util/gnuplot.lua
@@ -1217,26 +1217,6 @@ function gnuplot.plot(filename, data, options)
 	return 0
 end
 
-
--- return a new array containing the concatenation of all of its 
--- parameters. Scaler parameters are included in place, and array 
--- parameters have their values shallow-copied to the final array.
--- Note that userdata and function values are treated as scalar.
-function gnuplot.array_concat(...) 
-    local t = {}
-    for n = 1,select("#",...) do
-        local arg = select(n,...)
-        if type(arg)=="table" then
-            for _,v in ipairs(arg) do
-                t[#t+1] = v
-            end
-        else
-            t[#t+1] = arg
-        end
-    end
-    return t
-end
-
 --[[!
 \}
 ]]--
