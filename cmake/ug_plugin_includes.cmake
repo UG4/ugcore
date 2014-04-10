528898dc189e7bd2b45375eca1a9b473f5f9c66b
Martin Rupp <martin.rupp@gcsc.uni-frankfurt.de>
2014-04-10 10:17:47 +0000

    added function(UGInstall name returned_path).  usage: e.g. UGInstall("HYPRE" HYPRE_INSTALL_PATH), then uginstall installs HYPRE HYPRE_INSTALL_PATH is the path to hypre.

--- cmake/ug_plugin_includes.cmake
+++ cmake/ug_plugin_includes.cmake
@@ -14,3 +14,26 @@ include(${PLUGIN_ROOT_PATH}/ug_includes.cmake)
 set(CMAKE_LIBRARY_OUTPUT_DIRECTORY ${UG_ROOT_PATH}/bin/plugins)
 set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY ${UG_ROOT_PATH}/lib)
 set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${UG_ROOT_PATH}/bin/plugins)
+
+# use uginstall to install the library
+# usage: UGInstall("HYPRE" HYPRE_INSTALL_PATH)
+# then HYPRE_INSTALL_PATH is the path to hypre.
+function(UGInstall name returned_path)
+     
+   	 # in other cases we use uginstall to download & install HYPRE   	 
+	 if(NOT EXISTS "$ENV{UG4_LOCAL_INSTALL_DIR}/${name}/used/installed")
+        message(STATUS "Info: ${name} is not provided, but we need ${name}.")
+        message(STATUS "Info: Trying to install ${name} with uginstall ${name}.")
+        if(NOT defined $ENV{UG4_LOCAL_INSTALL_DIR})
+            message(FATAL_ERROR "can't run uginstall. UG4_LOCAL_INSTALL_DIR not available. you need to add 'source YOURUG4DIR/scripts/shell/ugbash' to your .bashrc / .bash_profile")
+        endif()
+    
+        message(STATUS "Install directory is $ENV{UG4_LOCAL_INSTALL_DIR}/${name}.")
+        execute_process(COMMAND bash ${UG_ROOT_PATH}/scripts/shell/uginstall ${name})
+        if(NOT EXISTS "$ENV{UG4_LOCAL_INSTALL_DIR}/${name}/used/installed")
+            message(FATAL_ERROR "${name} could not be installed by uginstall.")
+        endif()        
+    endif(NOT EXISTS "$ENV{UG4_LOCAL_INSTALL_DIR}/${name}/used/installed")
+    
+    set(${returned_path} "$ENV{UG4_LOCAL_INSTALL_DIR}/${name}/used/" PARENT_SCOPE)        
+endfunction(UGInstall)
