146960725e480281a0e74c1a2e2ed697bef5b6c3
Arne Nägel <arne.naegle@gcsc.uni-frankfurt.de>
2014-04-11 12:58:15 +0000

    Fixing undefined variable (m_baseDir -> get_base_dir())

--- ugbase/lib_disc/function_spaces/grid_function_util.h
+++ ugbase/lib_disc/function_spaces/grid_function_util.h
@@ -534,13 +534,17 @@ public:
 
 	///	write vector
 	virtual void write_vector(const vector_type& vec, const char* filename) {
+
+
+		std::string name(get_base_dir()); name.append("/").append(filename);
+
 		//	write to conn viewer
 		if (bConnViewerOut)
-			write_vector_to_conn_viewer(vec, filename);
+			write_vector_to_conn_viewer(vec, name.c_str());
 
 		//	write to vtk
 		if (bVTKOut)
-			write_vector_to_vtk(vec, filename);
+			write_vector_to_vtk(vec, name.c_str());
 	}
 
 	virtual void update_positions()
