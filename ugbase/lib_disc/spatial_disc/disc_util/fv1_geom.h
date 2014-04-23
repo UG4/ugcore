7d5f75a2b5e1f87ede98ff61efbb8b2e82965d66
Dmitry Logashenko <LogashenkoDL@mail.ru>
2014-04-23 15:50:46 +0000

    Access to coordinates of the mass center of the element.

--- ugbase/lib_disc/spatial_disc/disc_util/fv1_geom.h
+++ ugbase/lib_disc/spatial_disc/disc_util/fv1_geom.h
@@ -449,6 +449,11 @@ class FV1Geometry : public FVGeometryBase
 	/// returns all ips of scv as they appear in scv loop
 		const MathVector<worldDim>* scv_global_ips() const {return &(m_vvGloMid[0][0]);}
 
+	///	returns the local coordinates of the center of mass of the element
+		const MathVector<dim>* coe_local() const {return &(m_vvLocMid[dim][0]);}
+		
+	///	returns the global coordinates of the center of mass of the element
+		const MathVector<worldDim>* coe_global() const {return &(m_vvGloMid[dim][0]);}
 
 	protected:
 	//	global and local ips on SCVF
