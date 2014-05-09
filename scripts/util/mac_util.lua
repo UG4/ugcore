7da2c10ab23cc4ecb8ee25c9624c2583fe1fee03
Martin Rupp <martin.rupp@gcsc.uni-frankfurt.de>
2014-05-09 10:13:22 +0000

    parallel fix

--- scripts/util/mac_util.lua
+++ scripts/util/mac_util.lua
@@ -6,10 +6,13 @@
 --! @param name a std filename (don't use special characters or whitespace)
 --! @param sizeMB the size of the RAM Disk in MB 
 function util.createMacRAMDisk(name, sizeMB)
-	os.execute("if [ ! -e \"/Volumes/"..name.."\" ]; then\n"..
-		"echo \"Creating ug4 RAM Disk with "..sizeMB.." MB..\"\n".. 
-		"diskutil erasevolume HFS+ '"..name.."' `hdiutil attach -nomount ram://"..(1024*1024*sizeMB/512).."`\n"..
-		"echo \"Access the RamDisk in /Volumes/"..name.."\"\n"..
-		"fi\n")
-	return "/Volumes/"..name
+	if ProcRank() == 0 then
+		os.execute("if [ ! -e \"/Volumes/"..name.."\" ]; then\n"..
+			"echo \"Creating ug4 RAM Disk with "..sizeMB.." MB..\"\n".. 
+			"diskutil erasevolume HFS+ '"..name.."' `hdiutil attach -nomount ram://"..(1024*1024*sizeMB/512).."`\n"..
+			"echo \"Access the RamDisk in /Volumes/"..name.."\"\n"..
+			"fi\n")
+	end
+	SynchronizeProcesses()
+	return "/Volumes/"..name	
 end
