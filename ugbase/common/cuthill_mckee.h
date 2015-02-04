34dd16f9ca1b995f5a32dd1e2cc8bbc9694cce2c
Martin Rupp <martin.rupp@gcsc.uni-frankfurt.de>
2015-02-04 11:28:34 +0000

    moved cuthill_mckee common code to common (only vector-based) because it is used by ILUT (to get a seperation algebra / disc / grid)

---
+++ ugbase/common/cuthill_mckee.h
@@ -0,0 +1,34 @@
+/*
+ * cuthill_mckee.h
+ *
+ *  Created on: 21.03.2011
+ *      Author: andreasvogel
+ */
+
+#ifndef __H__UG__COMMON__CUTHILL_MCKEE__
+#define __H__UG__COMMON__CUTHILL_MCKEE__
+
+#include <vector>
+
+namespace ug{
+
+/// returns an array describing the needed index mapping for Cuthill-McKee ordering
+/**
+ * This function computes a index mapping, that transforms a index-graph into
+ * Cuthill-McKee ordering. For each index an vector of all adjacent indices
+ * must be passed. (If no adjacent index is passed for an index, this index is
+ * skipped and not sorted). On exit the index field vNewIndex is filled with
+ * the index mapping: newInd = vNewIndex[oldInd]
+ *
+ * \param[out]	vNewIndex		vector returning new index for old index
+ * \param[in]	vvNeighbour		vector of adjacent indices for each index
+ * \param[in]	bReverse		flag if "reverse Cuthill-McKee" is used
+ * \returns		flag if ordering was successful
+ */
+void ComputeCuthillMcKeeOrder(std::vector<size_t>& vNewIndex,
+                              std::vector<std::vector<size_t> >& vvNeighbour,
+                              bool bReverse = true);
+
+} // end namespace ug
+
+#endif /* __H__UG__LIB_DISC__DOF_MANAGER__CUTHILL_MCKEE__ */
