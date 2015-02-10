a2b3966e679d9963bae595ba67f04d0ac5e48202
Martin Rupp <martin.rupp@gcsc.uni-frankfurt.de>
2015-02-10 15:22:50 +0000

    docu

--- scripts/util/vec_util.lua
+++ scripts/util/vec_util.lua
@@ -1,14 +1,70 @@
-
-
 --[[
+see also bridge/algebra_bridges/bridge_mat_vec_operations.h
+These functions enable us to do some matrix stuff in lua e.g.
+
+you will need to have gridFunctions for this to work:
+
+x = GridFunction(approxSpace)
+e = GridFunction(approxSpace)
+d = GridFunction(approxSpace)
 b = GridFunction(approxSpace)
-ev[1][1] = Eval(ev[1][1] / VecNorm(ev[1][1]))
-print(ev[1][1])
-print(VecProd(ev[1][1], ev[1][1]))
-print(ev[1][1]*(ev[1][1]+ev[2][2]))
-Assign(b, ev[1][1] + 3.0*ev[2][2])
-SaveVectorForConnectionViewer(u, "u.vec")
-SaveVectorForConnectionViewer(b, "b.vec")
+temp = GridFunction(approxSpace)
+
+-- and some assembled Matrix Operators
+A = MatrixOperator()
+B = MatrixOperator()
+
+
+-- now you can calc some stuff
+print(VecNorm(A*x-b))
+-- or
+print(VecProd(x, A*b))
+
+-- or assign stuff
+Dinv = MatrixDiagonalInverse(A)
+Assign(e, Dinv*x)
+
+-- VecNorm, VecScale and Assign all accept GridFunctions and VecScaleAdd-Objects
+-- VecScaleAdd are of the form 
+-- VSA_1 +/- VSA_2 +/- VSA_3 +/- ...
+--- where VSA is a GridFunction, double*GridFunction, MatrixOperator*GridFunction or double*(MatrixOperator*GridFunction)
+
+-- EXAMPLES:
+
+-- Eigenvalue stuff
+
+function rayleigh_quotient(u, A, B)
+	return math.sqrt(VecProd(u, A*u)/VecProd(u, B*u))
+end
+
+function eigenvalue_defect(u, A, B)
+	def = temp
+	lambda = rayleigh_quotient(u, A, B)
+	
+	-- lua doesn't accept lambda*B*e. you have to use lambda*(B*e)
+	Assign(def, A*e - lambda*(B*e))
+end
+
+-- smoothing stuff
+
+function calcSmoothing(e, b)
+	Se = GridFunction(approxSpace)
+	Assign(Se, 1.0*e)
+
+	-- perform 2 jacobi smooths
+	solver=LinearSolver(Jacobi(0.6), ConvCheck(2, 1e-12, 1e-12, false) )	 
+	solver:apply_return_defect(Se, b:clone())
+	
+	D = MatrixDiagonal(A)
+	DInv = MatrixDiagonalInverse(A)
+	
+	-- check how amg norms changed
+	Se1 = VecProd(A*Se, Se)
+	e0 = VecProd(D*e, e)
+	e1 = VecProd(A*e, e)
+	e2 = VecProd(DInv*(A*e), A*e)
+end
+
 ]]--
 
 function __ug__TemplateExpressions_add(a, b)
