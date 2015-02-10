a8a14b715ac5efb5fb093226e01cb023a03b2724
Martin Rupp <martin.rupp@gcsc.uni-frankfurt.de>
2015-02-10 14:26:30 +0000

    mbreit fixed this for me. thanks!

--- ugbase/lib_disc/dof_manager/ordering/cuthill_mckee.h
+++ ugbase/lib_disc/dof_manager/ordering/cuthill_mckee.h
@@ -13,23 +13,6 @@
 
 namespace ug{
 
-/// returns an array describing the needed index mapping for Cuthill-McKee ordering
-/**
- * This function computes a index mapping, that transforms a index-graph into
- * Cuthill-McKee ordering. For each index an vector of all adjacent indices
- * must be passed. (If no adjacent index is passed for an index, this index is
- * skipped and not sorted). On exit the index field vNewIndex is filled with
- * the index mapping: newInd = vNewIndex[oldInd]
- *
- * \param[out]	vNewIndex		vector returning new index for old index
- * \param[in]	vvNeighbour		vector of adjacent indices for each index
- * \param[in]	bReverse		flag if "reverse Cuthill-McKee" is used
- * \returns		flag if ordering was successful
- */
-void ComputeCuthillMcKeeOrder(std::vector<size_t>& vNewIndex,
-                              std::vector<std::vector<size_t> >& vvNeighbour,
-                              bool bReverse = true);
-
 /// orders the dof distribution using Cuthill-McKee
 inline void OrderCuthillMcKee(DoFDistribution& dofDistr, bool bReverse);
 
