9ea2791c77cd79446c958e07440f72ef7234290d
Markus Breit <markus.breit@gcsc.uni-frankfurt.de>
2015-04-20 07:44:16 +0000

    NOW, it is a fixme :P

--- ugbase/lib_disc/io/vtkoutput.cpp
+++ ugbase/lib_disc/io/vtkoutput.cpp
@@ -177,7 +177,7 @@ pvtu_filename(std::string& nameOut, std::string nameIn,
               int si, int maxSi, int step)
 {
 //	copy name
-	// TODO: It might not be the best of ideas to use find_last_of('.')
+	// FIXME: It might not be the best of ideas to use find_last_of('.')
 	// here, as this will cause unexpected results if filename is a path
 	// that includes a ".", e.g. "./path/to/someFileName" or "stupid.path.name/fileName"
 	nameOut = nameIn.substr(0, nameIn.find_last_of('.'));
