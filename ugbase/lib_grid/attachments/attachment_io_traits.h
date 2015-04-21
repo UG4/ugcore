f624395f8fee1fbc8790e0d1b113a4b38b297d5f
Sebastian Reiter <sebastian.reiter@gcsc.uni-frankfurt.de>
2015-04-21 11:12:57 +0000

    Fixed attachment_io_traits for ABool

--- ugbase/lib_grid/attachments/attachment_io_traits.h
+++ ugbase/lib_grid/attachments/attachment_io_traits.h
@@ -8,59 +8,88 @@ namespace ug{
 
 template <class TAttachment>
 struct attachment_io_traits {
-	typedef typename TAttachment::ValueType	value_type;
+	typedef typename TAttachment::ValueType									value_type;
+	typedef typename attachment_value_traits<value_type>::reference			reference_type;
+	typedef typename attachment_value_traits<value_type>::const_reference 	const_reference_type;
 
-	static void write_value (std::ostream& out, const value_type& v)	{out << v;}
-	static void read_value(std::istream& in, value_type& v)				{in >> v;}
+	static void write_value (std::ostream& out, const_reference_type v)	{out << v;}
+	static void read_value(std::istream& in, reference_type v)			{in >> v;}
 };
+
+
+template <>
+struct attachment_io_traits<Attachment<bool> > {
+	typedef bool															value_type;
+	typedef typename attachment_value_traits<value_type>::reference			reference_type;
+	typedef typename attachment_value_traits<value_type>::const_reference 	const_reference_type;
+
+	static void write_value (std::ostream& out, const_reference_type v)	{out << v;}
+	static void read_value(std::istream& in, reference_type v)			{value_type tmp; in >> tmp; v = tmp;}
+};
+
 
 template <>
 struct attachment_io_traits<Attachment<vector1> > {
-	typedef vector1	value_type;
-	static void write_value (std::ostream& out, const value_type& v)
+	typedef vector1															value_type;
+	typedef typename attachment_value_traits<value_type>::reference			reference_type;
+	typedef typename attachment_value_traits<value_type>::const_reference 	const_reference_type;
+
+	static void write_value (std::ostream& out, const_reference_type v)
 	{
 		out << v[0];
 	}
-	static void read_value(std::istream& in, value_type& v)	
+	static void read_value(std::istream& in, reference_type v)	
 	{
 		in >> v[0];
 	}
 };
 
+
 template <>
 struct attachment_io_traits<Attachment<vector2> > {
-	typedef vector2	value_type;
-	static void write_value (std::ostream& out, const value_type& v)
+	typedef vector2															value_type;
+	typedef typename attachment_value_traits<value_type>::reference			reference_type;
+	typedef typename attachment_value_traits<value_type>::const_reference 	const_reference_type;
+
+	static void write_value (std::ostream& out, const_reference_type v)
 	{
 		out << v[0] << " " << v[1];
 	}
-	static void read_value(std::istream& in, value_type& v)	
+	static void read_value(std::istream& in, reference_type v)	
 	{
 		in >> v[0] >> v[1];
 	}
 };
 
+
 template <>
 struct attachment_io_traits<Attachment<vector3> > {
-	typedef vector3	value_type;
-	static void write_value (std::ostream& out, const value_type& v)
+	typedef vector3															value_type;
+	typedef typename attachment_value_traits<value_type>::reference			reference_type;
+	typedef typename attachment_value_traits<value_type>::const_reference 	const_reference_type;
+
+	static void write_value (std::ostream& out, const_reference_type v)
 	{
 		out << v[0] << " " << v[1] << " " << v[2];
 	}
-	static void read_value(std::istream& in, value_type& v)	
+	static void read_value(std::istream& in, reference_type v)	
 	{
 		in >> v[0] >> v[1] >> v[2];
 	}
 };
 
+
 template <>
 struct attachment_io_traits<Attachment<vector4> > {
-	typedef vector4	value_type;
-	static void write_value (std::ostream& out, const value_type& v)
+	typedef vector4															value_type;
+	typedef typename attachment_value_traits<value_type>::reference			reference_type;
+	typedef typename attachment_value_traits<value_type>::const_reference 	const_reference_type;
+
+	static void write_value (std::ostream& out, const_reference_type v)
 	{
 		out << v[0] << " " << v[1] << " " << v[2] << " " << v[3];
 	}
-	static void read_value(std::istream& in, value_type& v)	
+	static void read_value(std::istream& in, reference_type v)	
 	{
 		in >> v[0] >> v[1] >> v[2] >> v[3];
 	}
