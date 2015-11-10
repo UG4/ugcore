0a01f11073493168e18623667b6ff1f3dfb3f0b1
mbreit <markus.breit@gcsc.uni-frankfurt.de>
2015-11-10 20:05:45 +0100

    just to be safe: initializing bool member m_bSortIsIdentity in ILU-T

--- ugbase/lib_algebra/operator/preconditioner/ilut.h
+++ ugbase/lib_algebra/operator/preconditioner/ilut.h
@@ -1,34 +1,3 @@
-/*
- * Copyright (c) 2010-2015:  G-CSC, Goethe University Frankfurt
- * Author: Martin Rupp
- * 
- * This file is part of UG4.
- * 
- * UG4 is free software: you can redistribute it and/or modify it under the
- * terms of the GNU Lesser General Public License version 3 (as published by the
- * Free Software Foundation) with the following additional attribution
- * requirements (according to LGPL/GPL v3 §7):
- * 
- * (1) The following notice must be displayed in the Appropriate Legal Notices
- * of covered and combined works: "Based on UG4 (www.ug4.org/license)".
- * 
- * (2) The following notice must be displayed at a prominent place in the
- * terminal output of covered works: "Based on UG4 (www.ug4.org/license)".
- * 
- * (3) The following bibliography is recommended for citation and must be
- * preserved in all covered files:
- * "Reiter, S., Vogel, A., Heppner, I., Rupp, M., and Wittum, G. A massively
- *   parallel geometric multigrid solver on hierarchically distributed grids.
- *   Computing and visualization in science 16, 4 (2013), 151-164"
- * "Vogel, A., Reiter, S., Rupp, M., Nägel, A., and Wittum, G. UG4 -- a novel
- *   flexible software system for simulating pde based models on high performance
- *   computers. Computing and visualization in science 16, 4 (2013), 165-179"
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Lesser General Public License for more details.
- */
 
 #ifndef __H__UG__LIB_DISC__OPERATOR__LINEAR_OPERATOR__ILUT__
 #define __H__UG__LIB_DISC__OPERATOR__LINEAR_OPERATOR__ILUT__
@@ -79,18 +48,19 @@ class ILUTPreconditioner : public IPreconditioner<TAlgebra>
 		typedef IPreconditioner<TAlgebra> base_type;
 
 	public:
-	//	Constructor
-		ILUTPreconditioner(double eps=1e-6) :
-			m_eps(eps), m_info(false), m_bSort(true)
+	///	Constructor
+		ILUTPreconditioner(double eps=1e-6)
+			: m_eps(eps), m_info(false), m_bSort(true), m_bSortIsIdentity(false)
 		{};
 
 	/// clone constructor
-		ILUTPreconditioner( const ILUTPreconditioner<TAlgebra> &parent )
+		ILUTPreconditioner(const ILUTPreconditioner<TAlgebra> &parent)
 			: base_type(parent)
 		{
 			m_eps = parent.m_eps;
 			set_info(parent.m_info);
 			set_sort(parent.m_bSort);
+			m_bSortIsIdentity = parent.m_bSortIsIdentity;
 		}
 
 	///	Clone
@@ -100,10 +70,9 @@ class ILUTPreconditioner : public IPreconditioner<TAlgebra>
 		}
 
 
-	// 	Destructor
+	///	Destructor
 		virtual ~ILUTPreconditioner()
-		{
-		};
+		{};
 
 	///	returns if parallel solving is supported
 		virtual bool supports_parallel() const {return true;}
@@ -122,7 +91,8 @@ class ILUTPreconditioner : public IPreconditioner<TAlgebra>
 		
 		virtual std::string config_string() const
 		{
-			std::stringstream ss ; ss << "ILUT(threshold = " << m_eps << ", sort = " << (m_bSort?"true":"false") << ")";
+			std::stringstream ss;
+			ss << "ILUT(threshold = " << m_eps << ", sort = " << (m_bSort?"true":"false") << ")";
 			if(m_eps == 0.0) ss << " = Sparse LU";
 			return ss.str();
 		}
