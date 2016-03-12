a81918bdb7b6896668b883a9f85a0b2c7bb19b80
Arne Naegel <naegel@gcsc.uni-frankfurt.de>
2016-03-12 04:34:46 +0100

    Removing obsolete comment

--- ugbase/bridge/algebra_bridges/eigensolver_bridge.cpp
+++ ugbase/bridge/algebra_bridges/eigensolver_bridge.cpp
@@ -145,11 +145,6 @@ void RegisterBridge_Eigensolver(Registry& reg, string grp)
 {
 	grp.append("/Algebra/Solver");
 	typedef Eigensolver::Functionality Functionality;
-//#ifdef UG_CPU_1
-//	typedef boost::mpl::list<CPUAlgebra> AlgList;
-/*#else
-	typedef boost::mpl::list<> AlgList;
-#endif*/
 
 	try{
 		RegisterAlgebraDependent<Functionality>(reg,grp);
