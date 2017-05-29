28d6a559317de9dbc8409fd78c1a614854076239
Sebastian Reiter <s.b.reiter@gmail.com>
2017-05-29 22:32:05 +0200

    added missing include

--- ugbase/lib_grid/algorithms/remeshing/resolve_intersections_impl.hpp
+++ ugbase/lib_grid/algorithms/remeshing/resolve_intersections_impl.hpp
@@ -41,6 +41,7 @@
 #include "lib_grid/algorithms/grid_generation/triangle_fill_sweep_line.h"
 #include "lib_grid/algorithms/orientation_util.h"
 #include "lib_grid/algorithms/remove_duplicates_util.h"
+#include "lib_grid/algorithms/selection_util.h"
 #include "lib_grid/algorithms/space_partitioning/lg_ntree.h"
 #include "common/space_partitioning/ntree_traverser.h"
 
