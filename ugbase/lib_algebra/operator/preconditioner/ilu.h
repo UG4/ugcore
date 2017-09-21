f719d5f6918c5d091abe4b70de0ef46c383c5eed
Sebastian Reiter <s.b.reiter@gmail.com>
2017-09-21 14:56:11 +0200

    Fixed spelling in comment

--- ugbase/lib_algebra/operator/preconditioner/ilu.h
+++ ugbase/lib_algebra/operator/preconditioner/ilu.h
@@ -469,7 +469,7 @@ class ILU : public IPreconditioner<TAlgebra>
 
 			m_ILU = mat;
 
-		//	this is an experimental trigger. Best to leav it false for now.
+		//	this is an experimental trigger. Best to leave it false for now.
 			const bool sortSlaveToEnd = false;
 
 			#ifdef 	UG_PARALLEL
