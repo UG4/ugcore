1d321cdb112e45da793d983ecd42c87d189345dd
Sebastian Reiter <s.b.reiter@gmail.com>
2018-05-03 17:13:40 +0200

    added ExportLinkPaths cmake function

--- cmake/ug/export_dependencies.cmake
+++ cmake/ug/export_dependencies.cmake
@@ -68,3 +68,23 @@ function(ExportShellDependencies deps)
             set_property(GLOBAL PROPERTY ugShellDependencies ${tmp} "${l}")
     endforeach(l)
 endfunction(ExportShellDependencies)
+
+################################################################################
+# Declare a method that allows all sub-cmake-files to add their link paths
+# to a common library.
+#######################
+# Export link paths to global variable.
+# PURPOSE: use this function to add local package link directories to global
+#          ug4LinkPaths property which is used to build libug4
+# @param includes list of link directories to be exported
+function(ExportLinkPaths linkPaths)
+    # iterate over all arguments and insert given prefix
+    foreach(linkPath ${ARGV})
+            # retrieve the global property ugLinkPaths and store its values
+            # in a temp variable
+            # NOTE: properties must be assigned to variables before being used
+            get_property(temp GLOBAL PROPERTY ugLinkPaths)
+            # append tmp to the global ugLinkPaths property using the correct prefix
+            set_property(GLOBAL PROPERTY ugLinkPaths ${temp} "${linkPath}")
+    endforeach(linkPath)
+endfunction(ExportLinkPaths)
