e5889217aff3f923ccf1c587ef9d887a686b9b0a
mbreit <markus.breit@gcsc.uni-frankfurt.de>
2018-07-11 14:01:44 +0200

    added -Wno-unneeded-internal-declaration as cxx flag for clang compiler

--- cmake/ug_includes.cmake
+++ cmake/ug_includes.cmake
@@ -381,6 +381,7 @@ elseif("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
 	add_cxx_flag(-Wno-unused-local-typedef)
 	add_cxx_flag(-Wno-unknown-warning-option)
 	add_cxx_flag(-Wno-undefined-var-template)
+	add_cxx_flag(-Wno-unneeded-internal-declaration)
 	#set(CMAKE_CPP_FLAGS	"${CMAKE_CPP_FLAGS} -Wno-overloaded-virtual -Wno-autological-compare" CACHE STRING "overriden flags!" FORCE)
 endif()
 
