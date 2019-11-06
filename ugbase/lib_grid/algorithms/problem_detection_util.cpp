1d00e18b10d2ad08c5c0480fde6fb0eb159aef75
Stephan Grein <stephan@syntaktischer-zucker.de>
2019-11-06 15:29:34 +0100

    Adding count of unconnected sides.

--- ugbase/lib_grid/algorithms/problem_detection_util.cpp
+++ ugbase/lib_grid/algorithms/problem_detection_util.cpp
@@ -78,9 +78,10 @@ static bool CheckForUnconnectedSidesIMPL(Grid& grid, const ISubsetHandler& sh)
 								 grid.template end<TSide>()))
 	{
 		gotOne = true;
-		UG_LOG("WARNING: Found unconnected sides (those may lead to solver issues!): \n");
-		UG_ERR_LOG("Found unconnected sides (those may lead to solver issues!): \n");
-		for(size_t i = 0; i < sides.size(); ++i){
+		const size_t numSides = sides.size();
+		UG_LOG("WARNING: Found " << numSides << " unconnected sides (those may lead to solver issues!):" << std::endl);
+		UG_ERR_LOG("Found " << numSides << " unconnected sides (those may lead to solver issues!):" << std::endl);
+		for(size_t i = 0; i < numSides; ++i){
 			UG_LOG("  - " << ElementDebugInfo(grid, sides[i]) << std::endl);
 			UG_ERR_LOG("  - " << ElementDebugInfo(grid, sides[i]) << std::endl);
 			UG_LOG("  - " << ElementSubsetInfo(sh, sides[i]) << std::endl);
