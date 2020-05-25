f77eb173b8516c153c3f8d53d69ab07bd983f38a
Arne Naegel <naegel@gcsc.uni-frankfurt.de>
2020-05-25 23:37:48 +0200

    Adjusting copyright date/author

--- ugbase/lib_disc/spatial_disc/elem_disc/dirac_source/lagrange_dirac_source_impl.h
+++ ugbase/lib_disc/spatial_disc/elem_disc/dirac_source/lagrange_dirac_source_impl.h
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 2011-2015:  G-CSC, Goethe University Frankfurt
- * Author: Markus Breit
+ * Copyright (c) 2019-2020:  G-CSC, Goethe University Frankfurt
+ * Author: Arne Naegel
  * 
  * This file is part of UG4.
  * 
