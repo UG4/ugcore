535ce9e54b839d9cb0ec36a68344789852bc5abe
Junxi Wang <junxi.wang@gcsc.uni-frankfurt.de>
2020-11-03 14:30:16 +0100

    TensorEntryAdapter, TenColumnAdapter in user_data_util.lua

--- scripts/util/user_data_util.lua
+++ scripts/util/user_data_util.lua
@@ -1,19 +1,19 @@
 -- Copyright (c) 2012-2015:  G-CSC, Goethe University Frankfurt
 -- Author: Andreas Vogel
--- 
+--
 -- This file is part of UG4.
--- 
+--
 -- UG4 is free software: you can redistribute it and/or modify it under the
 -- terms of the GNU Lesser General Public License version 3 (as published by the
 -- Free Software Foundation) with the following additional attribution
 -- requirements (according to LGPL/GPL v3 §7):
--- 
+--
 -- (1) The following notice must be displayed in the Appropriate Legal Notices
 -- of covered and combined works: "Based on UG4 (www.ug4.org/license)".
--- 
+--
 -- (2) The following notice must be displayed at a prominent place in the
 -- terminal output of covered works: "Based on UG4 (www.ug4.org/license)".
--- 
+--
 -- (3) The following bibliography is recommended for citation and must be
 -- preserved in all covered files:
 -- "Reiter, S., Vogel, A., Heppner, I., Rupp, M., and Wittum, G. A massively
@@ -22,7 +22,7 @@
 -- "Vogel, A., Reiter, S., Rupp, M., Nägel, A., and Wittum, G. UG4 -- a novel
 --   flexible software system for simulating pde based models on high performance
 --   computers. Computing and visualization in science 16, 4 (2013), 165-179"
--- 
+--
 -- This program is distributed in the hope that it will be useful,
 -- but WITHOUT ANY WARRANTY; without even the implied warranty of
 -- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
@@ -51,9 +51,9 @@ function ListUserDataInTable(t, name)
       if type(v) == "userdata" then
 		 print(name.."["..n.."]")
       end
-  
+
 	  if type(v) == "table" then
-		if(n ~= "_G" and n ~= "io" and n ~= "package" and n ~= "gnuplot") then 
+		if(n ~= "_G" and n ~= "io" and n ~= "package" and n ~= "gnuplot") then
 			ListUserDataInTable(v, name.."["..n.."]")
 		end
   	  end
@@ -67,10 +67,10 @@ function ListUserData()
 	   if type(v) == "userdata" then
 		 print(n)
 	   end
-    
+
 	    -- userdata in table
 		if type(v) == "table" then
-			if(n ~= "_G" and n ~= "io" and n ~= "package" and n ~= "gnuplot") then 
+			if(n ~= "_G" and n ~= "io" and n ~= "package" and n ~= "gnuplot") then
 				ListUserDataInTable(_G[n], n)
 			end
 		end
@@ -82,13 +82,13 @@ function FreeUserDataInTable(t)
       if type(v) == "userdata" then
       	 t[n] = nil
       end
-      
+
       if type(v) == "table" then
 		if(n ~= "_G" and n ~= "io" and n ~= "package" and n ~= "gnuplot") then
 			FreeUserDataInTable(v)
 		end
   	  end
-      
+
     end
 end
 
@@ -99,14 +99,14 @@ function FreeUserData()
       if type(v) == "userdata" then
 		 _G[n] = nil
       end
-      
+
       if type(v) == "table" then
 		if(n ~= "_G" and n ~= "io" and n ~= "package" and n ~= "gnuplot") then
  	     	FreeUserDataInTable(_G[n])
  	     end
       end
    end
-   
+
    -- call garbage collector
    collectgarbage("collect")
 end
@@ -131,7 +131,7 @@ function __ug__CheckUserDataArgType(r, l)
 		error("Internal Error: No ug4 class as operand")
 	end
 
-	-- check that userdata are UserData	
+	-- check that userdata are UserData
 	if rType ~= "" then
 		if not ug_is_base_class("UserDataInfo", rType) then
 			error("Error: Can only operate on UserData, but summand is not derived from it.")
@@ -147,7 +147,7 @@ function __ug__CheckUserDataArgType(r, l)
 		end
 		lDim = l:get_dim()
 		Dim = lDim
-		lData = l:type()		
+		lData = l:type()
 		Data = lData
 	end
 
@@ -162,7 +162,7 @@ function __ug__CheckUserDataArgType(r, l)
 	---------------------------------------------------------
 	-- Check match of types for operands
 	---------------------------------------------------------
-	
+
 	-- both operands are UserData
 	-- check for same dimension
 	if rType ~= "" and lType ~= "" then
@@ -170,7 +170,7 @@ function __ug__CheckUserDataArgType(r, l)
 			error("Error: Dimensions of UserData does not match")
 		end
 	end
-	
+
 	return rType, lType, rDim, lDim, Dim, rData, lData, Data
 end
 
@@ -181,15 +181,15 @@ end
 --! functions user when '+/-' is called on an UserData (or a derived implementation)
 function __ug__UserNumber_sum(lScale, l, rScale, r)
 	local rType, lType, rDim, lDim, Dim, rData, lData, Data = __ug__CheckUserDataArgType(r, l)
-	
+
 	if l == nil or r == nil then
 		error("Error in '+' or '-': Added a nil value (possibly uninitialized value?)")
 	end
-		
+
 	---------------------------------------------------------
 	-- Check match of types for operands
 	---------------------------------------------------------
-	
+
 	-- both operands are UserData
 	-- check for same data type
 	if rType ~= "" and lType ~= "" then
@@ -215,12 +215,12 @@ function __ug__UserNumber_sum(lScale, l, rScale, r)
 			else
 				error("Error in '+' or '-': Summand must be scalar number or UserData.")
 			end
-		end		
-	end	
+		end
+	end
 
 	-- All checks passed: Can create name for Class to return
 	local ScaleAddLinkerName = "ScaleAddLinker"..Data..Dim.."d"
-	
+
 	---------------------------------------------------------
 	-- a) Case: An operand is ScaleAddLinker
 	---------------------------------------------------------
@@ -235,21 +235,21 @@ function __ug__UserNumber_sum(lScale, l, rScale, r)
 	-- NOTE: if right operand is subtracted (i.e. rScale == -1) we must use
 	--       the general case
 	if rType == ScaleAddLinkerName then
-	 	if rScale == 1.0 then 
+	 	if rScale == 1.0 then
 			-- copy linker and add also other operand
 			local linker =  _G[ScaleAddLinkerName](r)
 			linker:add(lScale, l)
 			return linker
 		end
 	end
-	
+
 	---------------------------------------------------------
 	-- b) Case: No operand is ScaleAddLinker
 	---------------------------------------------------------
 	local linker =  _G[ScaleAddLinkerName]()
 	linker:add(lScale, l)
 	linker:add(rScale, r)
-	return linker				
+	return linker
 end
 
 --! functions used when '+' is called on an UserData (or a derived implementation)
@@ -269,14 +269,14 @@ end
 --! functions user when '*' is called on an UserData (or a derived implementation)
 function __ug__UserNumber_mul(l, r)
 	local rType, lType, rDim, lDim, Dim, rData, lData, Data = __ug__CheckUserDataArgType(r, l)
-		
+
 	if l == nil or r == nil then
 		error("Error in '*' : nil value (possibly uninitialized value?)")
 	end
 	---------------------------------------------------------
 	-- Check match of types for operands
 	---------------------------------------------------------
-	
+
 	-- if operand is not UserData it must be a number
 	if rType == "" then
 		if not tonumber(r) then
@@ -287,7 +287,7 @@ function __ug__UserNumber_mul(l, r)
 		if not tonumber(l) then
 			error("Error in '*': Operand must be scalar number or UserData.")
 		end
-	end		
+	end
 
 	-- if plain lua-numbers, just multiply
 	if rType == "" and lType == "" then
@@ -300,44 +300,44 @@ function __ug__UserNumber_mul(l, r)
 		local linker =  _G[ScaleAddLinkerName]()
 		if rData == "" or rData == "Number" then linker:add(r, l)
 		else linker:add(l, r) end
-		return linker				
+		return linker
 	end
 
 	if lData == "Matrix" and rData == "Vector" then
 		local ScaleAddLinkerName = "ScaleAddLinkerVectorMatrix"..Dim.."d"
 		local linker =  _G[ScaleAddLinkerName]()
 		linker:add(l, r)
-		return linker				
+		return linker
 	end
 
 	if lData == "Vector" and rData == "Vector" then
 		local ScaleAddLinkerName = "ScaleAddLinkerVectorVector"..Dim.."d"
 		local linker =  _G[ScaleAddLinkerName]()
 		linker:add(l, r)
-		return linker				
+		return linker
 	end
 
-	error("Error in '*': Cannot multiply "..lData.."*"..rData)	
+	error("Error in '*': Cannot multiply "..lData.."*"..rData)
 end
 
 --! functions user when '/' is called on an UserData (or a derived implementation)
 function __ug__UserNumber_div(l, r)
 	local rType, lType, rDim, lDim, Dim, rData, lData, Data = __ug__CheckUserDataArgType(r, l)
-		
+
 	if l == nil or r == nil then
 		error("Error in '/': nil value (possibly uninitialized value?)")
-	end	
-		
+	end
+
 	---------------------------------------------------------
 	-- Check match of types for operands
 	---------------------------------------------------------
 
-	-- check r is number	
+	-- check r is number
 	if rType ~= "" and rData ~= "Number" then
 		error("Error in '/': Cannot divide by "..rData)
 	end
 
-	-- case: dividing by lua number	
+	-- case: dividing by lua number
 	if rType == "" then
 		if not(tonumber(r)) then
 			error("Error in '/': Cannot only divide by Number")
@@ -359,26 +359,26 @@ function __ug__UserNumber_div(l, r)
 			return linker
 		end
 
-		-- right operand is linker 		
+		-- right operand is linker
 		if rType == InverseLinkerName then
 		 		local linker =  _G[InverseLinkerName](r)
 				linker:divide(l, 1.0)
 				return linker
 		end
-		
+
 		---------------------------------------------------------
 		-- b) Case: No operand is InverseLinker
 		---------------------------------------------------------
 		local linker =  _G[InverseLinkerName]()
 		linker:divide(l, r)
-		return linker	
+		return linker
 	end
-	
+
 	-- case: user-data by user-data-number
 	local linker = _G[InverseLinkerName]()
 	linker:divide(1, r)
 	return __ug__UserNumber_mul(l, linker)
-	
+
 end
 
 --------------------------------------------------------------------------------
@@ -389,8 +389,8 @@ end
 function __ug__UserNumber_pow(l, r)
 	if l == nil or r == nil then
 		error("Error in '^': nil value (possibly uninitialized value?)")
-	end	
-	
+	end
+
 	-- check that exponent is integer
 	if not tonumber(r) or not (r%1==0) then
 		error("Error in '^': Currently exponent must be plain lua integer")
@@ -403,13 +403,13 @@ function __ug__UserNumber_pow(l, r)
 				error("Error in '^': base must be a number-UserData.")
 			end
 		end
-		
-		
+
+
 		-- case 1
 		if r == 1 then return l end
 
-		-- case > 1 
-		if r > 1 then 
+		-- case > 1
+		if r > 1 then
 			local value = l
 			for i = 2,r do
 			value = __ug__UserNumber_mul(value, l)
@@ -437,19 +437,19 @@ function set_user_data_overloads(name)
 	-- request metatable for the classname
 	mt = ug_get_metatable(name)
 	if mt == nil then return end
-	
+
 	-- set __add function in metatable
 	mt.__add = _G["__ug__UserNumber_add"]
 
 	-- set __sub function in metatable
 	mt.__sub = _G["__ug__UserNumber_sub"]
-	
+
 	-- set __mul function in metatable
 	mt.__mul = _G["__ug__UserNumber_mul"]
 
 	-- set __div function in metatable
 	mt.__div = _G["__ug__UserNumber_div"]
-	
+
 	-- set __pow function in metatable
 	mt.__pow = _G["__ug__UserNumber_pow"]
 end
@@ -469,7 +469,7 @@ for k, class in ipairs({"CplUser", "ConstUser", "LuaUser", "ScaleAddLinker", "Lu
 end
 
 -- add GridFunctionData
-for k, class in ipairs({"GridFunctionGradientData", "GridFunctionNumberData"}) do
+for k, class in ipairs({"GridFunctionGradientData", "GridFunctionNumberData", "EffectiveUserTensor"}) do
 	-- loop algebra
 	for j, algebra in ipairs({"CPU1", "CPU2", "CPU3", "CPU4", "CPUVAR"}) do
 		-- loop dimensions
@@ -490,6 +490,9 @@ for dim =  1,3 do
 		set_user_data_overloads("UserTensor"..dim.."d")
 		set_user_data_overloads("UserTensorEntryAdapter"..dim.."d")
 		set_user_data_overloads("UserTensorColumnAdapter"..dim.."d")
+    set_user_data_overloads("UserTensorMatrix"..dim.."d")
+  set_user_data_overloads("TensorEntryAdapter"..dim.."d")
+  set_user_data_overloads("TensorColumnAdapter"..dim.."d")
 	end
 end
 
