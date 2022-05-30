6af107f83e246eea0ae50bb7e733d6475ba1fdeb
Lukas Larisch <lukas.larisch@kaust.edu.sa>
2022-05-30 19:46:11 +0200

    add sparsematrixgraph_bidirectional.lua (TopologicalOrdering)

--- tests/lua/sparsematrixgraph_bidirectional.lua
+++ tests/lua/sparsematrixgraph_bidirectional.lua
@@ -40,10 +40,10 @@ approxSpace:print_statistic()
 slope = 0.4
 
 function velocity(x, y)
-	x_dir = - (slope * x - y)
-	y_dir = - (slope * y + x)
-	--x_dir = 1
-	--y_dir = 0
+	--x_dir = - (slope * x - y)
+	--y_dir = - (slope * y + x)
+	x_dir = 1
+	y_dir = 0
 	return x_dir, y_dir
 end
 
@@ -75,8 +75,7 @@ SaveParallelGridLayout(dom:grid(), "grid_layout_p" .. ProcRank() .. ".ugx", zWri
 ilu = ILU()
 ilu:set_damp(1)
 
---algo = BoostCuthillMcKeeOrdering() --old version not using UndirectedMatrix
-algo = BoostCuthillMcKeeNewOrdering() --using UndirectedMatrix
+algo = TopologicalOrdering()
 
 ilu:set_ordering_algorithm(algo)
 
