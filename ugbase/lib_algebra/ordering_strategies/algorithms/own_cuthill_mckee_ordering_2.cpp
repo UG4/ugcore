468f2da4b2cec3c1ec1bff0ae01ad83656105575
Felix Salfelder <felix@salfelder.org>
2022-08-13 00:00:00 +0000

    OwnCMKOrdering2::compute() draft

--- ugbase/lib_algebra/ordering_strategies/algorithms/own_cuthill_mckee_ordering_2.cpp
+++ ugbase/lib_algebra/ordering_strategies/algorithms/own_cuthill_mckee_ordering_2.cpp
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2022:  G-CSC, Goethe University Frankfurt
- * Author: Lukas Larisch
+ * Author: Lukas Larisch, Felix Salfelder
  * 
  * This file is part of UG4.
  * 
@@ -48,6 +48,10 @@
 
 #include <assert.h>
 #include "common/error.h"
+#include "common/util/bucket_sorter.hpp"
+
+#define TRACE_UNTESTED
+#include "common/util/trace.h"
 
 // is this already implemented somewhere else?
 #ifndef HAVE_MYABS
@@ -63,6 +67,8 @@ double my_abs(double v){return abs(v);}
 
 namespace ug{
 
+static unsigned INVALID = -1u;
+
 template <typename TAlgebra, typename O_t>
 class OwnCuthillMcKeeOrdering2 : public IOrderingAlgorithm<TAlgebra, O_t>
 {
@@ -74,19 +80,26 @@ public:
 	typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::bidirectionalS> G_t;
 
 	typedef typename boost::graph_traits<G_t>::vertex_descriptor vd;
+	typedef unsigned deg_t;
 	typedef typename boost::graph_traits<G_t>::edge_descriptor ed_t;
 	typedef typename boost::graph_traits<G_t>::vertex_iterator vIt_t;
 	typedef typename boost::graph_traits<G_t>::adjacency_iterator adj_iter;
 	typedef typename boost::graph_traits<G_t>::out_edge_iterator oute_iter;
+	typedef boost::iterator_property_map<unsigned*,
+			boost::identity_property_map, unsigned, unsigned&> map_type;
+	typedef boost::bucket_sorter<unsigned, unsigned,
+			map_type, boost::identity_property_map > bucket_sorter;
 
+	std::vector<unsigned> _degrees;
+
+public:
 	OwnCuthillMcKeeOrdering2() : m_bReverse(false), m_look_for_sources(true){}
 
 	/// clone constructor
 	OwnCuthillMcKeeOrdering2( const OwnCuthillMcKeeOrdering2<TAlgebra, O_t> &parent )
 			: baseclass(), m_bReverse(parent.m_bReverse), m_look_for_sources(parent.m_look_for_sources){}
 
-	SmartPtr<IOrderingAlgorithm<TAlgebra, O_t> > clone()
-	{
+	SmartPtr<IOrderingAlgorithm<TAlgebra, O_t> > clone() {
 		return make_sp(new OwnCuthillMcKeeOrdering2<TAlgebra, O_t>(*this));
 	}
 
@@ -101,113 +114,127 @@ public:
 		 }
 	}
 
-	//overload
-	void compute(){
+	void compute() override { untested();
 		unsigned n = boost::num_vertices(g);
+		unsigned min_bucket = -1u;
 
-		if(n == 0){
+		if(n == 0){ untested();
 			UG_THROW(name() << "::compute: Graph is empty!");
 			return;
+		}else{ untested();
 		}
 
-		o.resize(n);
+		o.resize(0);
+		o.resize(n, INVALID);
 
-		std::vector<int> numbering(n, -1);
-		std::vector<size_t> indegs(n);
+		vd s = *boost::vertices(g).first;
+		o[s] = 0;
+		_degrees_[s] = INVALID;
+		_tags[s] = 0;
 
-		vIt_t vIt, vEnd;
-		for(boost::tie(vIt, vEnd) = boost::vertices(g); vIt != vEnd; ++vIt){
-			indegs[*vIt] = boost::in_degree(*vIt, g);
+		auto i = boost::adjacent_vertices(s, g);
+		for(; i.first != i.second; ++i.first){ untested();
+			vd n = *i.first;
+			assert(n!=s);
+			deg_t d = boost::in_degree(n, g);
+
+			_degrees_[n] = d;
+			_bs.push_front(n);
+
+			assert(_tags[n] == INVALID);
+
+			_tags[n] = s;
+			min_bucket = std::min(_degrees_[n], min_bucket);
 		}
 
-		size_t k = 0;
-
-		if(m_look_for_sources){
-			for(size_t i = 0; i < n; ++i){
-				if(indegs[i] == 0){
-					numbering[i] = k;
-					front.push_back(i);
-					unregister_indegree(i, indegs);
-					++k;
-				}
-			}
-			UG_COND_THROW(front.empty(), name() << ": no sources numbered, front empty! [1]\n");
-
-			UG_LOG("#sources numbered: " << k << ", #vertices: " << n << "\n");
-		}
-		else{
-			UG_COND_THROW(front.empty(), name() << ": no sources numbered, front empty! [2]\n");
-
-			//TODO
-		}
+		size_t k = 1;
 
 		std::pair<oute_iter, oute_iter> e;
+
 		//main loop
-		for(; k < n;){
-			size_t min_indeg_v = -1u;
-			size_t min_indeg_val = n;
+		for(; k < n;){ untested();
+			unsigned min_tag = -1u;
+			vd next = INVALID;
+			for(unsigned j=0; j<min_bucket; ++j){ untested();
+				assert(_bs[j].empty());
+			}
+			assert(!_bs[min_bucket].empty());
 
-			std::vector<std::list<size_t>::iterator> to_delete;
-
-			for(auto it = front.begin(); it != front.end(); ++it){
-				auto candidate = *it;
-
-				size_t misses = 0;
-				e = boost::out_edges(candidate, g);
-				for(; e.first != e.second; ++e.first){
-					ed_t const& edg = *e.first;
-					auto t = boost::target(edg, g);
-
-					//unnumbered vertex
-					if(numbering[t] < 0){
-						if(indegs[t] < min_indeg_val){
-							min_indeg_val = indegs[t];
-							min_indeg_v = t;
-						}
-					}
-					else{
-						++misses;
-					}
-				 }
-
-				if(misses == boost::out_degree(candidate, g)){
-					to_delete.push_back(it);
+			for(vd cand : _bs[min_bucket]){ untested();
+				unsigned tag = _tags[cand];
+				assert(tag!=INVALID);
+				if(tag < min_tag){ untested();
+					next = cand;
+					min_tag = tag;
+				}else{ untested();
 				}
 			}
 
-			for(size_t u = 0; u < to_delete.size(); ++u){
-				front.erase(to_delete[u]);
-			}
+			assert(_degrees_[next] == min_bucket);
+			assert(_tags[next] != INVALID);
+			assert(next!=INVALID);
 
-			if(min_indeg_val == n){
-				for(unsigned i = 0; i < n; ++i){
-					if(numbering[i] == -1){
-						numbering[i] = k++;
+			auto i = boost::adjacent_vertices(next, g);
+			for(; i.first != i.second; ++i.first){ untested();
+				vd n = *i.first;
+				assert(n!=next);
+
+				if(_tags[n] == INVALID){
+					// not a candidate, add it, initialise degree.
+
+					deg_t d = boost::in_degree(n, g);
+					assert(d);
+					_tags[n] = k;
+					
+					_degrees_[n] = d;
+					_bs.push_front(n);
+
+					min_bucket = std::min(_degrees_[n], min_bucket);
+				}else if(_degrees_[n] == INVALID){ untested();
+					// n already numbered.
+				}else{ untested();
+					// already reachable from S. Degree drop.
+					assert(_tags[n]<k);
+					assert(_degrees_[n]);
+
+
+					if(min_bucket == _degrees_[n]){ untested();
+						--min_bucket;
+					}else{ untested();
+						assert(min_bucket < _degrees_[n]);
 					}
-				}
 
-				break;
+					--_degrees_[n];
+					_bs.update(n);
+				}
 			}
 
-			numbering[min_indeg_v] = k;
-			front.push_back(min_indeg_v);
-
-			unregister_indegree(min_indeg_v, indegs);
+			_bs.remove(next);
+			_degrees_[next] = INVALID;
+			assert(o[next] == INVALID);
+			o[next] = k;
 			++k;
-		}
 
-		for(unsigned i = 0; i < n; ++i){
-			o[i] = numbering[i];
-		}
+			if(k==o.size()){ untested();
+			}else{ untested();
+				for(unsigned i=0; i<min_bucket; ++i){
+					assert(_bs[i].empty());
+				}
+				while(_bs[min_bucket].empty()){ untested();
+					++min_bucket;
+					assert(min_bucket < o.size()); // not connected?
+				}
+			}
+		} // main loop
 
 		g = G_t(0);
 	}
 
-	void init(M_t* A, const V_t&){
+	void init(M_t* A, const V_t&){ untested();
 		init(A);
 	}
 
-	void init(M_t* A){
+	void init(M_t* A){ untested();
 		unsigned n = A->num_rows();
 
 		g = G_t(n);
@@ -219,13 +246,16 @@ public:
 				}
 			}
 		}
+
+		init_bs();
 	}
 
-	void init(M_t* A, const V_t&, const O_t& inv_map, const O_t& start){
+	void init(M_t* A, const V_t&, const O_t& inv_map, const O_t& start){ untested();
 		init(A, inv_map, start);
+		init_bs();
 	}
 
-	void init(M_t* A, const O_t& inv_map, const O_t& start){
+	void init(M_t* A, const O_t& inv_map, const O_t& start){ untested();
 		//TODO: replace this by UG_DLOG if permutation_util does not depend on this file anymore
 		#ifdef UG_ENABLE_DEBUG_LOGS
 		UG_LOG("Using " << name() << " on induced matrix of size " << inv_map.size() << "\n");
@@ -234,7 +264,21 @@ public:
 		own_cmk_induced_subgraph<G_t, M_t>(g, A, inv_map);
 		m_look_for_sources = false;
 
-		UG_LOG("n: " << boost::num_vertices(g) << ", e: " << boost::num_edges(g) << "\n");
+		size_t n = boost::num_vertices(g);
+		UG_LOG("n: " << n << ", e: " << boost::num_edges(g) << "\n");
+
+		init_bs();
+	}
+
+	void init_bs(){ untested();
+		size_t n = boost::num_vertices(g);
+		assert(n);
+		_degrees.resize(0);
+		_degrees.resize(n, INVALID);
+		_tags.resize(0);
+		_tags.resize(n, INVALID);
+		_degrees_ = map_type(&_degrees[0], boost::identity_property_map());
+		_bs = bucket_sorter(n+1, n+1, _degrees_);
 	}
 
 	void check(){
@@ -243,7 +287,10 @@ public:
 		}
 	}
 
-	O_t& ordering(){
+	O_t& ordering(){ untested();
+#ifndef NDEBUG
+		check();
+#endif
 		return o;
 	}
 
@@ -258,11 +305,13 @@ private:
 	O_t o;
 
 	std::list<size_t> front;
-
 	bool m_bReverse;
-
 	bool m_look_for_sources;
-};
+
+	std::vector<unsigned> _tags;
+	map_type _degrees_;
+	bucket_sorter _bs; // (10, 10, M);
+}; // OwnCuthillMcKeeOrdering2
 
 } //namespace
 
