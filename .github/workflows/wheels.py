134138ef6a9bd6c391784932b643deffbcc4d0b6
Arne Naegel <anaegel@users.noreply.github.com>
2024-04-16 17:43:22 +0200

    Update wheels.py -> simplification

--- .github/workflows/wheels.py
+++ .github/workflows/wheels.py
@@ -1,135 +1,22 @@
-# Workflow to build and test wheels
-name: Wheel builder
+name: Build
 
-on: [push]
-  #schedule:
-    # Nightly build at 3:42 A.M.
-   # - cron: "42 3 */1 * *"
-  #push:
-  #  branches:
-      # - main
-    #  # Feature branch
-   #   - feature-cibuildwheel
-    #  # Release branches
-    #  - "[0-9]+.[0-9]+.X"
-  #pull_request:
-  #  branches:
-   #   - main
-    #  - "[0-9]+.[0-9]+.X"
-  # Manual run
-  #workflow_dispatch:
-
-concurrency:
-  group: ${{ github.workflow }}-${{ github.head_ref || github.run_id }}
-  cancel-in-progress: true
+on: [push, pull_request]
 
 jobs:
-  # Check whether to build the wheels and the source tarball
-  check_build_trigger:
-    name: Check build trigger
-    runs-on: ubuntu-latest
-    if: github.repository == 'UG4/ugcore'
-    outputs:
-      build: ${{ steps.check_build_trigger.outputs.build }}
-
-    steps:
-      - name: Checkout UG4
-        uses: actions/checkout@v3
-      #  with:
-      #    ref: ${{ github.event.pull_request.head.sha }}
-
-      #- id: check_build_trigger
-      #  name: Check build trigger
-      #  run: bash build_tools/github/check_build_trigger.sh
-
-  # Build the wheels for Linux, Windows and macOS for Python 3.9 and newer
   build_wheels:
-    name: Build wheel for cp${{ matrix.python }}-${{ matrix.platform_id }}-${{ matrix.manylinux_image }}
+    name: Build wheels on ${{ matrix.os }}
     runs-on: ${{ matrix.os }}
-    needs: check_build_trigger
-    if: needs.check_build_trigger.outputs.build
-
     strategy:
-      # Ensure that a wheel builder finishes even if another fails
-      fail-fast: false
       matrix:
-        include:
-          # Window 64 bit
-          # Note: windows-2019 is needed for older Python versions:
-          # https://github.com/scikit-learn/scikit-learn/issues/22530
-          - os: windows-latest
-            python: 39
-            platform_id: win_amd64
-          - os: windows-latest
-            python: 310
-            platform_id: win_amd64
-          - os: windows-latest
-            python: 311
-            platform_id: win_amd64
-          - os: windows-latest
-            python: 312
-            platform_id: win_amd64
-
-          # Linux 64 bit manylinux2014
-          - os: ubuntu-latest
-            python: 39
-            platform_id: manylinux_x86_64
-            manylinux_image: manylinux2014
-
-          # NumPy on Python 3.10 only supports 64bit and is only available with manylinux2014
-          - os: ubuntu-latest
-            python: 310
-            platform_id: manylinux_x86_64
-            manylinux_image: manylinux2014
-
-          - os: ubuntu-latest
-            python: 311
-            platform_id: manylinux_x86_64
-            manylinux_image: manylinux2014
-          - os: ubuntu-latest
-            python: 312
-            platform_id: manylinux_x86_64
-            manylinux_image: manylinux2014
-
-          # MacOS x86_64
-          - os: macos-latest
-            python: 39
-            platform_id: macosx_x86_64
-          - os: macos-latest
-            python: 310
-            platform_id: macosx_x86_64
-          - os: macos-latest
-            python: 311
-            platform_id: macosx_x86_64
-          - os: macos-latest
-            python: 312
-            platform_id: macosx_x86_64
-
-          # MacOS arm64
-          - os: macos-14
-            python: 39
-            platform_id: macosx_arm64
-          - os: macos-14
-            python: 310
-            platform_id: macosx_arm64
-          - os: macos-14
-            python: 311
-            platform_id: macosx_arm64
-          - os: macos-14
-            python: 312
-            platform_id: macosx_arm64
+        os: [ubuntu-latest, windows-latest, macos-13, macos-14]
 
     steps:
-      - name: Checkout scikit-learn
-        uses: actions/checkout@v3
+      - uses: actions/checkout@v4
 
-      - name: Setup Python
-        uses: actions/setup-python@v5
-        with:
-          python-version: "3.11" # update once build dependencies are available
+      # Used to host cibuildwheel
+      - uses: actions/setup-python@v3
 
-      #  run: bash build_tools/wheels/build_wheels.sh
-     - name: Install cibuildwheel
+      - name: Install cibuildwheel
         run: python -m pip install cibuildwheel==2.17.0
 
       - name: Build wheels
@@ -154,7 +41,7 @@ jobs:
       #    CIBW_TEST_COMMAND_WINDOWS: bash {project}/build_tools/github/test_windows_wheels.sh ${{ matrix.python }}
       #    CIBW_BUILD_VERBOSITY: 1
 
-      - name: Store artifacts
-        uses: actions/upload-artifact@v3
+      - uses: actions/upload-artifact@v4
         with:
-          path: wheelhouse/*.whl
+          name: cibw-wheels-${{ matrix.os }}-${{ strategy.job-index }}
+          path: ./wheelhouse/*.whl
