fdd061fad219becf406eef9b216cdb1a5fbbb368
julian <j.hilbert@em.uni-frankfurt.de>
2024-09-12 13:59:20 +0200

    fix defaults for gcc

--- ugbase/bindings/json/json_basics.cpp
+++ ugbase/bindings/json/json_basics.cpp
@@ -1,25 +1,28 @@
 
 
 #include "json_basics.hpp"
-
-
-
+#include "common/util/path_provider.h"
 // Config for solvers.
 namespace ug {
 
-#define _JSON_DEFAULTS_SOLVER_ R"(
-#include "../scripts/util/solver.defaults.json"
+#define _JSON_DEFAULTS_SOLVER_ R"(\
+#include "../scripts/util/solver.defaults.json"\
 )"
 
-#define _JSON_SCHEMAS_SOLVER_ R"(
-#include "../scripts/util/solver.defaults.json"
+#define _JSON_SCHEMAS_SOLVER_ R"(\
+#include "../scripts/util/solver.defaults.json"\
 )"
+// Hack for gcc compiler bug where no raw string literals over multiple lines in macros are allowed
+const char *defaults =
+#include "../scripts/util/solver.defaults.json"
+;
 
 const nlohmann::json json_predefined_defaults::solvers
-		= nlohmann::json::parse(_JSON_DEFAULTS_SOLVER_);
+		= nlohmann::json::parse(defaults);//= nlohmann::json::parse(solver_defaults_path);
 
 const nlohmann::json json_predefined_schemas::solvers
-		= nlohmann::json::parse(_JSON_SCHEMAS_SOLVER_);
+        = nlohmann::json::parse(defaults);//= nlohmann::json::parse(solver_defaults_path);
+
 
 #undef json_defaults_solver
 #undef json_schemas_solver
