ba068e0ad1d0c21388dfd9d4209218655870e248
julian <j.hilbert@em.uni-frankfurt.de>
2024-09-12 14:01:47 +0200

    Revert "fix defaults for gcc"
    This reverts commit fdd061fad219becf406eef9b216cdb1a5fbbb368.

--- ugbase/bindings/json/json_basics.cpp
+++ ugbase/bindings/json/json_basics.cpp
@@ -1,28 +1,25 @@
 
 
 #include "json_basics.hpp"
-#include "common/util/path_provider.h"
+
+
+
 // Config for solvers.
 namespace ug {
 
-#define _JSON_DEFAULTS_SOLVER_ R"(\
-#include "../scripts/util/solver.defaults.json"\
+#define _JSON_DEFAULTS_SOLVER_ R"(
+#include "../scripts/util/solver.defaults.json"
 )"
 
-#define _JSON_SCHEMAS_SOLVER_ R"(\
-#include "../scripts/util/solver.defaults.json"\
-)"
-// Hack for gcc compiler bug where no raw string literals over multiple lines in macros are allowed
-const char *defaults =
+#define _JSON_SCHEMAS_SOLVER_ R"(
 #include "../scripts/util/solver.defaults.json"
-;
+)"
 
 const nlohmann::json json_predefined_defaults::solvers
-		= nlohmann::json::parse(defaults);//= nlohmann::json::parse(solver_defaults_path);
+		= nlohmann::json::parse(_JSON_DEFAULTS_SOLVER_);
 
 const nlohmann::json json_predefined_schemas::solvers
-        = nlohmann::json::parse(defaults);//= nlohmann::json::parse(solver_defaults_path);
-
+		= nlohmann::json::parse(_JSON_SCHEMAS_SOLVER_);
 
 #undef json_defaults_solver
 #undef json_schemas_solver
