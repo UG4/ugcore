3e401324fc57de423bf5007dd7c6878bc72d4597
Markus <markus.m.knodel@gmx.de>
2025-08-08 21:18:21 +0200

    removed now unnecessary Macro defining file

--- ugbase/lib_disc/operator/non_linear_operator/line_search.h
+++ ugbase/lib_disc/operator/non_linear_operator/line_search.h
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2010-2015:  G-CSC, Goethe University Frankfurt
- * Author: Andreas Vogel
+ * Author: Andreas Vogel, modifications nested Newton: Markus Knodel
  * 
  * This file is part of UG4.
  * 
